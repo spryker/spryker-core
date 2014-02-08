9ce9a67c9c8af6cfd0999d89cff9764558133850
Marek Obuchowicz <marek.obuchowicz@project-a.com>
2014-02-09 00:21:53 +0100

    fix firewall

--- salt/app/files/deploy_config.rb
+++ salt/app/files/deploy_config.rb
@@ -3,8 +3,14 @@
 # deploy_config.rb
 
 {% set netif = pillar.get('project_network_interface') %}
-{% set appservers = salt['mine.get']('roles:app', 'network.interfaces', expr_form = 'grain') %}
+{% set appservers = salt['mine.get']('roles:app', 'network.interfaces', expr_form = 'grain').items() %}
 
 {{ netif }}
 {{ appservers }}
 
+
+{% for hostname, network_settings in appservers %}
+Host: {{ hostname }}
+{{ network_settings[netif] }}
+{{ network_settings[netif]['inet'][0]['address'] }}
+{% endfor %}
