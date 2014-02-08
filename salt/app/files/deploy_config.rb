83591547c98c33a5f5d5676bd29e78fa8f7fdcd0
Marek Obuchowicz <marek.obuchowicz@project-a.com>
2014-02-09 00:12:01 +0100

    app setup

--- salt/app/files/deploy_config.rb
+++ salt/app/files/deploy_config.rb
@@ -3,7 +3,7 @@
 # deploy_config.rb
 
 {% set netif = pillar.get('project_network_interface') %}
-{% set appservers = mine.get('roles:app', 'network.interfaces', 'grain') %}
+{% set appservers = salt['mine.get']('roles:app', 'network.interfaces', expr_form = 'grain') %}
 
 {{ netif }}
 {{ appservers }}
