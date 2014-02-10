9a2ea793fd905e1096223a359873c9f2244d916e
Marek Obuchowicz <marek.obuchowicz@project-a.com>
2014-02-10 21:04:39 +0100

    deploy

--- salt/app/files/deploy/deploy_config.rb
+++ salt/app/files/deploy/deploy_config.rb
@@ -46,9 +46,8 @@ $environments = [
 # List of stores
 {{ pillar['stores'] }}
 $stores = [
-{%- for store, store_details in pillar['environments'].items() %}
-{{ store_details }}
-   { 'store' => '{{ store }}', 'locale' => ' store_details['locale'] }}, 'appdomain' => 'store_details['appdomain'] }} },
+{%- for store, store_details in pillar['stores'].items() %}
+   { 'store' => '{{ store }}', 'locale' => '{{ store_details['locale'] }}', 'appdomain' => '{{ store_details['appdomain'] }} },
 {%- endfor %}
 ]
 
