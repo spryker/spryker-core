c681c5e689805cba2154bed30f0ed7f6d03b0b71
adamvert <ab@adam-butler.com>
2015-04-13 14:52:05 +0200

    some scaffolding, including an OK initial gulp file (needs browserify)

---
+++ gulpfile.js
@@ -0,0 +1,199 @@
+// NB. reload doesn't currently work with partials
+// so after editing a partial, you will need to stop and restart the watch task
+
+'use strict';
+
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    jshint      = require('gulp-jshint'),
+    maps        = require('gulp-sourcemaps'),
+    hb          = require('gulp-hb'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload;
+
+
+var path = require('path'),
+    del  = require('del');
+
+var dirBase     = '.',
+    dirCss      = '/styles',
+    dirJs       = '/scripts',
+    dirImg      = '/images',
+    dirFont     = '/fonts',
+    dirPartials = '/partials';
+
+var dirTemplates = dirBase + '/templates',
+    dirAssets    = dirBase + '/assets',
+    dirPublic    = dirBase + '/public';
+
+var paths = {
+  source: {
+    templates : dirTemplates + '/**/*.html',
+    partials  : dirTemplates + dirPartials + '/**/*.hbs',
+    css       : dirAssets + dirCss + '/**/*.scss',
+    js        : dirAssets + dirJs + '/**/*.js',
+    img       : dirAssets + dirImg + '/**/*.{svg,png,jpeg,gif,ico}',
+    font      : dirAssets + dirFont + '/**/*.{svg,woff,otf,ttf,eot}'
+  },
+  dest: {
+    html      : dirPublic,
+    css       : dirPublic + dirCss,
+    js        : dirPublic + dirJs,
+    img       : dirPublic + dirImg,
+    font      : dirPublic + dirFont
+  }
+};
+
+function _copy(source, target) {
+  return gulp
+    .src(source)
+    .pipe(gulp.dest(target));
+}
+
+gulp.task('clean-html', function(done) {
+  del([paths.dest.html + '/**/*.html'], done);
+});
+
+gulp.task('clean-css', function(done) {
+  del([paths.dest.css], done);
+});
+
+gulp.task('clean-js', function(done) {
+  del([paths.dest.js], done);
+});
+
+gulp.task('clean-images', function(done) {
+  del([paths.dest.img], done);
+});
+
+gulp.task('clean-fonts', function(done) {
+  del([paths.dest.font], done);
+});
+
+gulp.task('dev-html', function(done) {
+  return gulp
+    .src(paths.source.templates)
+    .pipe(hb({
+      partials: [paths.source.partials]
+    }))
+    .pipe(gulp.dest(paths.dest.html))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('dev-lint-js', function() {
+  return gulp
+    .src(paths.source.js)
+    .pipe(jshint(path.join(dirBase, '.jshintrc')))
+    .pipe(jshint.reporter('default'))
+});
+
+gulp.task('dev-css', ['clean-css'], function() {
+  return gulp
+    .src(paths.source.css)
+    .pipe(maps.init())
+    .pipe(sass({
+      'errorLogToConsole' : true,
+      'outputStyle' : 'nested',
+      'precision' : 3
+    }))
+    .pipe(maps.write())
+    .pipe(gulp.dest(paths.dest.css))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('dev-js', ['clean-js'], function() {
+  return _copy(
+    paths.source.js,
+    paths.dest.js
+  );
+});
+
+gulp.task('dev-images', ['clean-images'], function() {
+  return _copy(
+    paths.source.img,
+    paths.dest.img
+  );
+});
+
+gulp.task('dev-fonts', ['clean-fonts'], function() {
+  return _copy(
+    paths.source.font,
+    paths.dest.font
+  );
+});
+
+gulp.task('dist-lint-js', function() {
+  return gulp
+    .src(paths.source.js)
+    .pipe(jshint(path.join(dirBase, '.jshintrc')))
+    .pipe(jshint.reporter('default'))
+});
+
+gulp.task('dist-css', ['clean-css'], function() {
+  return gulp
+    .src(paths.source.css)
+    .pipe(sass({
+      'errorLogToConsole' : true,
+      'outputStyle' : 'compressed',
+      'precision' : 3
+    }))
+    .pipe(gulp.dest(paths.dist.css));
+});
+
+gulp.task('dist-html', ['clean-html', 'dev-html']);
+
+gulp.task('dist-js', ['clean-js', 'dev-js']);
+
+gulp.task('dist-images', ['clean-images', 'dev-images']);
+
+gulp.task('dist-fonts', ['clean-fonts', 'dev-fonts']);
+
+gulp.task('watch', ['dev'], function() {
+  browserSync({
+    server: {
+      baseDir: paths.dest.html
+    }
+  });
+
+  gulp.watch(paths.source.templates, ['clean-html', 'dev-html']);
+  gulp.watch(paths.source.css,  ['clean-css', 'dev-css']);
+  gulp.watch(paths.source.js,   ['clean-js', 'dev-js']);
+  gulp.watch(paths.source.img,  ['clean-images', 'dev-images']);
+  gulp.watch(paths.source.font, ['clean-fonts', 'dev-fonts' ]);
+});
+
+
+// gulp.task('test', [
+//   'dev-lint-js'
+// ]);
+
+gulp.task('dev', [
+  'clean-html',
+  'clean-css',
+  'clean-js',
+  'clean-images',
+  'clean-fonts',
+  'dev-lint-js',
+  'dev-html',
+  'dev-css',
+  'dev-js',
+  'dev-images',
+  'dev-fonts'
+]);
+
+gulp.task('dist', [
+  'clean-html',
+  'clean-css',
+  'clean-js',
+  'clean-images',
+  'clean-fonts',
+  'dist-html',
+  'dist-css',
+  'dist-js',
+  'dist-images',
+  'dist-fonts'
+]);
+
+gulp.task('default', [
+  'dist'
+]);
