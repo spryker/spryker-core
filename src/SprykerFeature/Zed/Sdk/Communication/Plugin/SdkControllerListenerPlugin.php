5226d1e63db5ae3e3f4b213243aebfa48a7737cd
René Klatt <rene.klatt@project-a.com>
2015-04-22 08:56:02 +0000

    Spryke!

---
+++ src/SprykerFeature/Zed/Sdk/Communication/Plugin/SdkControllerListenerPlugin.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace SprykerFeature\Zed\Sdk\Communication\Plugin;
+
+use SprykerFeature\Shared\Library\CodeGenerator\ClassnameExplanation;
+use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
+use SprykerEngine\Zed\Kernel\Locator;
+use SprykerFeature\Zed\Sdk\Communication\SdkControllerListenerInterface;
+use SprykerFeature\Zed\ZedRequest\Business\Client\Request;
+use SprykerFeature\Zed\ZedRequest\Business\Client\Response;
+use SprykerFeature\Shared\Library\TransferObject\TransferInterface;
+use SprykerFeature\Zed\Application\Communication\Controller\AbstractSdkController;
+use SprykerFeature\Zed\Application\Communication\Plugin\TransferObject\TransferServer;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+
+class SdkControllerListenerPlugin extends AbstractPlugin implements SdkControllerListenerInterface
+{
+
+    /**
+     * @param FilterControllerEvent $event
+     * @return callable
+     */
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        $currentController = $event->getController();
+        $controller = $currentController[0];
+        $action = $currentController[1];
+
+        if (!($controller instanceof AbstractSdkController)) {
+            return $currentController;
+        }
+
+        $newController = function() use ($controller, $action) {
+            $requestTransfer = $this->getRequestTransfer($controller, $action);
+            $result = $controller->$action($requestTransfer->getTransfer(), $requestTransfer);
+            $response = $this->getResponse($controller, $result);
+
+            return TransferServer::getInstance()
+                ->setResponse($response)
+                ->send();
+        };
+
+        $event->setController($newController);
+    }
+
+    /**
+     * @param AbstractSdkController $controller
+     * @param string $action
+     *
+     * @return Request
+     * @throw \LogicException
+     */
+    private function getRequestTransfer(AbstractSdkController $controller, $action)
+    {
+        $classRef = new \ReflectionObject($controller);
+        $methodRef = $classRef->getMethod($action);
+        $parameters = $methodRef->getParameters();
+        $countParameters = count($parameters);
+
+        if ($countParameters > 2 || $countParameters === 2 && end($parameters)->getClass() === 'SprykerFeature\\Shared\\Library\\Transfer\\Request') {
+            throw new \LogicException('Only one transfer object can be received in yves-action');
+        }
+
+        /* @var $parameter \ReflectionParameter */
+        $parameter = array_shift($parameters);
+        if ($parameter) {
+            $class = $parameter->getClass();
+            if (empty($class)) {
+                throw new \LogicException('You need to specify a class for the parameter in the yves-action.');
+            }
+
+            $this->isSharedTransferClass($class);
+        }
+
+        return TransferServer::getInstance()->getRequest();
+    }
+
+    /**
+     * @param AbstractSdkController $controller
+     * @param $result
+     * @return Response
+     */
+    private function getResponse(AbstractSdkController $controller, $result)
+    {
+        $response = new Response(Locator::getInstance());
+
+        if ($result instanceof TransferInterface) {
+            $result->validate();
+            $response->setTransfer($result);
+        }
+
+        $response->addMessages($controller->getMessages());
+        $response->addErrorMessages($controller->getErrorMessages());
+        $response->setSuccess($controller->getSuccess());
+
+        return $response;
+    }
+
+    /**
+     * @param \ReflectionClass $class
+     *
+     * @throw \LogicException
+     */
+    private function isSharedTransferClass(\ReflectionClass $class)
+    {
+        $namespaceParts = explode('\\', $class->getNamespaceName());
+
+        if ((count($namespaceParts) > 4) && $namespaceParts[1] === 'Shared' && $namespaceParts[3] === 'Transfer') {
+            throw new \LogicException('Only transfer classes are allowed in yves action as parameter');
+        }
+    }
+}
---
+++ tests/_helpers/CodeHelper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sdk\Codeception\Module;
+
+use Codeception\Module;
+
+class CodeHelper extends Module
+{
+}
