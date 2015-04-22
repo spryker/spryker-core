d52218a57a1e9047d998978f45b017d3e8136856
René Klatt <rene.klatt@project-a.com>
2015-04-22 08:56:34 +0000

    Spryke!

---
+++ src/SprykerFeature/Zed/StockSalesConnector/Business/StockSalesConnectorDependencyContainer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SprykerFeature\Zed\StockSalesConnector\Business;
+
+use SprykerEngine\Zed\Kernel\Business\AbstractDependencyContainer;
+use SprykerFeature\Zed\StockSalesConnector\Dependency\Facade\StockToSalesFacadeInterface;
+
+class StockSalesConnectorDependencyContainer extends AbstractDependencyContainer
+{
+    /**
+     * @return StockToSalesFacadeInterface
+     */
+    public function getStockFacade()
+    {
+        return $this->getLocator()->stock()->facade();
+    }
+
+}
---
+++ src/SprykerFeature/Zed/StockSalesConnector/Communication/Plugin/ManagerStockPlugin.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SprykerFeature\Zed\StockSalesConnector\Communication\Plugin;
+
+use SprykerFeature\Shared\Stock\Transfer\StockProduct;
+use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
+use SprykerFeature\Zed\Sales\Dependency\Plugin\ManagerStockPluginInterface;
+use SprykerFeature\Zed\Stock\Persistence\Propel\SpyStockProduct;
+use SprykerFeature\Zed\StockSalesConnector\Business\StockSalesConnectorDependencyContainer;
+
+/**
+ * @method StockSalesConnectorDependencyContainer getDependencyContainer()
+ */
+class UpdateStockPlugin extends AbstractPlugin implements ManagerStockPluginInterface
+{
+    // TODO not sure this Connector/Plugin will be needed after refactor sales Bundle!
+
+    /**
+     * @param string $sku
+     * @param string $stockType
+     * @param int $incrementBy
+     */
+    public function incrementStockProduct($sku, $stockType, $incrementBy = 1)
+    {
+        $this->getDependencyContainer()->getStockFacade()->incrementStockProduct($sku, $stockType, $incrementBy);
+    }
+
+    /**
+     * @param string $sku
+     * @param string $stockType
+     * @param int $decrementBy
+     */
+    public function decrementStockProduct($sku, $stockType, $decrementBy = 1)
+    {
+        $this->getDependencyContainer()->getStockFacade()->decrementStockProduct($sku, $stockType, $decrementBy);
+    }
+
+    /**
+     * @param StockProduct $transferStockProduct
+     * @return SpyStockProduct
+     */
+    public function updateStockProduct(StockProduct $transferStockProduct)
+    {
+        return $this->getDependencyContainer()->getStockFacade()->updateStockProduct($transferStockProduct);
+    }
+
+}
