cf326ece2abee36ecc27f2604b1fef8b48d649bc
René Klatt <rene.klatt@project-a.com>
2015-04-22 15:19:38 +0000

    Fixed some naming issues

--- tests/YvesUnit/YvesGuy.php
+++ tests/YvesUnit/YvesGuy.php
@@ -3,7 +3,7 @@
 // You should not change it manually as it will be overwritten on next build
 // @codingStandardsIgnoreFile
 
-namespace YvesPackage;
+namespace ProductImage;
 
 use \Codeception\Maybe;
 
