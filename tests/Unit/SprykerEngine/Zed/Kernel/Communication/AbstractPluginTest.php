4a3f137a8d0b622792bdb0f8f0b028e082481b00
René Klatt <rene.klatt@project-a.com>
2015-04-23 10:08:09 +0000

    merged with y and z package

--- tests/Unit/SprykerEngine/Zed/Kernel/Communication/AbstractPluginTest.php
+++ tests/Unit/SprykerEngine/Zed/Kernel/Communication/AbstractPluginTest.php
@@ -4,6 +4,8 @@ namespace Unit\SprykerEngine\Zed\Kernel\Communication;
 
 use SprykerEngine\Zed\Kernel\Communication\PluginLocator;
 use SprykerEngine\Zed\Kernel\Locator;
+use Unit\SprykerEngine\Zed\Kernel\Communication\Fixtures\AbstractPlugin\FooPlugin;
+use Unit\SprykerEngine\Zed\Kernel\Communication\Fixtures\FooMessenger;
 
 /**
  * @group Kernel
@@ -24,5 +26,24 @@ class AbstractPluginTest extends \PHPUnit_Framework_TestCase
             'Unit\SprykerEngine\Zed\Kernel\Communication\Fixtures\PluginLocator\Plugin\Foo',
             $locatedClass
         );
+    }
+
+    public function testSetMessenger()
+    {
+        $locator = new PluginLocator(
+            '\\Unit\\SprykerEngine\\Zed\\{{bundle}}{{store}}\\Communication\\Fixtures\\PluginLocator\\Factory'
+        );
+        /** @var FooPlugin $plugin */
+        $plugin = $locator->locate('Kernel', Locator::getInstance(), 'Foo');
+
+        $messengerMock = new FooMessenger();
+        $plugin->setMessenger($messengerMock);
+        $plugin->log('warning', 'foo', ['key' => 'value']);
+        $return = $messengerMock->getLogMock();
+        $this->assertEquals('warning', $return['level']);
+        $this->assertEquals('foo', $return['message']);
+        $this->assertTrue(is_array($return['context']));
+        $this->arrayHasKey('key', $return['context']);
+        $this->assertEquals('value', $return['context']['key']);
     }
 }
