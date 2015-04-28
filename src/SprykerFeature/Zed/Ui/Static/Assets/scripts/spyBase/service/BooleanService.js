93d2797df9b265f5e0ba36943650c8cb81caba18
chkt <christoph.kettelhoit@project-a.com>
2015-04-28 17:19:31 +0200

    Added return values for true, false

--- src/SprykerFeature/Zed/Ui/Static/Assets/scripts/spyBase/service/BooleanService.js
+++ src/SprykerFeature/Zed/Ui/Static/Assets/scripts/spyBase/service/BooleanService.js
@@ -5,6 +5,8 @@ require('Ui').ng
 	.factory('BooleanService', function() {
 		return function(val) {
 			switch(val) {
+				case true    :
+				case false   : return val;
 				case 'false' :
 				case '0'     : return false;
 				default      : return Boolean(val);
