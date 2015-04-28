408f93b40e060e9da95a37a0d67a0f74cb35753e
René Klatt <rene.klatt@project-a.com>
2015-04-28 14:51:55 +0000

    Added missing config files and fixed some tests

---
+++ src/SprykerFeature/Zed/DiscountCalculationConnector/DiscountCalculationConnectorConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SprykerFeature\Zed\DiscountCalculationConnector;
+
+use SprykerEngine\Zed\Kernel\AbstractBundleConfig;
+
+class DiscountCalculationConnectorConfig extends AbstractBundleConfig
+{
+
+}
