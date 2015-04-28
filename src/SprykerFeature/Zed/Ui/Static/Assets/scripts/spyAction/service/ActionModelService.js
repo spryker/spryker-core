a4b87a4ab88a2f8d32bb835ddfcaadc62e974c33
chkt <christoph.kettelhoit@project-a.com>
2015-04-28 14:11:14 +0200

    Added interceptor to ActionModelService

--- src/SprykerFeature/Zed/Ui/Static/Assets/scripts/spyAction/service/ActionModelService.js
+++ src/SprykerFeature/Zed/Ui/Static/Assets/scripts/spyAction/service/ActionModelService.js
@@ -14,16 +14,24 @@ require('Ui').ng
 	.module('spyAction')
 	.factory('ActionModelService', [
 		'$resource',
+		'$q',
 		'JSONModelDenormalizeService',
 
-		function($resource, denormalizeResponse) {
+		function($resource, $q, denormalizeResponse) {
 
 			return function(url) {
 				return $resource(url, {}, {
 					get : {
 						method : 'get',
 						isArray : false,
-						transformResponse : denormalizeResponse
+						transformResponse : denormalizeResponse,
+						interceptor : {
+							response : function(response) {
+								if (status >= 400) return $q.reject(response);
+
+								return response;
+							}
+						}
 					}
 				});
 			};
