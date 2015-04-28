5b4b1ed4df7b1a66b385c6622f41d3f3ec1101c5
Lucas Manzke <lmanzke@outlook.com>
2015-04-28 18:15:21 +0200

    use locale string in key generation

--- src/SprykerFeature/Zed/ProductFrontendExporterConnector/Business/Processor/ProductProcessor.php
+++ src/SprykerFeature/Zed/ProductFrontendExporterConnector/Business/Processor/ProductProcessor.php
@@ -47,7 +47,7 @@ class ProductProcessor implements ProductProcessorInterface
         $exportChunk = [];
 
         foreach ($products as $index => $productData) {
-            $productKey = $this->productKeyGenerator->generateKey($productData['id_product'], $locale);
+            $productKey = $this->productKeyGenerator->generateKey($productData['id_product'], $locale->getLocaleName());
             $productData['url'] = $productData['product_url'];
             $exportChunk[$productKey] = $this->filterProductData($productData);
         }
