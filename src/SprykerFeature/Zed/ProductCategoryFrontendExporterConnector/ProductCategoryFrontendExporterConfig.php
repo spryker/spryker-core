a1859977ab8112b12511437b234fc659b856a076
René Klatt <rene.klatt@project-a.com>
2015-04-28 15:01:25 +0000

    Add missing config

---
+++ src/SprykerFeature/Zed/ProductCategoryFrontendExporterConnector/ProductCategoryFrontendExporterConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SprykerFeature\Zed\ProductCategoryFrontendExporter;
+
+use SprykerEngine\Zed\Kernel\AbstractBundleConfig;
+
+class ProductCategoryFrontendExporterConfig extends AbstractBundleConfig
+{
+
+}
