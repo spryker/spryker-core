651b5b4bfa12c7e93dc6c04d5f48036e364486c2
René Klatt <rene.klatt@project-a.com>
2015-04-28 15:17:50 +0000

    Update config

---
+++ src/SprykerFeature/Zed/ProductCategoryFrontendExporterConnector/ProductCategoryFrontendExporterConnectorConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SprykerFeature\Zed\ProductCategoryFrontendExporterConnector;
+
+use SprykerEngine\Zed\Kernel\AbstractBundleConfig;
+
+class ProductCategoryFrontendExporterConnectorConfig extends AbstractBundleConfig
+{
+
+}
