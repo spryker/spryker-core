6d4d324024d9c6ae80be7f1c87bcc305ba45e353
chkt <christoph.kettelhoit@project-a.com>
2015-04-29 16:03:51 +0200

    Changed .state to .value; Changed sent value to represent the new state

--- src/SprykerFeature/Zed/Ui/Static/Assets/scripts/spyFormAction/directive/spyActionSwitch.js
+++ src/SprykerFeature/Zed/Ui/Static/Assets/scripts/spyFormAction/directive/spyActionSwitch.js
@@ -56,7 +56,7 @@ require('Ui').ng
 
 						$scope
 							.update({
-								value : switchModel.value
+								value : $scope.switchModel.value
 							})
 							.then(function(model) {
 								$scope.state = model.value;
