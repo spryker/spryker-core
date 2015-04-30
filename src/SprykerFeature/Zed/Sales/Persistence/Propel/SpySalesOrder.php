d8bba907a8a2befa23fe069d129ba059ca2545b1
Benjamin Nadland <me@benjaminnadland.de>
2015-04-30 14:49:04 +0200

    add correct entities for current schema

--- src/SprykerFeature/Zed/Sales/Persistence/Propel/SpySalesOrder.php
+++ src/SprykerFeature/Zed/Sales/Persistence/Propel/SpySalesOrder.php
@@ -2,8 +2,6 @@
 
 namespace SprykerFeature\Zed\Sales\Persistence\Propel;
 
-use SprykerFeature\Shared\Calculation\Dependency\Transfer\TotalsInterface;
-use SprykerEngine\Zed\Kernel\Locator;
 use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrder as BaseSpySalesOrder;
 
 /**
@@ -19,96 +17,4 @@ use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrder as BaseSpySal
 class SpySalesOrder extends BaseSpySalesOrder
 {
 
-    /**
-     * @var TotalsInterface
-     */
-    protected $totals;
-
-    /**
-     * @var \Generated\Zed\Ide\AutoCompletion
-     */
-    protected $locator;
-
-    public function __construct()
-    {
-        $this->locator = Locator::getInstance();
-        parent::__construct();
-    }
-
-    /**
-     * This is needed because propel reuses the entities. getTotals() did not work because the instance variable has
-     * also been initialized before.
-     * @param null $col
-     */
-    public function resetModified($col = null)
-    {
-        parent::resetModified($col);
-        $this->totals = null;
-    }
-
-    /**
-     * @param \Traversable $orderItems
-     *
-     * @return TotalsInterface
-     */
-    public function getTotals(\Traversable $orderItems = null)
-    {
-        if ($orderItems) {
-            return $this->locator->calculation()->facade()->recalculateTotalsForEntity($this, $orderItems);
-        }
-
-        if (!$this->totals) {
-            $this->totals = $this->locator->calculation()->facade()->recalculateTotalsForEntity($this);
-        }
-
-        return $this->totals;
-    }
-
-    /**
-     * @return int
-     */
-    public function getGrandTotal()
-    {
-        if (!parent::getGrandTotal()) {
-            return $this->getTotals()->getGrandTotalWithDiscounts();
-        } else {
-            return parent::getGrandTotal();
-        }
-    }
-
-    /**
-     * @return int
-     */
-    public function getSubtotal()
-    {
-        if (!parent::getSubtotal()) {
-            return $this->getTotals()->getSubtotal();
-        } else {
-            return parent::getSubtotal();
-        }
-    }
-
-    /**
-     * @return int
-     */
-    public function getTaxAmount()
-    {
-        return $this->getTotals()->getTax()->getTotalAmount();
-    }
-
-    /**
-     * @return int
-     */
-    public function getDiscountAmount()
-    {
-        return $this->getTotals()->getDiscount()->getTotalAmount();
-    }
-
-    /**
-     * @return boolean
-     */
-    public function isGuestOrder()
-    {
-        return ($this->getFkCustomer() ? false : true);
-    }
 }
--- src/SprykerFeature/Zed/Sales/Persistence/Propel/SpySalesOrderItem.php
+++ src/SprykerFeature/Zed/Sales/Persistence/Propel/SpySalesOrderItem.php
@@ -2,13 +2,7 @@
 
 namespace SprykerFeature\Zed\Sales\Persistence\Propel;
 
-use SprykerFeature\Zed\Oms\Persistence\Propel\SpyOmsOrderItemStatusHistory;
-use SprykerFeature\Zed\Oms\Persistence\Propel\SpyOmsOrderItemStatusHistoryQuery;
 use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrderItem as BaseSpySalesOrderItem;
-use SprykerFeature\Zed\Sales\Persistence\Propel\Map\SpySalesOrderItemTableMap;
-use SprykerFeature\Zed\Salesrule\Business\Model\DiscountableItemInterface;
-use Propel\Runtime\ActiveQuery\Criteria;
-use Propel\Runtime\Connection\ConnectionInterface;
 
 /**
  * Skeleton subclass for representing a row from the 'spy_sales_order_item' table.
@@ -20,68 +14,7 @@ use Propel\Runtime\Connection\ConnectionInterface;
  * long as it does not already exist in the output directory.
  *
  */
-class SpySalesOrderItem extends BaseSpySalesOrderItem implements DiscountableItemInterface
+class SpySalesOrderItem extends BaseSpySalesOrderItem
 {
-    public function preSave(ConnectionInterface $con = null)
-    {
-        $this->updateHistory();
-        $this->updateLastStatusChangeDate();
-        return true;
-    }
 
-    protected function updateHistory()
-    {
-        $statusChanged = $this->isColumnModified(SpySalesOrderItemTableMap::COL_FK_OMS_ORDER_ITEM_STATUS);
-
-        if ($statusChanged) {
-            $entity = new SpyOmsOrderItemStatusHistory();
-            $entity->setFkOmsOrderItemStatus($this->getFkOmsOrderItemStatus());
-            $date = new \DateTime();
-            $entity->setCreatedAt($date);
-            $entity->setUpdatedAt($date);
-            $entity->setOrderItem($this); // saved by automatic-cascade !
-        }
-    }
-
-    /**
-     * @param string $format
-     * @return \DateTime|null
-     */
-    public function getLastStatusChange($format = 'Y-m-d H:i:s')
-    {
-        $lastStatusChange = parent::getLastStatusChange();
-        if (isset($lastStatusChange)) {
-            if (is_null($format)) {
-                return new \DateTime($lastStatusChange);
-            } else {
-                return $lastStatusChange;
-            }
-        }
-
-        $item = SpyOmsOrderItemStatusHistoryQuery::create()
-            ->orderByCreatedAt(Criteria::DESC)
-            ->findOneByFkSalesOrderItem($this->getIdSalesOrderItem());
-
-        if (isset($item)) {
-            return $item->getCreatedAt($format);
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * @return bool
-     */
-    public function isBundle()
-    {
-        return (null !== $this->fk_sales_order_item_bundle);
-    }
-
-    protected function updateLastStatusChangeDate()
-    {
-        $statusChanged = $this->isColumnModified(SpySalesOrderItemTableMap::COL_FK_OMS_ORDER_ITEM_STATUS);
-        if ($statusChanged) {
-            $this->setLastStatusChange(new \DateTime());
-        }
-    }
 }
