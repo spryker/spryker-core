eff420062a6e3d14178de2f4a6fe8745dffcbc62
chkt <christoph.kettelhoit@project-a.com>
2015-04-30 11:19:30 +0200

    Updated initialization script

--- src/SprykerFeature/Zed/Application/Static/Assets/scripts/index.js
+++ src/SprykerFeature/Zed/Application/Static/Assets/scripts/index.js
@@ -1,31 +1,48 @@
 'use strict';
 
-var Menu = require('./Menu.js');
+var Menu     = require('./Menu');
+var Progress = require('./Progress');
+
+var _progress = null;
+
+
+(function(node) {
+	if (node === null) return;
+
+	node.classList.add('progress');
+
+	_progress = new Progress(node);
+
+	window.addEventListener('query-add'    , _progress.addDistance.bind(_progress, 1), false);
+	window.addEventListener('query-resolve', _progress.addProgress.bind(_progress, 1), false);
+})(document.createElement('section'));
 
 
 
 window.addEventListener('DOMContentLoaded', function _onDOM(e) {
 	window.removeEventListener('DOMContentLoaded', _onDOM, false);
 
-	var node = document.querySelector('body.spy-page>nav.spy-page-nav');
+	(function(node) {
+		if (node !== null && _progress !== null) node.appendChild(_progress.node);
+	})(document.querySelector('body.spy-page>header.spy-page-header'));
 
-	if (node === null) return;
+	(function(node) {
+		if (node === null) return;
 
-	exports.mainMenu = new Menu('main', node);
+		exports.mainMenu = new Menu('main', node);
 
-	node = document.querySelector('body.spy-page>header.spy-page-header nav.menu');
+		node = document.querySelector('body.spy-page>header.spy-page-header nav.menu');
 
-	if (node === null) return;
+		if (node === null) return;
 
-	node.addEventListener('click', function _onHomeClick(e) {
-		exports.mainMenu.open = !exports.mainMenu.open;
-	});
+		node.addEventListener('click', function _onHomeClick(e) {
+			exports.mainMenu.open = !exports.mainMenu.open;
+		});
 
-	window.addEventListener('unload', function _onUnload(e) {
-		window.removeEventListener('unload', _onUnload, false);
-
-		exports.mainMenu.undefine();
-	}, false);
+		window.addEventListener('unload', function _onUnload(e) {
+			exports.mainMenu.undefine();
+		}, false);
+	})(document.querySelector('body.spy-page>nav.spy-page-nav'));
 }, false);
 
 
