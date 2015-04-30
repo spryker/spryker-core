d497c85cf39312a5917b7509b2dc5a6b19cefc9a
René Klatt <rene.klatt@project-a.com>
2015-04-30 08:10:01 +0200

    Add new transferDefinition file

--- src/SprykerFeature/Shared/Document/Transfer/DocumentRenderEngine/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SprykerFeature\Shared\Document\Transfer\DocumentRenderEngine;
-
-class Collection extends \SprykerFeature\Shared\Library\TransferObject\AbstractTransferCollection
-{
-
-    protected $transferObjectClass = 'Document\\DocumentRenderEngine';
-}
--- src/SprykerFeature/Shared/Document/Transfer/DocumentType/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SprykerFeature\Shared\Document\Transfer\DocumentType;
-
-class Collection extends \SprykerFeature\Shared\Library\TransferObject\AbstractTransferCollection
-{
-
-    protected $transferObjectClass = 'Document\\DocumentType';
-}
