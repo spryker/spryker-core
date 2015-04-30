bd5bb13bf0e102c2cb5ceb033785f331bcccdc0a
Alberto Assmann <alberto.assmann@spryker.com>
2015-04-30 14:55:35 +0200

    - re add missing interface to cart

---
+++ src/SprykerFeature/Zed/Cart/Dependency/ItemExpanderPluginInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SprykerFeature\Zed\Cart\Dependency;
+
+use SprykerFeature\Shared\Cart\Transfer\ItemCollectionInterface;
+use SprykerFeature\Shared\Cart\Transfer\ItemInterface;
+
+interface ItemExpanderPluginInterface
+{
+    /**
+     * @param ItemCollectionInterface|ItemInterface[] $items
+     *
+     * @return ItemCollectionInterface|ItemInterface[]
+     */
+    public function expandItems(ItemCollectionInterface $items);
+}
+ 
