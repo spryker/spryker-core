e3aa1e7738f3026b20674c579e79cbc53e3dd16c
Paul Sturm <paul.sturm@project-a.com>
2015-05-05 15:38:01 +0200

    basic category page

--- Bundles/UrlExporter/src/SprykerFeature/Shared/UrlExporter/Code/KeyBuilder/ResourceKeyBuilder.php
+++ Bundles/UrlExporter/src/SprykerFeature/Shared/UrlExporter/Code/KeyBuilder/ResourceKeyBuilder.php
@@ -16,6 +16,16 @@ abstract class ResourceKeyBuilder implements KeyBuilderInterface
      */
     protected function buildKey($identifier)
     {
+        /**
+         * TODO: remove this quick hack
+         *
+         * called from CategoryTreeBuilder->createTreeFromCategoryNode()
+         * in ./vendor/spryker/spryker/Bundles/CategoryExporter/src/SprykerFeature/Sdk/CategoryExporter/Builder/CategoryTreeBuilder.php
+         */
+        if (is_array($identifier) && isset($identifier['resourceType'])) {
+            $identifier = $identifier['resourceType'];
+        }
+
         return $this->getResourceType() . '.' . $identifier;
     }
 
