f1b7267c51889fdc1ab9454625fcb2c756923738
René Klatt <rene.klatt@project-a.com>
2015-05-11 05:46:56 +0000

    Added Interface to functional test

--- Bundles/Transfer/tests/Functional/SprykerEngine/Zed/Transfer/Business/Model/Fixtures/Transfer/Transfer.php
+++ Bundles/Transfer/tests/Functional/SprykerEngine/Zed/Transfer/Business/Model/Fixtures/Transfer/Transfer.php
@@ -4,13 +4,14 @@ namespace Generated\Shared\Transfer;
 
 use SprykerEngine\Shared\Transfer\AbstractTransfer;
 
+use Path\To\Interface;
 use Generated\Shared\Transfer\OrderItem;
 
 /**
  * !!! THIS FILE IS AUTO-GENERATED, EVERY CHANGE WILL BE LOST WITH THE NEXT RUN OF TRANSFER GENERATOR
  * !!! DO NOT CHANGE ANYTHING IN THIS FILE
  */
-class Transfer extends AbstractTransfer
+class Transfer extends AbstractTransfer implements Interface
 {
 
     /**
