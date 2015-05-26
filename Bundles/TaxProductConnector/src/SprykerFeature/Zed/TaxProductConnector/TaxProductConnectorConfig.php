f89636d4ac5b314d12ac5d2633b10cbeebcb1bda
Freddie Frantzen <freddie@freddiefrantzen.com>
2015-05-26 12:31:40 +0200

    Updated the bundle config

--- Bundles/TaxProductConnector/src/SprykerFeature/Zed/TaxProductConnector/TaxProductConnectorConfig.php
+++ Bundles/TaxProductConnector/src/SprykerFeature/Zed/TaxProductConnector/TaxProductConnectorConfig.php
@@ -3,15 +3,8 @@
 namespace SprykerFeature\Zed\TaxProductConnector;
 
 use SprykerEngine\Zed\Kernel\AbstractBundleConfig;
-use SprykerFeature\Zed\Tax\Dependency\Plugin\TaxChangePluginInterface;
 
 class TaxProductConnectorConfig extends AbstractBundleConfig
 {
-    /**
-     * @return TaxChangePluginInterface[]
-     */
-    public function getTaxChangePlugins()
-    {
-        return [];
-    }
+
 }
