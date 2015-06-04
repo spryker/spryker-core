d6cae1442ab72579d534116479d7d218cbffa2a7
Philipp Wahala <philipp.wahala@gmail.com>
2015-06-04 15:14:11 +0200

    Fix wrong SprykerEngine\Zed\Maintenance namespace

---
+++ Bundles/Maintenance/tests/Unit/SprykerFeature/Zed/Maintenance/Business/InstalledPackages/Composer/InstalledPackageFinderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Unit\SprykerFeature\Zed\Maintenance\Business\InstalledPackages\Composer;
+
+use Generated\Shared\Transfer\InstalledPackagesTransfer;
+use SprykerFeature\Zed\Maintenance\Business\InstalledPackages\Composer\InstalledPackageFinder;
+use SprykerFeature\Zed\Maintenance\Business\InstalledPackages\InstalledPackageCollector;
+
+/**
+ * @group SprykerFeature
+ * @group Zed
+ * @group Maintenance
+ * @group Business
+ * @group InstalledPackageFinder
+ */
+class InstalledPackageFinderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @return string
+     */
+    private function getFixturesDirectory()
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . 'Fixtures';
+    }
+
+    public function testFindInstalledPackagesShouldReturnCollectionWithInstalledPackages()
+    {
+        $collection = new InstalledPackagesTransfer();
+        $finder = new InstalledPackageFinder($collection, $this->getFixturesDirectory() . DIRECTORY_SEPARATOR . 'composerLock.mock');
+
+        $this->assertInstanceOf(
+            'Generated\Shared\Maintenance\InstalledPackagesInterface',
+            $finder->findInstalledPackages()
+        );
+    }
+
+}
---
+++ Bundles/Maintenance/tests/Unit/SprykerFeature/Zed/Maintenance/Business/MaintenanceDependencyContainerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Functional\SprykerFeature\Zed\Maintenance\Business\Model;
+
+use Generated\Shared\Transfer\InstalledPackagesTransfer;
+use SprykerEngine\Shared\Config;
+use SprykerEngine\Zed\Kernel\Business\Factory;
+use SprykerEngine\Zed\Kernel\Locator;
+use SprykerFeature\Zed\Maintenance\Business\MaintenanceDependencyContainer;
+use SprykerFeature\Zed\Maintenance\MaintenanceConfig;
+
+/**
+ * @group SprykerFeature
+ * @group Zed
+ * @group Maintenance
+ * @group Business
+ * @group MaintenanceDependencyContainer
+ */
+class MaintenanceDependencyContainerTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @return MaintenanceDependencyContainer
+     */
+    private function getDependencyContainer()
+    {
+        $factory = new Factory('Maintenance');
+        $config = new MaintenanceConfig(Config::getInstance(), Locator::getInstance());
+
+        return new MaintenanceDependencyContainer($factory, Locator::getInstance(), $config);
+    }
+
+    public function testCreatePackageCollectorShouldReturnFullConfiguredInstance()
+    {
+        $this->assertInstanceOf(
+            'SprykerFeature\Zed\Maintenance\Business\InstalledPackages\InstalledPackageCollectorInterface',
+            $this->getDependencyContainer()->createPackageCollector()
+        );
+    }
+
+    public function testCreateMarkDownWriterShouldReturnFullConfiguredInstance()
+    {
+        $this->assertInstanceOf(
+            'SprykerFeature\Zed\Maintenance\Business\InstalledPackages\MarkDownWriter',
+            $this->getDependencyContainer()->createMarkDownWriter(new InstalledPackagesTransfer())
+        );
+    }
+
+}
