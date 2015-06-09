525e9760bf0fd5d8fa5ed6d44c1b45c2407e34bb
kvalentin <karl.valentin@kvis.de>
2015-06-09 14:54:32 +0200

    imported class

--- Bundles/Catalog/src/SprykerFeature/Sdk/Catalog/Model/FacetSearch.php
+++ Bundles/Catalog/src/SprykerFeature/Sdk/Catalog/Model/FacetSearch.php
@@ -5,6 +5,7 @@ namespace SprykerFeature\Sdk\Catalog\Model;
 use Elastica\Filter\Term;
 use Elastica\Index;
 use Elastica\Query;
+use Elastica\Query\Filtered;
 use SprykerFeature\Sdk\Catalog\Model\Builder\FacetAggregationBuilderInterface;
 use SprykerFeature\Sdk\Catalog\Model\Builder\NestedFilterBuilderInterface;
 use SprykerFeature\Sdk\Catalog\Model\Extractor\AggregationExtractorInterface;
@@ -85,7 +86,7 @@ class FacetSearch extends AbstractSearch
     protected function addCategoryFilterToQuery(Query $query)
     {
         $query->setQuery(
-            (new Query\Filtered())
+            (new Filtered())
                 ->setFilter(new Term([
                     'category.direct-parents' => $this->category['node_id']
                 ]))
