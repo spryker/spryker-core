56a455ed70ac281fd1d5e825a12a689ba2732d81
Benjamin Nadland <benjamin.nadland@spryker.com>
2015-06-09 11:00:43 +0200

    add docstring

--- Bundles/Customer/src/SprykerFeature/Sdk/Customer/CustomerDependencyContainer.php
+++ Bundles/Customer/src/SprykerFeature/Sdk/Customer/CustomerDependencyContainer.php
@@ -5,12 +5,16 @@ namespace SprykerFeature\Sdk\Customer;
 use SprykerEngine\Sdk\Kernel\AbstractDependencyContainer;
 use SprykerFeature\Sdk\Customer\Model\Customer;
 use Generated\Yves\Ide\FactoryAutoCompletion\Customer as CustomerFactory;
+use SprykerFeature\Sdk\ZedRequest\Provider\ZedClientProvider;
 
 /**
  * @method CustomerFactory getFactory()
  */
 class CustomerDependencyContainer extends AbstractDependencyContainer
 {
+    /**
+     * @return ZedClientProvider
+     */
     protected function createZedClient()
     {
         return $this->getLocator()->zedRequest()->zedClient();
