a9a443b61ac7696adda6aaf4fbfb60282c3acbc0
Benjamin Nadland <benjamin.nadland@spryker.com>
2015-06-15 11:02:47 +0200

    add param type to docstring

--- Bundles/Oms/src/SprykerFeature/Zed/Oms/Persistence/OmsQueryContainer.php
+++ Bundles/Oms/src/SprykerFeature/Zed/Oms/Persistence/OmsQueryContainer.php
@@ -136,7 +136,7 @@ class OmsQueryContainer extends AbstractQueryContainer
     }
 
     /**
-     * @param $idOrder
+     * @param int $idOrder
      *
      * @return SpySalesOrderItemQuery
      */
