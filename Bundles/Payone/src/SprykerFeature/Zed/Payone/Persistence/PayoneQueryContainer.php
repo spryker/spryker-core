00e1a21f7bf90cb31759b0e0ad62e2892ccd0c5c
Anusch Athari <anusch@athari.de>
2015-06-16 13:25:48 +0200

    add todo comment to payone query container

--- Bundles/Payone/src/SprykerFeature/Zed/Payone/Persistence/PayoneQueryContainer.php
+++ Bundles/Payone/src/SprykerFeature/Zed/Payone/Persistence/PayoneQueryContainer.php
@@ -8,17 +8,20 @@ use SprykerEngine\Zed\Kernel\Persistence\AbstractQueryContainer;
 use SprykerFeature\Zed\Payone\Persistence\Propel\SpyPaymentPayoneTransactionStatusLogQuery;
 use SprykerFeature\Zed\Payone\Persistence\Propel\SpyPaymentPayoneApiLogQuery;
 
+
 class PayoneQueryContainer extends AbstractQueryContainer implements PayoneQueryContainerInterface
 {
 
     /**
-     * @todo think of doing it with MAX(sequence_number) ?
-     * @param int $transactionId
+     * @param $transactionId
      * @return SpyPaymentPayoneTransactionStatusLogQuery
      */
     public function getCurrentSequenceNumberQuery($transactionId)
     {
         $query = SpyPaymentPayoneTransactionStatusLogQuery::create();
+        //@todo think of doing it with MAX(sequence_number) ?
+        //$c = new Criteria();
+        //$c->add(SomeTable::CREATED_AT, '(SELECT MAX('.SomeTable::CREATED_AT.') FROM some_table)', Criteria::CUSTOM);
         $query->filterByTransactionId($transactionId)
               ->orderBySequenceNumber(Criteria::DESC);
 
@@ -26,8 +29,8 @@ class PayoneQueryContainer extends AbstractQueryContainer implements PayoneQuery
     }
 
     /**
-     * @param int $transactionId
-     * @return SpyPaymentPayoneQuery
+     * @param $transactionId
+     * @return Propel\SpyPaymentPayoneQuery
      */
     public function getPaymentByTransactionIdQuery($transactionId)
     {
