54bb16c96c8d456455f40b4b60c6ae90a857ffd8
Lucas Manzke <lucas.manzke@spryker.com>
2015-06-23 11:23:49 +0200

    DEM-10 Implement CartCheckoutConnector

---
+++ Bundles/CartCheckoutConnector/src/SprykerFeature/Zed/CartCheckoutConnector/Communication/Plugin/OrderCartHydrationPlugin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\CartCheckoutConnector\Communication\Plugin;
+
+use Generated\Shared\Transfer\CheckoutRequestTransfer;
+use Generated\Shared\Transfer\OrderTransfer;
+use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
+use SprykerFeature\Zed\Checkout\Dependency\Plugin\CheckoutOrderHydrationInterface;
+use SprykerFeature\Zed\CartCheckoutConnector\Business\CartCheckoutConnectorFacade;
+
+/**
+ * @method CartCheckoutConnectorFacade getFacade()
+ */
+class OrderCartHydrationPlugin extends AbstractPlugin implements CheckoutOrderHydrationInterface
+{
+
+    /**
+     * @param OrderTransfer $orderTransfer
+     * @param CheckoutRequestTransfer $checkoutRequest
+     */
+    public function hydrateOrder(OrderTransfer $orderTransfer, CheckoutRequestTransfer $checkoutRequest)
+    {
+        $this->getFacade()->hydrateOrderTransfer($orderTransfer, $checkoutRequest);
+    }
+}
