99098854362266d10d3a221bcc4ce29c8badb310
Lucas Manzke <lucas.manzke@spryker.com>
2015-06-23 11:46:40 +0200

    Tidy up Checkout Dependency Containers

---
+++ Bundles/Checkout/src/SprykerFeature/Yves/Checkout/CheckoutDependencyContainer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SprykerFeature\Yves\Checkout;
+
+use SprykerEngine\Yves\Kernel\AbstractDependencyContainer;
+use Silex\Application;
+
+class CheckoutDependencyContainer extends AbstractDependencyContainer
+{
+
+}
