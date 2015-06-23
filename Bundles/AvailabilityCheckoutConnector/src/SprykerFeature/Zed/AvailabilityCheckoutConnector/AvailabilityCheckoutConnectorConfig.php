982ce6b89f1efd465543d821ec7999359693a729
Lucas Manzke <lucas.manzke@spryker.com>
2015-06-23 11:46:19 +0200

    DEM-10 Implement Precondition if products are available

---
+++ Bundles/AvailabilityCheckoutConnector/src/SprykerFeature/Zed/AvailabilityCheckoutConnector/AvailabilityCheckoutConnectorConfig.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\AvailabilityCheckoutConnector;
+
+use SprykerEngine\Zed\Kernel\AbstractBundleConfig;
+
+class AvailabilityCheckoutConnectorConfig extends AbstractBundleConfig
+{
+
+}
