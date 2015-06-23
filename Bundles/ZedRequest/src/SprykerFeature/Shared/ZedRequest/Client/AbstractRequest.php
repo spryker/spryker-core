2437833def48b52834429721ceae9f5c12ef3a85
Konstantin Scheumann <konstantin.scheumann@spryker.com>
2015-06-23 11:40:41 +0200

    Fix Transfers not being transmitted

--- Bundles/ZedRequest/src/SprykerFeature/Shared/ZedRequest/Client/AbstractRequest.php
+++ Bundles/ZedRequest/src/SprykerFeature/Shared/ZedRequest/Client/AbstractRequest.php
@@ -167,7 +167,7 @@ abstract class AbstractRequest extends AbstractObject implements EmbeddedTransfe
      */
     public function setTransfer(TransferInterface $transferObject)
     {
-        $this->values['transfer'] = $transferObject->toArray(false);
+        $this->values['transfer'] = $transferObject->toArray(true);
         $this->values['transferClassName'] = get_class($transferObject);
 
         return $this;
