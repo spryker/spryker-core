d746d9010f846e47ce5657b6a2f2bea3d5985f29
Anusch Athari <anusch@athari.de>
2015-06-24 12:17:19 +0200

    add order manager

---
+++ Bundles/Payone/src/SprykerFeature/Zed/Payone/Business/Order/OrderManager.php
@@ -0,0 +1,40 @@
+<?php
+  /**
+  * (c) Spryker Systems GmbH copyright protected
+  */
+
+namespace SprykerFeature\Zed\Payone\Business\Order;
+
+use Generated\Shared\Payone\OrderInterface as PayoneOrderInterface;
+use Generated\Shared\Payone\PayonePaymentInterface;
+use SprykerFeature\Zed\Payone\Persistence\Propel\SpyPaymentPayone;
+use SprykerFeature\Zed\Payone\Persistence\Propel\SpyPaymentPayoneDetails;
+
+class OrderManager {
+    /**
+     * @param PayoneOrderInterface $orderTransfer
+     */
+    public function saveOrder(PayoneOrderInterface $orderTransfer)
+    {
+        $paymentTransfer = $orderTransfer->getPayonePayment();
+        $this->persistPayment($paymentTransfer);
+    }
+
+    /**
+     * @param PayonePaymentInterface $paymentTransfer
+     */
+    protected function persistPayment(PayonePaymentInterface $paymentTransfer)
+    {
+        $payment = new SpyPaymentPayone();
+        $payment->setPaymentMethod($paymentTransfer->getPaymentMethod());
+        $payment->setAuthorizationType($paymentTransfer->getAuthorizationType());
+        $payment->setTransactionId($paymentTransfer->getTransactionId());
+        $payment->save();
+
+        $paymentDetailsTransfer = $paymentTransfer->getPaymentDetails();
+        $paymentDetails = new SpyPaymentPayoneDetails();
+        $paymentDetails->setSpyPaymentPayone($payment);
+        $paymentDetails->setPseudocardpan($paymentDetailsTransfer->getPseudocardpan());
+        $paymentDetails->save();
+    }
+} 
