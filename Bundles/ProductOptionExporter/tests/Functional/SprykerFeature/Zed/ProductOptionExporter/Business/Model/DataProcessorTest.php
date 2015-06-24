73b4bee42ba7526e69688ce148cfb9b3b243f442
Freddie Frantzen <freddie@freddiefrantzen.com>
2015-06-24 11:01:48 +0200

    Added more test asertions to the DataProcessor test

--- Bundles/ProductOptionExporter/tests/Functional/SprykerFeature/Zed/ProductOptionExporter/Business/Model/DataProcessorTest.php
+++ Bundles/ProductOptionExporter/tests/Functional/SprykerFeature/Zed/ProductOptionExporter/Business/Model/DataProcessorTest.php
@@ -57,5 +57,32 @@ class DataProcessorTest extends AbstractFunctionalTest
         $processedResultSet = $this->facade->processDataForExport($resultSetFixture, $processedResultSetFixture);
 
         $this->assertCount(1, $processedResultSet);
+        $product = $processedResultSet['xx.xx_xx.dummyIndex1']['concrete_products'][0];
+        $this->assertEquals('DEF456', $product['sku']);
+
+        $this->assertCount(2, $product['configs']);
+        $config = $product['configs'][0];
+        $this->assertEquals($ids['idUsageRed'], $config['values'][0]);
+        $this->assertEquals($ids['idUsageMedium'], $config['values'][1]);
+
+        $this->assertCount(2, $product['options']);
+        $option = $product['options'][0];
+        $this->assertEquals('Color', $option['label']);
+        $this->assertFalse($option['isOptional']);
+        $this->assertNull($option['taxRate']);
+        $this->assertEquals($ids['idUsageSize'], $option['excludes'][0]);
+
+        $this->assertCount(4, $option['values']);
+        $value = $option['values'][3];
+        $this->assertEquals('Green', $value['label']);
+        $this->assertNull($value['price']);
+
+        $this->assertArrayHasKey('allow', $value['constraints']);
+        $allowConstraints = $value['constraints']['allow'];
+        $this->assertCount(2, $allowConstraints);
+        $this->assertEquals($ids['idUsageLarge'], $allowConstraints[0]);
+
+        $this->assertEquals('15.00', $product['options'][1]['taxRate']);
+        $this->assertEquals('199', $product['options'][1]['values'][0]['price']);
     }
 }
