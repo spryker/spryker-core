9b715414c67b22ba311de7f65810063857952dd5
Anusch Athari <anusch@athari.de>
2015-06-29 14:25:39 +0200

    add payone config constant

--- Bundles/Payone/src/SprykerFeature/Shared/Payone/PayoneConfigConstants.php
+++ Bundles/Payone/src/SprykerFeature/Shared/Payone/PayoneConfigConstants.php
@@ -10,6 +10,7 @@ use SprykerFeature\Shared\Library\ConfigInterface;
 interface PayoneConfigConstants extends ConfigInterface
 {
 
+    const PAYONE = 'PAYONE';
     const PAYONE_CREDENTIALS = 'PAYONE_CREDENTIALS';
     const PAYONE_CREDENTIALS_ENCODING = 'PAYONE_CREDENTIALS_ENCODING';
     const PAYONE_PAYMENT_GATEWAY_URL = 'PAYONE_PAYMENT_GATEWAY_URL';
