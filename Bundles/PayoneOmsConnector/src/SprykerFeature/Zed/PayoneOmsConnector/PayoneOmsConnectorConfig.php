c9ad91a7553d06d9d9668f361eb819ea6f88afe4
Anusch Athari <anusch@athari.de>
2015-06-30 11:11:38 +0200

    add empty PayoneOmsConnector config

---
+++ Bundles/PayoneOmsConnector/src/SprykerFeature/Zed/PayoneOmsConnector/PayoneOmsConnectorConfig.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\PayoneOmsConnector;
+
+use SprykerEngine\Zed\Kernel\AbstractBundleConfig;
+
+class PayoneOmsConnector extends AbstractBundleConfig
+{
+}
