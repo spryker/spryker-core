b5d5e717c59e35fcf877c9f281be16ef8b5c371a
Marco Podien <marco@lazystar.de>
2015-07-01 14:12:00 +0000

    Redirection upon cart adding

--- Bundles/Cart/src/SprykerFeature/Yves/Cart/Communication/Controller/AjaxController.php
+++ Bundles/Cart/src/SprykerFeature/Yves/Cart/Communication/Controller/AjaxController.php
@@ -22,8 +22,6 @@ class AjaxController extends AbstractController
 
         return $this->viewResponse([
             'cart' => $cartClient->getCart(),
-            'products' => []
-//            'products' => $this->getProductsForCartItems($cartClient->getCart()->getItems()),
         ]);
     }
 
@@ -31,16 +29,14 @@ class AjaxController extends AbstractController
      * @param string $sku
      * @param int $quantity
      *
-     * @return JsonResponse
+     * @return RedirectResponse
      */
     public function addAction($sku, $quantity)
     {
         $cartClient = $this->getLocator()->cart()->client();
         $cartClient->addItem($sku, $quantity);
 
-        return $this->jsonResponse([
-            'count' => $cartClient->getItemCount(),
-        ]);
+        return $this->redirectResponseInternal(CartControllerProvider::ROUTE_CART_OVERLAY);
     }
 
     /**
@@ -81,51 +77,5 @@ class AjaxController extends AbstractController
 
         return $this->redirectResponseInternal(CartControllerProvider::ROUTE_CART_OVERLAY);
     }
-
-    /**
-     * @param \ArrayObject $cartItems
-     * @return array
-     */
-//    public function getProductsForCartItems(\ArrayObject $cartItems)
-//    {
-//        if (count($cartItems === 0)) {
-//            return [];
-//        }
-//
-//        $products = [];
-//        /** @var CartItemTransfer $item */
-//        foreach ($cartItems as $item) {
-//            $product = [
-//                'name' => '',
-//                'price' => 0,
-//                'quantity' => $item->getQuantity(),
-//            ];
-//
-//            $abstractProduct = $this->getLocator()->catalog()->sdk()->createCatalogModel()->getProductDataById($item->getId());
-//            if (isset($abstractProduct['abstract_Attributes']) && isset($abstractProduct['abstract_attributes']['thumbnail_url'])) {
-//                $product['thumbnail'] = $abstractProduct['abstract_attributes']['thumbnail_url'];
-//            }
-//
-//            if (isset($abstractProduct['concrete_products'])) {
-//                foreach ($abstractProduct['concrete_products'] as $concreteProduct) {
-//                    if (isset($concreteProduct['sku']) && $concreteProduct['sku'] == $item->getSku()) {
-//                        if (isset($concreteProduct['name'])) {
-//                            //@todo fall back to abstract name?
-//                            $product['name'] = $concreteProduct['name'];
-//                        }
-//                    }
-//                }
-//            }
-//
-//            //@todo price from item?
-//            if (isset($abstractProduct['valid_price'])) {
-//                $product['price'] =  $abstractProduct['valid_price'];
-//            }
-//
-//            $products[] = $product;
-//        }
-//
-//        return $products;
-//    }
 
 }
