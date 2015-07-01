27c97074393da1f73065d81f569ade6075fd41ae
Lucas Manzke <lucas.manzke@spryker.com>
2015-07-01 16:09:57 +0200

    add deprecated increment id to sales order for gradual refactoring

--- Bundles/Sales/src/SprykerFeature/Zed/Sales/Persistence/Propel/SpySalesOrder.php
+++ Bundles/Sales/src/SprykerFeature/Zed/Sales/Persistence/Propel/SpySalesOrder.php
@@ -6,6 +6,7 @@
 namespace SprykerFeature\Zed\Sales\Persistence\Propel;
 
 use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrder as BaseSpySalesOrder;
+use SprykerFeature\Zed\Sales\Persistence\Propel\Map\SpySalesOrderTableMap;
 
 /**
  * Skeleton subclass for representing a row from the 'spy_sales_order' table.
@@ -20,4 +21,35 @@ use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrder as BaseSpySal
 class SpySalesOrder extends BaseSpySalesOrder
 {
 
+    /**
+     * Set the value of [increment_id] column.
+     *
+     * @param string $v new value
+     * @deprecated
+     * @return $this|\SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrder The current object (for fluent API support)
+     */
+    public function setIncrementId($v)
+    {
+        if ($v !== null) {
+            $v = (string)$v;
+        }
+
+        if ($this->order_reference !== $v) {
+            $this->order_reference = $v;
+            $this->modifiedColumns[SpySalesOrderTableMap::COL_ORDER_REFERENCE] = true;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the [increment_id] column value.
+     *
+     * @deprecated
+     * @return string
+     */
+    public function getIncrementId()
+    {
+        return $this->order_reference;
+    }
 }
