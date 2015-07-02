05f901558289acbb18ce9d54a5c0460b773f8dd1
Lucas Manzke <lucas.manzke@spryker.com>
2015-07-02 12:49:06 +0200

    add/generate bootstraps and Codeception Guys for suites in SalesCheckoutConnector

---
+++ Bundles/SalesCheckoutConnector/tests/Unit/_bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+$bootstrap = SprykerFeature\Shared\Library\SystemUnderTest\SystemUnderTestBootstrap::getInstance();
+
+$application = 'Zed';
+$bootstrap->bootstrap($application);
