5ff8f5f0cda3593adb050a6c5d5ebe79b3ca5e95
Benjamin Nadland <me@benjaminnadland.de>
2015-07-03 10:47:05 +0200

    Remove commented code.

--- Bundles/Search/src/SprykerFeature/Client/Search/Service/SearchDependencyContainer.php
+++ Bundles/Search/src/SprykerFeature/Client/Search/Service/SearchDependencyContainer.php
@@ -5,16 +5,9 @@
 
 namespace SprykerFeature\Client\Search\Service;
 
-use Generated\Client\Ide\FactoryAutoCompletion\ZedRequest;
 use SprykerEngine\Client\Kernel\Service\AbstractDependencyContainer;
 use SprykerFeature\Client\ZedRequest\Service\Client\ZedClient;
-use SprykerFeature\Shared\Library\Config;
-use SprykerFeature\Shared\System\SystemConfig;
-use SprykerFeature\Shared\Yves\YvesConfig;
 
-/**
- * @method ZedRequest getFactory()
- */
 class SearchDependencyContainer extends AbstractDependencyContainer
 {
 
@@ -23,16 +16,6 @@ class SearchDependencyContainer extends AbstractDependencyContainer
      */
     public function createIndexClient()
     {
-//        $httpClient = $this->getFactory()->createClientHttpClient(
-//            $this->getFactory(),
-//            $this->getLocator(),
-//            'http://' . Config::get(SystemConfig::HOST_ZED_API)
-//        );
-//
-//
-//        return $this->getFactory()->createClientZedClient(
-//            $httpClient
-//        );
         return $this->createProviderIndexClientProvider()->getClient();
     }
 
