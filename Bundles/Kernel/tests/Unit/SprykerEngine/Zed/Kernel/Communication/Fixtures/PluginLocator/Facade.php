eabc5c29cbfb35b0f4464a720e614e9222dfc712
René Klatt <rene.klatt@project-a.com>
2015-07-06 10:45:04 +0000

    Make query container in plugin available

---
+++ Bundles/Kernel/tests/Unit/SprykerEngine/Zed/Kernel/Communication/Fixtures/PluginLocator/Facade.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace Unit\SprykerEngine\Zed\Kernel\Communication\Fixtures\PluginLocator;
+
+use SprykerEngine\Zed\Kernel\Business\AbstractFacade;
+
+class Facade extends AbstractFacade
+{
+}
