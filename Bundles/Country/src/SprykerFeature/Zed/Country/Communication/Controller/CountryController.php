fb591f9b6bcb73a0e42269b451c5b1c88ca13e28
Fabian Wesner <fabian.wesner@spryker.com>
2015-07-08 15:55:06 +0200

    Cleanup

--- Bundles/Country/src/SprykerFeature/Zed/Country/Communication/Controller/CountryController.php
+++ Bundles/Country/src/SprykerFeature/Zed/Country/Communication/Controller/CountryController.php
@@ -1,24 +1,15 @@
 <?php
 
 namespace SprykerFeature\Zed\Country\Communication\Controller;
-
-use Generated\Shared\Country\CountryInterface;
-use Generated\Shared\Transfer\CartTransfer;
-use Generated\Shared\Transfer\CountryTransfer;
-use Generated\Zed\Ide\FactoryAutoCompletion\CountryCommunication;
-
 use SprykerFeature\Zed\Application\Communication\Controller\AbstractController;
-use SprykerFeature\Zed\Country\Business\CountryFacade;
-use SprykerFeature\Zed\Country\Communication\CountryDependencyContainer;
-use SprykerFeature\Zed\Country\Persistence\CountryQueryContainer;
+
 use Symfony\Component\HttpFoundation\Request;
+use SprykerFeature\Zed\Country\Communication\Form\CountryForm;
 
 /**
  * @method CountryQueryContainer getQueryContainer()
- * @method CountryFacade getFacade()
- * @method CountryDependencyContainer getDependencyContainer()
  */
-class CountryController extends AbstractController
+class CountryController extends  AbstractController
 {
 
     public function indexAction()
@@ -47,49 +38,6 @@ class CountryController extends AbstractController
         return $this->viewResponse([
             'form' => $form->createView(),
         ]);
-    }
-
-    public function ajaxAction(Request $request)
-    {
-        $data = [
-            'results' => [
-                [
-                    'id'    => 1,
-                    'text' => 'Blablacar',
-                ],
-                [
-                    'id'    => 2,
-                    'text' => 'Uber',
-                ],
-                [
-                    'id'    => 3,
-                    'text' => 'Uberwe',
-                ],
-                [
-                    'id' => 4,
-                    'text' => 'Uberer',
-                ],
-                [
-                    'id'=> 5,
-                    'text' => 'Ubegj r',
-                ],
-                [
-                    'id' => 6,
-                    'text' => 'Ub   gher',
-                ],
-            ],
-            'more' => false,
-        ];
-
-        return $this->jsonResponse($data);
-    }
-
-    /**
-     * @return CountryTransfer
-     */
-    protected function createCountryTransfer()
-    {
-        return new CountryTransfer();
     }
 
 }
