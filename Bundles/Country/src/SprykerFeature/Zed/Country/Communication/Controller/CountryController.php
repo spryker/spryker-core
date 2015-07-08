c8885e4360e64e534dc86dd2cd53b08d89a93126
Fabian Wesner <fabian.wesner@spryker.com>
2015-07-08 17:53:04 +0200

    Cleanup in new forms and country bundle

--- Bundles/Country/src/SprykerFeature/Zed/Country/Communication/Controller/CountryController.php
+++ Bundles/Country/src/SprykerFeature/Zed/Country/Communication/Controller/CountryController.php
@@ -20,10 +20,10 @@ class CountryController extends  AbstractController
         if ($form->isValid()) {
             $data = $form->getData();
 
-           # $country = $this->createCountryTransfer();
-           # $country->fromArray($data, true);
+            // $country = $this->createCountryTransfer();
+            // $country->fromArray($data, true);
 
-            #$lastInsertedId = $this->getFacade()->saveCountry($country);
+            // $lastInsertedId = $this->getFacade()->saveCountry($country);
         }
 
         if ($request->isMethod('POST')) {
