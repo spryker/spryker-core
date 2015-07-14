876bbc0ad91b811780f52e9b68e5fe9d5dfc7091
Andrey Orsoev <andrey.orsoev@spryker.com>
2015-07-14 16:12:00 +0000

    drop unused code

--- Bundles/Country/src/SprykerFeature/Zed/Country/Communication/Controller/CountryController.php
+++ Bundles/Country/src/SprykerFeature/Zed/Country/Communication/Controller/CountryController.php
@@ -23,24 +23,6 @@ class CountryController extends AbstractController
             $data = $form->getData();
         }
 
-        if ($request->isMethod('POST')) {
-            if (false === $data = $form->processRequest($request)) {
-                $errors = $this->getErrors();
-                // show errors
-            } else {
-                // save
-            }
-        }
-
-        if ($request->isMethod('POST')) {
-            if (false === $data = $form->processRequest($request)) {
-                $errors = $this->getErrors();
-                // show errors
-            } else {
-                // save
-            }
-        }
-
         return $this->viewResponse([
             'form' => $form->createView(),
         ]);
