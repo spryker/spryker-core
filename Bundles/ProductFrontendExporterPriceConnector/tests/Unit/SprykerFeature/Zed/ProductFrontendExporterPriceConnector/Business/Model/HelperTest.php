38dde1e6757630a80b5f60291ebe8ca7da99b07e
Philipp Wahala <philipp.wahala@gmail.com>
2015-07-16 13:20:32 +0200

    ProductFrontendExporterPriceConnector TestHelper CS fix

--- Bundles/ProductFrontendExporterPriceConnector/tests/Unit/SprykerFeature/Zed/ProductFrontendExporterPriceConnector/Business/Model/HelperTest.php
+++ Bundles/ProductFrontendExporterPriceConnector/tests/Unit/SprykerFeature/Zed/ProductFrontendExporterPriceConnector/Business/Model/HelperTest.php
@@ -1,13 +1,16 @@
 <?php
+
 /**
  * (c) Spryker Systems GmbH copyright protected
  */
+
 namespace Unit\SprykerFeature\Zed\ProductFrontendExporterPriceConnector\Business\Model;
 
 use SprykerFeature\Zed\ProductFrontendExporterPriceConnector\Business\Model\Helper;
 
 class HelperTest extends \PHPUnit_Framework_TestCase
 {
+
     public function testOrganizeData()
     {
         $mock = $this->getMock('SprykerFeature\Zed\Price\Business\PriceFacade', [], [], '', false);
@@ -34,4 +37,5 @@ class HelperTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('5.11', $organizedData['foo']['price']);
         $this->assertEquals('10.42', $organizedData['bar']['price']);
     }
+
 }
