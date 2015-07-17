c48bd26dd08f1b88ebd2b686ac010affc41ddc0f
Marius <marius.rizac@spryker.com>
2015-07-17 11:57:48 +0000

    CD-250 Remove search grid globaly

--- Bundles/Gui/static/Assets/scripts/spryker.js
+++ Bundles/Gui/static/Assets/scripts/spryker.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    $('.gui-table-data').dataTable();
+    $('.gui-table-data').dataTable({
+        bFilter: false
+    });
     /**
      * all elements with the same class will have the same height
      */
