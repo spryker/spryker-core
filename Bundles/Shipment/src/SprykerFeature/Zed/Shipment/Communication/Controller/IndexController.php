e485e8877683bc877bb9b740421082f7093b0f20
Dmytro <savin.dmitry@gmail.com>
2015-07-22 08:41:24 +0200

    СD-271 Add index action to show table with methods

--- Bundles/Shipment/src/SprykerFeature/Zed/Shipment/Communication/Controller/IndexController.php
+++ Bundles/Shipment/src/SprykerFeature/Zed/Shipment/Communication/Controller/IndexController.php
@@ -6,9 +6,26 @@
 
 namespace SprykerFeature\Zed\Shipment\Communication\Controller;
 
+use Generated\Zed\Ide\Shipment;
 use SprykerFeature\Zed\Application\Communication\Controller\AbstractController;
+use SprykerFeature\Zed\Shipment\Communication\Table\ShipmentMethodTable;
+use SprykerFeature\Zed\Shipment\Persistence\Propel\SpyShipmentMethod;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class IndexController extends AbstractController
 {
+
+    public function indexAction()
+    {
+        /** @var ShipmentMethodTable $table */
+        $table = $this->getDependencyContainer()
+            ->createShipmentMethodTable()
+        ;
+        $table->init();
+
+        return $this->viewResponse([
+            'shipmentMethodTable' => $table,
+        ]);
+
+    }
 }
