68fef860c5ce93e73eb0e6f6d5e9b6a5a28d8b5d
Dmytro <savin.dmitry@gmail.com>
2015-07-23 15:50:35 +0200

    СD-271 Clean up method table

---
+++ Bundles/Shipment/src/SprykerFeature/Zed/Shipment/Communication/Table/MethodTable.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\Shipment\Communication\Table;
+
+use Propel\Runtime\Collection\ObjectCollection;
+use SprykerFeature\Zed\Shipment\Persistence\Propel\ShipmentMethodQuery;
+use SprykerFeature\Zed\Gui\Communication\Table\AbstractTable;
+use SprykerFeature\Zed\Gui\Communication\Table\TableConfiguration;
+
+class MethodTable extends AbstractTable
+{
+    const CARRIER = 'carrier';
+
+    /**
+     * @var ShipmentMethodQuery
+     */
+    protected $methodQuery;
+
+    /**
+     * @param ShipmentMethodQuery $methodQuery
+     */
+    public function __construct(ShipmentMethodQuery $methodQuery)
+    {
+        $this->customerQuery = $methodQuery;
+    }
+
+    /**
+     * @param TableConfiguration $config
+     * @return TableConfiguration
+     */
+    protected function configure(TableConfiguration $config)
+    {
+        $config->setHeaders([ 'isActive' => 'Active']);
+        $config->setUrl('table');
+
+        return $config;
+    }
+
+    /**
+     * @param TableConfiguration $config
+     *
+     * @return ObjectCollection
+     */
+    protected function prepareData(TableConfiguration $config)
+    {
+
+    }
+
+    /**
+     * @param $details
+     *
+     * @return array|string
+     */
+    private function buildLinks($details)
+    {
+
+    }
+}
