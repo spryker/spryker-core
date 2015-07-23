66a725673c0100b1316ed74df10b6cd0ee3ac4a9
Dmytro <savin.dmitry@gmail.com>
2015-07-23 15:51:38 +0200

    СD-271 Clean up index controller

--- Bundles/Shipment/src/SprykerFeature/Zed/Shipment/Communication/Controller/IndexController.php
+++ Bundles/Shipment/src/SprykerFeature/Zed/Shipment/Communication/Controller/IndexController.php
@@ -6,26 +6,23 @@
 
 namespace SprykerFeature\Zed\Shipment\Communication\Controller;
 
-use Generated\Zed\Ide\Shipment;
 use SprykerFeature\Zed\Application\Communication\Controller\AbstractController;
-use SprykerFeature\Zed\Shipment\Communication\Table\ShipmentMethodTable;
-use SprykerFeature\Zed\Shipment\Persistence\Propel\SpyShipmentMethod;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use SprykerFeature\Zed\Shipment\Business\ShipmentFacade;
+use SprykerFeature\Zed\Shipment\Communication\ShipmentDependencyContainer;
 
+/**
+ * @method ShipmentDependencyContainer getDependencyContainer()
+ * @method ShipmentFacade getFacade()
+ */
 class IndexController extends AbstractController
 {
 
     public function indexAction()
     {
-        /** @var ShipmentMethodTable $table */
         $table = $this->getDependencyContainer()
-            ->createShipmentMethodTable()
-        ;
+            ->createMethodTable();
         $table->init();
 
-        return $this->viewResponse([
-            'shipmentMethodTable' => $table,
-        ]);
-
+        return $this->viewResponse(['methodTable' => $table]);
     }
 }
