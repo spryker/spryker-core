bcb7796f797034f10c8ecee33d14e2868dfa1ac5
Dmytro <savin.dmitry@gmail.com>
2015-07-27 16:05:05 +0200

    СD-271 Add method controller

---
+++ Bundles/Shipment/src/SprykerFeature/Zed/Shipment/Communication/Controller/MethodController.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\Shipment\Communication\Controller;
+
+use Generated\Shared\Transfer\ShipmentCarrierTransfer;
+use SprykerFeature\Zed\Application\Communication\Controller\AbstractController;
+use SprykerFeature\Zed\Shipment\Business\ShipmentFacade;
+use SprykerFeature\Zed\Shipment\Communication\ShipmentDependencyContainer;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @method ShipmentDependencyContainer getDependencyContainer()
+ * @method ShipmentFacade getFacade()
+ */
+
+class MethodController extends AbstractController
+{
+    const ADD = 'add';
+    const UPDATE = 'update';
+    /**
+     * @return Response
+     */
+    public function addAction()
+    {
+        $form = $this->getDependencyContainer()->createMethodForm(self::ADD);
+        $form->handleRequest();
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+            $carrierTransfer = new ShipmentCarrierTransfer();
+            $carrierTransfer->fromArray($data, true);
+            $this->getFacade()->createCarrier($carrierTransfer);
+
+            return $this->redirectResponse('/shipment/');
+        }
+
+        return $this->viewResponse([
+            'form' => $form->createView(),
+        ]);
+    }
+}
