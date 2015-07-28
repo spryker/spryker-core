eaaa7c3454ee16bdca65d83af59e9840fe3b2462
René Klatt <rene.klatt@project-a.com>
2015-07-28 08:52:03 +0000

    CD-190 update command to handle "create database if exists" also for postgres

--- Bundles/Propel/src/SprykerEngine/Zed/Propel/Communication/Console/CreateDatabaseConsole.php
+++ Bundles/Propel/src/SprykerEngine/Zed/Propel/Communication/Console/CreateDatabaseConsole.php
@@ -5,6 +5,9 @@
 
 namespace SprykerEngine\Zed\Propel\Communication\Console;
 
+use Propel\Runtime\Connection\ConnectionManagerSingle;
+use Propel\Runtime\Propel;
+use Propel\Runtime\ServiceContainer\StandardServiceContainer;
 use SprykerEngine\Shared\Config;
 use SprykerFeature\Shared\System\SystemConfig;
 use SprykerFeature\Zed\Console\Business\Model\Console;
@@ -33,15 +36,54 @@ class CreateDatabaseConsole extends Console
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
         $this->info('Creating Database');
-        if(Config::get(SystemConfig::ZED_DB_ENGINE) == 'pgsql') {
-            $this->info('!! Postgresql database has to be created beforehand');
-            return;
+
+        if(Config::get(SystemConfig::ZED_DB_ENGINE) === 'pgsql') {
+            $this->createPostgresDatabaseIfNotExists();
+        } else {
+            $this->createMysqlDatabaseIfNotExists();
+        }
+    }
+
+    /**
+     * @throws \Exception
+     * @todo no sudo, vagrant user is missing for pgsql
+     */
+    private function createPostgresDatabaseIfNotExists()
+    {
+        $databaseExists = $this->existsPostgresDatabase();
+        if (!$databaseExists) {
+            $createDatabaseCommand = 'sudo createdb '  . Config::get(SystemConfig::ZED_DB_DATABASE) . ' -E UTF8 -T template0';
+            $process = new Process($createDatabaseCommand);
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                throw new \RuntimeException($process->getErrorOutput());
+            }
+        }
+    }
+
+    /**
+     * @throws \Exception
+     * @return bool
+     * @todo no sudo, vagrant user is missing for pgsql
+     */
+    private function existsPostgresDatabase()
+    {
+        $databaseExistsCommand = 'echo -n "$(sudo psql -lqt | cut -d \| -f 1 | grep -w ' . Config::get(SystemConfig::ZED_DB_DATABASE) . ' | wc -l)"';
+        $process = new Process($databaseExistsCommand);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException($process->getErrorOutput());
         }
 
+        return $process->getOutput();
+    }
 
-        $con = new \PDO(
+    private function createMysqlDatabaseIfNotExists()
+    {
+        $connection = new \PDO(
             Config::get(SystemConfig::ZED_DB_ENGINE)
             . ':host='
             . Config::get(SystemConfig::ZED_DB_HOST)
@@ -49,8 +91,9 @@ class CreateDatabaseConsole extends Console
             Config::get(SystemConfig::ZED_DB_USERNAME),
             Config::get(SystemConfig::ZED_DB_PASSWORD)
         );
-        $q = 'CREATE DATABASE IF NOT EXISTS ' . Config::get(SystemConfig::ZED_DB_DATABASE) . ' CHARACTER SET "utf8"';
-        $con->exec($q);
+
+        $query = 'CREATE DATABASE IF NOT EXISTS ' . Config::get(SystemConfig::ZED_DB_DATABASE) . ' CHARACTER SET "utf8"';
+        $connection->exec($query);
     }
 
 }
