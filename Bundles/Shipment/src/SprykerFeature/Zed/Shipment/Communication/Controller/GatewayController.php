256fa21aa7073c27428e68de60754c596aed48d7
Dmytro Savin <savin.dmitry@gmail.com>
2015-07-30 19:59:08 +0200

    СD-272 Add gateway interface

---
+++ Bundles/Shipment/src/SprykerFeature/Zed/Shipment/Communication/Controller/GatewayController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\Shipment\Communication\Controller;
+
+use Generated\Shared\Cart\CartInterface;
+use Generated\Shared\Shipment\ShipmentInterface;
+use SprykerFeature\Zed\Kernel\Communication\Controller\AbstractGatewayController;
+use SprykerFeature\Zed\Shipment\Business\ShipmentFacade;
+use SprykerFeature\Zed\Shipment\Communication\ShipmentDependencyContainer;
+
+/**
+ * @method ShipmentDependencyContainer getDependencyContainer()
+ * @method ShipmentFacade getFacade()
+ */
+class GatewayController extends AbstractGatewayController
+{
+
+    /**
+     * @param CartInterface $cartTransfer
+     *
+     * @return ShipmentInterface
+     */
+    public function getAvailableMethodsAction(CartInterface $cartTransfer)
+    {
+        return $this->getFacade()
+            ->getAvailableMethods($cartTransfer)
+            ;
+    }
+}
