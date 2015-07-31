d5ea932fa62bc94401e76fdb23f2b3e56044f99d
Freddie Frantzen <freddie@freddiefrantzen.com>
2015-07-31 12:30:04 +0200

    CD-335 hydrator and hydrator test implementation

--- Bundles/ProductOptionCheckoutConnector/src/SprykerFeature/Zed/ProductOptionCheckoutConnector/Business/ProductOptionOrderHydrator.php
+++ Bundles/ProductOptionCheckoutConnector/src/SprykerFeature/Zed/ProductOptionCheckoutConnector/Business/ProductOptionOrderHydrator.php
@@ -7,10 +7,9 @@
 namespace SprykerFeature\Zed\ProductOptionCheckoutConnector\Business;
 
 use Generated\Shared\ProductOptionCheckoutConnector\CheckoutRequestInterface;
+use Generated\Shared\ProductOptionCheckoutConnector\CartItemInterface;
 use Generated\Shared\ProductOptionCheckoutConnector\OrderInterface;
 use Generated\Shared\ProductOptionCheckoutConnector\OrderItemInterface;
-use Generated\Shared\Transfer\CartItemTransfer;
-use Generated\Shared\Transfer\OrderItemTransfer;
 
 class ProductOptionOrderHydrator implements ProductOptionOrderHydratorInterface
 {
@@ -22,8 +21,38 @@ class ProductOptionOrderHydrator implements ProductOptionOrderHydratorInterface
     {
         $cart = $request->getCart();
 
-        // ... copy ProductOption DTOs from CartItems, to OrderItems, which should alread have been hydrated
-        // by the CartCheckoutConnector plugin
+        $this->transferProductOptionsFromCartToOrder($cart->getItems(), $order->getItems());
+    }
+
+    /**
+     * @param \ArrayObject|CartItemInterface[] $cartItems
+     * @param \ArrayObject|OrderItemInterface[] $orderItems
+     */
+    private function transferProductOptionsFromCartToOrder(\ArrayObject $cartItems, \ArrayObject $orderItems)
+    {
+        foreach ($cartItems as $cartItem) {
+            $this->transferItemOptions($cartItem, $orderItems);
+        }
+    }
+
+    /**
+     * @param CartItemInterface $cartItem
+     * @param \ArrayObject|OrderItemInterface[] $orderItems
+     */
+    private function transferItemOptions(CartItemInterface $cartItem, \ArrayObject $orderItems)
+    {
+        foreach ($orderItems as $orderItem) {
+
+            if ($cartItem->getSku() !== $orderItem->getSku()) {
+                continue;
+            }
+
+            if (empty($cartItem->getProductOptions())) {
+                continue;
+            }
+
+            $orderItem->setProductOptions(clone $cartItem->getProductOptions());
+        }
     }
 
 }
---
+++ Bundles/ProductOptionCheckoutConnector/tests/Unit/SprykerFeature/Zed/ProductOptionCheckoutConnector/Business/ProductOptionOrderHydratorTest.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace Unit\SprykerFeature\Zed\ProductOptionCheckoutConnector\Business;
+
+use Generated\Shared\Transfer\CartItemTransfer;
+use Generated\Shared\Transfer\CartTransfer;
+use Generated\Shared\Transfer\CheckoutRequestTransfer;
+use Generated\Shared\Transfer\OrderItemTransfer;
+use Generated\Shared\Transfer\OrderTransfer;
+use Generated\Shared\Transfer\ProductOptionTransfer;
+use SprykerFeature\Zed\ProductOptionCheckoutConnector\Business\ProductOptionOrderHydrator;
+
+/**
+ * @group SprykerFeature
+ * @group Zed
+ * @group ProducOptionCheckoutConnector
+ * @group Business
+ * @group ProductOptionOrderHydrator
+ */
+class ProductOptionOrderHydratorTest extends \PHPUnit_Framework_TestCase
+{
+    const CONCRETE_SKU = 'concrete sku';
+
+    const LABEL_OPTION_TYPE = 'label option type';
+
+    public function testHydratorTransfersProductOptionsFromCartItemToOrderItem()
+    {
+        $hydrator = $this->getHydrator();
+
+        $order = $this->getOrderFixture();
+
+        $hydrator->hydrateOrderTransfer($order, $this->getRequestFixture());
+
+        $hydratedOrderItem = $order->getItems()[0];
+        $productOptions = $hydratedOrderItem->getProductOptions();
+
+        $this->assertCount(1, $productOptions);
+        $this->assertEquals(self::LABEL_OPTION_TYPE, $productOptions[0]->getLabelOptionType());
+    }
+
+    /**
+     * @return ProductOptionOrderHydrator
+     */
+    public function getHydrator()
+    {
+        return new ProductOptionOrderHydrator();
+    }
+
+    /**
+     * @return OrderTransfer
+     */
+    private function getOrderFixture()
+    {
+        $orderItem = new OrderItemTransfer();
+        $orderItem->setSku(self::CONCRETE_SKU);
+
+        $order = new OrderTransfer();
+        $order->addItem($orderItem);
+
+        return $order;
+    }
+
+    /**
+     * @return CheckoutRequestTransfer
+     */
+    private function getRequestFixture()
+    {
+        $productOption = new ProductOptionTransfer();
+        $productOption->setLabelOptionType(self::LABEL_OPTION_TYPE);
+
+        $cartItem = new CartItemTransfer();
+        $cartItem->setSku(self::CONCRETE_SKU);
+        $cartItem->addProductOption($productOption);
+
+        $cart = new CartTransfer();
+        $cart->addItem($cartItem);
+
+        $request = new CheckoutRequestTransfer();
+        $request->setCart($cart);
+
+        return $request;
+    }
+
+}
