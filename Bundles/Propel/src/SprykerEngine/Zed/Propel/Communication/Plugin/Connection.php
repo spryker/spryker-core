84183f9eaa37ecc991857f0c6a844dcb5187d188
Dmytro Savin <savin.dmitry@gmail.com>
2015-08-04 12:50:45 +0200

    CSN-19 Add propel connection plugin

---
+++ Bundles/Propel/src/SprykerEngine/Zed/Propel/Communication/Plugin/Connection.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerEngine\Zed\Propel\Communication\Plugin;
+
+use Propel\Runtime\Connection\ConnectionInterface;
+use Propel\Runtime\Propel;
+use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
+use SprykerEngine\Zed\Propel\Business\PropelFacade;
+
+/**
+ * @method PropelFacade getFacade()
+ */
+class Connection extends AbstractPlugin
+{
+
+    /**
+     * @return ConnectionInterface
+     */
+    public function get()
+    {
+        return Propel::getConnection();
+    }
+}
