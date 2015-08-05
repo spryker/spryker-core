5de3e602378d2770593b98b30d217d19945d949d
Konstantin Scheumann <konstantin.scheumann@spryker.com>
2015-08-05 14:34:23 +0000

    Bundle based Glossary Files and Setup Command

---
+++ Bundles/Installer/src/SprykerFeature/Zed/Installer/Business/Model/GlossaryInstaller.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * (c) Copyright Spryker Systems GmbH 2015
+ */
+
+namespace SprykerFeature\Zed\Installer\Business\Model;
+
+use Generated\Shared\Transfer\LocaleTransfer;
+use SprykerFeature\Zed\Glossary\Business\GlossaryFacade;
+use SprykerFeature\Zed\Glossary\Communication\GlossaryDependencyContainer;
+use SprykerFeature\Zed\Glossary\Persistence\GlossaryQueryContainer;
+use Symfony\Component\Yaml\Yaml;
+
+class GlossaryInstaller extends AbstractInstaller
+{
+
+    private $glossaryFacade;
+
+    private $paths;
+
+    /**
+     * @param GlossaryFacade $glossaryFacade
+     * @param array $paths
+     */
+    public function __construct(GlossaryFacade $glossaryFacade, array $paths = [])
+    {
+        $this->glossaryFacade = $glossaryFacade;
+        $this->paths = $paths;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function install()
+    {
+        $results = array();
+
+        foreach ($this->paths as $filePath) {
+            $translations = $this->parseYamlFile($filePath);
+            $result = $this->installKeysAndTranslations($translations);
+            $results[$filePath] = $result;
+        }
+
+        return $results;
+    }
+
+    /**
+     * @param string $filePath
+     *
+     * @return array
+     */
+    protected function parseYamlFile($filePath)
+    {
+        $yamlParser = new Yaml();
+
+        return $yamlParser->parse(file_get_contents($filePath));
+    }
+
+    /**
+     * @param array $translations
+     * @return array
+     */
+    protected function installKeysAndTranslations(array $translations)
+    {
+        $results = [];
+        foreach ($translations['keys'] as $keyName => $data) {
+            $results[$keyName]['created'] = false;
+            if (!$this->glossaryFacade->hasKey($keyName)) {
+                $this->glossaryFacade->createKey($keyName);
+                $results[$keyName]['created'] = true;
+            }
+
+            foreach ($data['translations'] as $localeName => $text) {
+                $locale = new LocaleTransfer();
+                $locale->setLocaleName($localeName);
+                $results[$keyName]['translation'][$localeName]['text'] = $text;
+                $results[$keyName]['translation'][$localeName]['created'] = false;
+                $results[$keyName]['translation'][$localeName]['updated'] = false;
+
+                if (!$this->glossaryFacade->hasTranslation($keyName, $locale)) {
+                    $this->glossaryFacade->createAndTouchTranslation($keyName, $locale, $text, true);
+                    $results[$keyName]['translation'][$localeName]['created'] = true;
+                } elseif ($this->glossaryFacade->getTranslation($keyName, $locale)->getValue() != $text) {
+                    $this->glossaryFacade->updateAndTouchTranslation($keyName, $locale, $text, true);
+                    $results[$keyName]['translation'][$localeName]['updated'] = true;
+                }
+            }
+        }
+
+        return $results;
+    }
+
+}
--- Bundles/Installer/src/SprykerFeature/Zed/Installer/InstallerDependencyProvider.php
+++ Bundles/Installer/src/SprykerFeature/Zed/Installer/InstallerDependencyProvider.php
@@ -7,8 +7,27 @@
 namespace SprykerFeature\Zed\Installer;
 
 use SprykerEngine\Zed\Kernel\AbstractBundleDependencyProvider;
+use SprykerEngine\Zed\Kernel\Container;
 
 class InstallerDependencyProvider extends AbstractBundleDependencyProvider
 {
+
+    const GLOSSARY_FACADE = 'facade_glossary';
+
+    /**
+     * @param Container $container
+     * @return Container
+     */
+    public function provideBusinessLayerDependencies(Container $container)
+    {
+        $container = parent::provideBusinessLayerDependencies($container);
+
+        $container[InstallerDependencyProvider::GLOSSARY_FACADE] = function (Container $container) {
+            return $container->getLocator()->glossary()->facade();
+        };
+
+        return $container;
+    }
+
 
 }
