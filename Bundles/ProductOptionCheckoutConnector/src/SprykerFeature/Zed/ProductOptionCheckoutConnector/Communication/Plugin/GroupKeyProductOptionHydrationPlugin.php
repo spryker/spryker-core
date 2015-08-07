2bcee08e4ef7ec14829742f33439d75a31cade7a
Aurimas Lickus <aurimas.lickus@spryker.com>
2015-08-07 14:06:57 +0200

    CD-366: product option plugins

---
+++ Bundles/ProductOptionCheckoutConnector/src/SprykerFeature/Zed/ProductOptionCheckoutConnector/Communication/Plugin/GroupKeyProductOptionHydrationPlugin.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\ProductOptionCheckoutConnector\Communication\Plugin;
+
+use Generated\Shared\Transfer\CheckoutRequestTransfer;
+use Generated\Shared\Transfer\OrderTransfer;
+use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
+use SprykerFeature\Zed\Checkout\Dependency\Plugin\CheckoutOrderHydrationInterface;
+use Generated\Shared\ProductOption\ProductOptionInterface;
+use Generated\Shared\ProductOptionCartConnector\ItemInterface;
+
+class GroupKeyProductOptionHydrationPlugin extends AbstractPlugin implements CheckoutOrderHydrationInterface
+{
+    /**
+     * @param OrderTransfer           $orderTransfer
+     * @param CheckoutRequestTransfer $checkoutRequest
+     */
+    public function hydrateOrder(OrderTransfer $orderTransfer, CheckoutRequestTransfer $checkoutRequest)
+    {
+        foreach ($orderTransfer as $orderItem) {
+            $orderItem->setGroupKey($this->buildGroupKey($orderItem));
+        }
+    }
+
+    /**
+     * @param ItemInterface $item
+     *
+     * @return string
+     */
+    protected function buildGroupKey(ItemInterface $item)
+    {
+        $currentGroupKey = $item->getGroupKey();
+        if (empty($item->getProductOptions())) {
+            return $currentGroupKey;
+        }
+
+        $sortedProductOptions = $this->sortOptions((array) $item->getProductOptions());
+        $optionGroupKey = $this->combineOptionParts($sortedProductOptions);
+
+        if (empty($optionGroupKey)) {
+            return $currentGroupKey;
+        }
+
+        return !empty($currentGroupKey) ? $currentGroupKey . '-' . $optionGroupKey : $optionGroupKey;
+    }
+
+    /**
+     * @param ProductOptionInterface[] $options
+     *
+     * @return array
+     */
+    protected function sortOptions(array $options)
+    {
+        usort(
+            $options,
+            function (ProductOptionInterface $a, ProductOptionInterface $b) {
+                return ($a->getIdOptionValueUsage() < $b->getIdOptionValueUsage()) ? -1 : 1;
+            }
+        );
+
+        return $options;
+    }
+
+    /**
+     * @param ProductOptionInterface[] $sortedProductOptions
+     *
+     * @return string
+     */
+    protected function combineOptionParts(array $sortedProductOptions)
+    {
+        $groupKeyPart = [];
+        foreach ($sortedProductOptions as $option) {
+            $groupKeyPart[] = $option->getIdOptionValueUsage();
+        }
+        return implode('-', $groupKeyPart);
+    }
+}
---
+++ Bundles/ProductOptionWishlistConnector/src/SprykerFeature/Zed/ProductOptionWishlistConnector/Communication/Plugin/PreSaveGroupKeyProductOptionPlugin.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\ProductOptionWishlistConnector\Communication\Plugin;
+
+use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
+use Generated\Shared\ProductOption\ProductOptionInterface;
+use Generated\Shared\ProductOptionCartConnector\ItemInterface;
+use SprykerFeature\Zed\Wishlist\Dependency\PreSavePluginInterface;
+
+class PreSaveGroupKeyProductOptionPlugin extends AbstractPlugin implements PreSavePluginInterface
+{
+    /**
+     * @param \ArrayObject $items
+     */
+    public function trigger(\ArrayObject $items)
+    {
+        foreach ($items as $item) {
+            $item->setGroupKey($this->buildGroupKey($item));
+        }
+    }
+
+    /**
+     * @param ItemInterface $item
+     *
+     * @return string
+     */
+    protected function buildGroupKey(ItemInterface $item)
+    {
+        $currentGroupKey = $item->getGroupKey();
+        if (empty($item->getProductOptions())) {
+            return $currentGroupKey;
+        }
+
+        $sortedProductOptions = $this->sortOptions((array) $item->getProductOptions());
+        $optionGroupKey = $this->combineOptionParts($sortedProductOptions);
+
+        if (empty($optionGroupKey)) {
+            return $currentGroupKey;
+        }
+
+        return !empty($currentGroupKey) ? $currentGroupKey . '-' . $optionGroupKey : $optionGroupKey;
+    }
+
+    /**
+     * @param ProductOptionInterface[] $options
+     *
+     * @return array
+     */
+    protected function sortOptions(array $options)
+    {
+        usort(
+            $options,
+            function (ProductOptionInterface $a, ProductOptionInterface $b) {
+                return ($a->getIdOptionValueUsage() < $b->getIdOptionValueUsage()) ? -1 : 1;
+            }
+        );
+
+        return $options;
+    }
+
+    /**
+     * @param ProductOptionInterface[] $sortedProductOptions
+     *
+     * @return string
+     */
+    protected function combineOptionParts(array $sortedProductOptions)
+    {
+        $groupKeyPart = [];
+        foreach ($sortedProductOptions as $option) {
+            $groupKeyPart[] = $option->getIdOptionValueUsage();
+        }
+        return implode('-', $groupKeyPart);
+    }
+
+
+}
