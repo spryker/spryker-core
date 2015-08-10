c4edbbf75533688a10e66c7b37d85d8831466f69
Dmytro Savin <savin.dmitry@gmail.com>
2015-08-10 09:30:51 +0200

    CSN-19 Add todo to clear mock process

--- Bundles/ProductOption/tests/Functional/SprykerFeature/Zed/ProductOption/Mock/ProductFacade.php
+++ Bundles/ProductOption/tests/Functional/SprykerFeature/Zed/ProductOption/Mock/ProductFacade.php
@@ -3,6 +3,9 @@
  * (c) Spryker Systems GmbH copyright protected
  */
 
+/**
+ * TODO: Check if we need to mock exactly this class or just an interface
+ */
 namespace Functional\SprykerFeature\Zed\ProductOption\Mock;
 
 use SprykerFeature\Zed\Product\Business\ProductFacade as SprykerProductFacade;
