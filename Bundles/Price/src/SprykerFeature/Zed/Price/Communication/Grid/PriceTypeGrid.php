9d089d7c9f6502d25ac2fc07503fad0e82696e03
Marius Bogdan <marius.rizac@spryker.com>
2015-08-20 14:04:04 +0000

    CD-364 Price - grids

--- Bundles/Price/src/SprykerFeature/Zed/Price/Communication/Grid/PriceTypeGrid.php
+++ Bundles/Price/src/SprykerFeature/Zed/Price/Communication/Grid/PriceTypeGrid.php
@@ -6,12 +6,23 @@
 
 namespace SprykerFeature\Zed\Price\Communication\Grid;
 
-use SprykerFeature\Zed\Ui\Dependency\Grid\AbstractGrid;
+use SprykerFeature\Zed\Gui\Communication\Table\AbstractTable;
+use SprykerFeature\Zed\Gui\Communication\Table\TableConfiguration;
 
-class PriceTypeGrid extends AbstractGrid
+class PriceTypeGrid extends AbstractTable
 {
 
     const NAME = 'name';
+
+    protected function configure(TableConfiguration $config)
+    {
+        // @todo: Implement configure() method.
+    }
+
+    protected function prepareData(TableConfiguration $config)
+    {
+        // @todo: Implement prepareData() method.
+    }
 
     /**
      * @return array
