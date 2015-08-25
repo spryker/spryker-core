4868445e3fcd2ee43b343fe518880fe8e5651d6c
Fabian Wesner <fabian.wesner@spryker.com>
2015-08-25 19:46:24 +0200

    CD-225 Added config for twig caching in Zed and Yves

--- Bundles/Application/src/SprykerFeature/Shared/Application/ApplicationConfig.php
+++ Bundles/Application/src/SprykerFeature/Shared/Application/ApplicationConfig.php
@@ -13,4 +13,8 @@ interface ApplicationConfig extends ConfigInterface
 
     const NAVIGATION_CACHE_ENABLED = 'navigation cache enabled';
 
+    const ZED_TWIG_OPTIONS = 'ZED_TWIG_OPTIONS'; // see http://twig.sensiolabs.org/doc/api.html#environment-options
+
+    const YVES_TWIG_OPTIONS = 'YVES_TWIG_OPTIONS';
+
 }
