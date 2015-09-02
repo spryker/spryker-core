84d6db8a100c28c88286e04f7579c2a36af3c0b7
Oliwier Ptak <oliwier.ptak@spryker.com>
2015-09-02 18:03:34 +0200

    KSP-666 added assigned products in Category Edit

---
+++ Bundles/Category/src/SprykerFeature/Zed/Category/Communication/Controller/EditController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace SprykerFeature\Zed\Category\Communication\Controller;
+
+use Generated\Shared\Transfer\CategoryTransfer;
+use Generated\Shared\Transfer\NodeTransfer;
+use SprykerFeature\Zed\Application\Communication\Controller\AbstractController;
+use SprykerFeature\Zed\Category\Business\CategoryFacade;
+use SprykerFeature\Zed\Category\Communication\CategoryDependencyContainer;
+use SprykerFeature\Zed\Category\Persistence\CategoryQueryContainer;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * @method CategoryFacade getFacade()
+ * @method CategoryDependencyContainer getDependencyContainer()
+ * @method CategoryQueryContainer getQueryContainer()
+ */
+class EditController extends AbstractController
+{
+
+    const PARAM_ID_CATEGORY = 'id-category';
+
+    /**
+     * @param Request $request
+     *
+     * @return array|RedirectResponse
+     */
+    public function indexAction(Request $request)
+    {
+        $idCategory = $request->get(self::PARAM_ID_CATEGORY);
+
+        $categoryExists = $this->getQueryContainer()->queryCategoryById($idCategory)->count() > 0;
+        
+        if (!$categoryExists) {
+            $this->addErrorMessage(sprintf('The category you are trying to edit %s does not exist.', $idCategory));
+            return new RedirectResponse('/category');
+        }
+
+        $locale = $this->getDependencyContainer()
+            ->createCurrentLocale()
+        ;
+
+        /**
+         * @var Form $form
+         */
+        $form = $this->getDependencyContainer()
+            ->createCategoryFormEdit($idCategory)
+        ;
+        $form->handleRequest();
+
+        if ($form->isValid()) {
+            $categoryTransfer = (new CategoryTransfer())
+                ->fromArray($form->getData(), true)
+            ;
+            
+            $this->getFacade()
+                ->updateCategory($categoryTransfer, $locale)
+            ;
+
+            $categoryNodeTransfer = (new NodeTransfer())
+                ->fromArray($form->getData(), true)
+            ;
+
+            $this->getFacade()
+                ->updateNodeWithTreeWriter($categoryNodeTransfer)
+            ;
+
+            $this->addSuccessMessage('The category was saved successfully.');
+            
+            return $this->redirectResponse('/category');
+        }
+
+        $productCategories = $this->getDependencyContainer()
+            ->createProductCategoryTable($locale, $idCategory)
+        ;
+        
+        return $this->viewResponse([
+            'idCategory' => $idCategory,
+            'form' => $form->createView(),
+            'productCategories' => $productCategories->render(),
+        ]);
+    }
+
+
+    /**
+     * @return JsonResponse
+     */
+    public function productCategoryTableAction(Request $request)
+    {
+        $idCategory = $request->get(self::PARAM_ID_CATEGORY);
+        $locale = $this->getDependencyContainer()
+            ->createCurrentLocale()
+        ;
+
+        $table = $this->getDependencyContainer()
+            ->createProductCategoryTable($locale, $idCategory)
+        ;
+
+        return $this->jsonResponse(
+            $table->fetchData()
+        );
+    }
+
+}
---
+++ Bundles/Category/src/SprykerFeature/Zed/Category/Communication/Table/ProductCategoryTable.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\Category\Communication\Table;
+
+use Generated\Shared\Transfer\LocaleTransfer;
+use SprykerFeature\Zed\Gui\Communication\Table\AbstractTable;
+use SprykerFeature\Zed\Gui\Communication\Table\TableConfiguration;
+use SprykerFeature\Zed\Product\Persistence\Propel\Map\SpyAbstractProductTableMap;
+use SprykerFeature\Zed\Product\Persistence\Propel\Map\SpyLocalizedAbstractProductAttributesTableMap;
+use SprykerFeature\Zed\ProductCategory\Persistence\ProductCategoryQueryContainer;
+use SprykerFeature\Zed\ProductCategory\Persistence\Propel\Map\SpyProductCategoryTableMap;
+
+
+class ProductCategoryTable extends AbstractTable
+{
+    const TABLE_IDENTIFIER = 'product-category-table';
+
+    /**
+     * @var ProductCategoryQueryContainer
+     */
+    protected $productCategoryQueryContainer;
+
+
+    /**
+     * @var LocaleTransfer
+     */
+    protected $locale;
+
+    /**
+     * @var int
+     */
+    protected $idCategory;
+
+    /**
+     * @param ProductCategoryQueryContainer $productCategoryQueryContainer
+     * @param LocaleTransfer $locale
+     * @param int $idCategory
+     */
+    public function __construct(ProductCategoryQueryContainer $productCategoryQueryContainer, LocaleTransfer $locale, $idCategory)
+    {
+        $this->productCategoryQueryContainer = $productCategoryQueryContainer;
+        $this->locale = $locale;
+        $this->idCategory = $idCategory;
+        $this->defaultUrl = sprintf('productCategoryTable?id-category=%d', $this->idCategory);
+        $this->setTableIdentifier(self::TABLE_IDENTIFIER);
+    }
+
+    /**
+     * @param TableConfiguration $config
+     *
+     * @return TableConfiguration
+     */
+    protected function configure(TableConfiguration $config)
+    {
+        $config->setHeader([
+            SpyAbstractProductTableMap::COL_SKU => 'SKU',
+            SpyLocalizedAbstractProductAttributesTableMap::COL_NAME => 'Name',
+            SpyProductCategoryTableMap::COL_FK_PRECONFIG_PRODUCT => 'Preconfig',
+
+        ]);
+        $config->setSortable([
+            SpyAbstractProductTableMap::COL_SKU,
+            //SpyLocalizedAbstractProductAttributesTableMap::COL_NAME,
+        ]);
+
+        return $config;
+    }
+
+    /**
+     * @param TableConfiguration $config
+     *
+     * @return array
+     */
+    protected function prepareData(TableConfiguration $config)
+    {
+        $query = $this->productCategoryQueryContainer->queryProductsByCategoryId($this->idCategory, $this->locale);
+        $query->setModelAlias('spy_abstract_product');
+        
+        $queryResults = $this->runQuery($query, $config);
+        $results = [];
+        foreach ($queryResults as $productCategory) {
+            //die(dump($productCategory));
+            $results[] = [
+                SpyAbstractProductTableMap::COL_SKU => $productCategory['sku'],
+                SpyLocalizedAbstractProductAttributesTableMap::COL_NAME => $productCategory['name'],
+                SpyProductCategoryTableMap::COL_FK_PRECONFIG_PRODUCT => $productCategory[SpyProductCategoryTableMap::COL_FK_PRECONFIG_PRODUCT],
+            ];
+        }
+        unset($queryResults);
+        return $results;
+    }
+}
