3d7e51de48b6f83de578e9e0794d9d32b78dc1c8
Stefan Langwald <stefanlangwald+github.com@gmail.com>
2015-09-03 16:24:56 +0200

    CD-375 optimistic locking, random filewrites with preference handles added.

--- Bundles/Lumberjack/src/SprykerEngine/Shared/Lumberjack/Model/Writer/File.php
+++ Bundles/Lumberjack/src/SprykerEngine/Shared/Lumberjack/Model/Writer/File.php
@@ -12,6 +12,13 @@ class File extends AbstractWriter
 {
 
     /**
+     * @var array
+     */
+    static $fileHandles = [];
+
+    static $preferedHandle;
+
+    /**
      * @inheritdoc
      */
     public function write(EventInterface $event)
@@ -21,22 +28,71 @@ class File extends AbstractWriter
             return false;
         }
 
-        return (bool)file_put_contents(
-            $this->getFilePath(),
-            $output,
-            FILE_APPEND | LOCK_EX
-        );
+        return $this->optimisticRandomWrite($output);
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return bool
+     */
+    protected function optimisticRandomWrite($content)
+    {
+        for ($i = 0; $i <= 9; $i++) {
+            $handle = $this->getOrCreateRandomFileHandle();
+            if ($this->acquireNonBlockingLock($handle)) {
+                self::$preferedHandle = $handle;
+                // @todo add silent exception when this operation fails
+                fwrite($handle, $content);
+                $this->unlock($handle);
+
+                return true;
+            }
+            self::$preferedHandle = null;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getOrCreateRandomFileHandle()
+    {
+        if(null !== self::$preferedHandle) {
+            return self::$preferedHandle;
+        }
+        $fileName = $this->getRandomFileName();
+        if (!isset(static::$fileHandles[$fileName])) {
+            $fileHandle = fopen($fileName, 'a');
+            static::$fileHandles[$fileName] = $fileHandle;
+        }
+
+        return static::$fileHandles[$fileName];
     }
 
     /**
      * @return string
      */
-    protected function getFilePath()
+    protected function getRandomFileName()
     {
         $path = \SprykerFeature_Shared_Library_Data::getLocalCommonPath('lumberjack');
-        $path .= 'lumberjack-' . date('Y-m-d') . '.log';
+        $path .= sprintf(
+            '%s.%s.%d.log',
+            'lumberjack',
+            date('Y-m-d'),
+            $this->getRandomizedFileIndex()
+        );
 
         return $path;
+    }
+
+    /**
+     * @return int
+     */
+    protected function getRandomizedFileIndex()
+    {
+        return rand(0, 9);
     }
 
     /**
@@ -46,12 +102,30 @@ class File extends AbstractWriter
      */
     protected function getJsonEntry(EventInterface $event)
     {
-        $json = json_encode($event->getFields());
+        $json = json_encode($event->getFields(), JSON_PRETTY_PRINT);
         if ($json === false) {
             return '';
         }
 
         return $json . "\n";
+    }
+
+    /**
+     * @param $handle
+     *
+     * @return bool
+     */
+    protected function acquireNonBlockingLock($handle)
+    {
+        return flock($handle, LOCK_EX | LOCK_NB);
+    }
+
+    /**
+     * @param $handle
+     */
+    protected function unlock($handle)
+    {
+        flock($handle, LOCK_UN);
     }
 
 }
