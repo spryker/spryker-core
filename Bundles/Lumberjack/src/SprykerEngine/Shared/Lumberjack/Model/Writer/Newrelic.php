5bdaffed5b360d266025ebe1a2398e09d0c4fbc4
Stefan Langwald <stefanlangwald+github.com@gmail.com>
2015-09-07 15:31:10 +0200

    Newrelic writer implemented

--- Bundles/Lumberjack/src/SprykerEngine/Shared/Lumberjack/Model/Writer/Newrelic.php
+++ Bundles/Lumberjack/src/SprykerEngine/Shared/Lumberjack/Model/Writer/Newrelic.php
@@ -7,15 +7,18 @@
 namespace SprykerEngine\Shared\Lumberjack\Model\Writer;
 
 use SprykerEngine\Shared\Lumberjack\Model\EventInterface;
+use \SprykerFeature\Shared\Library\NewRelic\Api as NewRelicApi;
 
 class Newrelic extends AbstractWriter
 {
 
     public function write(EventInterface $event)
     {
-        $api = \SprykerFeature\Shared\Library\NewRelic\Api()::getInstance();
+        $api = NewRelicApi::getInstance();
 
-        //foreach($event)
+        foreach($event as $field => $value) {
+            $api->addCustomParameter($field, $value);
+        }
         return true;
     }
 }
