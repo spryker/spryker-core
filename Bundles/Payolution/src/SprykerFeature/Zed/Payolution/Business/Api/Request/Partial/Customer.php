a53753f93f4891890543b1ccc242044510e5d8f5
Thomas Kühnel <thomas.kuehnel@spryker.com>
2015-09-08 10:49:05 +0200

    CD-408 Adds missing getter/setter methods

--- Bundles/Payolution/src/SprykerFeature/Zed/Payolution/Business/Api/Request/Partial/Customer.php
+++ Bundles/Payolution/src/SprykerFeature/Zed/Payolution/Business/Api/Request/Partial/Customer.php
@@ -24,4 +24,52 @@ class Customer extends AbstractRequest
      * @var Contact
      */
     protected $contact;
+
+    /**
+     * @return Name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param Name $name
+     */
+    public function setName(Name $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return Address
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param Address $address
+     */
+    public function setAddress(Address $address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return Contact
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * @param Contact $contact
+     */
+    public function setContact(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
 }
