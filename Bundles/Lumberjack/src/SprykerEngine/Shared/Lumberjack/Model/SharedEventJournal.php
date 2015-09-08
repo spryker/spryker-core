db2cf57fd26657867f4e2a953028022b7c5de6a5
Stefan Langwald <stefanlangwald+github.com@gmail.com>
2015-09-08 16:23:00 +0200

    CD-375 boots are rapping!

--- Bundles/Lumberjack/src/SprykerEngine/Shared/Lumberjack/Model/SharedEventJournal.php
+++ Bundles/Lumberjack/src/SprykerEngine/Shared/Lumberjack/Model/SharedEventJournal.php
@@ -7,7 +7,7 @@
 namespace SprykerEngine\Shared\Lumberjack\Model;
 
 /**
- * DO NOT USE THIS CLASS in general. It is meant to be used in the bootsrapping when it would be inconvenient to
+ * DO NOT USE THIS CLASS in general. It is meant to be used in the bootstrapping when it would be inconvenient to
  * determine every time whether to use the Yves or Zed Journal.
  */
 class SharedEventJournal extends AbstractEventJournal
