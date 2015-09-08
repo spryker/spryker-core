236a56d1eb5ce8b355e1b28317bd4b8fedf9fa36
Thomas Kühnel <thomas.kuehnel@spryker.com>
2015-09-08 10:55:15 +0200

    CD-408 Adds toArray() implementation in AbstractRequest

--- Bundles/Payolution/src/SprykerFeature/Zed/Payolution/Business/Api/Request/AbstractRequest.php
+++ Bundles/Payolution/src/SprykerFeature/Zed/Payolution/Business/Api/Request/AbstractRequest.php
@@ -5,11 +5,39 @@
 
 namespace SprykerFeature\Zed\Payolution\Business\Api\Request;
 
-
 abstract class AbstractRequest
 {
+    /**
+     * @return array
+     */
     public function toArray()
     {
+        $requestData = [];
+        $segmentName = $this->extractSegmentNameFromClassName();
 
+        foreach ($this as $propertyName => $propertyValue) {
+            if ($propertyValue instanceof AbstractRequest) {
+                $requestData = array_merge($requestData, $propertyValue->toArray());
+                continue;
+            }
+
+            if ($propertyValue === null) {
+                continue;
+            }
+
+            $dataKey = sprintf('%s.%s', $segmentName, strtoupper($propertyName));
+            $requestData[$dataKey] = $propertyValue;
+        }
+
+        return $requestData;
+    }
+
+    /**
+     * @return string
+     */
+    private function extractSegmentNameFromClassName()
+    {
+        $reflection = new \ReflectionClass($this);
+        return strtoupper($reflection->getShortName());
     }
 }
