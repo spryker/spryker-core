c2fc6e03a6a9becee587877c1a1ab75baa40a8e6
Stefan Langwald <stefanlangwald+github.com@gmail.com>
2015-09-09 12:51:47 +0200

    CD-375 call to parent dependency provider

--- Bundles/Lumberjack/src/SprykerFeature/Zed/Lumberjack/LumberjackDependencyProvider.php
+++ Bundles/Lumberjack/src/SprykerFeature/Zed/Lumberjack/LumberjackDependencyProvider.php
@@ -19,6 +19,7 @@ class LumberjackDependencyProvider extends AbstractBundleDependencyProvider
      */
     public function provideBusinessLayerDependencies(Container $container)
     {
+        parent::provideBusinessLayerDependencies($container);
         return $container;
     }
 
