7ac97410874a58f4749521104e4744471e67077d
Thomas Kühnel <thomas.kuehnel@spryker.com>
2015-09-09 10:06:35 +0000

    CD-408 Adds missing customer when setting up test data

--- Bundles/Payolution/tests/Functional/SprykerFeature/Zed/Payolution/Business/PayolutionFacadeTest.php
+++ Bundles/Payolution/tests/Functional/SprykerFeature/Zed/Payolution/Business/PayolutionFacadeTest.php
@@ -9,6 +9,8 @@ use Codeception\TestCase\Test;
 use Generated\Zed\Ide\AutoCompletion;
 use SprykerEngine\Zed\Kernel\Locator;
 use SprykerFeature\Zed\Country\Persistence\Propel\SpyCountryQuery;
+use SprykerFeature\Zed\Customer\Persistence\Propel\Map\SpyCustomerTableMap;
+use SprykerFeature\Zed\Customer\Persistence\Propel\SpyCustomer;
 use SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrder;
 use SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrderAddress;
 
@@ -39,6 +41,15 @@ class PayolutionFacadeTest extends Test
             ->setZipCode('10623')
             ->save();
 
+        $customer = (new SpyCustomer())
+            ->setFirstName('John')
+            ->setLastName('Doe')
+            ->setEmail('john@doe.com')
+            ->setDateOfBirth('1970-01-01')
+            ->setGender(SpyCustomerTableMap::COL_GENDER_MALE)
+            ->setCustomerReference('payolution-pre-authorization-test');
+        $customer->save();
+
         $orderEntity = (new SpySalesOrder())
             ->setEmail('john@doe.com')
             ->setGrandTotal(100.00)
@@ -46,6 +57,7 @@ class PayolutionFacadeTest extends Test
             ->setIsTest(true)
             ->setFkSalesOrderAddressBilling($billingAddress)
             ->setFkSalesOrderAddressShipping($billingAddress)
+            ->setCustomer($customer)
             ->setOrderReference('foo-bar-baz-2');
         $orderEntity->save();
 
