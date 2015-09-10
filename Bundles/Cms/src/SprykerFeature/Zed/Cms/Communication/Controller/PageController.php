171a04972cd296943b38529b266e032cceeb5081
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2015-09-10 14:42:15 +0000

    KSP-800 added touch for editing page

--- Bundles/Cms/src/SprykerFeature/Zed/Cms/Communication/Controller/PageController.php
+++ Bundles/Cms/src/SprykerFeature/Zed/Cms/Communication/Controller/PageController.php
@@ -104,6 +104,7 @@ class PageController extends AbstractController
 
             $pageTransfer = $this->createPageTransfer($data);
             $pageTransfer = $this->getFacade()->savePage($pageTransfer);
+            $this->getFacade()->touchPageActive($pageTransfer);
 
             if (intval($data[CmsPageForm::CURRENT_TEMPLATE]) !== intval($data[CmsPageForm::FK_TEMPLATE])) {
                 $this->getFacade()->deleteGlossaryKeysByIdPage($idPage);
