42965cd7156934c54aa187324a43d39d2dc68c1b
Oliwier Ptak <oliwier.ptak@spryker.com>
2015-09-11 14:13:30 +0200

    KSP-666 cs fixer

--- Bundles/Category/src/SprykerFeature/Zed/Category/Communication/Controller/EditController.php
+++ Bundles/Category/src/SprykerFeature/Zed/Category/Communication/Controller/EditController.php
@@ -9,8 +9,8 @@ use SprykerFeature\Zed\Category\CategoryConfig;
 use SprykerFeature\Zed\Category\Communication\CategoryDependencyContainer;
 use SprykerFeature\Zed\Category\Persistence\CategoryQueryContainer;
 use Symfony\Component\Form\Form;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @method CategoryFacade getFacade()
@@ -19,7 +19,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
  */
 class EditController extends AddController
 {
-
     /**
      * @param Request $request
      *
@@ -30,9 +29,10 @@ class EditController extends AddController
         $idCategory = $request->get(CategoryConfig::PARAM_ID_CATEGORY);
 
         $categoryExists = $this->getQueryContainer()->queryCategoryById($idCategory)->count() > 0;
-        
+
         if (!$categoryExists) {
             $this->addErrorMessage(sprintf('The category you are trying to edit %s does not exist.', $idCategory));
+
             return new RedirectResponse('/category');
         }
 
@@ -41,7 +41,7 @@ class EditController extends AddController
         ;
 
         /**
-         * @var Form $form
+         * @var Form
          */
         $form = $this->getDependencyContainer()
             ->createCategoryFormEdit($idCategory)
@@ -49,40 +49,39 @@ class EditController extends AddController
         $form->handleRequest();
 
         if ($form->isValid()) { //TODO Ugly and dirty, some stuff must be moved into Facades
-            $data = $form->getData();
+            $data             = $form->getData();
             $categoryTransfer = (new CategoryTransfer())->fromArray($data, true);
             $this->getFacade()->updateCategory($categoryTransfer, $locale);
             $parentIdList = $form->getData()['fk_parent_category_node'];
-            
+
             foreach ($parentIdList as $parentNodeId) {
                 $data['fk_parent_category_node'] = $parentNodeId;
-                $data['fk_category'] = $categoryTransfer->getIdCategory();
-                
+                $data['fk_category']             = $categoryTransfer->getIdCategory();
+
                 $categoryNodeTransfer = (new NodeTransfer())->fromArray($data, true);
                 $existingCategoryNode = $this->getFacade()->getNodeByIdCategoryAndParentNode($categoryTransfer->getIdCategory(), $parentNodeId);
-                
+
                 if ($existingCategoryNode) {
                     $categoryNodeTransfer->setIdCategoryNode($existingCategoryNode->getIdCategoryNode());
                     $this->getFacade()->moveCategoryNode($categoryNodeTransfer, $locale);
-                }
-                else {
+                } else {
                     $new_data = $data;
                     unset($new_data['id_category_node']);
                     $categoryNodeTransfer = (new NodeTransfer())->fromArray($new_data, true);
                     $this->getFacade()->createCategoryNode($categoryNodeTransfer, $locale);
                 }
             }
-            
+
             $existingParents = $this->getFacade()->getNodesByIdCategory($categoryTransfer->getIdCategory());
-            $parentIdList = array_flip($parentIdList);
-            
+            $parentIdList    = array_flip($parentIdList);
+
             //remove deselected parents
             foreach ($existingParents as $parent) {
                 if (!array_key_exists($parent->getFkParentCategoryNode(), $parentIdList)) {
                     $this->getFacade()->deleteNode($parent->getIdCategoryNode(), $locale);
                 }
             }
-            
+
             if (trim($data['products_to_be_de_assigned']) !== '') {
                 //de assign products
                 $this->getDependencyContainer()
@@ -98,7 +97,7 @@ class EditController extends AddController
             }
 
             $this->addSuccessMessage('The category was saved successfully.');
-            
+
             return $this->redirectResponse('/category');
         }
 
@@ -109,13 +108,12 @@ class EditController extends AddController
         $products = $this->getDependencyContainer()
             ->createProductTable($locale, $idCategory)
         ;
-        
+
         return $this->viewResponse([
-            'idCategory' => $idCategory,
-            'form' => $form->createView(),
+            'idCategory'             => $idCategory,
+            'form'                   => $form->createView(),
             'productCategoriesTable' => $productCategories->render(),
-            'productsTable' => $products->render(),
+            'productsTable'          => $products->render(),
         ]);
     }
-
 }
