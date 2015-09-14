62d153a34f082cf675a324e746166740642026cb
Aurimas Lickus <aurimas.lickus@spryker.com>
2015-09-14 16:07:09 +0200

    Wishlist grouping not grouping properly

--- Bundles/ItemGrouperWishlistConnector/src/SprykerFeature/Zed/ItemGrouperWishlistConnector/Communication/Plugin/PreSaveItemGroupingPlugin.php
+++ Bundles/ItemGrouperWishlistConnector/src/SprykerFeature/Zed/ItemGrouperWishlistConnector/Communication/Plugin/PreSaveItemGroupingPlugin.php
@@ -8,7 +8,6 @@ namespace SprykerFeature\Zed\ItemGrouperWishlistConnector\Communication\Plugin;
 
 use Generated\Shared\Transfer\GroupableContainerTransfer;
 use Generated\Shared\Wishlist\ItemInterface;
-use Generated\Shared\Wishlist\WishlistChangeInterface;
 use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
 use SprykerFeature\Zed\ItemGrouperWishlistConnector\Business\ItemGrouperWishlistConnectorFacade;
 use SprykerFeature\Zed\Wishlist\Dependency\PreSavePluginInterface;
@@ -18,6 +17,7 @@ use SprykerFeature\Zed\Wishlist\Dependency\PreSavePluginInterface;
  */
 class PreSaveItemGroupingPlugin extends AbstractPlugin implements PreSavePluginInterface
 {
+
     /**
      * @param ItemInterface[] $items
      */
@@ -26,6 +26,10 @@ class PreSaveItemGroupingPlugin extends AbstractPlugin implements PreSavePluginI
         $groupAbleItems = new GroupableContainerTransfer();
         $groupAbleItems->setItems($items);
         $groupedItems = $this->getFacade()->groupOrderItems($groupAbleItems);
-        $items->exchangeArray($groupedItems->getItems());
+
+        if (count($groupedItems->getItems()) > 0) {
+            $items->exchangeArray((array) $groupedItems->getItems());
+        }
     }
+
 }
