084f61f4705b4fa2fea951e8fce0dfa27647281b
René Klatt <rene.klatt@spryker.com>
2015-09-17 10:54:43 +0000

    update scripts

--- ci_build.sh
+++ ci_build.sh
@@ -5,5 +5,5 @@ trigger_build_url=https://circleci.com/api/v1/project/spryker/demoshop/tree/deve
 curl \
   -H "Accept: application/json" \
   -H "Content-Type: application/json" \
-  -d '{"build_parameters":{"RUN_NIGHTLY_BUILD": "true","SPRYKER_BRANCH": "${CIRCLE_BRANCH}"}}' \
+  -d '{"build_parameters":{"RUN_NIGHTLY_BUILD": "true","SPRYKER_BRANCH": "$1"}}' \
   -X POST ${trigger_build_url}
