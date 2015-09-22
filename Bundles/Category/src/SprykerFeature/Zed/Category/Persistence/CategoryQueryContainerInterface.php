fcd5fc7fada129e580428547d3a3e102e9f09e45
mark <mark.scherer@spryker.com>
2015-09-22 16:23:56 +0200

    More corrections.

--- Bundles/Category/src/SprykerFeature/Zed/Category/Persistence/CategoryQueryContainerInterface.php
+++ Bundles/Category/src/SprykerFeature/Zed/Category/Persistence/CategoryQueryContainerInterface.php
@@ -44,7 +44,7 @@ interface CategoryQueryContainerInterface
      * @param $idCategory
      * @param $idParentNode
      *
-     * @return self|ModelCriteria
+     * @return SpyCategoryNodeQuery
      */
     public function queryNodeByIdCategoryAndParentNode($idCategory, $idParentNode);
 
