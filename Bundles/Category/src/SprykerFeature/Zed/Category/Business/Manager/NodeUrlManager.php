f34841763f8d0595ace377d4dd05c92dab184519
Oliwier Ptak <oliwier.ptak@spryker.com>
2015-09-23 18:57:05 +0200

    KSP-487 prototyping

--- Bundles/Category/src/SprykerFeature/Zed/Category/Business/Manager/NodeUrlManager.php
+++ Bundles/Category/src/SprykerFeature/Zed/Category/Business/Manager/NodeUrlManager.php
@@ -13,6 +13,11 @@ use SprykerFeature\Shared\Category\CategoryConfig;
 use SprykerFeature\Zed\Category\Business\Generator\UrlPathGeneratorInterface;
 use SprykerFeature\Zed\Category\Business\Tree\CategoryTreeReaderInterface;
 use SprykerFeature\Zed\Category\Dependency\Facade\CategoryToUrlInterface;
+use SprykerFeature\Zed\Category\Persistence\Propel\Map\SpyCategoryAttributeTableMap;
+use SprykerFeature\Zed\Category\Persistence\Propel\Map\SpyCategoryClosureTableTableMap;
+use SprykerFeature\Zed\Category\Persistence\Propel\Map\SpyCategoryNodeTableMap;
+use SprykerFeature\Zed\Category\Persistence\Propel\SpyCategoryClosureTableQuery;
+use SprykerFeature\Zed\Category\Persistence\Propel\SpyCategoryNodeQuery;
 
 class NodeUrlManager implements NodeUrlManagerInterface
 {
@@ -61,12 +66,12 @@ class NodeUrlManager implements NodeUrlManagerInterface
         $this->urlFacade->touchUrlActive($url->getIdUrl());
     }
 
-
     /**
-     * @param NodeTransfer   $categoryNode
+     * @param NodeTransfer $categoryNode
      * @param LocaleTransfer $locale
+     * @param NodeTransfer|null $parentNode
      */
-    public function updateUrl(NodeTransfer $categoryNode, LocaleTransfer $locale)
+    public function updateUrl(NodeTransfer $categoryNode, LocaleTransfer $locale, NodeTransfer $parentNode=null)
     {
         $path = $this->categoryTreeReader->getPath($categoryNode->getIdCategoryNode(), $locale);
         $categoryUrl = $this->urlPathGenerator->generate($path);
@@ -90,6 +95,59 @@ class NodeUrlManager implements NodeUrlManagerInterface
         }
 
         //TODO implement deep fix //https://spryker.atlassian.net/browse/CD-459
+        //$nodes = $this->categoryTreeReader->getChildren($idNode, $locale);
+
+
+        $query = new SpyCategoryClosureTableQuery();
+            $query->filterByFkCategoryNode($categoryNode->getIdCategoryNode())
+            //->withColumn(SpyCategoryNodeTableMap::COL_ID_CATEGORY_NODE, 'id_category_node')
+            //->withColumn(SpyCategoryClosureTableTableMap::COL_ID_CATEGORY_CLOSURE_TABLE, 'id_category_closure_table')
+            //->withColumn(SpyCategoryClosureTableTableMap::COL_DEPTH, 'depth')
+            ->where(SpyCategoryClosureTableTableMap::COL_DEPTH . '> ?', 0)
+        ;
+
+        $nodes = $query->find();
+        foreach ($nodes as $child) {
+            //$childTransfer = (new nodeTransfer())->fromArray($child->toArray());
+
+            $query = new SpyCategoryClosureTableQuery();
+            $query->filterByFkCategoryNodeDescendant($child->getFkCategoryNodeDescendant())
+                ->innerJoinNode()
+                ->useNodeQuery()
+                  ->innerJoinCategory()
+                  ->useCategoryQuery()
+                    ->innerJoinAttribute()
+                  ->endUse()
+                ->endUse()
+                //->where(SpyCategoryClosureTableTableMap::COL_DEPTH . ' > ?', 0)
+                ->where(SpyCategoryNodeTableMap::COL_IS_ROOT . ' = false')
+                ->withColumn(SpyCategoryAttributeTableMap::COL_NAME, 'name')
+                ->withColumn(SpyCategoryAttributeTableMap::COL_URL_KEY, 'url_key')
+                ->orderBy(SpyCategoryClosureTableTableMap::COL_DEPTH, 'DESC')
+            ;
+
+            $parents = $query->find();
+            $nodes = [];
+            foreach ($parents as $parent) {
+                $nodes[] = $parent->toArray();
+            }
+
+            //$nodes[] = $categoryNode->toArray();
+            dump($nodes);
+
+            $categoryUrl = $this->urlPathGenerator->generate($nodes);
+            dump($categoryUrl);
+
+            //$childPath = array_merge($path, $childPath);
+/*            $categoryUrl = $this->urlPathGenerator->generate($childPath);
+            $url->setUrl($categoryUrl);
+            $this->urlFacade->saveUrlAndTouch($url);*/
+            //$childTransfer = (new NodeTransfer())->fromArray($child->toArray());
+            //$this->updateUrl($childTransfer, $locale);
+
+        }
+
+        die();
     }
 
     /**
