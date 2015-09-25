c21f22c34305c29b7c7324b5a208f080ffe83fe3
René Klatt <rene.klatt@spryker.com>
2015-09-25 07:38:42 +0000

    CD-476 add check if connection already setup

--- Bundles/Propel/src/SprykerEngine/Zed/Propel/Communication/Plugin/ServiceProvider/PropelServiceProvider.php
+++ Bundles/Propel/src/SprykerEngine/Zed/Propel/Communication/Plugin/ServiceProvider/PropelServiceProvider.php
@@ -16,7 +16,6 @@ use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
 use SprykerEngine\Zed\Kernel\Communication\Factory;
 use SprykerEngine\Zed\Kernel\Locator;
 use SprykerEngine\Zed\Propel\Communication\PropelDependencyContainer;
-use SprykerEngine\Zed\Propel\PropelConfig;
 use SprykerFeature\Shared\System\SystemConfig;
 
 /**
@@ -27,6 +26,11 @@ class PropelServiceProvider extends AbstractPlugin implements ServiceProviderInt
 
     const BUNDLE = 'Propel';
 
+    /**
+     * ServiceProvider is created with new in many places e.g. tests
+     * To setup the Plugin, we have to have a factory and the locator.
+     * To prevent doing this in all places, we add them here.
+     */
     public function __construct()
     {
         parent::__construct(new Factory(self::BUNDLE), Locator::getInstance());
@@ -48,18 +52,44 @@ class PropelServiceProvider extends AbstractPlugin implements ServiceProviderInt
         $manager->setConfiguration($this->getConfig());
         $manager->setName('zed');
 
-        /** @var StandardServiceContainer $serviceContainer */
-        $serviceContainer = Propel::getServiceContainer();
+        $serviceContainer = $this->getServiceContainer();
         $serviceContainer->setAdapterClass('zed', Config::get(SystemConfig::ZED_DB_ENGINE));
         $serviceContainer->setConnectionManager('zed', $manager);
         $serviceContainer->setDefaultDatasource('zed');
 
         $this->addLogger($serviceContainer);
 
-//        if (Config::get(SystemConfig::PROPEL_DEBUG)) {
-//            $connection = Propel::getConnection();
-//            $connection->useDebug(true);
-//        }
+        if (Config::get(SystemConfig::PROPEL_DEBUG) && $this->hasConnection()) {
+            $connection = Propel::getConnection();
+            $connection->useDebug(true);
+        }
+    }
+
+    /**
+     * @return StandardServiceContainer
+     */
+    protected function getServiceContainer()
+    {
+        $serviceContainer = Propel::getServiceContainer();
+
+        return $serviceContainer;
+    }
+
+    /**
+     * Allowed try/catch. If we have no database setup, getConnection throws an Exception
+     * ServiceProvider is called more then once and after setup of database we can enable debug
+     *
+     * @return bool
+     */
+    private function hasConnection()
+    {
+        try {
+            $connection = Propel::getConnection();
+
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 
     /**
@@ -74,12 +104,12 @@ class PropelServiceProvider extends AbstractPlugin implements ServiceProviderInt
         $propelConfig['password'] = Config::get(SystemConfig::ZED_DB_PASSWORD);
         $propelConfig['dsn'] = Config::get(SystemConfig::PROPEL)['database']['connections']['default']['dsn'];
 
-
         return $propelConfig;
     }
 
     /**
      * @param StandardServiceContainer $serviceContainer
+     *
      * @throws \ErrorException
      */
     private function addLogger(StandardServiceContainer $serviceContainer)
@@ -90,4 +120,5 @@ class PropelServiceProvider extends AbstractPlugin implements ServiceProviderInt
             $serviceContainer->setLogger($logger->getName(), $logger);
         }
     }
+
 }
