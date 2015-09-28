027c0e5b5820ff2555391698edc665cc0898b49f
Marius Bogdan <marius.rizac@spryker.com>
2015-09-28 11:52:53 +0200

    KSP-999 moved get plugins method to abstract class

--- Bundles/Discount/src/SprykerFeature/Zed/Discount/Communication/Form/AbstractRuleType.php
+++ Bundles/Discount/src/SprykerFeature/Zed/Discount/Communication/Form/AbstractRuleType.php
@@ -11,6 +11,33 @@ abstract class AbstractRuleType extends AbstractType
     const DECISION_COLLECTOR_PREFIX = 'PLUGIN_COLLECTOR_';
 
     /**
+     * @var array
+     */
+    protected $availableCalculatorPlugins;
+
+    /**
+     * @var array
+     */
+    protected $availableCollectorPlugins;
+
+    /**
+     * @var array
+     */
+    protected $availableDecisionRulePlugins;
+
+    /**
+     * @param array $availableCalculatorPlugins
+     * @param array $availableCollectorPlugins
+     * @param array $availableDecisionRulePlugins
+     */
+    public function __construct(array $availableCalculatorPlugins, array $availableCollectorPlugins, array $availableDecisionRulePlugins)
+    {
+        $this->availableCalculatorPlugins = $availableCalculatorPlugins;
+        $this->availableCollectorPlugins = $availableCollectorPlugins;
+        $this->availableDecisionRulePlugins = $availableDecisionRulePlugins;
+    }
+
+    /**
      * @param string $decisionRuleName
      *
      * @return string
@@ -24,5 +51,35 @@ abstract class AbstractRuleType extends AbstractType
         );
 
         return mb_convert_case($decisionRuleName, MB_CASE_TITLE, "UTF-8");
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAvailableCalculatorPlugins()
+    {
+        $plugins = [];
+        $availablePlugins = array_keys($this->availableCalculatorPlugins);
+
+        foreach ($availablePlugins as $plugin) {
+            $plugins[$plugin] = $this->filterChoicesLabels($plugin);
+        }
+
+        return $plugins;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAvailableCollectorPlugins()
+    {
+        $plugins = [];
+        $availablePlugins = array_keys($this->availableCollectorPlugins);
+
+        foreach ($availablePlugins as $plugin) {
+            $plugins[$plugin] = $this->filterChoicesLabels($plugin);
+        }
+
+        return $plugins;
     }
 }
