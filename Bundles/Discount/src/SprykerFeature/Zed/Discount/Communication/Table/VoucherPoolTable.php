1bd62667503463c6125a679ae8bfd922f34bff45
Marius Bogdan <marius.rizac@spryker.com>
2015-09-30 16:34:55 +0200

    KSP-995 added buttons in voucher list

--- Bundles/Discount/src/SprykerFeature/Zed/Discount/Communication/Table/VoucherPoolTable.php
+++ Bundles/Discount/src/SprykerFeature/Zed/Discount/Communication/Table/VoucherPoolTable.php
@@ -15,9 +15,13 @@ class VoucherPoolTable extends AbstractTable
 {
     const COL_OPTIONS = 'options';
     const COL_CATEGORY_NAME = 'category_name';
+    const COL_VOUCHERS_COUNT = 'Vouchers';
+
     const URL_DISCOUNT_POOL_EDIT = '/discount/pool/edit?%s=%d';
     const PARAM_ID_POOL = 'id-pool';
     const CONTROLLER_TABLE_ACTION = 'poolTable';
+
+    const SPACE_SEPARATOR = ' ';
 
 
     /**
@@ -46,6 +50,7 @@ class VoucherPoolTable extends AbstractTable
             SpyDiscountVoucherPoolTableMap::COL_CREATED_AT => 'Date Created',
             SpyDiscountVoucherPoolTableMap::COL_NAME => 'Pool Name',
             self::COL_CATEGORY_NAME => 'Category Name',
+            self::COL_VOUCHERS_COUNT => 'Vouchers',
             self::COL_OPTIONS => 'Options',
         ]);
 
@@ -65,6 +70,9 @@ class VoucherPoolTable extends AbstractTable
             ->withColumn(SpyDiscountVoucherPoolCategoryTableMap::COL_NAME, 'category_name')
             ->useVoucherPoolCategoryQuery()
             ->endUse()
+            ->useDiscountVoucherQuery()
+                ->withColumn('COUNT(' . SpyDiscountVoucherPoolTableMap::COL_ID_DISCOUNT_VOUCHER_POOL . ')', self::COL_VOUCHERS_COUNT)
+            ->endUse()
             ->groupByIdDiscountVoucherPool()
         ;
 
@@ -72,15 +80,13 @@ class VoucherPoolTable extends AbstractTable
         $queryResults = $this->runQuery($query, $config);
 
         foreach ($queryResults as $item) {
-            $editUrl = $this->getEditUrl($item);
+
             $results[] = [
                 SpyDiscountVoucherPoolTableMap::COL_CREATED_AT => $item[SpyDiscountVoucherPoolTableMap::COL_CREATED_AT],
                 SpyDiscountVoucherPoolTableMap::COL_NAME => $item[SpyDiscountVoucherPoolTableMap::COL_NAME],
                 self::COL_CATEGORY_NAME => $item[self::COL_CATEGORY_NAME],
-                self::COL_OPTIONS => sprintf(
-                    '<a href="%s" class="btn btn-sm btn-primary">Edit</a>',
-                    $editUrl
-                ),
+                self::COL_VOUCHERS_COUNT => $item[self::COL_VOUCHERS_COUNT],
+                self::COL_OPTIONS => $this->createRowOptions($item),
             ];
         }
 
@@ -94,13 +100,30 @@ class VoucherPoolTable extends AbstractTable
      */
     private function getEditUrl(array $item)
     {
-        $editUrl = sprintf(
+        return sprintf(
             self::URL_DISCOUNT_POOL_EDIT,
             self::PARAM_ID_POOL,
             $item[SpyDiscountVoucherPoolTableMap::COL_ID_DISCOUNT_VOUCHER_POOL]
         );
+    }
 
-        return $editUrl;
+    /**
+     * @param array $item
+     *
+     * @return string
+     */
+    protected function createRowOptions(array $item)
+    {
+        $editUrl = $this->getEditUrl($item);
+
+        return $this->generateEditButton($editUrl, 'Edit Voucher')
+            . self::SPACE_SEPARATOR
+            . $this->generateViewButton('/discount/voucher/view/?' . self::PARAM_ID_POOL . '=' . $item[SpyDiscountVoucherPoolTableMap::COL_ID_DISCOUNT_VOUCHER_POOL], 'View Codes')
+            . self::SPACE_SEPARATOR
+            . $this->generateCreateButton('/discount/voucher/create-single', 'Add Single Voucher')
+            . self::SPACE_SEPARATOR
+            . $this->generateCreateButton('/discount/voucher/create-multiple', 'Add Multiple Vouchers')
+        ;
     }
 
 }
