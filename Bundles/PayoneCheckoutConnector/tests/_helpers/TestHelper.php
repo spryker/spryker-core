e5a57d73404bf1d25ace1ac57eee47a7e7149af7
René Klatt <rene.klatt@spryker.com>
2015-10-09 06:00:23 +0000

    Removed YvesUnit.suite.yml

--- Bundles/PayoneCheckoutConnector/tests/_helpers/TestHelper.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace PayoneCheckoutConnector\Codeception\Module;
-
-// here you can define custom functions for TestGuy 
-
-class TestHelper extends \Codeception\Module
-{
-}
--- Bundles/PayoneCheckoutConnector/tests/_helpers/WebHelper.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace PayoneCheckoutConnector\Codeception\Module;
-
-// here you can define custom functions for WebGuy 
-
-class WebHelper extends \Codeception\Module
-{
-}
