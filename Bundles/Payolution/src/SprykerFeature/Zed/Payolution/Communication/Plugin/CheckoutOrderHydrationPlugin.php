d2b3498738a9dceb47f87b71c50ee9dbeff6c033
Hussam Hebbo <hussam.hebbo@spryker.com>
2015-10-13 09:57:05 +0000

    CD-408 removed PayolutionCheckoutConnector and added the plugins to payolution as there is no need for a connector between checkout and payolution

--- Bundles/Payolution/src/SprykerFeature/Zed/Payolution/Communication/Plugin/CheckoutOrderHydrationPlugin.php
+++ Bundles/Payolution/src/SprykerFeature/Zed/Payolution/Communication/Plugin/CheckoutOrderHydrationPlugin.php
@@ -4,7 +4,7 @@
  * (c) Spryker Systems GmbH copyright protected
  */
 
-namespace SprykerFeature\Zed\PayolutionCheckoutConnector\Communication\Plugin;
+namespace SprykerFeature\Zed\Payolution\Communication\Plugin;
 
 use Generated\Shared\Transfer\CheckoutRequestTransfer;
 use Generated\Shared\Transfer\OrderTransfer;
--- Bundles/PayolutionCheckoutConnector/tests/_helpers/CodeHelper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PayolutionCheckoutConnector\Codeception\Module;
-
-use Codeception\Module;
-
-class CodeHelper extends Module
-{
-}
