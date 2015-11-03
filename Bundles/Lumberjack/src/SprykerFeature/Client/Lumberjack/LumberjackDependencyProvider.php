bfa180f9398766a846a62a0087218327ced80876
Stefan Langwald <stefanlangwald+github.com@gmail.com>
2015-11-03 17:50:29 +0100

    Added Lumberjack Facade fixes for Zed and make it compatible with other Client usage patterns

---
+++ Bundles/Lumberjack/src/SprykerFeature/Client/Lumberjack/LumberjackDependencyProvider.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * (c) Copyright Spryker Systems GmbH 2015
+ */
+
+namespace SprykerFeature\Client\Lumberjack;
+
+use SprykerEngine\Client\Kernel\AbstractDependencyProvider;
+
+class LumberjackDependencyProvider extends AbstractDependencyProvider
+{
+
+}
---
+++ Bundles/Lumberjack/src/SprykerFeature/Client/Lumberjack/Service/LumberjackDependencyContainer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Client\Lumberjack\Service;
+
+use Generated\Client\Ide\FactoryAutoCompletion\LumberjackService;
+use SprykerEngine\Client\Kernel\Service\AbstractServiceDependencyContainer;
+
+/**
+ * @method LumberjackService getFactory()
+ */
+class LumberjackDependencyContainer extends AbstractServiceDependencyContainer
+{
+
+    /**
+     * @return EventJournalClientInterface
+     */
+    public function createEventJournalClient() {
+        return $this->getFactory()->createEventJournalClient();
+    }
+
+}
