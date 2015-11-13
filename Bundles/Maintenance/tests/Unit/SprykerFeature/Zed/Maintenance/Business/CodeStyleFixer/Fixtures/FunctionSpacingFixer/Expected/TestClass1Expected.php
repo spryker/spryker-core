fca26467acb441d7e94a6182bd74c3f59a8cb319
mscherer <mark.scherer@spryker.com>
2015-11-13 10:28:28 +0100

    Add FunctionSpacingFixer

---
+++ Bundles/Maintenance/tests/Unit/SprykerFeature/Zed/Maintenance/Business/CodeStyleFixer/Fixtures/FunctionSpacingFixer/Expected/TestClass1Expected.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Unit\SprykerFeature\Zed\Maintenance\Business\CodeStyleFixer\Fixtures\FunctionSpacingFixer\Input;
+
+class TestClass1Input
+{
+    public function aFunction()
+    {
+        $x = function ($y) use ($z) {
+        };
+    }
+
+    public function bFunction()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function cFunction()
+    {
+    }
+
+}
---
+++ Bundles/Maintenance/tests/Unit/SprykerFeature/Zed/Maintenance/Business/CodeStyleFixer/FunctionSpacingFixerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Unit\SprykerFeature\Zed\Maintenance\Business\CodeStyleFixer;
+
+use SprykerFeature\Zed\Maintenance\Business\CodeStyleFixer\FunctionSpacingFixer;
+
+/**
+ * @group SprykerFeature
+ * @group Zed
+ * @group Maintenance
+ * @group CodeStyleFixer
+ */
+class FunctionSpacingFixerTest extends \PHPUnit_Framework_TestCase
+{
+
+    const FIXER_NAME = 'FunctionSpacingFixer';
+
+    /**
+     * @var FunctionSpacingFixer
+     */
+    protected $fixer;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->fixer = new FunctionSpacingFixer();
+    }
+
+    /**
+     * @dataProvider provideFixCases
+     *
+     * @param string $expected
+     * @param string $input
+     */
+    public function testFix($expected, $input = null)
+    {
+        $fileInfo = new \SplFileInfo(__FILE__);
+        $this->assertSame($expected, $this->fixer->fix($fileInfo, $input));
+    }
+
+    public function provideFixCases()
+    {
+        $fixturePath = __DIR__ . '/Fixtures/' . self::FIXER_NAME . '/';
+
+        return [
+            [
+                file_get_contents($fixturePath . 'Expected/TestClass1Expected.php'),
+                file_get_contents($fixturePath . 'Input/TestClass1Input.php'),
+            ],
+            [
+                file_get_contents($fixturePath . 'Expected/TestClass2Expected.php'),
+                file_get_contents($fixturePath . 'Input/TestClass2Input.php'),
+            ],
+        ];
+    }
+
+}
