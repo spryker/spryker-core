1ba223398a750defec5188d2189a3059137456ec
Lucas Manzke <lucas.manzke@spryker.com>
2015-11-13 17:59:02 +0100

    add test

---
+++ Bundles/Storage/tests/Unit/SprykerFeature/Client/Storage/Service/Redis/ServiceTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace Unit\SprykerFeature\Client\Storage\Service\Redis;
+
+use Codeception\TestCase\Test;
+use Predis\ClientInterface;
+use SprykerFeature\Client\Storage\Service\Redis\Service;
+
+/**
+ * @group Storage
+ * @group Client
+ * @group ServiceTest
+ */
+class ServiceTest extends Test
+{
+
+    /**
+     * @var Service
+     */
+    protected $redisService;
+
+    /**
+     * @var ClientInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $clientMock;
+
+    protected function _before()
+    {
+        $this->clientMock = $this->getMock(
+            ClientInterface::class,
+            [
+                'keys',
+
+                'getProfile',
+                'getOptions',
+                'connect',
+                'disconnect',
+                'createCommand',
+                'executeCommand',
+                'getConnection',
+                '__call',
+            ]
+        );
+
+        $this->redisService = new Service(
+            $this->clientMock
+        );
+    }
+
+    public function testGetAllKeysTriggersRightCommand()
+    {
+        $this->clientMock->expects($this->once())->method('keys')->with($this->equalTo('kv:*'));
+
+        $this->redisService->getAllKeys();
+    }
+
+    public function testGetKeysPassesPatternCorrectly()
+    {
+        $this->clientMock->expects($this->once())->method('keys')->with($this->equalTo('kv:aPattern*'));
+
+        $this->redisService->getKeys('aPattern*');
+    }
+
+}
