cd2dca851bf05e3b2af7f6fa378b6afdc7dc0fd1
Marius Bogdan <marius.rizac@spryker.com>
2015-11-27 11:56:25 +0100

    added NullFormTransfer

---
+++ Bundles/Gui/src/SprykerEngine/Zed/Gui/Communication/Form/NullFormTransfer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bundles\Gui\src\SprykerEngine\Zed\Gui\Communication\Form;
+
+use SprykerEngine\Shared\Transfer\AbstractTransfer;
+
+class NullFormTransfer extends AbstractTransfer
+{
+
+}
