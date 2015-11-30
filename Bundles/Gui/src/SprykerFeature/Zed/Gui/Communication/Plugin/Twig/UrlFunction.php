9bec787a64cb893ddbba229dec7058b54bd75646
mscherer <mark.scherer@spryker.com>
2015-11-30 15:35:28 +0100

    Enable renamed function

--- Bundles/Gui/src/SprykerFeature/Zed/Gui/Communication/Plugin/Twig/UrlFunction.php
+++ Bundles/Gui/src/SprykerFeature/Zed/Gui/Communication/Plugin/Twig/UrlFunction.php
@@ -7,7 +7,6 @@
 namespace SprykerFeature\Zed\Gui\Communication\Plugin\Twig;
 
 use SprykerFeature\Zed\Application\Business\Url\Url;
-use SprykerFeature\Zed\Library\Sanitize\Html;
 use SprykerFeature\Zed\Library\Twig\TwigFunction;
 
 class UrlFunction extends TwigFunction
@@ -28,7 +27,7 @@ class UrlFunction extends TwigFunction
     {
         return function ($url, array $query = [], array $options = []) {
             $url = Url::generate($url, $query, $options);
-            $html = Html::escape($url);
+            $html = $url->buildEscaped();
 
             return $html;
         };
