015b1e6ff23a194715a1561c768c34f1f505a82e
mscherer <mark.scherer@spryker.com>
2015-11-30 10:50:17 +0100

    Add NoWhitespaceBeforeSemicolonFixer

---
+++ Bundles/Development/src/SprykerFeature/Zed/Development/Business/CodeStyleFixer/NoWhitespaceBeforeSemicolonFixer.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Zed\Development\Business\CodeStyleFixer;
+
+use Symfony\CS\AbstractFixer;
+use Symfony\CS\FixerInterface;
+use Symfony\CS\Tokenizer\Token;
+use Symfony\CS\Tokenizer\Tokens;
+
+class NoWhitespaceBeforeSemicolonFixer extends AbstractFixer
+{
+
+    /**
+     * @param \SplFileInfo $file
+     * @param string $content
+     *
+     * @return string
+     */
+    public function fix(\SplFileInfo $file, $content)
+    {
+        $tokens = Tokens::fromCode($content);
+
+        $this->fixWhitespace($tokens);
+
+        return $tokens->generateCode();
+    }
+
+    /**
+     * @param Tokens|Token[] $tokens
+     *
+     * @return void
+     */
+    protected function fixWhitespace(Tokens $tokens)
+    {
+        foreach ($tokens as $index => $token) {
+            if (!$token->equals(';')) {
+                continue;
+            }
+
+            $prevIndex = $tokens->getPrevMeaningfulToken($index);
+
+            if ($prevIndex === $index - 1) {
+                continue;
+            }
+
+            $whitespace = $tokens[$index - 1];
+            $whitespace->clear();
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return -100;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLevel()
+    {
+        return FixerInterface::NONE_LEVEL;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return 'Fix whitespace before semicolon.';
+    }
+
+}
---
+++ Bundles/Development/tests/Unit/SprykerFeature/Zed/Development/Business/CodeStyleFixer/Fixtures/NoWhitespaceBeforeSemicolonFixer/Expected/TestClass1Expected.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Unit\SprykerFeature\Zed\Development\Business\CodeStyleFixer\Fixtures\NoWhitespaceBeforeSemicolonFixer\Input;
+
+class TestClass1Input
+{
+
+    public function replaceFunction()
+    {
+        $x = $y + $z;
+    }
+
+    public function replaceFunctionB()
+    {
+        $x = $this->foo
+            ->bar;
+    }
+
+    public function replaceFunctionC()
+    {
+        return $foo;
+    }
+
+    public function doNotReplaceFunction()
+    {
+        return;
+    }
+
+}
