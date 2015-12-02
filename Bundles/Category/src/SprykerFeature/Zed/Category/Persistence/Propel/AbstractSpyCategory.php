cf39cb83cc2c0f3fc6bed50e28ad788597c89d19
Oliwier Ptak <oliwier.ptak@spryker.com>
2015-12-02 13:58:02 +0100

    SP-176 moved getLocalisedAttributes() into core

--- Bundles/Category/src/SprykerFeature/Zed/Category/Persistence/Propel/AbstractSpyCategory.php
+++ Bundles/Category/src/SprykerFeature/Zed/Category/Persistence/Propel/AbstractSpyCategory.php
@@ -6,6 +6,10 @@
 namespace SprykerFeature\Zed\Category\Persistence\Propel;
 
 use Orm\Zed\Category\Persistence\Base\SpyCategory as BaseSpyCategory;
+use Orm\Zed\Category\Persistence\Map\SpyCategoryAttributeTableMap;
+use Orm\Zed\Category\Persistence\SpyCategoryAttribute;
+use Propel\Runtime\ActiveQuery\Criteria;
+use Propel\Runtime\Collection\ObjectCollection;
 
 /**
  * Skeleton subclass for representing a row from the 'spy_category' table.
@@ -18,4 +22,22 @@ use Orm\Zed\Category\Persistence\Base\SpyCategory as BaseSpyCategory;
  */
 abstract class AbstractSpyCategory extends BaseSpyCategory
 {
+
+    /**
+     * @param int $idLocale
+     *
+     * @return SpyCategoryAttribute[]|ObjectCollection
+     */
+    public function getLocalisedAttributes($idLocale)
+    {
+        $criteria = new Criteria();
+        $criteria->addAnd(
+            SpyCategoryAttributeTableMap::COL_FK_LOCALE,
+            $idLocale,
+            Criteria::EQUAL
+        );
+
+        return $this->getAttributes($criteria);
+    }
+
 }
