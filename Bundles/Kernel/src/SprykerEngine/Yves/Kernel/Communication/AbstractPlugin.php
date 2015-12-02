d64a8b863d3ca1eccaeb196d925082cdcd8e5d6c
Tamas Nyulas <tamas.nyulas@spryker.com>
2015-12-02 16:51:58 +0100

    spryker/spryker#687 Implement getClient() for yves AbstractPlugin

--- Bundles/Kernel/src/SprykerEngine/Yves/Kernel/Communication/AbstractPlugin.php
+++ Bundles/Kernel/src/SprykerEngine/Yves/Kernel/Communication/AbstractPlugin.php
@@ -6,6 +6,7 @@
 
 namespace SprykerEngine\Yves\Kernel\Communication;
 
+use SprykerEngine\Client\Kernel\Service\AbstractClient;
 use SprykerEngine\Shared\Kernel\LocatorLocatorInterface;
 use SprykerEngine\Yves\Kernel\DependencyContainer\DependencyContainerInterface;
 
@@ -20,6 +21,11 @@ abstract class AbstractPlugin
     private $dependencyContainer;
 
     /**
+     * @var AbstractClient
+     */
+    private $client;
+
+    /**
      * @param Factory $factory
      * @param LocatorLocatorInterface $locator
      */
@@ -28,6 +34,19 @@ abstract class AbstractPlugin
         if ($factory->exists(self::DEPENDENCY_CONTAINER)) {
             $this->dependencyContainer = $factory->create(self::DEPENDENCY_CONTAINER, $factory, $locator);
         }
+
+        $this->setClient($factory);
+    }
+
+    /**
+     * @param Factory $factory
+     *
+     * @return void
+     */
+    private function setClient(Factory $factory)
+    {
+        $bundleName = lcfirst($factory->getBundle());
+        $this->client = $factory->$bundleName()->client();
     }
 
     /**
@@ -36,6 +55,14 @@ abstract class AbstractPlugin
     protected function getDependencyContainer()
     {
         return $this->dependencyContainer;
+    }
+
+    /**
+     * @return AbstractClient
+     */
+    protected function getClient()
+    {
+        return $this->client;
     }
 
 }
