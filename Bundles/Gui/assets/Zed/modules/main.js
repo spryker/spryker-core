f937f25bcf88ad9ad9f50c249dc33ac6d17f7606
Marius Bogdan <marius.rizac@spryker.com>
2015-12-14 16:18:07 +0100

    1002 added new Gui bundle assets structure

---
+++ Bundles/Gui/assets/Zed/modules/main.js
@@ -0,0 +1,18 @@
+require('bootstrap-sass');
+require('jquery-slimscroll/jquery.slimscroll');
+require('metismenu');
+require('pace');
+
+require('inspinia');
+require('inspinia/img/profile_small.jpg');
+require('inspinia/img/dashbard4_1.jpg');
+require('inspinia/img/dashbard4_2.jpg');
+require('inspinia/img/full_height.jpg');
+require('inspinia/img/off_canvas.jpg');
+
+require('../styles/main.scss');
+var test = require('./test');
+
+test.run();
+
+
