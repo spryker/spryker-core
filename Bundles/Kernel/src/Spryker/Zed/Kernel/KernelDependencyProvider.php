b654c2400d6c25d3990d45f0e920dfa8b7eddd92
Tamas Nyulas <tamas.nyulas@spryker.com>
2015-12-15 10:21:04 +0100

    #965 Fix wrong folder structure

--- Bundles/Kernel/src/Spryker/Zed/Kernel/KernelDependencyProvider.php
+++ Bundles/Kernel/src/Spryker/Zed/Kernel/KernelDependencyProvider.php
@@ -8,4 +8,23 @@ namespace Spryker\Zed\Kernel;
 
 class KernelDependencyProvider extends AbstractBundleDependencyProvider
 {
+
+    const FACADE_MESSENGER = 'messenger facade';
+
+    /**
+     * @param Container $container
+     *
+     * @return Container
+     */
+    public function provideCommunicationLayerDependencies(Container $container)
+    {
+        $container = parent::provideBusinessLayerDependencies($container);
+
+        $container[self::FACADE_MESSENGER] = function (Container $container) {
+            return $container->getLocator()->messenger()->facade();
+        };
+
+        return $container;
+    }
+
 }
