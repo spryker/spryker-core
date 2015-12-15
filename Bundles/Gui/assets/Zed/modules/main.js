c37034cf1ac2c78d81e55fcb707a17d52f051ec8
Alessandro Bellini <alessandro.bellini@spryker.com>
2015-12-15 16:30:19 +0100

    1002 gui bundle: code and assets cleaning

--- Bundles/Gui/assets/Zed/modules/main.js
+++ Bundles/Gui/assets/Zed/modules/main.js
@@ -1,8 +1,11 @@
+// base dependencies
+require('jquery');
 require('bootstrap-sass');
 require('jquery-slimscroll/jquery.slimscroll');
 require('metismenu');
 require('pace');
 
+// Inspinia
 require('../../Inspinia/inspinia');
 require('../../Inspinia/img/profile_small.jpg');
 require('../../Inspinia/img/dashbard4_1.jpg');
@@ -10,7 +13,5 @@ require('../../Inspinia/img/dashbard4_2.jpg');
 require('../../Inspinia/img/full_height.jpg');
 require('../../Inspinia/img/off_canvas.jpg');
 
+// Spryker customization
 require('../sass/main.scss');
-var test = require('./test');
-
-test.run();
--- Bundles/Gui/assets/Zed/modules/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    run: function() {
-        console.warn('this is the test module into ZED GUI bundle');
-
-        setTimeout(function(){
-        	alert('Test performed: everything is fine, buddy!');
-        }, 2000);
-    }
-}
