d0ca8455cea61f8d9aec7905f5177ad5a651b89e
René Klatt <rene.klatt@spryker.com>
2015-12-22 11:01:34 +0000

    #1026 code review changes

--- Bundles/Kernel/src/Spryker/Shared/Kernel/ClassResolver/Controller/AbstractControllerResolver.php
+++ Bundles/Kernel/src/Spryker/Shared/Kernel/ClassResolver/Controller/AbstractControllerResolver.php
@@ -51,4 +51,47 @@ abstract class AbstractControllerResolver extends AbstractClassResolver
         return false;
     }
 
+    /**
+     * @return string
+     */
+    public function getClassPattern()
+    {
+        return sprintf(
+            $this->getClassNamePattern(),
+            self::KEY_NAMESPACE,
+            self::KEY_BUNDLE,
+            self::KEY_STORE,
+            self::KEY_CONTROLLER
+        );
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract protected function getClassNamePattern();
+
+    /**
+     * @param string $namespace
+     * @param string|null $store
+     *
+     * @return string
+     */
+    protected function buildClassName($namespace, $store = null)
+    {
+        $searchAndReplace = [
+            self::KEY_NAMESPACE => $namespace,
+            self::KEY_BUNDLE => $this->bundleControllerAction->getBundle(),
+            self::KEY_STORE => $store,
+            self::KEY_CONTROLLER => $this->bundleControllerAction->getController(),
+        ];
+
+        $className = str_replace(
+            array_keys($searchAndReplace),
+            array_values($searchAndReplace),
+            $this->getClassPattern()
+        );
+
+        return $className;
+    }
+
 }
--- Bundles/Kernel/src/Spryker/Zed/Kernel/ClassResolver/Controller/ControllerResolver.php
+++ Bundles/Kernel/src/Spryker/Zed/Kernel/ClassResolver/Controller/ControllerResolver.php
@@ -16,39 +16,9 @@ class ControllerResolver extends AbstractControllerResolver
     /**
      * @return string
      */
-    public function getClassPattern()
+    protected function getClassNamePattern()
     {
-        return sprintf(
-            self::CLASS_NAME_PATTERN,
-            self::KEY_NAMESPACE,
-            self::KEY_BUNDLE,
-            self::KEY_STORE,
-            self::KEY_CONTROLLER
-        );
-    }
-
-    /**
-     * @param string $namespace
-     * @param string|null $store
-     *
-     * @return string
-     */
-    protected function buildClassName($namespace, $store = null)
-    {
-        $searchAndReplace = [
-            self::KEY_NAMESPACE => $namespace,
-            self::KEY_BUNDLE => $this->bundleControllerAction->getBundle(),
-            self::KEY_STORE => $store,
-            self::KEY_CONTROLLER => $this->bundleControllerAction->getController(),
-        ];
-
-        $className = str_replace(
-            array_keys($searchAndReplace),
-            array_values($searchAndReplace),
-            $this->getClassPattern()
-        );
-
-        return $className;
+        return self::CLASS_NAME_PATTERN;
     }
 
 }
