6b5db67ac50da80e20687c8f48861d268c536c06
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-01-18 16:41:26 +0100

    SP-176 removed obsolete condition and parameter

--- Bundles/ProductCategory/src/Spryker/Zed/ProductCategory/Communication/Table/ProductTable.php
+++ Bundles/ProductCategory/src/Spryker/Zed/ProductCategory/Communication/Table/ProductTable.php
@@ -79,7 +79,7 @@ class ProductTable extends AbstractTable
      */
     protected function prepareData(TableConfiguration $config)
     {
-        $query = $this->productCategoryQueryContainer->queryProductAbstractCollectionBySearchTerm(null, $this->locale, $this->idCategory);
+        $query = $this->productCategoryQueryContainer->queryAbstractProductsBySearchTerm(null, $this->locale);
         $query->setModelAlias('spy_product_abstract');
 
         $queryResults = $this->runQuery($query, $config);
