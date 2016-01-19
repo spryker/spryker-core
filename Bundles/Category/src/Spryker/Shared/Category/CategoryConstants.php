6645d77e1476c2d0df86f009dfc60547741d307a
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-01-19 15:20:09 +0100

    SP-176 cleanups

--- Bundles/Category/src/Spryker/Shared/Category/CategoryConstants.php
+++ Bundles/Category/src/Spryker/Shared/Category/CategoryConstants.php
@@ -12,7 +12,6 @@ interface CategoryConstants
     const RESOURCE_TYPE_CATEGORY_NODE = 'categorynode';
     const RESOURCE_TYPE_NAVIGATION = 'navigation';
     const RESOURCE_TYPE_URL = 'url';
-    const RESOURCE_TYPE_PRODUCT_ABSTRACT = 'product_abstract';
 
     const PARAM_ID_NODE = 'id-node';
 
