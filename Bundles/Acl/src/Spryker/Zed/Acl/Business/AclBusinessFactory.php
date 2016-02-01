a3d3680697886b0f7bfe5d0df6b276a6d82869c2
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-02-01 15:16:03 +0100

    1114 include order

--- Bundles/Acl/src/Spryker/Zed/Acl/Business/AclBusinessFactory.php
+++ Bundles/Acl/src/Spryker/Zed/Acl/Business/AclBusinessFactory.php
@@ -6,17 +6,19 @@
 
 namespace Spryker\Zed\Acl\Business;
 
-use Spryker\Zed\Acl\Business\Model\Role;
-use Spryker\Zed\Acl\Business\Model\Group;
-use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
+use Spryker\Zed\Acl\AclConfig;
 use Spryker\Zed\Acl\AclDependencyProvider;
-use Spryker\Zed\Acl\Business\Model\RuleValidator;
+use Spryker\Zed\Acl\Business\Model\Group;
 use Spryker\Zed\Acl\Business\Model\Installer;
+use Spryker\Zed\Acl\Business\Model\Role;
+use Spryker\Zed\Acl\Business\Model\RuleValidator;
 use Spryker\Zed\Acl\Business\Model\Rule;
+use Spryker\Zed\Acl\Persistence\AclQueryContainer;
+use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 
 /**
- * @method \Spryker\Zed\Acl\AclConfig getConfig()
- * @method \Spryker\Zed\Acl\Persistence\AclQueryContainer getQueryContainer()
+ * @method AclConfig getConfig()
+ * @method AclQueryContainer getQueryContainer()
  */
 class AclBusinessFactory extends AbstractBusinessFactory
 {
