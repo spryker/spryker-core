d16d453d22d8ee1970e8631ab0486ba57691f9fe
Auris <aurimas.lickus@spryker.com>
2016-02-04 14:34:54 +0100

    added calculator client interface

--- Bundles/Calculation/src/Spryker/Client/Calculation/CalculationClientInterface.php
+++ Bundles/Calculation/src/Spryker/Client/Calculation/CalculationClientInterface.php
@@ -2,25 +2,19 @@
 /**
  * (c) Spryker Systems GmbH copyright protected
  */
-
 namespace Spryker\Client\Calculation;
 
 use Generated\Shared\Transfer\QuoteTransfer;
-use Spryker\Client\Kernel\AbstractClient;
 
 /**
  * @method \Spryker\Client\Calculation\CalculationFactory getFactory()
  */
-class CalculationClient extends AbstractClient implements CalculationClientInterface
+interface CalculationClientInterface
 {
     /**
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
      *
      * @return \Generated\Shared\Transfer\QuoteTransfer
      */
-    public function recalculate(QuoteTransfer $quoteTransfer)
-    {
-        return $this->getFactory()->createZedStub()->recalculate($quoteTransfer);
-    }
-
+    public function recalculate(QuoteTransfer $quoteTransfer);
 }
