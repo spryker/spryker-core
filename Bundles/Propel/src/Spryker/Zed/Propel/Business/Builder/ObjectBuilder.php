da16550900bee414b48bbf7dab95d67986907ccd
René Klatt <rene.klatt@spryker.com>
2016-02-04 07:18:34 +0000

    #1259 add different error message

--- Bundles/Propel/src/Spryker/Zed/Propel/Business/Builder/ObjectBuilder.php
+++ Bundles/Propel/src/Spryker/Zed/Propel/Business/Builder/ObjectBuilder.php
@@ -8,6 +8,10 @@ namespace Spryker\Zed\Propel\Business\Builder;
 
 use Propel\Generator\Model\Column;
 use Propel\Generator\Builder\Om\ObjectBuilder as PropelObjectBuilder;
+use Propel\Generator\Model\IdMethod;
+use Propel\Generator\Platform\PlatformInterface;
+use Spryker\Shared\Application\ApplicationConstants;
+use Spryker\Shared\Config;
 
 class ObjectBuilder extends PropelObjectBuilder
 {
@@ -55,6 +59,156 @@ class ObjectBuilder extends PropelObjectBuilder
         }
 ';
         $this->addMutatorClose($script, $col);
+    }
+
+    /**
+     * Boosts ActiveRecord::doInsert() by doing more calculations at buildtime.
+     */
+    protected function addDoInsertBodyRaw()
+    {
+        $this->declareClasses(
+            '\Propel\Runtime\Propel',
+            '\PDO'
+        );
+        $table = $this->getTable();
+        $platform = $this->getPlatform();
+        $primaryKeyMethodInfo = '';
+        if ($table->getIdMethodParameters()) {
+            $params = $table->getIdMethodParameters();
+            $imp = $params[0];
+            $primaryKeyMethodInfo = $imp->getValue();
+        } elseif ($table->getIdMethod() == IdMethod::NATIVE && ($platform->getNativeIdMethod() == PlatformInterface::SEQUENCE || $platform->getNativeIdMethod() == PlatformInterface::SERIAL)) {
+            $primaryKeyMethodInfo = $platform->getSequenceName($table);
+        }
+        $query = 'INSERT INTO ' . $this->quoteIdentifier($table->getName()) . ' (%s) VALUES (%s)';
+        $script = "
+        \$modifiedColumns = array();
+        \$index = 0;
+";
+
+        foreach ($table->getPrimaryKey() as $column) {
+            if (!$column->isAutoIncrement()) {
+                continue;
+            }
+            $constantName = $this->getColumnConstant($column);
+            if ($platform->supportsInsertNullPk()) {
+                $script .= "
+        \$this->modifiedColumns[$constantName] = true;";
+            }
+            $columnProperty = $column->getLowercasedName();
+            if (!$table->isAllowPkInsert()) {
+                $script .= "
+        if (null !== \$this->{$columnProperty}) {
+            throw new PropelException('Cannot insert a value for auto-increment primary key (' . $constantName . ')');
+        }";
+            } elseif (!$platform->supportsInsertNullPk()) {
+                $script .= "
+        // add primary key column only if it is not null since this database does not accept that
+        if (null !== \$this->{$columnProperty}) {
+            \$this->modifiedColumns[$constantName] = true;
+        }";
+            }
+        }
+
+        // if non auto-increment but using sequence, get the id first
+        if (!$platform->isNativeIdMethodAutoIncrement() && $table->getIdMethod() == "native") {
+            $column = $table->getFirstPrimaryKeyColumn();
+            $columnProperty = $column->getLowercasedName();
+            $script .= "
+        if (null === \$this->{$columnProperty}) {
+            try {";
+            $script .= $platform->getIdentifierPhp('$this->'. $columnProperty, '$con', $primaryKeyMethodInfo, '                ');
+            $script .= "
+            } catch (Exception \$e) {
+                throw new PropelException('Unable to get sequence id.', 0, \$e);
+            }
+        }
+";
+        }
+
+        $script .= "
+
+         // check the columns in natural order for more readable SQL queries";
+        foreach ($table->getColumns() as $column) {
+            $constantName = $this->getColumnConstant($column);
+            $identifier = var_export($this->quoteIdentifier($column->getName()), true);
+            $script .= "
+        if (\$this->isColumnModified($constantName)) {
+            \$modifiedColumns[':p' . \$index++]  = $identifier;
+        }";
+        }
+
+        $script .= "
+
+        \$sql = sprintf(
+            '$query',
+            implode(', ', \$modifiedColumns),
+            implode(', ', array_keys(\$modifiedColumns))
+        );
+
+        try {
+            \$stmt = \$con->prepare(\$sql);
+            foreach (\$modifiedColumns as \$identifier => \$columnName) {
+                switch (\$columnName) {";
+        foreach ($table->getColumns() as $column) {
+            $columnNameCase = var_export($this->quoteIdentifier($column->getName()), true);
+            $script .= "
+                    case $columnNameCase:";
+            $script .= $platform->getColumnBindingPHP($column, "\$identifier", '$this->' . $column->getLowercasedName(), '                        ');
+            $script .= "
+                        break;";
+        }
+        $script .= "
+                }
+            }
+            \$stmt->execute();
+        } catch (Exception \$e) {
+            Propel::log(\$e->getMessage(), Propel::LOG_ERR);
+";
+
+        if (Config::get(ApplicationConstants::PROPEL_DEBUG)) {
+            $script .= "
+            \$message = \$e->getMessage() . PHP_EOL . PHP_EOL
+                . 'Executed query: ' . PHP_EOL
+                . Propel::getConnection()->getLastExecutedQuery()
+            ;
+            throw new PropelException(\$message);
+";
+        }
+
+        if (!Config::get(ApplicationConstants::PROPEL_DEBUG)) {
+            $script .= "
+            throw new PropelException(sprintf('Unable to execute INSERT statement [%s]', \$sql), 0, \$e);
+        }
+";
+        }
+
+        // if auto-increment, get the id after
+        if ($platform->isNativeIdMethodAutoIncrement() && $table->getIdMethod() == "native") {
+            $script .= "
+        try {";
+            $script .= $platform->getIdentifierPhp('$pk', '$con', $primaryKeyMethodInfo);
+            $script .= "
+        } catch (Exception \$e) {
+            throw new PropelException('Unable to get autoincrement id.', 0, \$e);
+        }";
+            $column = $table->getFirstPrimaryKeyColumn();
+            if ($column) {
+                if ($table->isAllowPkInsert()) {
+                    $script .= "
+        if (\$pk !== null) {
+            \$this->set".$column->getPhpName()."(\$pk);
+        }";
+                } else {
+                    $script .= "
+        \$this->set".$column->getPhpName()."(\$pk);";
+                }
+            }
+            $script .= "
+";
+        }
+
+        return $script;
     }
 
 }
