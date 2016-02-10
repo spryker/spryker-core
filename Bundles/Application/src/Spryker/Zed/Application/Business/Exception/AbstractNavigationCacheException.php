2f1d6597ee35a6083ff3dcddcf55e33c3551b829
René Klatt <rene.klatt@spryker.com>
2016-02-10 10:57:32 +0000

    #1323 moved __construct to abstract class

--- Bundles/Application/src/Spryker/Zed/Application/Business/Exception/AbstractNavigationCacheException.php
+++ Bundles/Application/src/Spryker/Zed/Application/Business/Exception/AbstractNavigationCacheException.php
@@ -14,4 +14,16 @@ abstract class AbstractNavigationCacheException extends \Exception
     const MESSAGE = 'To create a navigation cache run:' . PHP_EOL
         . PHP_EOL . '$ vendor/bin/console ' . BuildNavigationConsole::COMMAND_NAME;
 
+    /**
+     * @param string $message
+     * @param int $code
+     * @param \Exception $previous = null
+     */
+    public function __construct($message = '', $code = 0, \Exception $previous = null)
+    {
+        $message .= PHP_EOL . PHP_EOL . self::MESSAGE;
+
+        parent::__construct($message, $code, $previous);
+    }
+
 }
--- Bundles/Application/src/Spryker/Zed/Application/Business/Exception/NavigationCacheEmptyException.php
+++ Bundles/Application/src/Spryker/Zed/Application/Business/Exception/NavigationCacheEmptyException.php
@@ -8,17 +8,4 @@ namespace Spryker\Zed\Application\Business\Exception;
 
 class NavigationCacheEmptyException extends AbstractNavigationCacheException
 {
-
-    /**
-     * @param string $message
-     * @param int $code
-     * @param \Exception $previous = null
-     */
-    public function __construct($message = '', $code = 0, \Exception $previous = null)
-    {
-        $message .= PHP_EOL . PHP_EOL . self::MESSAGE;
-
-        parent::__construct($message, $code, $previous);
-    }
-
 }
