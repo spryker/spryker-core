09ff95181d530c448a045ca1c4f0e9a9121e174e
René Klatt <rene.klatt@spryker.com>
2016-02-18 12:32:49 +0000

    #1344 update all file doc blocks

--- Bundles/Payolution/src/Spryker/Zed/Payolution/Persistence/Propel/AbstractSpyPaymentPayolutionTransactionRequestLog.php
+++ Bundles/Payolution/src/Spryker/Zed/Payolution/Persistence/Propel/AbstractSpyPaymentPayolutionTransactionRequestLog.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace Spryker\Zed\Payolution\Persistence\Propel;
 
 use Orm\Zed\Payolution\Persistence\Base\SpyPaymentPayolutionTransactionRequestLog as BaseSpyPaymentPayolutionTransactionRequestLog;
--- Bundles/Sales/src/Spryker/Zed/Sales/Business/Model/Split/CalculatorInterface.php
+++ Bundles/Sales/src/Spryker/Zed/Sales/Business/Model/Split/CalculatorInterface.php
@@ -1,8 +1,10 @@
 <?php
 
 /**
- * (c) Spryker Systems GmbH copyright protected
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
+
 namespace Spryker\Zed\Sales\Business\Model\Split;
 
 use Orm\Zed\Sales\Persistence\SpySalesOrderItem;
