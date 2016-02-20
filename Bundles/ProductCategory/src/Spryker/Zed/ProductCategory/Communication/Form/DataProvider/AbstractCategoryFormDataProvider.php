552e954fec16562c696a9268e3e8a97f22dc1837
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-02-20 07:11:22 +0100

    #448 removed state

--- Bundles/ProductCategory/src/Spryker/Zed/ProductCategory/Communication/Form/DataProvider/AbstractCategoryFormDataProvider.php
+++ Bundles/ProductCategory/src/Spryker/Zed/ProductCategory/Communication/Form/DataProvider/AbstractCategoryFormDataProvider.php
@@ -56,7 +56,7 @@ class AbstractCategoryFormDataProvider
     protected function getCategoriesWithPaths($idLocale)
     {
         $categoryEntityList = $this->categoryQueryContainer
-            ->queryCategory($this->locale->getIdLocale())
+            ->queryCategory($idLocale)
             ->find();
 
         $categories = [];
