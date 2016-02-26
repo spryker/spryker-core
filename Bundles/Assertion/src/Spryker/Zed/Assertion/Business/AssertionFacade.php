14d781beac33aa851d5c44a754ad28a236a3c19b
René Klatt <rene.klatt@spryker.com>
2016-02-26 12:54:34 +0000

    #1409 missing types in AssertionFacade

--- Bundles/Assertion/src/Spryker/Zed/Assertion/Business/AssertionFacade.php
+++ Bundles/Assertion/src/Spryker/Zed/Assertion/Business/AssertionFacade.php
@@ -16,7 +16,7 @@ class AssertionFacade extends AbstractFacade implements AssertionFacadeInterface
 {
 
     /**
-     * @param $value
+     * @param mixed $value
      * @param string|null $message
      *
      * @throws \Spryker\Zed\Assertion\Business\Exception\InvalidArgumentException
@@ -29,7 +29,7 @@ class AssertionFacade extends AbstractFacade implements AssertionFacadeInterface
     }
 
     /**
-     * @param $value
+     * @param mixed $value
      * @param string|null $message
      *
      * @throws \Spryker\Zed\Assertion\Business\Exception\InvalidArgumentException
@@ -42,7 +42,7 @@ class AssertionFacade extends AbstractFacade implements AssertionFacadeInterface
     }
 
     /**
-     * @param $value
+     * @param mixed $value
      * @param string|null $message
      *
      * @throws \Spryker\Zed\Assertion\Business\Exception\InvalidArgumentException
@@ -55,7 +55,7 @@ class AssertionFacade extends AbstractFacade implements AssertionFacadeInterface
     }
 
     /**
-     * @param $value
+     * @param mixed $value
      * @param string|null $message
      *
      * @throws \Spryker\Zed\Assertion\Business\Exception\InvalidArgumentException
@@ -68,7 +68,7 @@ class AssertionFacade extends AbstractFacade implements AssertionFacadeInterface
     }
 
     /**
-     * @param $value
+     * @param mixed $value
      * @param string|null $message
      *
      * @throws \Spryker\Zed\Assertion\Business\Exception\InvalidArgumentException
