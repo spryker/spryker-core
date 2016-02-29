9534398a89d77e53f4df402afbef1cfb2d73dd71
René Klatt <rene.klatt@spryker.com>
2016-02-29 12:06:24 +0000

    #1239 renamed class

--- Bundles/Maintenance/src/Spryker/Zed/Maintenance/Communication/Controller/PhpinfdxfghdfhoController.php
+++ Bundles/Maintenance/src/Spryker/Zed/Maintenance/Communication/Controller/PhpinfdxfghdfhoController.php
@@ -13,7 +13,7 @@ use Spryker\Zed\Application\Communication\Controller\AbstractController;
  * @method \Spryker\Zed\Maintenance\Business\MaintenanceFacade getFacade()
  * @method \Spryker\Zed\Maintenance\Communication\MaintenanceCommunicationFactory getFactory()
  */
-class PhpiniztdiztdcfoController extends AbstractController
+class PhpInfoController extends AbstractController
 {
 
     /**
