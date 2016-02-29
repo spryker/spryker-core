920f661ab3c4b60b14cec710399cb0def9aa98e8
Alessandro Bellini <alessandro.bellini@spryker.com>
2016-02-29 16:36:40 +0100

    1002 internal assets copyright header

---
+++ Bundles/Gui/assets/Inspinia/inspinia-helpers.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2016-present Spryker Systems GmbH. All rights reserved. 
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file. 
+ */
+
+'use strict';
+
+module.exports = {
+    // Full height of sidebar
+    fix_height: function() {
+        var heightWithoutNavbar = $("body > #wrapper").height() - 61;
+        $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
+
+        var navbarHeigh = $('nav.navbar-default').height();
+        var wrapperHeigh = $('#page-wrapper').height();
+
+        if (navbarHeigh > wrapperHeigh) {
+            $('#page-wrapper').css("min-height", navbarHeigh + "px");
+        }
+
+        if (navbarHeigh < wrapperHeigh) {
+            $('#page-wrapper').css("min-height", $(window).height() + "px");
+        }
+
+        if ($('body').hasClass('fixed-nav')) {
+            $('#page-wrapper').css("min-height", $(window).height() - 60 + "px");
+        }
+    },
+    localStorageSupport: function() {
+        return (('localStorage' in window) && window['localStorage'] !== null)
+    },
+    SmoothlyMenu: function() {
+        if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+            // Hide menu in order to smoothly turn on when maximize menu
+            $('#side-menu').hide();
+            // For smoothly turn on menu
+            setTimeout(
+                function () {
+                    $('#side-menu').fadeIn(500);
+                }, 100);
+        } else if ($('body').hasClass('fixed-sidebar')) {
+            $('#side-menu').hide();
+            setTimeout(
+                function () {
+                    $('#side-menu').fadeIn(500);
+                }, 300);
+        } else {
+            // Remove all inline style from jquery fadeIn function to reset menu state
+            $('#side-menu').removeAttr('style');
+        }
+    },
+    fixBodyClassByResolution: function(){
+        if ($(window).width() < 769) {
+            $('body').addClass('body-small')
+        } else {
+            $('body').removeClass('body-small')
+        }
+    }
+};
--- Bundles/Gui/assets/Inspinia/inspinia.js
+++ Bundles/Gui/assets/Inspinia/inspinia.js
@@ -1,8 +1,6 @@
-/*
- *
- *   INSPINIA - Responsive Admin Theme
- *   version 2.2
- *
+/**
+ * Copyright (c) 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
 
