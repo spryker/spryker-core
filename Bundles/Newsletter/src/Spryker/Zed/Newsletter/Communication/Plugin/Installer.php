032d2f704bd43d5c2b1ec1b601c6dd8ea827ffb6
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-03-08 21:58:25 +0100

    feature-1488: added missing installer

---
+++ Bundles/Newsletter/src/Spryker/Zed/Newsletter/Communication/Plugin/Installer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Newsletter\Communication\Plugin;
+
+use Spryker\Zed\Installer\Communication\Plugin\AbstractInstallerPlugin;
+
+/**
+ * @method \Spryker\Zed\Newsletter\Business\NewsletterFacade getFacade()
+ */
+class Installer extends AbstractInstallerPlugin
+{
+
+    /**
+     * @return void
+     */
+    public function install()
+    {
+        $this->getFacade()->install();
+    }
+
+}
