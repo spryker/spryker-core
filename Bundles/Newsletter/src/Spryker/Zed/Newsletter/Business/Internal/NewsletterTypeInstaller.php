7b65ae55d3c3605841d86aa37b786d0f794aea62
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-03-08 21:57:10 +0100

    feature-1488: added missing installer

---
+++ Bundles/Newsletter/src/Spryker/Zed/Newsletter/Business/Internal/NewsletterTypeInstaller.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Newsletter\Business\Internal;
+
+use Orm\Zed\Newsletter\Persistence\SpyNewsletterType;
+use Spryker\Zed\Installer\Business\Model\AbstractInstaller;
+use Spryker\Zed\Newsletter\Persistence\NewsletterQueryContainer;
+
+class NewsletterTypeInstaller extends AbstractInstaller
+{
+
+    /**
+     * @var \Spryker\Zed\Newsletter\Persistence\NewsletterQueryContainer
+     */
+    protected $queryContainer;
+
+    /**
+     * @var array
+     */
+    protected $newsletterTypeCollection;
+
+    /**
+     * @param \Spryker\Zed\Newsletter\Persistence\NewsletterQueryContainer $queryContainer
+     * @param array $newsletterTypeCollection
+     */
+    public function __construct(NewsletterQueryContainer $queryContainer, array $newsletterTypeCollection)
+    {
+        $this->queryContainer = $queryContainer;
+        $this->newsletterTypeCollection = $newsletterTypeCollection;
+    }
+
+    /**
+     * @return void
+     */
+    public function install()
+    {
+        if ($this->queryContainer->queryNewsletterType()->count() > 0) {
+            return;
+        }
+
+        $this->installNewsletterTypes();
+    }
+
+    /**
+     * @return void
+     */
+    protected function installNewsletterTypes()
+    {
+        foreach ($this->newsletterTypeCollection as $newsletterTypeName) {
+            $newsletterType = new SpyNewsletterType();
+            $newsletterType->setName($newsletterTypeName);
+            $newsletterType->save();
+        }
+    }
+
+}
