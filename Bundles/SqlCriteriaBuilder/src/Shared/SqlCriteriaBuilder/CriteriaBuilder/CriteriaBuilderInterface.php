bf5e8967d8dd3f67b056be34ff2e815b997bf10b
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-03-20 02:00:22 +0100

    1600: decoupled from Everon Criteria Builder

---
+++ Bundles/SqlCriteriaBuilder/src/Shared/SqlCriteriaBuilder/CriteriaBuilder/CriteriaBuilderInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\SqlCriteriaBuilder\CriteriaBuilder;
+
+use Everon\Component\CriteriaBuilder\CriteriaBuilderInterface as EveronCriteriaBuilderInterface;
+
+interface CriteriaBuilderInterface extends EveronCriteriaBuilderInterface
+{
+}
