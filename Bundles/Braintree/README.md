0023ed4c5c78ce5be19b72a212272685e125b3e0
mscherer <mark.scherer@spryker.com>
2016-03-21 12:27:13 +0100

    Init Braintree composer.

---
+++ Bundles/Braintree/README.md
@@ -0,0 +1,11 @@
+# Braintree Bundle
+
+## Installation
+
+```
+composer require spryker/braintree
+```
+
+## Documentation
+
+[Braintree Documentation](https://spryker.github.io/braintree/index.html)
---
+++ Bundles/Braintree/src/Spryker/Zed/Braintree/BraintreeDependencyProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Braintree;
+
+use Spryker\Zed\Braintree\Dependency\Facade\BraintreeToGlossaryBridge;
+use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Zed\Kernel\Container;
+
+class BraintreeDependencyProvider extends AbstractBundleDependencyProvider
+{
+
+    const FACADE_MAIL = 'mail facade';
+    const FACADE_GLOSSARY = 'glossary facade';
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideCommunicationLayerDependencies(Container $container)
+    {
+        $container[self::FACADE_GLOSSARY] = function (Container $container) {
+            return new BraintreeToGlossaryBridge($container->getLocator()->glossary()->facade());
+        };
+
+        return $container;
+    }
+
+}
