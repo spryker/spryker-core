ae63266ef4b59ed1067aae27bf093dfb90d72326
Alessandro Bellini <alessandro.bellini@spryker.com>
2016-03-22 20:32:53 +0100

    1650 summernote code optimisation

---
+++ Bundles/Gui/assets/Zed/js/modules/summernote.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2016-present Spryker Systems GmbH. All rights reserved. 
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file. 
+ */
+
+'use strict';
+
+module.export = {
+    getConfig: function(content) {
+    	content = content || '';
+
+        return {
+            maxHeight: 600,
+            inputText: content,
+            focus: true,
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough', 'superscript', 'subscript']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['insert', ['picture', 'link', 'video', 'table', 'hr']],
+                ['misc', ['undo', 'redo']]
+            ]
+        };
+    }
+};
