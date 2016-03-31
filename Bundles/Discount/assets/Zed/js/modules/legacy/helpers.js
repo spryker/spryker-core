387d5ee92e4b3f62b380d1422d8df3f8b232a5a4
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-03-31 15:15:11 +0200

    1794 Added missing javascript changes related to #1758

--- Bundles/Discount/assets/Zed/js/modules/legacy/helpers.js
+++ Bundles/Discount/assets/Zed/js/modules/legacy/helpers.js
@@ -5,36 +5,22 @@
 
 'use strict';
 
-SprykerAjax.loadDecisionRulesOptions = function(element, mainFormName){
+SprykerAjax.loadDecisionRulesOptions = function(element) {
     var elementsCount = $('#rules-container > .col-md-6').length;
-    var nextElementIndex = elementsCount + 1;
-    var options = {
-        elements: nextElementIndex
-    };
-    this.setUrl('/discount/cart-rule/decision-rule?mainFormName='+mainFormName)
-        .setDataType('html')
-        .ajaxSubmit(options, function(ajaxHtmlResponse, options){
-            var html = ajaxHtmlResponse.replace(/decision_rule\[/g, mainFormName + '[decision_rules][rule_' + nextElementIndex + '][');
-            $('#rules-container').append(html);
-            element.children('i').addClass('hidden');
-        }
-    );
+    var collectionHolder = $('#rules-container');
+    var prototype = collectionHolder.data('prototype');
+    var newForm = prototype.replace(/__name__/g, elementsCount);
+    collectionHolder.append(newForm);
+    element.children('i').addClass('hidden');
 };
 
-SprykerAjax.loadCollectorPlugins = function(element, mainFormName){
+SprykerAjax.loadCollectorPlugins = function(element) {
     var elementsCount = $('#collector-container > .col-md-6').length;
-    var nextElementIndex = elementsCount + 1;
-    var options = {
-        elements: nextElementIndex
-    };
-    this.setUrl('/discount/cart-rule/collector-plugins/')
-        .setDataType('html')
-        .ajaxSubmit(options, function(ajaxHtmlResponse){
-            var html = ajaxHtmlResponse.replace(/decision_rule\[/g, mainFormName + '[collector_plugins][plugin_' + nextElementIndex + '][');
-            $('#collector-container').append(html);
-            element.children('i').addClass('hidden');
-        }
-    );
+    var collectionHolder = $('#collector-container');
+    var prototype = collectionHolder.data('prototype');
+    var newForm = prototype.replace(/__name__/g, elementsCount);
+    collectionHolder.append(newForm);
+    element.children('i').addClass('hidden');
 };
 
 module.exports = {
