ba50ba6601c730422eb8fe8f48b863dbd8d536f2
Eugene <ievgen.varava@spryker.com>
2016-04-13 11:08:46 +0200

    1940 Added missing FileWriter.php to commit

--- Bundles/Collector/src/Spryker/Zed/Collector/Business/Exporter/Writer/File/FileWriter.php
+++ Bundles/Collector/src/Spryker/Zed/Collector/Business/Exporter/Writer/File/FileWriter.php
@@ -1,7 +1,58 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: ievgenvarava
- * Date: 11/04/16
- * Time: 17:50
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
+
+namespace Spryker\Zed\Collector\Business\Exporter\Writer\File;
+
+use Spryker\Shared\Library\Writer\Csv\CsvWriterInterface;
+use Spryker\Zed\Collector\Business\Exporter\Writer\MultiadapterWriterInterface;
+
+class FileWriter implements MultiadapterWriterInterface
+{
+
+    protected $fileWriterAdapter;
+
+    /**
+     * @param array $dataSet
+     * @param string $type
+     *
+     * @return bool
+     */
+    public function write(array $dataSet, $type = '')
+    {
+        return $this->fileWriterAdapter->write($dataSet);
+    }
+
+    /**
+     * @param array $dataSet
+     *
+     * @return bool
+     */
+    public function delete(array $dataSet)
+    {
+        // none for now
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'file-writer';
+    }
+
+    /**
+     * @param \Spryker\Shared\Library\Writer\Csv\CsvWriterInterface $fileWriterAdapter
+     *
+     * @return mixed
+     */
+    public function setWriterAdapter(CsvWriterInterface $fileWriterAdapter)
+    {
+        return $this->fileWriterAdapter = $fileWriterAdapter;
+    }
+
+}
