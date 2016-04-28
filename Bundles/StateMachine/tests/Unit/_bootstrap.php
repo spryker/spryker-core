3c79de027567bca8fd941e97c5c96cd101792fe6
Auris <aurimas.lickus@spryker.com>
2016-04-28 16:40:52 +0200

    1943: added isolated tests

--- Bundles/StateMachine/tests/Unit/_bootstrap.php
+++ Bundles/StateMachine/tests/Unit/_bootstrap.php
@@ -3,7 +3,3 @@
 /**
  * (c) Spryker Systems GmbH copyright protected
  */
-$bootstrap = Spryker\Shared\Library\SystemUnderTest\SystemUnderTestBootstrap::getInstance();
-
-$application = 'Zed';
-$bootstrap->bootstrap($application);
--- Bundles/StateMachine/tests/_bootstrap.php
+++ Bundles/StateMachine/tests/_bootstrap.php
@@ -3,5 +3,3 @@
 /**
  * (c) Spryker Systems GmbH copyright protected
  */
-$bootstrap = Spryker\Shared\Library\SystemUnderTest\SystemUnderTestBootstrap::getInstance();
-$bootstrap->bootstrap('Zed');
