ff2962a16b75655e1df5ccf2808d9bb5f1e63eaf
Marius Rizac <marius.rizac@spryker.com>
2016-05-03 12:37:05 +0200

    2019 created FilterController and page

---
+++ Bundles/Search/src/Spryker/Zed/Search/Communication/Controller/FiltersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Spryker\Zed\Search\Communication\Controller;
+
+use Spryker\Zed\Application\Communication\Controller\AbstractController;
+
+class FiltersController extends AbstractController
+{
+    public function indexAction()
+    {
+        return [];
+    }
+}
