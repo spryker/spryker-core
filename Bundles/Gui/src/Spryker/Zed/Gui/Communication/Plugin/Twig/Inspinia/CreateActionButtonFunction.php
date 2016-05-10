3a9af823658b542200b4c043ccfc839a3b9116c6
Marius Rizac <marius.rizac@spryker.com>
2016-05-10 18:08:05 +0200

    2044 readded inspinia twig functions and deprecated them

---
+++ Bundles/Gui/src/Spryker/Zed/Gui/Communication/Plugin/Twig/Inspinia/CreateActionButtonFunction.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Gui\Communication\Plugin\Twig\Inspinia;
+
+use Spryker\Zed\Gui\Communication\Plugin\Twig\ActionButtons\CreateActionButtonFunction as NewCreateActionButtonFunction;
+
+/**
+ * @deprecated use Spryker\Zed\Gui\Communication\Plugin\Twig\ActionButtons\CreateActionButtonFunction
+ */
+class CreateActionButtonFunction extends NewCreateActionButtonFunction
+{
+}
---
+++ Bundles/Gui/src/Spryker/Zed/Gui/Communication/Plugin/Twig/Inspinia/ViewActionButtonFunction.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Gui\Communication\Plugin\Twig\Inspinia;
+
+use Spryker\Zed\Gui\Communication\Plugin\Twig\ActionButtons\ViewActionButtonFunction as NewViewActionButtonFunction;
+
+/**
+ * @deprecated use Spryker\Zed\Gui\Communication\Plugin\Twig\ActionButtons\ViewActionButtonFunction
+ */
+class ViewActionButtonFunction extends NewViewActionButtonFunction
+{
+}
