eb50076652c54af38a89cae13d5afbe20079b15d
Marius Rizac <marius.rizac@spryker.com>
2016-05-10 15:07:06 +0200

    2044 run code fixer

--- Bundles/Application/src/Spryker/Zed/Application/Communication/Plugin/ServiceProvider/ZedExtensionServiceProvider.php
+++ Bundles/Application/src/Spryker/Zed/Application/Communication/Plugin/ServiceProvider/ZedExtensionServiceProvider.php
@@ -10,12 +10,11 @@ namespace Spryker\Zed\Application\Communication\Plugin\ServiceProvider;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 use Spryker\Zed\Application\Business\Model\Twig\ZedExtension;
-use Spryker\Zed\Gui\Communication\Plugin\Twig\GuiTwigExtensions;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
  * @deprecated All bundles which want to add a function or something else to twig does it now in a separate ServiceProvider of that bundle
- * 
+ *
  * @method \Spryker\Zed\Application\Business\ApplicationFacade getFacade()
  * @method \Spryker\Zed\Application\Communication\ApplicationCommunicationFactory getFactory()
  */
