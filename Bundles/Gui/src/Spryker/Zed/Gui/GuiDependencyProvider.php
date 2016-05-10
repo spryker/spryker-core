c6d0e72fd3e1dfb0587c80071f99fea6e7b74494
Marius Rizac <marius.rizac@spryker.com>
2016-05-10 09:42:49 +0200

    2044 move ZedExtensionServiceProvider in GuiDependencyProvider

--- Bundles/Gui/src/Spryker/Zed/Gui/GuiDependencyProvider.php
+++ Bundles/Gui/src/Spryker/Zed/Gui/GuiDependencyProvider.php
@@ -7,8 +7,33 @@
 
 namespace Spryker\Zed\Gui;
 
+use Spryker\Zed\Application\Communication\Plugin\ServiceProvider\ZedExtensionServiceProvider;
 use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Zed\Kernel\Container;
 
 class GuiDependencyProvider extends AbstractBundleDependencyProvider
 {
+    const GUI_SERVICE_PROVIDER = 'GUI_SERVICE_PROVIDER';
+
+    public function provideCommunicationLayerDependencies(Container $container)
+    {
+        dump('a');
+        die;
+
+        $container[self::GUI_SERVICE_PROVIDER] = function(Container $container){
+            return $this->getServiceProvider($container);
+        };
+
+        return $container;
+    }
+
+    protected function getServiceProvider(Container $container)
+    {
+        $providers = [
+            new ZedExtensionServiceProvider(),
+        ];
+
+        return $providers;
+    }
+
 }
