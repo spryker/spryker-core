0c8231b5015315ad53ecca8db9eeabc59d816d9e
Andriy Netseplyayev <andriy.netseplyayev@spryker.com>
2016-05-15 14:23:16 +0000

    2027 add Symfony form extension to prevent form double submition

---
+++ Bundles/Symfony/src/Spryker/Shared/Symfony/Form/Extension/DoubleSubmitProtection/FormEventSubscriber.php
@@ -0,0 +1,208 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection;
+
+use Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\StorageInterface;
+use Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\TokenGeneratorInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class FormEventSubscriber implements EventSubscriberInterface
+{
+
+    const DEFAULT_ERROR_MESSAGE = 'This form has been already submitted.';
+    const DEFAULT_TOKEN_FIELD_NAME = '_requestToken';
+
+    /**
+     * @var \Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\TokenGeneratorInterface
+     */
+    protected $tokenGenerator;
+
+    /**
+     * @var \Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\StorageInterface
+     */
+    protected $tokenStorage;
+
+    /**
+     * @var string
+     */
+    protected $errorMessage = self::DEFAULT_ERROR_MESSAGE;
+
+    /**
+     * @var string
+     */
+    protected $fieldName = self::DEFAULT_TOKEN_FIELD_NAME;
+
+    /**
+     * @var \Symfony\Component\Translation\TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @var null|string
+     */
+    protected $translationDomain;
+
+    /**
+     * @var array
+     */
+    protected $translationOptions = [];
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            FormEvents::PRE_SUBMIT => 'validateToken'
+        ];
+    }
+
+    /**
+     * @param \Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\TokenGeneratorInterface $generator
+     * @param \Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\StorageInterface $storage
+     * @param \Symfony\Component\Translation\TranslatorInterface|null $translator
+     * @param string|null $translationDomain
+     */
+    public function __construct(
+        TokenGeneratorInterface $generator,
+        StorageInterface $storage,
+        $translator,
+        $translationDomain = null
+    ) {
+        $this->tokenGenerator = $generator;
+        $this->tokenStorage = $storage;
+        $this->translator = $translator;
+        $this->translationDomain = $translationDomain;
+    }
+
+
+    /**
+     * @param \Symfony\Component\Form\FormEvent $event
+     *
+     * @return void
+     */
+    public function validateToken(FormEvent $event)
+    {
+        $form = $event->getForm();
+
+        if (!$form->isRoot()) {
+            return;
+        }
+
+        $data = $event->getData();
+        $formName = $form->getName() ?: get_class($form->getConfig()->getType()->getInnerType());
+
+        if (!$this->isTokenValid($data, $formName)) {
+            $errorMessage = $this->getTranslatedErrorMessage();
+            $form->addError(new FormError($errorMessage));
+        }
+
+        $this->tokenStorage->deleteToken($formName);
+
+        if (is_array($data)) {
+            unset($data[$this->fieldName]);
+        }
+
+        $event->setData($data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
+
+    /**
+     * @param string $errorMessage
+     *
+     * @return $this
+     */
+    public function setErrorMessage($errorMessage)
+    {
+        $this->errorMessage = $errorMessage;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFieldName()
+    {
+        return $this->fieldName;
+    }
+
+    /**
+     * @param string $fieldName
+     *
+     * @return $this
+     */
+    public function setFieldName($fieldName)
+    {
+        $this->fieldName = $fieldName;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTranslationOptions()
+    {
+        return $this->translationOptions;
+    }
+
+    /**
+     * @param array $translationOptions
+     *
+     * @return $this
+     */
+    public function setTranslationOptions($translationOptions)
+    {
+        $this->translationOptions = $translationOptions;
+        return $this;
+    }
+
+    /**
+     * @param array $data
+     * @param string $formName
+     *
+     * @return boolean
+     */
+    protected function isTokenValid($data, $formName)
+    {
+        $givenToken = $data[$this->fieldName];
+        $expectedToken = $this->tokenStorage->getToken($formName);
+
+        return
+            !empty($expectedToken) &&
+            !empty($givenToken) &&
+            $this->tokenGenerator->checkTokenEquals($expectedToken, $givenToken);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTranslatedErrorMessage()
+    {
+        $errorMessage = $this->errorMessage;
+        if ($this->translator !== null) {
+            $errorMessage = $this->translator->trans(
+                $this->errorMessage,
+                $this->translationOptions,
+                $this->translationDomain
+            );
+        }
+
+        return $errorMessage;
+    }
+
+}
---
+++ Bundles/Symfony/src/Spryker/Shared/Symfony/Plugin/ServiceProvider/DoubleSubmitProtectionServiceProvider.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\Symfony\Plugin\ServiceProvider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\DoubleSubmitProtectionExtension as DoubleSubmitProtectionExtension;
+use Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\SessionStorage;
+use Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\TokenHashGenerator;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+class DoubleSubmitProtectionServiceProvider extends AbstractPlugin implements ServiceProviderInterface
+{
+
+    /**
+     * @param \Silex\Application $app
+     *
+     * @return void
+     */
+    public function register(Application $app)
+    {
+
+        $app['form.extension.double_submit_protection'] = $app->share(function ($app) {
+            $translator = isset($app['translator']) ? $app['translator'] : null;
+
+            return new DoubleSubmitProtectionExtension(
+                $this->createTokenGenerator($app),
+                $this->createTokenStorage($app),
+                $translator
+            );
+        });
+
+        $app->extend('form.extensions', function ($extensions) use ($app) {
+            $extensions[] = $app['form.extension.double_submit_protection'];
+            return $extensions;
+        });
+    }
+
+    /**
+     * @param \Silex\Application $app
+     *
+     * @return \Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\TokenHashGenerator
+     */
+    protected function createTokenGenerator(Application $app)
+    {
+        return new TokenHashGenerator();
+    }
+
+    /**
+     * @param \Silex\Application $app
+     *
+     * @return \Spryker\Shared\Symfony\Form\Extension\DoubleSubmitProtection\RequestTokenProvider\SessionStorage
+     */
+    protected function createTokenStorage(Application $app)
+    {
+        return new SessionStorage($app['session']);
+    }
+
+    /**
+     * @param \Silex\Application $app
+     *
+     * @return void
+     */
+    public function boot(Application $app)
+    {
+    }
+
+}
