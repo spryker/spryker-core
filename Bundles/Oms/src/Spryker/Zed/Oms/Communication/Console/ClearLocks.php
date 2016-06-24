050d3ce24fa2bfaef1a2bebb6943aa0b2695dc0e
mscherer <mark.scherer@spryker.com>
2016-06-24 10:29:32 +0200

    Correct console commands to be suffixed with Console and deprecate the former one.

--- Bundles/Oms/src/Spryker/Zed/Oms/Communication/Console/ClearLocks.php
+++ Bundles/Oms/src/Spryker/Zed/Oms/Communication/Console/ClearLocks.php
@@ -6,39 +6,9 @@
 
 namespace Spryker\Zed\Oms\Communication\Console;
 
-use Spryker\Zed\Console\Business\Model\Console;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-
 /**
- * @method \Spryker\Zed\Oms\Business\OmsFacade getFacade()
+ * @deprecated Use \Spryker\Zed\Oms\Communication\ClearLocksConsole instead.
  */
-class ClearLocks extends Console
+class ClearLocks extends ClearLocksConsole
 {
-
-    const COMMAND_NAME = 'oms:clear-locks';
-    const COMMAND_DESCRIPTION = 'Clear OMS state machine expired locks from lock table';
-
-    /**
-     * @return void
-     */
-    protected function configure()
-    {
-        $this->setName(self::COMMAND_NAME);
-        $this->setDescription(self::COMMAND_DESCRIPTION);
-
-        parent::configure();
-    }
-
-    /**
-     * @param \Symfony\Component\Console\Input\InputInterface $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
-     * @return void
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $this->getFacade()->clearLocks();
-    }
-
 }
