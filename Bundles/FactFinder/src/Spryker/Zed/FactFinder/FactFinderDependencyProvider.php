788e65bafd16dfac892ba710e015110c9d5cda52
Artem Kuznetsov <wizardphp@gmail.com>
2016-07-01 15:17:36 +0000

    .

---
+++ Bundles/FactFinder/src/Spryker/Zed/FactFinder/FactFinderDependencyProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\FactFinder;
+
+use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Zed\Kernel\Container;
+
+class FactFinderDependencyProvider extends AbstractBundleDependencyProvider
+{
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideCommunicationLayerDependencies(Container $container)
+    {
+        return $container;
+    }
+
+}
