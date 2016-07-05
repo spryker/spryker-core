8f0ebaa827cfa149a961b885d9bd2f512d9e2d5d
mscherer <mark.scherer@spryker.com>
2016-07-05 14:40:49 +0200

    2142 Fix sorting issues by making sure sane defaults are set for properties.

--- Bundles/Gui/src/Spryker/Zed/Gui/Communication/Table/TableConfiguration.php
+++ Bundles/Gui/src/Spryker/Zed/Gui/Communication/Table/TableConfiguration.php
@@ -14,49 +14,49 @@ class TableConfiguration
     const SORT_DESC = 'desc';
 
     /**
-     * @var string
+     * @var string|null
      */
     protected $url;
 
     /**
      * @var array
      */
-    private $header;
+    protected $header = [];
 
     /**
      * @var array
      */
-    private $footer;
+    protected $footer = [];
 
     /**
      * @var int
      */
-    private $pageLength;
+    protected $pageLength = 0;
+
+    /**
+     * @var array|null
+     */
+    protected $searchableFields = null;
 
     /**
      * @var array
      */
-    private $searchableFields;
-
-    /**
-     * @var array
-     */
-    private $sortableFields;
+    protected $sortableFields = [];
 
     /**
      * @var int
      */
-    private $defaultSortColumnIndex = 0;
+    protected $defaultSortColumnIndex = 0;
 
     /**
      * @var string
      */
-    private $defaultSortDirection = self::SORT_ASC;
+    protected $defaultSortDirection = self::SORT_ASC;
 
     /**
      * @var array
      */
-    private $rawColumns = [];
+    protected $rawColumns = [];
 
     /**
      * @return array
@@ -102,7 +102,7 @@ class TableConfiguration
      * @todo Zed Translation in Template
      *
      * @param array $header Provide php names for table columns
-     *   if you are goin to user Propel Query as data population
+     *   if you are going to user Propel Query as data population
      *
      * @return void
      */
@@ -169,7 +169,7 @@ class TableConfiguration
      */
     public function getSearchable()
     {
-        return !empty($this->searchableFields) ? $this->searchableFields : array_keys($this->header);
+        return $this->searchableFields ?: array_keys($this->header);
     }
 
     /**
@@ -201,7 +201,7 @@ class TableConfiguration
     }
 
     /**
-     * @return string
+     * @return string|null
      */
     public function getUrl()
     {
@@ -259,7 +259,7 @@ class TableConfiguration
      *
      * @return bool
      */
-    private function isAssoc(array $arr)
+    protected function isAssoc(array $arr)
     {
         return (array_values($arr) !== $arr);
     }
