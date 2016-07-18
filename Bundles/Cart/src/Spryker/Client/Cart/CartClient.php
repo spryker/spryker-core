a4734132309efcbc979128f847454f7b5a71dc5c
Auris <aurimas.lickus@spryker.com>
2016-07-18 17:10:37 +0200

    CORE-206: change sales option schema, cart client find method bugfix

--- Bundles/Cart/src/Spryker/Client/Cart/CartClient.php
+++ Bundles/Cart/src/Spryker/Client/Cart/CartClient.php
@@ -118,7 +118,7 @@ class CartClient extends AbstractClient implements CartClientInterface
         $quoteTransfer = $this->getQuote();
 
         foreach ($quoteTransfer->getItems() as $itemTransfer) {
-            if ($itemTransfer->getSku() === $sku || ($groupKey !== null && $itemTransfer->getGroupKey() === $groupKey)) {
+            if (($itemTransfer->getSku() === $sku && $groupKey === null) || $itemTransfer->getGroupKey() === $groupKey) {
                 return $itemTransfer;
             }
         }
