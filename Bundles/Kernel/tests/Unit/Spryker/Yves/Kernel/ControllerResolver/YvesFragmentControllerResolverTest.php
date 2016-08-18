d8e646745f6a34e40ef9120fe5ea2576a7b9cc8d
mscherer <mark.scherer@spryker.com>
2016-08-18 15:55:44 +0200

    Fix up tests.

--- Bundles/Kernel/tests/Unit/Spryker/Yves/Kernel/ControllerResolver/YvesFragmentControllerResolverTest.php
+++ Bundles/Kernel/tests/Unit/Spryker/Yves/Kernel/ControllerResolver/YvesFragmentControllerResolverTest.php
@@ -43,11 +43,11 @@ class YvesFragmentControllerResolverTest extends \PHPUnit_Framework_TestCase
     public function getController()
     {
         return [
-            ['index/index/index', 'Unit\Spryker\Zed\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::indexAction'],
-            ['/index/index/index', 'Unit\Spryker\Zed\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::indexAction'],
-            ['Index/Index/Index', 'Unit\Spryker\Zed\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::indexAction'],
-            ['/Index/Index/Index', 'Unit\Spryker\Zed\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::indexAction'],
-            ['foo-bar/baz-bat/zip-zap', 'Unit\Spryker\Zed\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::zipZapAction'],
+            ['index/index/index', 'Unit\Spryker\Yves\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::indexAction'],
+            ['/index/index/index', 'Unit\Spryker\Yves\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::indexAction'],
+            ['Index/Index/Index', 'Unit\Spryker\Yves\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::indexAction'],
+            ['/Index/Index/Index', 'Unit\Spryker\Yves\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::indexAction'],
+            ['foo-bar/baz-bat/zip-zap', 'Unit\Spryker\Yves\Kernel\ControllerResolver\YvesFragmentControllerResolverTest::zipZapAction'],
         ];
     }
 
