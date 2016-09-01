c6241c1fc317e55796e255304ec48fdbd19a3015
René Klatt <rene.klatt@spryker.com>
2016-09-01 07:11:11 +0000

    update composer.json add AutoloadUpdater and tests

---
+++ Bundles/Development/src/Spryker/Zed/Development/Business/Composer/Updater/AutoloadUpdater.php
@@ -0,0 +1,150 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Development\Business\Composer\Updater;
+
+use Symfony\Component\Finder\SplFileInfo;
+
+class AutoloadUpdater implements UpdaterInterface
+{
+
+    /**
+     * @var array
+     */
+    protected $testDirectoryKeys = [
+        'Acceptance',
+        'Functional',
+        'Integration',
+        'Unit',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $applications = [
+        'Client',
+        'Shared',
+        'Yves',
+        'Zed',
+    ];
+
+    /**
+     * @param array $composerJson
+     * @param \Symfony\Component\Finder\SplFileInfo $composerJsonFile
+     *
+     * @return array
+     */
+    public function update(array $composerJson, SplFileInfo $composerJsonFile)
+    {
+        $composerJson = $this->updateAutoloadForTests($composerJson, $composerJsonFile);
+
+        return $composerJson;
+    }
+
+    /**
+     * @param array $composerJson
+     * @param \Symfony\Component\Finder\SplFileInfo $composerJsonFile
+     *
+     * @return array
+     */
+    protected function updateAutoloadForTests(array $composerJson, SplFileInfo $composerJsonFile)
+    {
+        $testDirectoryKeys = $this->buildTestDirectoryKeys();
+        $bundlePath = $composerJsonFile->getRelativePathname();
+
+        foreach ($testDirectoryKeys as $testDirectoryKey) {
+            $composerJson = $this->updateAutoload($composerJson, $testDirectoryKey, $bundlePath);
+        }
+
+        return $composerJson;
+    }
+
+    /**
+     * @param array $composerJson
+     * @param string $testDirectoryKey
+     * @param string $bundlePath
+     *
+     * @return array
+     */
+    protected function updateAutoload(array $composerJson, $testDirectoryKey, $bundlePath)
+    {
+        $directoryName = $this->getDirectoryName($testDirectoryKey, $bundlePath);
+
+        if ($this->directoryExists($directoryName)) {
+            $composerJson = $this->addAutoloadDevPsr0($composerJson);
+            $testPath = $bundlePath . DIRECTORY_SEPARATOR . 'tests';
+            $composerJson['autoload-dev']['psr-0'][$testDirectoryKey] = $testPath;
+        }
+
+        if (isset($composerJson['autoload']['psr-0'][$testDirectoryKey])) {
+            unset($composerJson['autoload']['psr-0'][$testDirectoryKey]);
+        }
+
+        return $composerJson;
+    }
+
+    /**
+     * @param string $directory
+     *
+     * @return bool
+     */
+    protected function directoryExists($directory)
+    {
+        return (is_dir($directory));
+    }
+
+    /**
+     * @return array
+     */
+    protected function buildTestDirectoryKeys()
+    {
+        $testDirectoryKeys = [];
+        foreach ($this->testDirectoryKeys as $testDirectoryKey) {
+            $testDirectoryKeys[] = $testDirectoryKey;
+            foreach ($this->applications as $application) {
+                $testDirectoryKeys[] = $application . $testDirectoryKey;
+            }
+        }
+
+        return $testDirectoryKeys;
+    }
+
+    /**
+     * @param string $testDirectoryKey
+     * @param string $bundlePath
+     *
+     * @return string
+     */
+    protected function getDirectoryName($testDirectoryKey, $bundlePath)
+    {
+        $pathParts = [
+            rtrim($bundlePath, DIRECTORY_SEPARATOR),
+            'tests',
+            $testDirectoryKey,
+        ];
+
+        return implode($pathParts, DIRECTORY_SEPARATOR);
+    }
+
+    /**
+     * @param array $composerJson
+     *
+     * @return array
+     */
+    protected function addAutoloadDevPsr0(array $composerJson)
+    {
+        if (!isset($composerJson['autoload-dev'])) {
+            $composerJson['autoload-dev'] = [];
+        }
+        if (!isset($composerJson['autoload-dev']['psr-0'])) {
+            $composerJson['autoload-dev']['psr-0'] = [];
+        }
+
+        return $composerJson;
+    }
+
+}
