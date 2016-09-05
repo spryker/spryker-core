00e8b912a3e76368ad1b7cfc3969e74294526d6b
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-09-05 11:56:54 +0000

    CORE-555 Removed deprecated method

--- Bundles/Collector/src/Spryker/Zed/Collector/Business/CollectorBusinessFactory.php
+++ Bundles/Collector/src/Spryker/Zed/Collector/Business/CollectorBusinessFactory.php
@@ -50,18 +50,6 @@ class CollectorBusinessFactory extends AbstractBusinessFactory
     }
 
     /**
-     * @deprecated Use getTouchQueryContainer() instead
-     *
-     * @return \Spryker\Zed\Touch\Persistence\TouchQueryContainer
-     */
-    protected function createTouchQueryContainer()
-    {
-        trigger_error('Deprecated, use getTouchQueryContainer() instead.', E_USER_DEPRECATED);
-
-        return $this->getTouchQueryContainer();
-    }
-
-    /**
      * @return \Spryker\Zed\Touch\Persistence\TouchQueryContainerInterface
      */
     protected function getTouchQueryContainer()
