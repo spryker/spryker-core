174c40974bce755a23937008d53a28f350f653e6
Auris <aurimas.lickus@spryker.com>
2016-09-08 11:28:01 +0200

    CORE-206: cr changes

--- Bundles/Cart/src/Spryker/Zed/Cart/Business/CartBusinessFactory.php
+++ Bundles/Cart/src/Spryker/Zed/Cart/Business/CartBusinessFactory.php
@@ -13,7 +13,6 @@ use Spryker\Zed\Cart\CartDependencyProvider;
 use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 
 /**
- * @method \Spryker\Zed\Cart\CartConfig createConfig()
  * @method \Spryker\Zed\Cart\CartConfig getConfig()
  */
 class CartBusinessFactory extends AbstractBusinessFactory
