7c2ec4b911075dd0a1fc327db7f2062115247be8
mscherer <mark.scherer@spryker.com>
2016-09-13 16:18:13 +0200

    CORE-372 Fix test.

--- Bundles/Session/tests/SharedUnit/Spryker/Shared/Session/Business/Handler/SessionHandlerFileTest.php
+++ Bundles/Session/tests/SharedUnit/Spryker/Shared/Session/Business/Handler/SessionHandlerFileTest.php
@@ -132,14 +132,14 @@ class SessionHandlerFileTest extends \PHPUnit_Framework_TestCase
     /**
      * @return void
      */
-    public function testCallDestroyMustReturnFalseIfNoFileExistsForSessionId()
+    public function testCallDestroyMustReturnTrueIfNoFileExistsForSessionId()
     {
         $sessionHandlerFile = new SessionHandlerFile($this->getSavePath(), self::LIFETIME, $this->createNewRelicApiMock());
         $sessionHandlerFile->open($this->getSavePath(), self::SESSION_NAME);
 
         $result = $sessionHandlerFile->destroy(self::SESSION_ID);
 
-        $this->assertFalse($result);
+        $this->assertTrue($result);
     }
 
     /**
