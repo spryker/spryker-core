26c1971a612944701f63e421f534c4d939fc7a57
mscherer <mark.scherer@spryker.com>
2016-09-22 10:53:08 +0200

    Correct config files.

--- Bundles/Session/config/config_default-test.php
+++ Bundles/Session/config/config_default-test.php
@@ -5,4 +5,3 @@ use Spryker\Shared\Session\SessionConstants;
 $config[SessionConstants::YVES_SESSION_TIME_TO_LIVE] = SessionConstants::SESSION_LIFETIME_1_HOUR;
 
 $config[SessionConstants::ZED_SESSION_TIME_TO_LIVE] = SessionConstants::SESSION_LIFETIME_30_DAYS;
-$config[SessionConstants::ZED_SESSION_SAVE_HANDLER] = null;
