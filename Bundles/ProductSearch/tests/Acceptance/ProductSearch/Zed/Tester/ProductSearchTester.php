7c703db33cd2317e3d72ef053e03293f69dc4a65
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-09-23 11:44:30 +0200

    CORE-351 Acceptance tests reloaded

---
+++ Bundles/ProductSearch/tests/Acceptance/ProductSearch/Zed/Tester/ProductSearchTester.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Acceptance\ProductSearch\Zed\Tester;
+
+use ProductSearch\ZedAcceptanceTester;
+
+class ProductSearchTester extends ZedAcceptanceTester
+{
+}
