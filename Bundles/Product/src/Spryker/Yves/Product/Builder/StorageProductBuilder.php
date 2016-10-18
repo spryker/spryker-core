a61514eef71162e87bdbc05b85446a46f8179abd
Auris <aurimas.lickus@spryker.com>
2016-10-18 13:54:11 +0200

    move yves variant builders to core

---
+++ Bundles/Product/src/Spryker/Yves/Product/Builder/StorageProductBuilder.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Spryker\Yves\Product\Builder;
+
+use Generated\Shared\Transfer\StorageProductTransfer;
+
+class StorageProductBuilder implements StorageProductBuilderInterface
+{
+
+    /**
+     * @var \Spryker\Yves\Product\Builder\AttributeVariantBuilderInterface
+     */
+    protected $attributeVariantBuilder;
+
+    /**
+     * @var \Spryker\Yves\Product\Builder\ImageSetBuilderInterface
+     */
+    protected $imageSetBuilder;
+
+    /**
+     * @param \Spryker\Yves\Product\Builder\AttributeVariantBuilderInterface $attributeVariantBuilder
+     * @param \Spryker\Yves\Product\Builder\ImageSetBuilderInterface $imageSetBuilder
+     */
+    public function __construct(AttributeVariantBuilderInterface $attributeVariantBuilder, ImageSetBuilderInterface $imageSetBuilder)
+    {
+        $this->attributeVariantBuilder = $attributeVariantBuilder;
+        $this->imageSetBuilder = $imageSetBuilder;
+    }
+
+    /**
+     * @param array $productData
+     * @param array $selectedAttributes
+     *
+     * @return \Generated\Shared\Transfer\StorageProductTransfer
+     */
+    public function buildProduct(array $productData, array $selectedAttributes = [])
+    {
+        $storageProductTransfer = $this->mapAbstractStorageProduct($productData);
+        $storageProductTransfer->setSelectedAttributes($selectedAttributes);
+
+        $storageProductTransfer = $this->attributeVariantBuilder->setSuperAttributes($storageProductTransfer);
+        if (count($selectedAttributes) > 0) {
+            $storageProductTransfer = $this->attributeVariantBuilder->setSelectedVariants(
+                $selectedAttributes,
+                $storageProductTransfer
+            );
+        }
+
+        return $storageProductTransfer;
+    }
+
+    /**
+     * @param array $productData
+     *
+     * @return \Generated\Shared\Transfer\StorageProductTransfer
+     */
+    protected function mapAbstractStorageProduct(array $productData)
+    {
+        $storageProductTransfer = new StorageProductTransfer();
+        $storageProductTransfer->fromArray($productData, true);
+        $storageProductTransfer->setImages(
+            $this->imageSetBuilder->getDisplayImagesFromPersistedProduct($productData)
+        );
+
+        return $storageProductTransfer;
+    }
+
+}
