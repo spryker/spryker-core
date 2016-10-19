5765f8ca8bf2793d3fcf0777ed6a791f72f84749
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-10-19 15:30:46 +0200

    feature/pim: updated filenames

---
+++ Bundles/Util/src/Spryker/Zed/Util/Persistence/UtilPersistenceFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Util\Persistence;
+
+use Spryker\Zed\Kernel\Persistence\AbstractPersistenceFactory;
+
+/**
+ * @method \Spryker\Zed\Util\UtilConfig getConfig()
+ * @method \Spryker\Zed\Util\Persistence\UtilQueryContainer getQueryContainer()
+ */
+class UtilPersistenceFactory extends AbstractPersistenceFactory
+{
+
+}
