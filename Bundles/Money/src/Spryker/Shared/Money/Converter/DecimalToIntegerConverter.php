554731e026b51e383bb852f603f9259425929a31
René Klatt <rene.klatt@spryker.com>
2016-11-03 13:47:58 +0000

    use round instead of bcmul

--- Bundles/Money/src/Spryker/Shared/Money/Converter/DecimalToIntegerConverter.php
+++ Bundles/Money/src/Spryker/Shared/Money/Converter/DecimalToIntegerConverter.php
@@ -30,7 +30,7 @@ class DecimalToIntegerConverter implements DecimalToIntegerConverterInterface
             ));
         }
 
-        return (int)bcmul($value, static::PRICE_PRECISION, 0);
+        return (int)round($value * static::PRICE_PRECISION);
     }
 
 }
