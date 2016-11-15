4eb3da874190b18cf46e3a22b52e1eceac29b17b
Auris <aurimas.lickus@spryker.com>
2016-11-15 12:06:18 +0100

    Change bootstrap

--- Bundles/UtilNetwork/tests/Unit/_bootstrap.php
+++ Bundles/UtilNetwork/tests/Unit/_bootstrap.php
@@ -3,7 +3,10 @@
 /**
  * (c) Spryker Systems GmbH copyright protected
  */
-$bootstrap = Spryker\Shared\Library\SystemUnderTest\SystemUnderTestBootstrap::getInstance();
+
+use Spryker\Shared\Testify\SystemUnderTestBootstrap;
+
+$bootstrap = SystemUnderTestBootstrap::getInstance();
 
 $application = 'Zed';
 $bootstrap->bootstrap($application);
--- Bundles/UtilText/tests/_bootstrap.php
+++ Bundles/UtilText/tests/_bootstrap.php
@@ -3,5 +3,8 @@
 /**
  * (c) Spryker Systems GmbH copyright protected
  */
-$bootstrap = Spryker\Shared\Library\SystemUnderTest\SystemUnderTestBootstrap::getInstance();
+
+use Spryker\Shared\Testify\SystemUnderTestBootstrap;
+
+$bootstrap = SystemUnderTestBootstrap::getInstance();
 $bootstrap->bootstrap('Zed');
