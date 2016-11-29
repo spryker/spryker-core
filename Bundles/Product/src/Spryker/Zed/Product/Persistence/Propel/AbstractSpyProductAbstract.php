eaeebbc289df11ad53c94a1d5e1206502364ff63
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-11-29 11:17:41 +0100

    Added missing and removed unnecessary entity and query classes

--- Bundles/Product/src/Spryker/Zed/Product/Persistence/Propel/AbstractSpyProductAbstract.php
+++ Bundles/Product/src/Spryker/Zed/Product/Persistence/Propel/AbstractSpyProductAbstract.php
@@ -7,10 +7,10 @@
 
 namespace Spryker\Zed\Product\Persistence\Propel;
 
-use Orm\Zed\Product\Persistence\Base\SpyProductAttributeTypeQuery as BaseSpyProductAttributeTypeQuery;
+use Orm\Zed\Product\Persistence\Base\SpyProductAbstract as BaseSpyProductAbstract;
 
 /**
- * Skeleton subclass for performing query and update operations on the 'spy_attribute_type' table.
+ * Skeleton subclass for representing a row from the 'spy_product_abstract' table.
  *
  *
  *
@@ -18,6 +18,6 @@ use Orm\Zed\Product\Persistence\Base\SpyProductAttributeTypeQuery as BaseSpyProd
  * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
-abstract class AbstractSpyProductAttributeTypeQuery extends BaseSpyProductAttributeTypeQuery
+class AbstractSpyProductAbstract extends BaseSpyProductAbstract
 {
 }
--- Bundles/Product/src/Spryker/Zed/Product/Persistence/Propel/AbstractSpyProductAttributeKey.php
+++ Bundles/Product/src/Spryker/Zed/Product/Persistence/Propel/AbstractSpyProductAttributeKey.php
@@ -7,17 +7,18 @@
 
 namespace Spryker\Zed\Product\Persistence\Propel;
 
-use Orm\Zed\Product\Persistence\Base\SpyProductAttributeTypeValueQuery as BaseSpyProductAttributeTypeValueQuery;
+use Orm\Zed\Product\Persistence\Base\SpyProductAttributeKey as BaseSpyProductAttributeKey;
 
 /**
- * Skeleton subclass for performing query and update operations on the 'spy_attribute_type_value' table.
+ * Skeleton subclass for representing a row from the 'spy_product_attribute_key' table.
  *
  *
  *
  * You should add additional methods to this class to meet the
  * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
+ *
  */
-abstract class AbstractSpyProductAttributeTypeValueQuery extends BaseSpyProductAttributeTypeValueQuery
+class AbstractSpyProductAttributeKey extends BaseSpyProductAttributeKey
 {
 }
