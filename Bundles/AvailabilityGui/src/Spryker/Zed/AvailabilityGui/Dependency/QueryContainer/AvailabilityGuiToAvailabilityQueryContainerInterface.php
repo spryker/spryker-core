2ae673f3b2532ed6c4522f745c01f9314318d980
Auris <aurimas.lickus@spryker.com>
2017-01-09 14:49:15 +0100

    core-520: change year

--- Bundles/AvailabilityGui/src/Spryker/Zed/AvailabilityGui/Dependency/QueryContainer/AvailabilityGuiToAvailabilityQueryContainerInterface.php
+++ Bundles/AvailabilityGui/src/Spryker/Zed/AvailabilityGui/Dependency/QueryContainer/AvailabilityGuiToAvailabilityQueryContainerInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 namespace Spryker\Zed\AvailabilityGui\Dependency\QueryContainer;
