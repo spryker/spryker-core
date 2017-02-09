15838ea4d7e118dd4cc4843de4121b1d553779c0
Marco Podien <marco@lazystar.de>
2017-02-09 15:24:26 +0100

    core-901: create command to generate PageIndexMap

---
+++ Bundles/Search/src/Spryker/Zed/Search/Communication/Console/GenerateIndexMapConsole.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Search\Communication\Console;
+
+use Spryker\Zed\Console\Business\Model\Console;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @method \Spryker\Zed\Search\Business\SearchFacade getFacade()
+ */
+class GenerateIndexMapConsole extends Console
+{
+
+    const COMMAND_NAME = 'setup:index-map';
+    const DESCRIPTION = 'This command will generate the PageIndexMap without requiring the actual Elasticsearch index';
+    const EXIT_CODE_ERROR = 1;
+    const EXIT_CODE_SUCCESS = 0;
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_NAME);
+        $this->setDescription(self::DESCRIPTION);
+
+        parent::configure();
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getFacade()->generatePageIndexMap($this->getMessenger());
+    }
+
+}
