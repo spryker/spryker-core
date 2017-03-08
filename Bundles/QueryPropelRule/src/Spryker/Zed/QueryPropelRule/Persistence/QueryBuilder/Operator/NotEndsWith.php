68d71c9c19b68415ff6a68fb821456898b3fba1c
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-03-08 13:27:41 +0100

    feature/core-1020-query-propel-rule PR comments 2

--- Bundles/QueryPropelRule/src/Spryker/Zed/QueryPropelRule/Persistence/QueryBuilder/Operator/NotEndsWith.php
+++ Bundles/QueryPropelRule/src/Spryker/Zed/QueryPropelRule/Persistence/QueryBuilder/Operator/NotEndsWith.php
@@ -30,7 +30,7 @@ class NotEndsWith extends AbstractOperator
      */
     public function getValue(RuleQuerySetTransfer $rule)
     {
-        return sprintf('%%%s%%', $rule->getValue());
+        return sprintf('%s%%', $rule->getValue());
     }
 
 }
