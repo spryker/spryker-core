2f71a17d2c16a078a5598dd005e1d827e41667ff
René Klatt <rene.klatt@spryker.com>
2017-03-14 14:29:16 +0000

    core-1012 removed test file

--- Bundles/Twig/tests/Unit/Spryker/Shared/Twig/Fixtures/cache/cache.php
+++
@@ -1,5 +0,0 @@
-<?php return [
-    '@CachedBundle/Controller/index.twig' => '/data/shop/development/current/vendor/spryker/spryker/Bundles/Twig/tests/Unit/Spryker/Shared/Twig/Fixtures/cache/cached.twig',
-    '@Invalid/Controller/index.twig' => false,
-    '@Bundle/Controller/index.twig' => false,
-];
