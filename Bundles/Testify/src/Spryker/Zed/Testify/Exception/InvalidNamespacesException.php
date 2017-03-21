c583b9c29942e8c30e8d13433ed574b3b0ca32dd
René Klatt <rene.klatt@spryker.com>
2017-03-21 13:42:55 +0000

    TestifyLocator for Business tests

--- Bundles/Testify/src/Spryker/Zed/Testify/Exception/InvalidNamespacesException.php
+++ Bundles/Testify/src/Spryker/Zed/Testify/Exception/InvalidNamespacesException.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Shared\Testify\Exception;
+namespace Spryker\Zed\Testify\Exception;
 
 use Exception;
 
