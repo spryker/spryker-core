9d3a47ead3e724778d91db0883f5cf0e75c7719b
René Klatt <rene.klatt@spryker.com>
2017-03-23 15:24:37 +0000

    test

--- Bundles/Sales/tests/_support/Helper/ZedPresentation.php
+++ Bundles/Sales/tests/_support/Helper/ZedPresentation.php
@@ -15,7 +15,6 @@ class ZedPresentation extends Module
      */
     public function _before(TestCase $e)
     {
-        $this->getZedModule()->amZed();
         $this->getZedModule()->amLoggedInUser();
     }
 
