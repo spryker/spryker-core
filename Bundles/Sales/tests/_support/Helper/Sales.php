eadac95bd49c7651b34b94d4dbb164cc8c628b91
René Klatt <rene.klatt@spryker.com>
2017-03-24 08:10:13 +0000

    refactorings

--- Bundles/Sales/tests/_support/Helper/Sales.php
+++ Bundles/Sales/tests/_support/Helper/Sales.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Sales\Module;
+namespace Sales\Helper;
 
 use Codeception\Module;
 use Orm\Zed\Country\Persistence\SpyCountryQuery;
