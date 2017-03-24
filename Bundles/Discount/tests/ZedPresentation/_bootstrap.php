20c92bf35129dbc245f10a9d4583647bae9df02d
René Klatt <rene.klatt@spryker.com>
2017-03-24 10:37:21 +0000

    revert changes made in doscunt

--- Bundles/Discount/tests/ZedPresentation/_bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Spryker\Shared\Testify\SystemUnderTestBootstrap;
-
-$systemUnderTestBootstrap = new SystemUnderTestBootstrap();
-$systemUnderTestBootstrap->bootstrap(SystemUnderTestBootstrap::APPLICATION_ZED);
