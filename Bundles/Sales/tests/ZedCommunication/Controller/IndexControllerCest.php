a83d58a9e54eb12396c3e043396ad77235fa1aed
René Klatt <rene.klatt@spryker.com>
2017-03-27 09:28:13 +0000

    Added Communication test

---
+++ Bundles/Sales/tests/ZedCommunication/Controller/IndexControllerCest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace ZedCommunication\Controller;
+
+use Sales\ZedCommunicationTester;
+
+/**
+ * @group IndexControllerCest
+ */
+class IndexControllerCest
+{
+
+    /**
+     * @var \Sales\ZedCommunicationTester
+     */
+    protected $tester;
+
+    /**
+     * @param \Sales\ZedCommunicationTester $i
+     *
+     * @return void
+     */
+    public function testThatICanOpenSalesIndexPage(ZedCommunicationTester $i)
+    {
+        $i->amOnPage('/sales');
+        $i->seeResponseCodeIs(200);
+    }
+
+}
