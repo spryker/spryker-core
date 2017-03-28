2641e8635a5034da4d9fe63e42bd45a3977c6a4c
Davert <davert.php@resend.cc>
2017-03-28 12:50:09 +0200

    improved communication tests for creating a customer

---
+++ Bundles/Customer/tests/ZedCommunication/IndexCest.php
@@ -0,0 +1,86 @@
+<?php
+namespace ZedCommunication;
+
+use Customer\ZedCommunicationTester;
+use Faker\Factory;
+
+class IndexCest
+{
+
+    /**
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    /**
+     * @return void
+     */
+    public function _inject()
+    {
+        $this->faker = Factory::create();
+    }
+
+    /**
+     * @param \Customer\ZedCommunicationTester $i
+     *
+     * @return void
+     */
+    public function listCustomers(ZedCommunicationTester $i)
+    {
+        $i->amOnPage('/customer');
+        $i->seeResponseCodeIs(200);
+        $i->see('Customers', 'h5');
+    }
+
+    /**
+     * @param \Customer\ZedCommunicationTester $i
+     *
+     * @return void
+     */
+    public function addCustomer(ZedCommunicationTester $i)
+    {
+        $email = $this->faker->email;
+
+        $formData = [
+            'customer' => [
+                'email' => $email,
+                'salutation' => 'Mr',
+                'first_name' => $this->faker->firstName,
+                'last_name' => $this->faker->lastName,
+            ]
+        ];
+
+        $i->amOnPage('/customer/add');
+        $i->submitForm(['name' => 'customer'], $formData);
+
+        $i->listDataTable('/customer/index/table');
+        $i->seeInLastRow([2 => $email]);
+    }
+
+    /**
+     * @group current
+     * @param ZedCommunicationTester $i
+     */
+    public function addCustomerWithoutNameAndFail(ZedCommunicationTester $i)
+    {
+        $email = $this->faker->email;
+
+        $formData = [
+            'customer' => [
+                'email' => $email,
+                'salutation' => 'Mr',
+                'last_name' => $this->faker->lastName,
+            ]
+        ];
+
+        $i->amOnPage('/customer/add');
+        $i->submitForm(['name' => 'customer'], $formData);
+        $i->expect('I am back on the form page');
+        $i->seeCurrentUrlEquals('/customer/add');
+        $i->see('This value should not be blank.', '#customer');
+        $i->listDataTable('/customer/index/table');
+        $i->dontSeeInLastRow([2 => $email]);
+    }
+
+
+}
