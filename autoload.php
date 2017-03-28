e0eac0e2551c50c06125391737624477e7ca3b6d
René Klatt <rene.klatt@spryker.com>
2017-03-28 15:32:24 +0000

    Working autoload.php for core

--- autoload.php
+++ autoload.php
@@ -13,13 +13,26 @@ $autoloader = function ($className) {
         'Module',
     ];
 
+    $testingNamespaces = [
+        'Acceptance',
+        'Functional',
+        'Unit',
+        'ZedBusiness',
+        'ZedCommunication',
+        'ZedPresentation',
+        'Yves',
+        'Client',
+    ];
+
     $classNameParts = explode('\\', $className);
 
     if (count($classNameParts) < 3) {
         return false;
     }
 
-    if (!in_array($classNameParts[0], $namespaces) && !in_array($classNameParts[1], $codeceptionSupportDirectories)) {
+    if (!in_array($classNameParts[0], $namespaces)
+        && !in_array($classNameParts[1], $codeceptionSupportDirectories)
+        && !in_array($classNameParts[0], $testingNamespaces)) {
         return false;
     }
 
@@ -47,6 +60,31 @@ $autoloader = function ($className) {
             require $filePath;
 
             return true;
+        }
+    }
+
+    if (in_array($classNameParts[0], $testingNamespaces)) {
+        if ($classNameParts[0] === 'Acceptance') {
+            $bundle = $classNameParts[1];
+            $className = implode(DIRECTORY_SEPARATOR, $classNameParts) . '.php';
+            $filePath = __DIR__ . '/Bundles/' . $bundle . '/tests/';
+            $filePath .= $className;
+            if (file_exists($filePath)) {
+                require $filePath;
+
+                return true;
+            }
+        }
+        if (in_array($classNameParts[0], ['Functional', 'Unit'])) {
+            $bundle = $classNameParts[3];
+            $className = implode(DIRECTORY_SEPARATOR, $classNameParts) . '.php';
+            $filePath = __DIR__ . '/Bundles/' . $bundle . '/tests/';
+            $filePath .= $className;
+            if (file_exists($filePath)) {
+                require $filePath;
+
+                return true;
+            }
         }
     }
 
