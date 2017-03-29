8b0f2946f8ee014690148fef35a8c0e85acf18da
René Klatt <rene.klatt@spryker.com>
2017-03-29 15:47:29 +0000

    Add Connection helper to be used in ZedPresentation tests to save data before WebDriver is running

--- Bundles/Propel/tests/_support/Helper/Connection.php
+++ Bundles/Propel/tests/_support/Helper/Connection.php
@@ -7,20 +7,21 @@
 
 namespace Propel\Helper;
 
-use Codeception\Lib\Interfaces\DependsOnModule;
 use Codeception\Module;
 use Codeception\TestInterface;
 use Propel\Runtime\Propel;
 use Silex\Application;
 use Spryker\Zed\Propel\Communication\Plugin\ServiceProvider\PropelServiceProvider;
 
-class Transaction extends Module
+class Connection extends Module
 {
 
     /**
+     * @param \Codeception\TestInterface $test
+     *
      * @return void
      */
-    public function _initialize()
+    public function _before(TestInterface $test)
     {
         $propelServiceProvider = new PropelServiceProvider();
         $propelServiceProvider->boot(new Application());
@@ -31,31 +32,10 @@ class Transaction extends Module
      *
      * @return void
      */
-    public function _before(TestInterface $test)
-    {
-        parent::_before($test);
-
-        Propel::getWriteConnection('zed')->beginTransaction();
-    }
-
-    /**
-     * @param \Codeception\TestInterface $test
-     *
-     * @return void
-     */
     public function _after(TestInterface $test)
-    {
-        parent::_after($test);
-
-        Propel::getWriteConnection('zed')->rollBack();
-    }
-
-    /**
-     * @return void
-     */
-    public function _afterSuite()
     {
         Propel::closeConnections();
     }
+
 
 }
