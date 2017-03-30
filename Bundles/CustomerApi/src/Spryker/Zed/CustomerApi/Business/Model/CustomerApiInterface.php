fe6d8df57e8728c023a445f590ae21c09a2ab928
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-03-30 11:06:48 +0200

    feature/core-908-rest-api [ci skip] added missing interface

---
+++ Bundles/CustomerApi/src/Spryker/Zed/CustomerApi/Business/Model/CustomerApiInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\CustomerApi\Business\Model;
+
+use Generated\Shared\Transfer\ApiDataTransfer;
+use Generated\Shared\Transfer\ApiFilterTransfer;
+use Generated\Shared\Transfer\ApiRequestTransfer;
+
+interface CustomerApiInterface
+{
+
+    /**
+     * @param \Generated\Shared\Transfer\ApiDataTransfer $apiDataTransfer
+     *
+     * @return \Generated\Shared\Transfer\ApiItemTransfer
+     */
+    public function add(ApiDataTransfer $apiDataTransfer);
+
+    /**
+     * @param int $idCustomer
+     * @param \Generated\Shared\Transfer\ApiFilterTransfer $apiFilterTransfer
+     *
+     * @return \Generated\Shared\Transfer\ApiItemTransfer
+     */
+    public function get($idCustomer, ApiFilterTransfer $apiFilterTransfer);
+
+    /**
+     * @param \Generated\Shared\Transfer\ApiDataTransfer $apiDataTransfer
+     *
+     * @return \Generated\Shared\Transfer\ApiItemTransfer
+     */
+    public function update(ApiDataTransfer $apiDataTransfer);
+
+    /**
+     * @param \Generated\Shared\Transfer\ApiRequestTransfer $apiRequestTransfer
+     *
+     * @return bool
+     */
+    public function delete(ApiRequestTransfer $apiRequestTransfer);
+
+    /**
+     * @param \Generated\Shared\Transfer\ApiRequestTransfer $apiRequestTransfer
+     *
+     * @return \Generated\Shared\Transfer\ApiCollectionTransfer
+     */
+    public function find(ApiRequestTransfer $apiRequestTransfer);
+
+}
