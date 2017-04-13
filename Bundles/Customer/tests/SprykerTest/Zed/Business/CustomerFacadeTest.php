f55f85486a1b5f7496cc762efc0e6c0cef506c23
René Klatt <rene.klatt@spryker.com>
2017-04-13 07:56:55 +0000

    Customer to new structure

--- Bundles/Customer/tests/SprykerTest/Zed/Business/CustomerFacadeTest.php
+++ Bundles/Customer/tests/SprykerTest/Zed/Business/CustomerFacadeTest.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Functional\Spryker\Zed\Customer\Business;
+namespace SprykerTest\Zed\Customer\Business;
 
 use Codeception\TestCase\Test;
 use Generated\Shared\Transfer\AddressTransfer;
@@ -15,6 +15,8 @@ use Spryker\Zed\Customer\Business\CustomerFacade;
 use Spryker\Zed\Customer\CustomerDependencyProvider;
 use Spryker\Zed\Customer\Dependency\Facade\CustomerToMailInterface;
 use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\Customer\Business\Customer\Address;
+use Spryker\Zed\Customer\Business\Customer\Customer;
 
 /**
  * @group Functional
@@ -473,6 +475,164 @@ class CustomerFacadeTest extends Test
         $this->assertTrue($isSuccess);
 
         $this->customerFacade->getAddress($addressTransfer);
+    }
+
+    /**
+     * @param \Spryker\Shared\Kernel\Transfer\TransferInterface|null $transfer
+     * @param bool $hasEmail
+     *
+     * @return \Spryker\Zed\Customer\Business\CustomerFacade
+     */
+    private function getFacade(TransferInterface $transfer = null, $hasEmail = true)
+    {
+        $customerFacade = new CustomerFacade();
+        $customerFacade->setFactory($this->getFactory($transfer, $hasEmail));
+
+        return $customerFacade;
+    }
+
+    /**
+     * @param \Spryker\Shared\Kernel\Transfer\TransferInterface|null $transfer
+     * @param bool $hasEmail
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Customer\Business\CustomerBusinessFactory
+     */
+    protected function getFactory(TransferInterface $transfer = null, $hasEmail = true)
+    {
+        $factoryMock = $this->getMockBuilder(CustomerBusinessFactory::class)
+            ->getMock();
+
+        if ($transfer instanceof CustomerTransfer || $transfer === null) {
+            $factoryMock->method('createCustomer')->willReturn($this->getCustomerMock($transfer, $hasEmail));
+        }
+
+        if ($transfer instanceof AddressTransfer) {
+            $factoryMock->method('createAddress')->willReturn($this->getAddressMock($transfer));
+        }
+
+        return $factoryMock;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CustomerTransfer|null $customerTransfer
+     * @param bool $hasEmail
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Customer\Business\Customer\Customer
+     */
+    private function getCustomerMock(CustomerTransfer $customerTransfer = null, $hasEmail = true)
+    {
+        $customerMock = $this->getMockBuilder(Customer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $customerMock->method('hasEmail')->willReturn($hasEmail);
+        $customerMock->method('register')->willReturn($customerTransfer);
+        $customerMock->method('confirmRegistration')->willReturn($customerTransfer);
+        $customerMock->method('sendPasswordRestoreMail')->willReturn($customerTransfer);
+        $customerMock->method('restorePassword')->willReturn($customerTransfer);
+        $customerMock->method('get')->willReturn($customerTransfer);
+        $customerMock->method('update')->willReturn($customerTransfer);
+        $customerMock->method('updatePassword')->willReturn($customerTransfer);
+
+        return $customerMock;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\AddressTransfer|null $addressTransfer
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Customer\Business\Customer\Address
+     */
+    private function getAddressMock(AddressTransfer $addressTransfer = null)
+    {
+        $addressMock = $this->getMockBuilder(Address::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $addressMock;
+    }
+
+    /**
+     * @return void
+     */
+    public function testHasEmail()
+    {
+        $this->assertTrue($this->getFacade()->hasEmail('foo@bar.com'));
+    }
+
+    /**
+     * @return void
+     */
+//    public function testRegisterCustomer()
+//    {
+//        $customerTransfer = new CustomerTransfer();
+//        $facade = $this->getFacade($customerTransfer);
+//
+//        $this->assertSame($customerTransfer, $facade->registerCustomer($customerTransfer));
+//    }
+//
+//    /**
+//     * @return void
+//     */
+//    public function testConfirmRegistration()
+//    {
+//        $customerTransfer = new CustomerTransfer();
+//        $facade = $this->getFacade($customerTransfer);
+//
+//        $this->assertSame($customerTransfer, $facade->confirmRegistration($customerTransfer));
+//    }
+
+    /**
+     * @return void
+     */
+    public function testSendPasswordRestoreMail()
+    {
+        $customerTransfer = new CustomerTransfer();
+        $facade = $this->getFacade($customerTransfer);
+
+        $this->assertSame($customerTransfer, $facade->sendPasswordRestoreMail($customerTransfer));
+    }
+
+    /**
+     * @return void
+     */
+//    public function testRestorePassword()
+//    {
+//        $customerTransfer = new CustomerTransfer();
+//        $facade = $this->getFacade($customerTransfer);
+//
+//        $this->assertSame($customerTransfer, $facade->restorePassword($customerTransfer));
+//    }
+//
+//    /**
+//     * @return void
+//     */
+//    public function testGetCustomer()
+//    {
+//        $customerTransfer = new CustomerTransfer();
+//        $facade = $this->getFacade($customerTransfer);
+//
+//        $this->assertSame($customerTransfer, $facade->getCustomer($customerTransfer));
+//    }
+//
+//    /**
+//     * @return void
+//     */
+//    public function testUpdateCustomer()
+//    {
+//        $customerTransfer = new CustomerTransfer();
+//        $facade = $this->getFacade($customerTransfer);
+//
+//        $this->assertSame($customerTransfer, $facade->updateCustomer($customerTransfer));
+//    }
+
+    /**
+     * @return void
+     */
+    public function testUpdateCustomerPassword()
+    {
+        $customerTransfer = new CustomerTransfer();
+        $facade = $this->getFacade($customerTransfer);
+
+        $this->assertSame($customerTransfer, $facade->updateCustomerPassword($customerTransfer));
     }
 
 }
