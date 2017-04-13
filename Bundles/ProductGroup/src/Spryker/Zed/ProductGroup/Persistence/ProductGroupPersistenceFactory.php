3d2b3c945edbb163beb02b81fdffd61efd5a0950
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-04-13 11:59:39 +0200

    CORE-905 Basic structure
    Signed-off-by: Tamas Nyulas <tamas.nyulas@spryker.com>

---
+++ Bundles/ProductGroup/src/Spryker/Zed/ProductGroup/Persistence/ProductGroupPersistenceFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductGroup\Persistence;
+
+use Spryker\Zed\Kernel\Persistence\AbstractPersistenceFactory;
+
+/**
+ * @method \Spryker\Zed\ProductGroup\ProductGroupConfig getConfig()
+ */
+class ProductGroupPersistenceFactory extends AbstractPersistenceFactory
+{
+}
---
+++ Bundles/ProductGroup/src/Spryker/Zed/ProductGroup/Persistence/Propel/SpyProductAbstractGroup.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductGroup\Persistence;
+
+use Orm\Zed\ProductGroup\Persistence\Base\SpyProductAbstractGroup as BaseSpyProductAbstractGroup;
+
+abstract class AbstractSpyProductAbstractGroup extends BaseSpyProductAbstractGroup
+{
+}
