04b39d56886685e525820e9b6a212dd91b2aae24
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-04-19 22:39:42 +0200

    feature/core-1213-filesystem-filemanager removed unused dependencies

--- Bundles/FileSystem/src/Spryker/Service/FileSystem/FileSystemDependencyProvider.php
+++ Bundles/FileSystem/src/Spryker/Service/FileSystem/FileSystemDependencyProvider.php
@@ -7,39 +7,8 @@
 
 namespace Spryker\Service\FileSystem;
 
-use Spryker\Service\FileSystem\Dependency\Facade\FileSystemToLocaleBridge;
 use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
-use Spryker\Zed\Kernel\Container;
 
 class FileSystemDependencyProvider extends AbstractBundleDependencyProvider
 {
-
-    const FACADE_LOCALE = 'locale facade';
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
-    public function provideServiceLayerDependencies(Container $container)
-    {
-        $container = $this->addLocaleFacade($container);
-
-        return $container;
-    }
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
-    protected function addLocaleFacade(Container $container)
-    {
-        $container[self::FACADE_LOCALE] = function (Container $container) {
-            return new FileSystemToLocaleBridge($container->getLocator()->locale()->facade());
-        };
-
-        return $container;
-    }
-
 }
