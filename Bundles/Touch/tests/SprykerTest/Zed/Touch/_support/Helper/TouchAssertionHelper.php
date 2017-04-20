68532606d084612ad85ccd91145fc4b0ee018cc8
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-04-20 15:36:05 +0200

    CORE-905 Added TouchAssertionHelper for tests to be able to evaluate active and deleted touch events.
    Signed-off-by: Tamas Nyulas <tamas.nyulas@spryker.com>

---
+++ Bundles/Touch/tests/SprykerTest/Zed/Touch/_support/Helper/TouchAssertionHelper.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\Touch\Helper;
+
+use Codeception\Module;
+use Codeception\Util\Shared\Asserts;
+use Orm\Zed\Touch\Persistence\Map\SpyTouchTableMap;
+use SprykerTest\Shared\Testify\Helper\LocatorHelperTrait;
+
+class TouchAssertionHelper extends Module
+{
+
+    use Asserts;
+    use LocatorHelperTrait;
+
+    /**
+     * @param string $itemType
+     * @param int $itemId
+     * @param string$message|string
+     *
+     * @return void
+     */
+    public function assertTouchActive($itemType, $itemId, $message = '')
+    {
+        $touchEntity = $this->getTouchQueryContainer()
+            ->queryTouchEntry($itemType, $itemId)
+            ->findOne();
+
+        $this->assertNotNull($touchEntity, $message);
+        $this->assertSame(SpyTouchTableMap::COL_ITEM_EVENT_ACTIVE, $touchEntity->getItemEvent(), $message);
+    }
+
+    /**
+     * @param string $itemType
+     * @param int $itemId
+     * @param string$message|string
+     *
+     * @return void
+     */
+    public function assertTouchDeleted($itemType, $itemId, $message = '')
+    {
+        $touchEntity = $this->getTouchQueryContainer()
+            ->queryTouchEntry($itemType, $itemId)
+            ->findOne();
+
+        $this->assertNotNull($touchEntity, $message);
+        $this->assertSame(SpyTouchTableMap::COL_ITEM_EVENT_DELETED, $touchEntity->getItemEvent(), $message);
+    }
+
+    /**
+     * @return \Spryker\Zed\Touch\Persistence\TouchQueryContainerInterface
+     */
+    private function getTouchQueryContainer()
+    {
+        return $this->getLocator()->touch()->queryContainer();
+    }
+
+}
