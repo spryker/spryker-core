53edb6f76db9774e9c6f3b8d9a8b8bd994422f08
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-04-21 12:06:48 +0200

    feature/core-1213-filesystem-filemanager added spryker/flysystem as dependency

---
+++ Bundles/FileSystem/src/Spryker/Zed/FileSystem/Dependency/Facade/FileSystemToFlysystemBridge.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Glossary\Dependency\Facade;
+
+class FileSystemToFlysystemBridge implements GlossaryToLocaleInterface
+{
+
+    /**
+     * @var
+     */
+    protected $flysystemService;
+
+    /**
+     * @param \Spryker\Zed\Locale\Business\LocaleFacadeInterface $localeFacade
+     */
+    public function __construct($localeFacade)
+    {
+        $this->flysystemService = $localeFacade;
+    }
+
+
+}
---
+++ Bundles/FileSystem/src/Spryker/Zed/FileSystem/Dependency/Facade/FileSystemToFlysystemInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Glossary\Dependency\Facade;
+
+interface GlossaryToLocaleInterface
+{
+
+    /**
+     * @param string $localeName
+     *
+     * @throws \Spryker\Zed\Locale\Business\Exception\MissingLocaleException
+     *
+     * @return \Generated\Shared\Transfer\LocaleTransfer
+     */
+    public function getLocale($localeName);
+
+    /**
+     * @return \Generated\Shared\Transfer\LocaleTransfer
+     */
+    public function getCurrentLocale();
+
+    /**
+     * @return array
+     */
+    public function getAvailableLocales();
+
+    /**
+     * @param string $localeName
+     *
+     * @throws \Spryker\Zed\Locale\Business\Exception\LocaleExistsException
+     *
+     * @return \Generated\Shared\Transfer\LocaleTransfer
+     */
+    public function createLocale($localeName);
+
+}
