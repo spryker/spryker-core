97360474aa5553322c4e6f5f6b8ddf452a7648f1
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-04-29 00:40:34 +0200

    feature/core-1213-filesystem-filemanager added FlysystemFtpFileSystem bundle

---
+++ Bundles/FlysystemFtpFileSystem/src/Spryker/Service/FlysystemFtpFileSystem/FlysystemFtpFileSystemService.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Service\FlysystemFtpFileSystem;
+
+use Spryker\Service\Kernel\AbstractService;
+
+/**
+ * @method \Spryker\Service\FlysystemFtpFileSystem\FlysystemFtpFileSystemServiceFactory getFactory()
+ */
+class FlysystemFtpFileSystemService extends AbstractService implements FlysystemFtpFileSystemServiceInterface
+{
+}
---
+++ Bundles/FlysystemFtpFileSystem/src/Spryker/Service/FlysystemFtpFileSystem/FlysystemFtpFileSystemServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Service\FlysystemFtpFileSystem;
+
+/**
+ * @method \Spryker\Service\FlysystemFtpFileSystem\FlysystemFtpFileSystemServiceFactory getFactory()
+ */
+interface FlysystemFtpFileSystemServiceInterface
+{
+}
