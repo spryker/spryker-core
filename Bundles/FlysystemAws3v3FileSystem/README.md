d0660698dc98a34d04b8d10d3b63d2b867b2cac6
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-05-02 13:36:58 +0200

    feature/core-1213-filesystem-filemanager added FlysystemAws3v3FileSystem bundle

---
+++ Bundles/FlysystemAws3v3FileSystem/README.md
@@ -0,0 +1,11 @@
+# FlysystemAws3v3FileSystem
+
+## Installation
+
+```
+composer require spryker/flysystem-aws3-filesystem
+```
+
+## Documentation
+
+[Documentation](https://spryker.github.io)
---
+++ Bundles/FlysystemAws3v3FileSystem/src/Spryker/Service/FlysystemAws3v3FileSystem/FlysystemAws3v3FileSystemConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Service\FlysystemAws3v3FileSystem;
+
+use Spryker\Service\Kernel\AbstractBundleConfig;
+
+class FlysystemAws3v3FileSystemConfig extends AbstractBundleConfig
+{
+}
