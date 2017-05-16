f5407f3a186c6aa54e82663b57ba7fd7c9ef2cc7
mscherer <mark.scherer@spryker.com>
2017-05-16 12:33:35 +0200

    CORE-990 Add test for PaginationByHeaderFilterPostProcessor

--- Bundles/Api/tests/Functional/Spryker/Zed/Api/Business/Model/Processor/Pre/Filter/Header/PaginationByHeaderFilterPreProcessorTest.php
+++ Bundles/Api/tests/Functional/Spryker/Zed/Api/Business/Model/Processor/Pre/Filter/Header/PaginationByHeaderFilterPreProcessorTest.php
@@ -5,12 +5,15 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Functional\Spryker\Zed\Api\Business\Model\Processor\Pre\Filter\Header;
+namespace Functional\Spryker\Zed\Api\Business\Model\Processor\Post\Filter\Header;
 
 use Codeception\TestCase\Test;
 use Generated\Shared\Transfer\ApiFilterTransfer;
+use Generated\Shared\Transfer\ApiPaginationTransfer;
 use Generated\Shared\Transfer\ApiRequestTransfer;
+use Generated\Shared\Transfer\ApiResponseTransfer;
 use Spryker\Zed\Api\ApiConfig;
+use Spryker\Zed\Api\Business\Model\Processor\Post\Filter\Header\PaginationByHeaderFilterPostProcessor;
 use Spryker\Zed\Api\Business\Model\Processor\Pre\Filter\Header\PaginationByHeaderFilterPreProcessor;
 
 /**
@@ -26,7 +29,7 @@ use Spryker\Zed\Api\Business\Model\Processor\Pre\Filter\Header\PaginationByHeade
  * @group Header
  * @group PaginationByHeaderFilterPreProcessorTest
  */
-class PaginationByHeaderFilterPreProcessorTest extends Test
+class PaginationByHeaderFilterPostProcessorTest extends Test
 {
 
     /**
@@ -43,17 +46,28 @@ class PaginationByHeaderFilterPreProcessorTest extends Test
     public function testProcessWithDefaultsPageOne()
     {
         $config = new ApiConfig();
-        $processor = new PaginationByHeaderFilterPreProcessor($config);
+        $processor = new PaginationByHeaderFilterPostProcessor($config);
 
         $apiRequestTransfer = new ApiRequestTransfer();
-        $apiRequestTransfer->setFilter(new ApiFilterTransfer());
+        $apiRequestTransfer->setResource('users');
         $apiRequestTransfer->setHeaderData([
-            PaginationByHeaderFilterPreProcessor::RANGE => ['users=0-19'],
+            strtolower(PaginationByHeaderFilterPreProcessor::RANGE) => ['users=0-19'],
         ]);
 
-        $apiRequestTransferAfter = $processor->process($apiRequestTransfer);
-        $this->assertSame(20, $apiRequestTransferAfter->getFilter()->getLimit());
-        $this->assertSame(0, $apiRequestTransferAfter->getFilter()->getOffset());
+        $apiResponseTransfer = new ApiResponseTransfer();
+        $pagination = new ApiPaginationTransfer();
+        $pagination->setPage(2);
+        $pagination->setItemsPerPage(10);
+        $pagination->setTotal(200);
+        $apiResponseTransfer->setPagination($pagination);
+
+        $apiResponseTransfer = $processor->process($apiRequestTransfer, $apiResponseTransfer);
+
+        $expected = [
+            'Accept-Ranges' => 'users',
+            'Content-Range' => 'users 19-29/200',
+        ];
+        $this->assertSame($expected, $apiResponseTransfer->getHeaders());
     }
 
     /**
