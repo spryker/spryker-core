794b5378843c0051ec69556f24b6f0d2934a903a
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-05-19 16:37:23 +0200

    CORE-1169 Introduced AbstractType with locatables
    Signed-off-by: Tamas Nyulas <tamas.nyulas@spryker.com>

---
+++ Bundles/Kernel/src/Spryker/Yves/Kernel/Form/AbstractType.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Yves\Kernel\Form;
+
+use Spryker\Client\Kernel\ClassResolver\Client\ClientResolver;
+use Spryker\Yves\Kernel\ClassResolver\Factory\FactoryResolver;
+use Symfony\Component\Form\AbstractType as SymfonyAbstractType;
+
+// TODO: add test
+abstract class AbstractType extends SymfonyAbstractType
+{
+
+    /**
+     * @var \Spryker\Yves\Kernel\FactoryInterface
+     */
+    private $factory;
+
+    /**
+     * @var \Spryker\Client\Kernel\AbstractClient
+     */
+    private $client;
+
+    /**
+     * @return \Spryker\Yves\Kernel\FactoryInterface
+     */
+    protected function getFactory()
+    {
+        if ($this->factory === null) {
+            $this->factory = $this->resolveFactory();
+        }
+
+        return $this->factory;
+    }
+
+    /**
+     * @return \Spryker\Yves\Kernel\AbstractFactory
+     */
+    private function resolveFactory()
+    {
+        return $this->getFactoryResolver()->resolve($this);
+    }
+
+    /**
+     * @return \Spryker\Yves\Kernel\ClassResolver\Factory\FactoryResolver
+     */
+    private function getFactoryResolver()
+    {
+        return new FactoryResolver();
+    }
+
+    /**
+     * @return \Spryker\Client\Kernel\AbstractClient
+     */
+    protected function getClient()
+    {
+        if ($this->client === null) {
+            $this->client = $this->resolveClient();
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * @return \Spryker\Client\Kernel\AbstractClient
+     */
+    private function resolveClient()
+    {
+        return $this->getClientResolver()->resolve($this);
+    }
+
+    /**
+     * @return \Spryker\Client\Kernel\ClassResolver\Client\ClientResolver
+     */
+    private function getClientResolver()
+    {
+        return new ClientResolver();
+    }
+
+}
