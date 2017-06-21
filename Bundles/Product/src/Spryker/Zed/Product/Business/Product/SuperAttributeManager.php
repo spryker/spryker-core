7455d8ae4beef669e69b118c16a93dbd046adb7d
Lucas Manzke <lmanzke@outlook.com>
2017-06-21 21:45:15 +0200

    CORE-783 Implement saving of the correct image

--- Bundles/Product/src/Spryker/Zed/Product/Business/Product/SuperAttributeManager.php
+++ Bundles/Product/src/Spryker/Zed/Product/Business/Product/SuperAttributeManager.php
@@ -74,11 +74,20 @@ class SuperAttributeManager
     /**
      * @param \Generated\Shared\Transfer\ItemTransfer $itemTransfer
      *
-     * @return string
+     * @return string|null
      */
     protected function determineImage(ItemTransfer $itemTransfer)
     {
-        return 'https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017';
+        $images = $itemTransfer->getImages();
+        if (!$images) {
+            return null;
+        }
+
+        if (count($images) === 0) {
+            return null;
+        }
+
+        return $images[0]->getExternalUrlSmall();
     }
 
     /**
