1df4076e935e07bfbb5d011801948f0f3b74427e
Lucas Manzke <lmanzke@outlook.com>
2017-06-21 21:46:00 +0200

    CORE-783 Add product grouping to simplify rendering of product bundles alongside unbundled products

--- Bundles/ProductBundle/src/Spryker/Zed/ProductBundle/Business/ProductBundle/Sales/ProductBundlesSalesOrderHydrate.php
+++ Bundles/ProductBundle/src/Spryker/Zed/ProductBundle/Business/ProductBundle/Sales/ProductBundlesSalesOrderHydrate.php
@@ -9,6 +9,7 @@ namespace Spryker\Zed\ProductBundle\Business\ProductBundle\Sales;
 
 use ArrayObject;
 use Generated\Shared\Transfer\OrderTransfer;
+use Generated\Shared\Transfer\ProductGroupTransfer;
 use Spryker\Zed\ProductBundle\Business\ProductBundle\Calculation\ProductBundlePriceCalculationInterface;
 use Spryker\Zed\ProductBundle\Dependency\QueryContainer\ProductBundleToSalesQueryContainerInterface;
 
@@ -44,6 +45,35 @@ class ProductBundlesSalesOrderHydrate implements ProductBundlesSalesOrderHydrate
      */
     public function hydrate(OrderTransfer $orderTransfer)
     {
+        $bundledProducts = $this->getBundledProducts($orderTransfer);
+        $orderTransfer->setBundleItems(new ArrayObject($bundledProducts));
+
+        $itemGroups = $this->getItemGroups($orderTransfer);
+        $orderTransfer->setItemGroups(new ArrayObject($itemGroups));
+
+        return $orderTransfer;
+    }
+
+    /**
+     * @param int $idSalesOrder
+     *
+     * @return \Orm\Zed\Sales\Persistence\SpySalesOrderItem[]|\Propel\Runtime\Collection\ObjectCollection
+     */
+    protected function findOrderItemsByIdSalesOrder($idSalesOrder)
+    {
+        return $this->salesQueryContainer
+            ->querySalesOrderItem()
+            ->orderBySku()
+            ->findByFkSalesOrder($idSalesOrder);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     *
+     * @return array
+     */
+    protected function getBundledProducts(OrderTransfer $orderTransfer)
+    {
         $orderItems = $this->findOrderItemsByIdSalesOrder($orderTransfer->getIdSalesOrder());
 
         $bundledProducts = [];
@@ -59,21 +89,99 @@ class ProductBundlesSalesOrderHydrate implements ProductBundlesSalesOrderHydrate
             );
         }
 
-        $orderTransfer->setBundleItems(new ArrayObject($bundledProducts));
-
-        return $orderTransfer;
+        return $bundledProducts;
     }
 
     /**
-     * @param int $idSalesOrder
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
      *
-     * @return \Orm\Zed\Sales\Persistence\SpySalesOrderItem[]|\Propel\Runtime\Collection\ObjectCollection
+     * @return \Generated\Shared\Transfer\ProductGroupTransfer[]
      */
-    protected function findOrderItemsByIdSalesOrder($idSalesOrder)
+    protected function getItemGroups(OrderTransfer $orderTransfer)
     {
-        return $this->salesQueryContainer
-            ->querySalesOrderItem()
-            ->findByFkSalesOrder($idSalesOrder);
+        $bundleGroups = $this->getBundleGroups($orderTransfer);
+        $singleItemGroups = $this->getSingleItemGroups($orderTransfer);
+
+        return $this->mergeGroups($bundleGroups, $singleItemGroups);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductGroupTransfer[]
+     */
+    protected function getBundleGroups(OrderTransfer $orderTransfer)
+    {
+        $bundleItems = $orderTransfer->getBundleItems();
+        $items = $orderTransfer->getItems();
+
+        $result = [];
+
+        foreach ($bundleItems as $bundleId => $bundleGroupItem) {
+            $associatedItems = [];
+            $productGroup = new ProductGroupTransfer();
+            $rowCount = 0;
+            foreach ($items as $itemTransfer) {
+                if ((int)$itemTransfer->getRelatedBundleItemIdentifier() === $bundleId) {
+                    $associatedItems[] = $itemTransfer;
+                    $rowCount += count($itemTransfer->getProductOptions()) + 1;
+                }
+            }
+
+            $productGroup->setGroupImage($bundleGroupItem->getMetadata()->getImage());
+            $productGroup->setBundleItem($bundleGroupItem);
+            $productGroup->setGroupItems(new ArrayObject($associatedItems));
+            $productGroup->setRowCount($rowCount);
+            $productGroup->setIsBundle(true);
+
+            $result[] = $productGroup;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductGroupTransfer[]
+     */
+    protected function getSingleItemGroups(OrderTransfer $orderTransfer)
+    {
+        $items = $orderTransfer->getItems();
+
+        $singleItems = [];
+        foreach ($items as $itemTransfer) {
+            if ($itemTransfer->getRelatedBundleItemIdentifier() === null) {
+                $singleItems[] = $itemTransfer;
+            }
+        }
+
+        $result = [];
+
+        foreach ($singleItems as $singleItem) {
+            $productGroup = new ProductGroupTransfer();
+            $productGroup->setGroupItems(new ArrayObject([$singleItem]));
+            $productGroup->setIsBundle(false);
+
+            $result[] = $productGroup;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $bundleGroups
+     * @param array $singleItemGroups
+     *
+     * @return array
+     */
+    protected function mergeGroups(array $bundleGroups, array $singleItemGroups)
+    {
+        foreach ($singleItemGroups as $singleItemGroup) {
+            $bundleGroups[] = $singleItemGroup;
+        }
+
+        return $bundleGroups;
     }
 
 }
