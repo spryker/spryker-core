728a1dddc023e66d828fd87d16e9cd8deac9195b
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-07-10 14:34:14 +0200

    bugfix/core-1378-attributes-translation-performance removed obsolete classes, business layer cleanups

--- Bundles/ProductAttributeGui/src/Spryker/Zed/ProductAttributeGui/Business/ProductAttributeGuiBusinessFactory.php
+++ Bundles/ProductAttributeGui/src/Spryker/Zed/ProductAttributeGui/Business/ProductAttributeGuiBusinessFactory.php
@@ -8,82 +8,10 @@
 namespace Spryker\Zed\ProductAttributeGui\Business;
 
 use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
-use Spryker\Zed\ProductAttributeGui\Business\Model\AttributeReader;
-use Spryker\Zed\ProductAttributeGui\Business\Model\AttributeWriter;
-use Spryker\Zed\ProductAttributeGui\Business\Model\ProductAttributeManager;
-use Spryker\Zed\ProductAttributeGui\ProductAttributeGuiDependencyProvider;
 
 /**
  * @method \Spryker\Zed\ProductAttributeGui\ProductAttributeGuiConfig getConfig()
  */
 class ProductAttributeGuiBusinessFactory extends AbstractBusinessFactory
 {
-
-    /**
-     * @return \Spryker\Zed\ProductAttributeGui\Business\Model\ProductAttributeManagerInterface
-     */
-    public function createProductAttributeManager()
-    {
-        return new ProductAttributeManager(
-            $this->createAttributeReader(),
-            $this->createAttributeWriter()
-        );
-    }
-
-    /**
-     * @return \Spryker\Zed\ProductAttributeGui\Business\Model\AttributeReaderInterface
-     */
-    public function createAttributeReader()
-    {
-        return new AttributeReader(
-            $this->getProductQueryContainer(),
-            $this->getProductManagementQueryContainer(),
-            $this->getEncodingService()
-        );
-    }
-
-    /**
-     * @return \Spryker\Zed\ProductAttributeGui\Business\Model\AttributeWriterInterface
-     */
-    public function createAttributeWriter()
-    {
-        return new AttributeWriter(
-            $this->createAttributeReader(),
-            $this->getLocaleFacade(),
-            $this->getProductQueryContainer()
-        );
-    }
-
-    /**
-     * @return \Spryker\Zed\Product\Persistence\ProductQueryContainerInterface
-     */
-    protected function getProductQueryContainer()
-    {
-        return $this->getProvidedDependency(ProductAttributeGuiDependencyProvider::QUERY_CONTAINER_PRODUCT);
-    }
-
-    /**
-     * @return \Spryker\Zed\ProductManagement\Persistence\ProductManagementQueryContainerInterface
-     */
-    protected function getProductManagementQueryContainer()
-    {
-        return $this->getProvidedDependency(ProductAttributeGuiDependencyProvider::QUERY_CONTAINER_PRODUCT_MANAGEMENT);
-    }
-
-    /**
-     * @return \Spryker\Zed\ProductAttributeGui\Dependency\Facade\ProductAttributeGuiToLocaleInterface
-     */
-    protected function getLocaleFacade()
-    {
-        return $this->getProvidedDependency(ProductAttributeGuiDependencyProvider::FACADE_LOCALE);
-    }
-
-    /**
-     * @return \Spryker\Zed\ProductAttributeGui\Dependency\Service\ProductAttributeGuiToUtilEncodingInterface
-     */
-    protected function getEncodingService()
-    {
-        return $this->getProvidedDependency(ProductAttributeGuiDependencyProvider::SERVICE_UTIL_ENCODING);
-    }
-
 }
