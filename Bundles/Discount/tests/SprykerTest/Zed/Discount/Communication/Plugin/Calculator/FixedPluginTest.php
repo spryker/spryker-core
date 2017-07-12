293497911dc464a5d71fc3a929a2bc4a49580a12
Denis Turkov <denis.turkov@spryker.com>
2017-07-12 10:05:39 +0000

    CORE-1335: Compare floats

--- Bundles/Discount/tests/SprykerTest/Zed/Discount/Communication/Plugin/Calculator/FixedPluginTest.php
+++ Bundles/Discount/tests/SprykerTest/Zed/Discount/Communication/Plugin/Calculator/FixedPluginTest.php
@@ -44,9 +44,8 @@ class FixedPluginTest extends Test
         $plugin = new FixedPlugin();
 
         $result = $plugin->transformFromPersistence(1113);
-        $result = round($result, 2);
 
-        $this->assertSame(11.13, $result);
+        $this->assertEquals(11.13, $result, '', 0.001);
     }
 
     /**
