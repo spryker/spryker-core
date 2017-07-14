ba91be445de20ac7553b286b7cf0b22ddf667be5
Denis Turkov <denis.turkov@spryker.com>
2017-07-14 16:02:06 +0000

    CORE-773: Remove int[] definition

--- Bundles/Transfer/src/Spryker/Zed/Transfer/Business/Model/Generator/ClassDefinition.php
+++ Bundles/Transfer/src/Spryker/Zed/Transfer/Business/Model/Generator/ClassDefinition.php
@@ -174,11 +174,7 @@ class ClassDefinition implements ClassDefinitionInterface
             $property['propertyConst'] = $this->getPropertyConstantName($property);
             $property['name_underscore'] = mb_strtolower($property['propertyConst']);
 
-            if (preg_match('/^int|^integer|^float|^string|^array|^\[\]|^bool|^boolean/', $property['type'])) {
-                if (preg_match('/\[\]$/', $property['type'])) {
-                    $property['is_collection'] = true;
-                }
-            } else {
+            if (!preg_match('/^int|^integer|^float|^string|^array|^\[\]|^bool|^boolean/', $property['type'])) {
                 $property['is_transfer'] = true;
                 $property[self::TYPE_FULLY_QUALIFIED] = 'Generated\\Shared\\Transfer\\';
                 if (preg_match('/\[\]$/', $property['type'])) {
