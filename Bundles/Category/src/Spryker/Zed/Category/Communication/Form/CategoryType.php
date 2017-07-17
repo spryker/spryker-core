2279abc2384e9de8593022de824a801e75983633
Denis Turkov <denis.turkov@spryker.com>
2017-07-17 12:21:08 +0000

    CORE-1559: Fix

--- Bundles/Category/src/Spryker/Zed/Category/Communication/Form/CategoryType.php
+++ Bundles/Category/src/Spryker/Zed/Category/Communication/Form/CategoryType.php
@@ -11,6 +11,7 @@ use ArrayObject;
 use Generated\Shared\Transfer\CategoryTransfer;
 use Spryker\Zed\Category\Persistence\CategoryQueryContainerInterface;
 use Spryker\Zed\Gui\Communication\Form\Type\Select2ComboBoxType;
+use Spryker\Zed\PropelOrm\Business\Runtime\ActiveQuery\Criteria;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -98,7 +99,7 @@ class CategoryType extends AbstractType
                             if ($data instanceof CategoryTransfer) {
                                 $exists = $categoryQueryContainer
                                         ->queryCategoryByKey($key)
-                                        ->filterByIdCategory($data->getIdCategory(), '<>')
+                                        ->filterByIdCategory($data->getIdCategory(), Criteria::NOT_EQUAL)
                                         ->count() > 0;
                             }
 
