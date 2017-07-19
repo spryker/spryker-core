e21bef7add0028da64dda4958616bd8e1368a500
René Klatt <rene.klatt@spryker.com>
2017-07-19 07:04:28 +0000

    core-1251 Added config option to define root path of import files

--- Bundles/DataImport/src/Spryker/Zed/DataImport/DataImportConfig.php
+++ Bundles/DataImport/src/Spryker/Zed/DataImport/DataImportConfig.php
@@ -7,8 +7,18 @@
 
 namespace Spryker\Zed\DataImport;
 
+use Spryker\Shared\DataImport\DataImportConstants;
 use Spryker\Zed\Kernel\AbstractBundleConfig;
 
 class DataImportConfig extends AbstractBundleConfig
 {
+
+    /**
+     * @return string
+     */
+    public function getDataImportRootPath()
+    {
+        return rtrim($this->get(DataImportConstants::IMPORT_FILE_ROOT_PATH), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+    }
+
 }
