37115253e97b29e0c02eff1ea155b5633cfa364c
René Klatt <rene.klatt@spryker.com>
2017-08-01 13:21:36 +0000

    core-1652 refactored Oms tests

---
+++ Bundles/Oms/tests/SprykerTest/Zed/Oms/Business/OrderStateMachine/BuilderTest.php
@@ -0,0 +1,196 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\Oms\Business\OrderStateMachine;
+
+use PHPUnit_Framework_TestCase;
+use Spryker\Zed\Oms\Business\Exception\StatemachineException;
+use Spryker\Zed\Oms\Business\OrderStateMachine\Builder;
+use Spryker\Zed\Oms\Business\Process\EventInterface;
+use Spryker\Zed\Oms\Business\Process\Process;
+use Spryker\Zed\Oms\Business\Process\ProcessInterface;
+use Spryker\Zed\Oms\Business\Process\StateInterface;
+use Spryker\Zed\Oms\Business\Process\TransitionInterface;
+use Spryker\Zed\Oms\Business\Util\DrawerInterface;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Zed
+ * @group Oms
+ * @group Business
+ * @group OrderStateMachine
+ * @group BuilderTest
+ * Add your own group annotations below this line
+ */
+class BuilderTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @return void
+     */
+    public function tearDown()
+    {
+        $processACopyTarget = $this->getProcessLocationB() . DIRECTORY_SEPARATOR . 'process-a.xml';
+        if (file_exists($processACopyTarget)) {
+            unlink($processACopyTarget);
+        }
+    }
+
+    /**
+     * This test can be removed when optional argument `$processDefinitionLocation` is mandatory
+     *
+     * @return void
+     */
+    public function testInstantiationWithoutXmlFolder()
+    {
+        $eventMock = $this->getEventMock();
+        $stateMock = $this->getStateMock();
+        $transitionMock = $this->getTransitionMock();
+        $process = $this->getProcess();
+        $builder = new Builder($eventMock, $stateMock, $transitionMock, $process, null);
+
+        $this->assertInstanceOf(Builder::class, $builder);
+    }
+
+    /**
+     * @return void
+     */
+    public function testInstantiationWithXmlFolderAsString()
+    {
+        $eventMock = $this->getEventMock();
+        $stateMock = $this->getStateMock();
+        $transitionMock = $this->getTransitionMock();
+        $process = $this->getProcess();
+        $builder = new Builder($eventMock, $stateMock, $transitionMock, $process, '');
+
+        $this->assertInstanceOf(Builder::class, $builder);
+    }
+
+    /**
+     * @return void
+     */
+    public function testInstantiationWithXmlFolderAsArray()
+    {
+        $eventMock = $this->getEventMock();
+        $stateMock = $this->getStateMock();
+        $transitionMock = $this->getTransitionMock();
+        $process = $this->getProcess();
+        $builder = new Builder($eventMock, $stateMock, $transitionMock, $process, []);
+
+        $this->assertInstanceOf(Builder::class, $builder);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetProcessShouldThrowExceptionWhenProcessFoundInMoreThenOneLocation()
+    {
+        $eventMock = $this->getEventMock();
+        $stateMock = $this->getStateMock();
+        $transitionMock = $this->getTransitionMock();
+        $process = $this->getProcess();
+        $builder = new Builder($eventMock, $stateMock, $transitionMock, $process, [$this->getProcessLocationA(), $this->getProcessLocationB()]);
+
+        $processACopyTarget = $this->getProcessLocationB() . DIRECTORY_SEPARATOR . 'process-a.xml';
+        copy($this->getProcessLocationA() . DIRECTORY_SEPARATOR . 'process-a.xml', $processACopyTarget);
+        $this->expectException(StatemachineException::class);
+        $this->expectExceptionMessage('"process-a.xml" found in more then one location. Could not determine which one to choose. Please check your process definition location');
+        $builder->createProcess('process-a');
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetProcessShouldThrowExceptionWhenNoProcessFound()
+    {
+        $eventMock = $this->getEventMock();
+        $stateMock = $this->getStateMock();
+        $transitionMock = $this->getTransitionMock();
+        $process = $this->getProcess();
+        $builder = new Builder($eventMock, $stateMock, $transitionMock, $process, [$this->getProcessLocationB()]);
+
+        $this->expectException(StatemachineException::class);
+        $this->expectExceptionMessage('Could not find "process-a.xml". Please check your process definition location');
+        $builder->createProcess('process-a');
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetProcess()
+    {
+        $eventMock = $this->getEventMock();
+        $stateMock = $this->getStateMock();
+        $transitionMock = $this->getTransitionMock();
+        $process = $this->getProcess();
+
+        $builder = new Builder($eventMock, $stateMock, $transitionMock, $process, [$this->getProcessLocationA(), $this->getProcessLocationB()]);
+
+        $result = $builder->createProcess('process-a');
+        $this->assertInstanceOf(ProcessInterface::class, $result);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Oms\Business\Process\EventInterface
+     */
+    private function getEventMock()
+    {
+        return $this->getMockBuilder(EventInterface::class)->getMock();
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Oms\Business\Process\StateInterface
+     */
+    private function getStateMock()
+    {
+        return $this->getMockBuilder(StateInterface::class)->getMock();
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Oms\Business\Process\TransitionInterface
+     */
+    private function getTransitionMock()
+    {
+        return $this->getMockBuilder(TransitionInterface::class)->getMock();
+    }
+
+    /**
+     * @return \Spryker\Zed\Oms\Business\Process\ProcessInterface
+     */
+    private function getProcess()
+    {
+        $drawerMock = $this->getDrawerMock();
+
+        return new Process($drawerMock);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Oms\Business\Util\DrawerInterface
+     */
+    private function getDrawerMock()
+    {
+        return $this->getMockBuilder(DrawerInterface::class)->getMock();
+    }
+
+    /**
+     * @return string
+     */
+    private function getProcessLocationA()
+    {
+        return __DIR__ . '/Builder/Fixtures/DefinitionLocationA';
+    }
+
+    /**
+     * @return string
+     */
+    private function getProcessLocationB()
+    {
+        return __DIR__ . '/Builder/Fixtures/DefinitionLocationB';
+    }
+
+}
---
+++ Bundles/Oms/tests/SprykerTest/Zed/Oms/Business/Util/DrawerTest.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\Oms\Business\Util;
+
+use PHPUnit_Framework_TestCase;
+use ReflectionClass;
+use Spryker\Shared\Graph\GraphInterface;
+use Spryker\Zed\Oms\Business\Util\Drawer;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Command\CommandCollection;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Command\CommandCollectionInterface;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Command\CommandInterface;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Condition\ConditionCollection;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Condition\ConditionCollectionInterface;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Condition\ConditionInterface;
+use Spryker\Zed\Oms\Dependency\Service\OmsToUtilTextInterface;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Zed
+ * @group Oms
+ * @group Business
+ * @group Util
+ * @group DrawerTest
+ * Add your own group annotations below this line
+ */
+class DrawerTest extends PHPUnit_Framework_TestCase
+{
+
+    const CONDITION_NAME = 'conditionName';
+    const COMMAND_NAME = 'commandName';
+
+    /**
+     * @return void
+     */
+    public function testInstantiationConditionsArrayShouldConvertedToCollection()
+    {
+        $drawer = new Drawer(
+            [],
+            [self::CONDITION_NAME => $this->getConditionMock()],
+            $this->getGraphMock(),
+            $this->getOmsToUtilTextServiceMock()
+        );
+        $reflection = new ReflectionClass(Drawer::class);
+        $reflectionProperty = $reflection->getProperty('conditions');
+        $reflectionProperty->setAccessible(true);
+        $conditions = $reflectionProperty->getValue($drawer);
+
+        $this->assertInstanceOf(ConditionCollectionInterface::class, $conditions);
+        $this->assertInstanceOf(ConditionInterface::class, $conditions->get(self::CONDITION_NAME));
+    }
+
+    /**
+     * @return void
+     */
+    public function testInstantiationWithConditionCollection()
+    {
+        $conditionCollection = new ConditionCollection();
+        $conditionCollection->add($this->getConditionMock(), self::CONDITION_NAME);
+
+        $drawer = new Drawer(
+            [],
+            $conditionCollection,
+            $this->getGraphMock(),
+            $this->getOmsToUtilTextServiceMock()
+        );
+        $reflection = new ReflectionClass(Drawer::class);
+        $reflectionProperty = $reflection->getProperty('conditions');
+        $reflectionProperty->setAccessible(true);
+        $conditions = $reflectionProperty->getValue($drawer);
+
+        $this->assertInstanceOf(ConditionCollectionInterface::class, $conditions);
+        $this->assertInstanceOf(ConditionInterface::class, $conditions->get(self::CONDITION_NAME));
+    }
+
+    /**
+     * @return void
+     */
+    public function testInstantiationCommandsArrayShouldConvertedToCollection()
+    {
+        $drawer = new Drawer(
+            [self::COMMAND_NAME => $this->getCommandMock()],
+            [],
+            $this->getGraphMock(),
+            $this->getOmsToUtilTextServiceMock()
+        );
+        $reflection = new ReflectionClass(Drawer::class);
+        $reflectionProperty = $reflection->getProperty('commands');
+        $reflectionProperty->setAccessible(true);
+        $commands = $reflectionProperty->getValue($drawer);
+
+        $this->assertInstanceOf(CommandCollectionInterface::class, $commands);
+        $this->assertInstanceOf(CommandInterface::class, $commands->get(self::COMMAND_NAME));
+    }
+
+    /**
+     * @return void
+     */
+    public function testInstantiationWithCommandCollection()
+    {
+        $commandCollection = new CommandCollection();
+        $commandCollection->add($this->getCommandMock(), self::COMMAND_NAME);
+
+        $drawer = new Drawer(
+            $commandCollection,
+            [],
+            $this->getGraphMock(),
+            $this->getOmsToUtilTextServiceMock()
+        );
+        $reflection = new ReflectionClass(Drawer::class);
+        $reflectionProperty = $reflection->getProperty('commands');
+        $reflectionProperty->setAccessible(true);
+        $commands = $reflectionProperty->getValue($drawer);
+
+        $this->assertInstanceOf(CommandCollectionInterface::class, $commands);
+        $this->assertInstanceOf(CommandInterface::class, $commands->get(self::COMMAND_NAME));
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Oms\Communication\Plugin\Oms\Condition\ConditionInterface
+     */
+    private function getConditionMock()
+    {
+        return $this->getMockBuilder(ConditionInterface::class)->getMock();
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Oms\Dependency\Plugin\Command\CommandInterface
+     */
+    private function getCommandMock()
+    {
+        return $this->getMockBuilder(CommandInterface::class)->getMock();
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Shared\Graph\GraphInterface
+     */
+    private function getGraphMock()
+    {
+        return $this->getMockBuilder(GraphInterface::class)->getMock();
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Spryker\Zed\Oms\Dependency\Service\OmsToUtilTextInterface
+     */
+    private function getOmsToUtilTextServiceMock()
+    {
+        return $this->getMockBuilder(OmsToUtilTextInterface::class)->getMock();
+    }
+
+}
