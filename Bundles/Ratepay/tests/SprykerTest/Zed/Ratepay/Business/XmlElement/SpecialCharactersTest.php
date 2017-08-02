aaa30283d136edf08070602bcbea0ddaf9e5dec1
René Klatt <rene.klatt@spryker.com>
2017-08-02 06:47:35 +0000

    core-1652 refactored Ratepay tests

---
+++ Bundles/Ratepay/tests/SprykerTest/Zed/Ratepay/Business/XmlElement/SpecialCharactersTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\Ratepay\Business\XmlElement;
+
+use Spryker\Zed\Ratepay\Business\Api\SimpleXMLElement;
+
+class SpecialCharactersTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var array
+     */
+    protected $characters = [
+        "–" => "-",
+        "´" => "'",
+        "‹" => "<",
+        "›" => ">",
+        "‘" => "'",
+        "’" => "'",
+        "‚" => ",",
+        "“" => '"',
+        "”" => '"',
+        "„" => '"',
+        "‟" => '"',
+        "•" => "-",
+        "‒" => "-",
+        "―" => "-",
+        "—" => "-",
+        "™" => "TM",
+        "¼" => "1/4",
+        "½" => "1/2",
+        "¾" => "3/4"
+    ];
+
+    /**
+     * @return void
+     */
+    public function testSpecialCharacters()
+    {
+        $simpleXmlElement = new SimpleXMLElement('<root></root>');
+        foreach ($this->characters as $character => $expected) {
+            $this->assertEquals($expected, (string)$simpleXmlElement->addCDataChild('test', $character));
+        }
+    }
+
+}
