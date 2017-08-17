60399a7fa902086ee87c59b07d4df928edf83a15
Denis Turkov <denis.turkov@spryker.com>
2017-08-17 13:39:03 +0000

    CORE-656: Add facade test for category removal

--- Bundles/Category/tests/SprykerTest/Zed/Category/Business/CategoryFacadeTest.php
+++ Bundles/Category/tests/SprykerTest/Zed/Category/Business/CategoryFacadeTest.php
@@ -7,8 +7,10 @@
 
 namespace SprykerTest\Zed\Category\Business;
 
+use ArrayObject;
 use Codeception\Test\Unit;
 use Generated\Shared\Transfer\CategoryTransfer;
+use Generated\Shared\Transfer\NodeTransfer;
 use Orm\Zed\Category\Persistence\SpyCategoryNodeQuery;
 use Spryker\Zed\Category\Business\CategoryFacade;
 
@@ -25,12 +27,14 @@ use Spryker\Zed\Category\Business\CategoryFacade;
 class CategoryFacadeTest extends Unit
 {
 
+    const CATEGORY_NODE_ID_ROOT = 1;
+
     /**
      * @return void
      */
     public function testReadWithRootCategoryReturnsCategoryTransfer()
     {
-        $categoryFacade = new CategoryFacade();
+        $categoryFacade = $this->createCategoryFacade();
 
         $this->assertInstanceOf(CategoryTransfer::class, $categoryFacade->read($this->getRootCategoryId()));
     }
@@ -40,9 +44,61 @@ class CategoryFacadeTest extends Unit
      */
     public function testReadWithNonRootCategoryReturnsCategoryTransfer()
     {
-        $categoryFacade = new CategoryFacade();
+        $categoryFacade = $this->createCategoryFacade();
 
         $this->assertInstanceOf(CategoryTransfer::class, $categoryFacade->read($this->getNonRootCategoryId()));
+    }
+
+    /**
+     * @return void
+     */
+    public function testDeleteNodeById()
+    {
+        $categoryFacade = $this->createCategoryFacade();
+        $rootCategoryNodeTransfer = $categoryFacade->getNodeById(static::CATEGORY_NODE_ID_ROOT);
+
+        //create initial category (inside root)
+        $categoryTransfer1 = (new CategoryTransfer())
+            ->fromArray([
+                'categoryKey' => 'Category 1',
+                'categoryNode' => new NodeTransfer(),
+                'parentCategoryNode' => $rootCategoryNodeTransfer,
+            ]);
+        $categoryFacade->create($categoryTransfer1);
+
+        //create a child to the initial category
+        $categoryTransfer2 = (new CategoryTransfer())
+            ->fromArray([
+                'categoryKey' => 'Category 2',
+                'categoryNode' => new NodeTransfer(),
+                'parentCategoryNode' => $categoryTransfer1->getCategoryNode(),
+            ]);
+        $categoryFacade->create($categoryTransfer2);
+
+        //create a control child to the child of initial
+        $categoryTransfer3 = (new CategoryTransfer())
+            ->fromArray([
+                'categoryKey' => 'Category 3',
+                'categoryNode' => new NodeTransfer(),
+                'parentCategoryNode' => $categoryTransfer2->getCategoryNode(),
+            ]);
+        $categoryFacade->create($categoryTransfer3);
+
+        //add extra parent to initial node (make c1 enveloped into c1 through c2)
+        $categoryTransfer1->setExtraParents(new ArrayObject([
+            $categoryTransfer2->getCategoryNode()
+        ]));
+        $categoryFacade->update($categoryTransfer1);
+
+        //test on delete
+        $categoryFacade->delete($categoryTransfer2->getIdCategory());
+
+        $resultNodes = $this->getCategoryNodeQuery()
+            ->filterByFkParentCategoryNode($categoryTransfer1->getCategoryNode()->getIdCategoryNode())
+            ->find();
+
+        $this->assertEquals(1, $resultNodes->count());
+        $this->assertEquals($categoryTransfer3->getCategoryNode()->getIdCategoryNode(), $resultNodes->getFirst()->getIdCategoryNode());
     }
 
     /**
@@ -67,6 +123,14 @@ class CategoryFacadeTest extends Unit
     protected function getCategoryNodeQuery()
     {
         return SpyCategoryNodeQuery::create();
+    }
+
+    /**
+     * @return \Spryker\Zed\Category\Business\CategoryFacadeInterface
+     */
+    protected function createCategoryFacade()
+    {
+        return new CategoryFacade();
     }
 
 }
