6115cb04784025d09048537b669d9f8213fc7cf2
Denis Turkov <denis.turkov@spryker.com>
2017-08-28 15:50:26 +0000

    CORE-1872: Update bridge signature

--- Bundles/CustomerGroup/src/Spryker/Zed/CustomerGroup/Dependency/QueryContainer/CustomerGroupToCustomerQueryContainerBridge.php
+++ Bundles/CustomerGroup/src/Spryker/Zed/CustomerGroup/Dependency/QueryContainer/CustomerGroupToCustomerQueryContainerBridge.php
@@ -1,9 +1,11 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace Spryker\Zed\CustomerGroup\Dependency\QueryContainer;
-
-
-use Spryker\Zed\Customer\Persistence\CustomerQueryContainerInterface;
 
 class CustomerGroupToCustomerQueryContainerBridge implements CustomerGroupToCustomerQueryContainerInterface
 {
--- Bundles/CustomerGroup/src/Spryker/Zed/CustomerGroup/Dependency/QueryContainer/CustomerGroupToCustomerQueryContainerInterface.php
+++ Bundles/CustomerGroup/src/Spryker/Zed/CustomerGroup/Dependency/QueryContainer/CustomerGroupToCustomerQueryContainerInterface.php
@@ -1,15 +1,17 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace Spryker\Zed\CustomerGroup\Dependency\QueryContainer;
-
-
-use Orm\Zed\Customer\Persistence\SpyCustomerQuery;
 
 interface CustomerGroupToCustomerQueryContainerInterface
 {
 
     /**
-     * @return SpyCustomerQuery
+     * @return \Orm\Zed\Customer\Persistence\SpyCustomerQuery
      */
     public function queryCustomers();
 
