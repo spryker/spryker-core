4927721d61fcfedc7c0c8d6c119609c86cce2809
Auris <aurimas.lickus@spryker.com>
2017-09-01 11:53:52 +0200

    core-1909: remove lower case, add underscore

--- Bundles/Product/src/Spryker/Zed/Product/Business/Product/Sku/SkuGenerator.php
+++ Bundles/Product/src/Spryker/Zed/Product/Business/Product/Sku/SkuGenerator.php
@@ -67,8 +67,7 @@ class SkuGenerator implements SkuGeneratorInterface
             $sku = iconv('UTF-8', 'ASCII//TRANSLIT', $sku);
         }
 
-        $sku = preg_replace("/[^a-zA-Z0-9\.-]/", "", trim($sku));
-        $sku = mb_strtolower($sku);
+        $sku = preg_replace("/[^a-zA-Z0-9\.\-\_]/", "", trim($sku));
         $sku = preg_replace('/\s+/', '-', $sku);
         $sku = preg_replace('/(\-)\1+/', '$1', $sku);
 
