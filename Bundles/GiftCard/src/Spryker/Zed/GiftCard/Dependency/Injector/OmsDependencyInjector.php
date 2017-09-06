1044e5532da4b60ab75753901ff6a9b849097465
Lucas Manzke <lmanzke@outlook.com>
2017-09-06 14:12:45 +0200

    CORE-777 Add injector for commands and conditions

---
+++ Bundles/GiftCard/src/Spryker/Zed/GiftCard/Dependency/Injector/OmsDependencyInjector.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\GiftCard\Dependency\Injector;
+
+use Spryker\Zed\GiftCard\Communication\Plugin\Oms\Command\CreateGiftCardCommandPlugin;
+use Spryker\Zed\GiftCard\Communication\Plugin\Oms\Condition\IsGiftCardConditionPlugin;
+use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\Kernel\Dependency\Injector\AbstractDependencyInjector;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Command\CommandCollectionInterface;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Condition\ConditionCollectionInterface;
+use Spryker\Zed\Oms\OmsDependencyProvider;
+
+class OmsDependencyInjector extends AbstractDependencyInjector
+{
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function injectBusinessLayerDependencies(Container $container)
+    {
+        $container = $this->injectCommands($container);
+        $container = $this->injectConditions($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function injectCommands(Container $container)
+    {
+        $container->extend(OmsDependencyProvider::COMMAND_PLUGINS, function (CommandCollectionInterface $commandCollection) {
+            $commandCollection
+                ->add(new CreateGiftCardCommandPlugin(), 'GiftCard/CreateGiftCard');
+
+            return $commandCollection;
+        });
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function injectConditions(Container $container)
+    {
+        $container->extend(OmsDependencyProvider::CONDITION_PLUGINS, function (ConditionCollectionInterface $conditionCollection) {
+            $conditionCollection
+                ->add(new IsGiftCardConditionPlugin(), 'GiftCard/IsGiftCard');
+
+            return $conditionCollection;
+        });
+
+        return $container;
+    }
+
+}
