df69a62dcb6778be6f1af61c65415c7a7fa98874
Auris <aurimas.lickus@spryker.com>
2017-09-07 16:46:49 +0200

    core-178: add currency and store tables

---
+++ Bundles/Store/src/Spryker/Zed/Store/Business/StoreFacade.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Store\Business;
+
+use Spryker\Zed\Kernel\Business\AbstractFacade;
+
+/**
+ * @method \Spryker\Zed\Store\Business\StoreBusinessFactory getFactory()
+ */
+class StoreFacade extends AbstractFacade
+{
+}
