556e70b47df6dc0213a5321efee0d9df1b83342c
Auris <aurimas.lickus@spryker.com>
2017-09-13 10:27:49 +0200

    core-1748: code style

--- Bundles/Currency/src/Spryker/Client/Currency/Dependency/Client/CurrencyToSessionBridge.php
+++ Bundles/Currency/src/Spryker/Client/Currency/Dependency/Client/CurrencyToSessionBridge.php
@@ -38,7 +38,7 @@ class CurrencyToSessionBridge implements CurrencyToSessionInterface
 
     /**
      * @param string $name The attribute name
-     * @param mixed $default The default value if not found
+     * @param mixed|null $default The default value if not found
      *
      * @return mixed
      */
