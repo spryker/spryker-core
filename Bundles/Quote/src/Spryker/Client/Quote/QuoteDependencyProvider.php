c6ed39ddaebf3fe0bbf7234bc153bd30985cae56
Auris <aurimas.lickus@spryker.com>
2017-09-19 13:12:27 +0200

    core-1755: store currency to quote

--- Bundles/Quote/src/Spryker/Client/Quote/QuoteDependencyProvider.php
+++ Bundles/Quote/src/Spryker/Client/Quote/QuoteDependencyProvider.php
@@ -7,13 +7,16 @@
 
 namespace Spryker\Client\Quote;
 
+use Spryker\Client\Currency\Plugin\CurrencyPlugin;
 use Spryker\Client\Kernel\AbstractDependencyProvider;
 use Spryker\Client\Kernel\Container;
+use Spryker\Client\Quote\Dependency\Plugin\QuoteToCurrencyBridge;
 
 class QuoteDependencyProvider extends AbstractDependencyProvider
 {
 
     const CLIENT_SESSION = 'session client';
+    const CURRENCY_PLUGIN = 'currency plugin';
 
     /**
      * @param \Spryker\Client\Kernel\Container $container
@@ -23,6 +26,7 @@ class QuoteDependencyProvider extends AbstractDependencyProvider
     public function provideServiceLayerDependencies(Container $container)
     {
         $container = $this->addSessionClient($container);
+        $container = $this->addCurrencyPlugin($container);
 
         return $container;
     }
@@ -36,6 +40,20 @@ class QuoteDependencyProvider extends AbstractDependencyProvider
     {
         $container[static::CLIENT_SESSION] = function (Container $container) {
             return $container->getLocator()->session()->client();
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Client\Kernel\Container $container
+     *
+     * @return \Spryker\Client\Kernel\Container
+     */
+    protected function addCurrencyPlugin(Container $container)
+    {
+        $container[static::CURRENCY_PLUGIN] = function (Container $container) {
+            return new QuoteToCurrencyBridge(new CurrencyPlugin());
         };
 
         return $container;
--- Bundles/Quote/src/Spryker/Client/Quote/Session/QuoteSession.php
+++ Bundles/Quote/src/Spryker/Client/Quote/Session/QuoteSession.php
@@ -8,6 +8,7 @@
 namespace Spryker\Client\Quote\Session;
 
 use Generated\Shared\Transfer\QuoteTransfer;
+use Spryker\Client\Quote\Dependency\Plugin\QuoteToCurrencyInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class QuoteSession implements QuoteSessionInterface
@@ -21,11 +22,20 @@ class QuoteSession implements QuoteSessionInterface
     protected $session;
 
     /**
-     * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session
+     * @var \Spryker\Client\Currency\Plugin\CurrencyPluginInterface
      */
-    public function __construct(SessionInterface $session)
-    {
+    protected $currencyPlugin;
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session
+     * @param \Spryker\Client\Quote\Dependency\Plugin\QuoteToCurrencyInterface|null $currencyPlugin
+     */
+    public function __construct(
+        SessionInterface $session,
+        QuoteToCurrencyInterface $currencyPlugin = null
+    ) {
         $this->session = $session;
+        $this->currencyPlugin = $currencyPlugin;
     }
 
     /**
@@ -34,7 +44,7 @@ class QuoteSession implements QuoteSessionInterface
     public function getQuote()
     {
         $quoteTransfer = new QuoteTransfer();
-
+        $this->setCurrency($quoteTransfer);
         return $this->session->get(static::QUOTE_SESSION_IDENTIFIER, $quoteTransfer);
     }
 
@@ -45,6 +55,7 @@ class QuoteSession implements QuoteSessionInterface
      */
     public function setQuote(QuoteTransfer $quoteTransfer)
     {
+        $this->setCurrency($quoteTransfer);
         $this->session->set(static::QUOTE_SESSION_IDENTIFIER, $quoteTransfer);
     }
 
@@ -56,6 +67,20 @@ class QuoteSession implements QuoteSessionInterface
         $this->setQuote(new QuoteTransfer());
 
         return $this;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return void
+     */
+    protected function setCurrency(QuoteTransfer $quoteTransfer)
+    {
+        if (!$this->currencyPlugin) {
+            return;
+        }
+
+        $quoteTransfer->setCurrency($this->currencyPlugin->getCurrent());
     }
 
 }
