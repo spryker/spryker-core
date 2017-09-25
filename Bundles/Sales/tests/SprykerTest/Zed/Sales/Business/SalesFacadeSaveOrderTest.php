bb4a9f9a2cb2d5bc91a9f85343c18483f2bd559c
Auris <aurimas.lickus@spryker.com>
2017-09-25 17:09:41 +0200

    core-1755: remove new line

--- Bundles/Sales/tests/SprykerTest/Zed/Sales/Business/SalesFacadeSaveOrderTest.php
+++ Bundles/Sales/tests/SprykerTest/Zed/Sales/Business/SalesFacadeSaveOrderTest.php
@@ -158,7 +158,6 @@ class SalesFacadeSaveOrderTest extends Unit
         $country->save();
 
         $quoteTransfer = new QuoteTransfer();
-
         $currencyTransfer = new CurrencyTransfer();
         $currencyTransfer->setCode('EUR');
         $quoteTransfer->setCurrency($currencyTransfer);
