37914e1b6fcdcbafed0488ba103d5f50c21064f2
Denis Turkov <denis.turkov@spryker.com>
2017-10-16 10:10:24 +0000

    CORE-2138: Apply structure for detail info for OMS locks

--- Bundles/Oms/src/Spryker/Zed/Oms/Business/OrderStateMachine/LockedOrderStateMachine.php
+++ Bundles/Oms/src/Spryker/Zed/Oms/Business/OrderStateMachine/LockedOrderStateMachine.php
@@ -227,7 +227,9 @@ class LockedOrderStateMachine implements OrderStateMachineInterface
             return null;
         }
 
-        return json_encode($orderItemIds);
+        return json_encode([
+            'id_sales_order_items' => $orderItemIds
+        ]);
     }
 
 }
