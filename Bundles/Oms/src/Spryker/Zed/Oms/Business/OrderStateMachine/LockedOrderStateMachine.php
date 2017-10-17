2e69a86e496fbc892f0b69a0c25cb7e9ec523b3a
Denis Turkov <denis.turkov@spryker.com>
2017-10-17 10:05:55 +0000

    CORE-2138: Remove deprecated buildIdentifierForOrderItemsLock

--- Bundles/Oms/src/Spryker/Zed/Oms/Business/OrderStateMachine/LockedOrderStateMachine.php
+++ Bundles/Oms/src/Spryker/Zed/Oms/Business/OrderStateMachine/LockedOrderStateMachine.php
@@ -172,21 +172,6 @@ class LockedOrderStateMachine implements OrderStateMachineInterface
     }
 
     /**
-     * @deprecated Use collectIdentifiersForOrderItemsLock() && buildIdentifierForOrderItemIdsLock() instead
-     * Will be removed with the next major.
-     *
-     * @param \Orm\Zed\Sales\Persistence\SpySalesOrderItem[] $orderItems
-     *
-     * @return string
-     */
-    protected function buildIdentifierForOrderItemsLock(array $orderItems)
-    {
-        $orderItemIds = $this->collectIdentifiersForOrderItemsLock($orderItems);
-
-        return $this->buildIdentifierForOrderItemIdsLock($orderItemIds);
-    }
-
-    /**
      * @param \Orm\Zed\Sales\Persistence\SpySalesOrderItem[] $orderItems
      *
      * @return array
