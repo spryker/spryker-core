b1d72a9393a719fc64260a83f74140a26d5d1fa5
Auris <aurimas.lickus@spryker.com>
2017-11-01 10:53:24 +0100

    core-1750: revert old factory and deprecate it

---
+++ Bundles/CmsContentWidgetProductSetConnector/src/Spryker/Yves/CmsContentWidgetProductSetConnector/CmsContentWidgetProductSetConnectorConnectorFactory.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Yves\CmsContentWidgetProductSetConnector;
+
+use Spryker\Yves\Kernel\AbstractFactory;
+
+/**
+ * @deprecated use \Spryker\Yves\CmsContentWidgetProductSetConnector\CmsContentWidgetProductSetConnectorFactory instead
+ */
+class CmsContentWidgetProductSetConnectorConnectorFactory extends AbstractFactory
+{
+    /**
+     * @return \Spryker\Yves\CmsContentWidgetProductSetConnector\Dependency\Client\CmsContentWidgetProductSetConnectorToProductSetInterface
+     */
+    public function getProductSetClient()
+    {
+        return $this->getProvidedDependency(CmsContentWidgetProductSetConnectorDependencyProvider::PRODUCT_SET_CLIENT);
+    }
+
+    /**
+     * @return \Spryker\Yves\CmsContentWidgetProductSetConnector\Dependency\Client\CmsContentWidgetProductSetConnectorToProductInterface
+     */
+    public function getProductClient()
+    {
+        return $this->getProvidedDependency(CmsContentWidgetProductSetConnectorDependencyProvider::PRODUCT_CLIENT);
+    }
+}
