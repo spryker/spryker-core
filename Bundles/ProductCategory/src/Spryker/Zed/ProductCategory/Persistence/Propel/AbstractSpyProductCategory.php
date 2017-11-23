90b7be0c684bd0f62a00e0a0cde8001f8f8f114a
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2017-11-23 15:45:18 +0000

    reverted docblock removal

--- Bundles/ProductCategory/src/Spryker/Zed/ProductCategory/Persistence/Propel/AbstractSpyProductCategory.php
+++ Bundles/ProductCategory/src/Spryker/Zed/ProductCategory/Persistence/Propel/AbstractSpyProductCategory.php
@@ -9,6 +9,15 @@ namespace Spryker\Zed\ProductCategory\Persistence\Propel;
 
 use Orm\Zed\ProductCategory\Persistence\Base\SpyProductCategory as BaseSpyProductCategory;
 
+/**
+ * Skeleton subclass for representing a row from the 'spy_product_category' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
 abstract class AbstractSpyProductCategory extends BaseSpyProductCategory
 {
 }
