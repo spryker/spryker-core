4fbadcb2f7a2cc8927c374429b5441c523c41337
René Klatt <rene.klatt@spryker.com>
2017-11-27 10:48:47 +0000

    core-2275 update Discount form js

--- Bundles/Discount/assets/Zed/js/modules/main.js
+++ Bundles/Discount/assets/Zed/js/modules/main.js
@@ -12,7 +12,7 @@ require('../../sass/main.scss');
 
 function setDiscountAmountSymbol() {
     var value = $(this).val();
-    var $amountAddon = $('#discount_discountCalculator_amount + .input-group-addon');
+    var $amountAddon = $('#discount_form_discountCalculator_amount + .input-group-addon');
 
     if (/percent/i.test(value)) {
         $amountAddon.html('&#37;');
@@ -23,9 +23,9 @@ function setDiscountAmountSymbol() {
 
 $(document).ready(function() {
 
-    var sqlCalculationBuilder = SqlFactory('#discount_discountCalculator_collector_query_string', '#builder_calculation');
-    var sqlConditionBuilder = SqlFactory('#discount_discountCondition_decision_rule_query_string', '#builder_condition', true);
-    var isQueryStringCollectorSelected = $('#discount_discountCalculator_collectorStrategyType_0').is(":checked");
+    var sqlCalculationBuilder = SqlFactory('#discount_form_discountCalculator_collector_query_string', '#builder_calculation');
+    var sqlConditionBuilder = SqlFactory('#discount_form_discountCondition_decision_rule_query_string', '#builder_condition', true);
+    var isQueryStringCollectorSelected = $('#discount_form_discountCalculator_collectorStrategyType_0').is(":checked");
 
     $('#create-discount-button').on('click', function (e) {
         e.preventDefault();
@@ -33,7 +33,7 @@ $(document).ready(function() {
         $(this)
             .prop('disabled', true)
             .addClass('disabled');
-        
+
         if (isQueryStringCollectorSelected) {
             sqlCalculationBuilder.saveQuery();
         }
@@ -51,55 +51,55 @@ $(document).ready(function() {
         sqlConditionBuilder.toggleButton(event);
     });
 
-    setDiscountAmountSymbol.apply($('#discount_discountCalculator_calculator_plugin'));
-    $('#discount_discountCalculator_calculator_plugin').on('change', setDiscountAmountSymbol);
+    setDiscountAmountSymbol.apply($('#discount_form_discountCalculator_calculator_plugin'));
+    $('#discount_form_discountCalculator_calculator_plugin').on('change', setDiscountAmountSymbol);
 
-    $('#discount_discountGeneral_valid_from').datepicker({
+    $('#discount_form_discountGeneral_valid_from').datepicker({
         dateFormat: 'yy-mm-dd',
         changeMonth: true,
         numberOfMonths: 3,
         defaultData: 0,
         onClose: function(selectedDate) {
-            $('#discount_discountGeneral_valid_to').datepicker('option', 'minDate', selectedDate);
+            $('#discount_form_discountGeneral_valid_to').datepicker('option', 'minDate', selectedDate);
         }
     });
 
-    $('#discount_discountGeneral_valid_to').datepicker({
+    $('#discount_form_discountGeneral_valid_to').datepicker({
         defaultData: 0,
         dateFormat: 'yy-mm-dd',
         changeMonth: true,
         numberOfMonths: 3,
         onClose: function(selectedDate) {
-            $('#discount_discountGeneral_valid_from').datepicker('option', 'maxDate', selectedDate);
+            $('#discount_form_discountGeneral_valid_from').datepicker('option', 'maxDate', selectedDate);
         }
     });
 
-    $('#discount_discountCalculator_collectorStrategyType input').each(function(index, element) {
+    $('#discount_form_discountCalculator_collectorStrategyType input').each(function(index, element) {
         $('#collector-type-' + $(element).val()).hide();
         if ($(element).is(":checked")) {
             $('#collector-type-' + $(element).val()).show();
         }
     });
 
-    $('#discount_discountCalculator_collectorStrategyType input').on('click', function(event) {
-          $('#discount_discountCalculator_collectorStrategyType input').each(function(index, element) {
+    $('#discount_form_discountCalculator_collectorStrategyType input').on('click', function(event) {
+          $('#discount_form_discountCalculator_collectorStrategyType input').each(function(index, element) {
                $('#collector-type-' + $(element).val()).hide();
           });
 
           $('#collector-type-' + $(event.target).val()).show();
     });
 
-    $('#discount_discountCalculator_calculator_plugin').on('change', function(event) {
+    $('#discount_form_discountCalculator_calculator_plugin').on('change', function(event) {
 
         $('.discount-calculation-input-type').each(function(index, element) {
             $(element).hide();
         });
 
-        var activeCalculatorInputType = $('#discount_discountCalculator_calculator_plugin :selected').data('calculator-input-type');
+        var activeCalculatorInputType = $('#discount_form_discountCalculator_calculator_plugin :selected').data('calculator-input-type');
         $('#' + activeCalculatorInputType).show();
     });
 
-    var activeCalculatorInputType = $('#discount_discountCalculator_calculator_plugin :selected').data('calculator-input-type');
+    var activeCalculatorInputType = $('#discount_form_discountCalculator_calculator_plugin :selected').data('calculator-input-type');
     $('#' + activeCalculatorInputType).show();
 
 });
