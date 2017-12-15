bd53093457b58f050ad6c000150e3a3593b7ab06
stereomon <rene.klatt@spryker.com>
2017-12-15 15:27:27 +0000

    core-2275 update forms

--- Bundles/Sales/src/Spryker/Zed/Sales/Communication/Form/AddressForm.php
+++ Bundles/Sales/src/Spryker/Zed/Sales/Communication/Form/AddressForm.php
@@ -9,6 +9,7 @@ namespace Spryker\Zed\Sales\Communication\Form;
 
 use Spryker\Zed\Kernel\Communication\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
