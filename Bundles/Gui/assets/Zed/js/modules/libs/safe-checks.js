3fe87fe60df68288e9753657e9955767677b1b43
ilmente <alessandro.bellini@spryker.com>
2018-01-02 18:23:49 +0100

    CORE-2399: add safe datetime check to hide navive picker

---
+++ Bundles/Gui/assets/Zed/js/modules/libs/safe-checks.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var safeDatetimeSelector = '.safe-datetime[type=date], .safe-datetime[type=datetime], .safe-datetime[type=datetime-local]';
+
+/* Prevent .save-submit items to be pressed twice */
+function addSafeSubmitCheck() { 
+    $('body').on('click', '.safe-submit', function () {
+        var $item = $(this);
+        var $forms = $item.parents('form');
+        var isValid = true;
+
+        function disableTrigger() {
+            $item
+                .prop('disabled', true)
+                .addClass('disabled')
+                .off('click');
+        }
+
+        if ($forms.length > 0) {
+            isValid = !!$forms[0].checkValidity ? $forms[0].checkValidity() : isValid;
+        }
+
+        if (isValid) {
+            setTimeout(disableTrigger);
+        }
+
+        return true;
+    });
+}
+
+/* Prevent .save-datetime inputs to show native datepickers */
+function addSafeDatetimeCheck() { 
+    $('body').on('click', safeDatetimeSelector, function (e) {
+        function disableNativeWindow() {
+            e.preventDefault();
+        }
+
+        setTimeout(disableNativeWindow);
+
+        return false;
+    });
+}
+
+module.exports = {
+    addSafeSubmitCheck: addSafeSubmitCheck,
+    addSafeDatetimeCheck: addSafeDatetimeCheck
+}
