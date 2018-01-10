b00325dd6035986752744250bdd3df7ecb094222
Roman Shopin <roman.shopin@spryker.com>
2018-01-10 19:43:22 +0100

    CORE-974: CompanyUnitAddress update

---
+++ Bundles/CompanyUnitAddress/src/Spryker/Zed/CompanyUnitAddress/Business/CompanyUnitAddressFacade.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\CompanyUnitAddress\Business;
+
+use Generated\Shared\Transfer\CompanyUnitAddressTransfer;
+
+class CompanyUnitAddressFacade implements CompanyUnitAddressFacadeInterface
+{
+    /**
+     * Specification:
+     * - Creates a company unit address
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CompanyUnitAddressTransfer $companyUnitAddressTransfer
+     *
+     * @return \Generated\Shared\Transfer\CompanyUnitAddressTransfer
+     */
+    public function create(CompanyUnitAddressTransfer $companyUnitAddressTransfer): CompanyUnitAddressTransfer
+    {
+        return new CompanyUnitAddressTransfer();
+    }
+
+    /**
+     * Specification:
+     * - Finds a company unit address by CompanyUnitAddressTransfer::idCompanyUnitAddress in the transfer
+     * - Updates fields in a company unit address entity
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CompanyUnitAddressTransfer $companyUnitAddressTransfer
+     *
+     * @return void
+     */
+    public function update(CompanyUnitAddressTransfer $companyUnitAddressTransfer): void
+    {
+    }
+
+    /**
+     * Specification:
+     * - Finds a company unit address by CompanyUnitAddressTransfer::idCompanyUnitAddress in the transfer
+     * - Deletes the company unit address
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CompanyUnitAddressTransfer $companyUnitAddressTransfer
+     *
+     * @return void
+     */
+    public function delete(CompanyUnitAddressTransfer $companyUnitAddressTransfer): void
+    {
+    }
+}
