4112b2b0ca3c9c039e419effc1b11eef1ce0fb75
khatsko <kirill.khatsko@spryker.com>
2018-01-17 17:03:21 +0200

    CORE-2427 correct deprecation message

--- Bundles/Sales/src/Spryker/Zed/Sales/Business/Model/Order/OrderHydrator.php
+++ Bundles/Sales/src/Spryker/Zed/Sales/Business/Model/Order/OrderHydrator.php
@@ -196,7 +196,7 @@ class OrderHydrator implements OrderHydratorInterface
         $orderTransfer = new OrderTransfer();
         $orderTransfer->fromArray($orderEntity->toArray(), true);
         $orderTransfer->setCustomerReference($orderEntity->getCustomerReference());
-        // using FK to cusotmer is obsolete, but needed to prevent BC-break
+        // Deprecated: Using FK to customer is obsolete, but needed to prevent BC break.
         $orderTransfer->setFkCustomer($orderEntity->getFkCustomer());
 
         return $orderTransfer;
@@ -396,7 +396,7 @@ class OrderHydrator implements OrderHydratorInterface
     {
         if (!$orderEntity->getCustomer()) {
             $orderTransfer->setCustomerReference(null);
-            // using FK to cusotmer is obsolete, but needed to prevent BC-break
+            // Deprecated: Using FK to customer is obsolete, but needed to prevent BC break.
             $orderTransfer->setFkCustomer(null);
         }
     }
