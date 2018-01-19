cdf6566699efd3299e3856cb3b8c1a1674bf6a9d
khatsko <kirill.khatsko@spryker.com>
2018-01-19 15:32:22 +0200

    CORE-2466 add console command

---
+++ Bundles/Product/src/Spryker/Zed/Product/Communication/Console/ProductAbstractValidityConsole.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Product\Communication\Console;
+
+use Spryker\Zed\Kernel\Communication\Console\Console;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @method \Spryker\Zed\Product\Business\ProductFacadeInterface getFacade()
+ */
+class ProductAbstractValidityConsole extends Console
+{
+    const COMMAND_NAME = 'product:validity';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName(static::COMMAND_NAME);
+        $this->setDescription('(Un)Publish product abstract based on validity date ranges');
+
+        parent::configure();
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getFacade()->checkProductValidityDateRangeAndTouch();
+    }
+}
