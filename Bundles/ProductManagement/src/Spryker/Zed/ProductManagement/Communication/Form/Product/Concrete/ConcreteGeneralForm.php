7be831b32ee330aed3d13d25e73ae642bfe63aa3
Kyrylo Khatsko <kirill.khatsko@gmail.com>
2018-01-22 09:19:14 +0000

    CORE-2466 update product form

--- Bundles/ProductManagement/src/Spryker/Zed/ProductManagement/Communication/Form/Product/Concrete/ConcreteGeneralForm.php
+++ Bundles/ProductManagement/src/Spryker/Zed/ProductManagement/Communication/Form/Product/Concrete/ConcreteGeneralForm.php
@@ -8,11 +8,14 @@
 namespace Spryker\Zed\ProductManagement\Communication\Form\Product\Concrete;
 
 use Spryker\Zed\ProductManagement\Communication\Form\Product\GeneralForm;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ConcreteGeneralForm extends GeneralForm
 {
     const FIELD_IS_SEARCHABLE = 'is_searchable';
+    const FIELD_VALID_FROM = 'valid_from';
+    const FIELD_VALID_UNTIL = 'valid_until';
 
     /**
      * @return string
@@ -47,6 +50,44 @@ class ConcreteGeneralForm extends GeneralForm
             ->add(self::FIELD_IS_SEARCHABLE, 'checkbox', [
                 'label' => 'Searchable',
                 'required' => false,
+            ]);
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     *
+     * @return $this
+     */
+    protected function addValidFromField(FormBuilderInterface $builder, array $options = [])
+    {
+        $builder
+            ->add(self::FIELD_VALID_FROM, TimeType::class, [
+                'label' => 'Valid From',
+                'required' => false,
+                'input'  => 'datetime',
+                'widget' => 'choice',
+            ]);
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     *
+     * @return $this
+     */
+    protected function addValidUntilField(FormBuilderInterface $builder, array $options = [])
+    {
+        $builder
+            ->add(self::FIELD_VALID_UNTIL, TimeType::class, [
+                'label' => 'Valid Until',
+                'required' => false,
+                'input'  => 'datetime',
+                'widget' => 'choice',
             ]);
 
         return $this;
