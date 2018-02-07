672b9281c24ba0d88fe2309c640a26cc62b8c8ad
Denis Turkov <denis.turkov@spryker.com>
2018-02-07 17:24:36 +0000

    CORE-974: Add propel entities

---
+++ Bundles/CompanyRole/src/Spryker/Zed/CompanyRole/Persistence/Propel/AbstractSpyCompanyRoleToPermissionQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Spryker\Zed\CompanyRole\Persistence\Propel;
+
+use Orm\Zed\CompanyRole\Persistence\Base\SpyCompanyRoleToPermissionQuery as BaseSpyCompanyRoleToPermissionQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_company_role_to_permission' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+abstract class AbstractSpyCompanyRoleToPermissionQuery extends BaseSpyCompanyRoleToPermissionQuery
+{
+
+}
