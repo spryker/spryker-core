6e415d56a44458706b1e3fd7ca69b1ee9888fb0a
Oleh Hladchenko <glad.oleg@gmail.com>
2018-02-16 18:01:35 +0200

    PRYM-349 Fixed sync files with redis.

--- Bundles/FileManager/src/Spryker/Zed/FileManager/Communication/Controller/DownloadController.php
+++ Bundles/FileManager/src/Spryker/Zed/FileManager/Communication/Controller/DownloadController.php
@@ -46,7 +46,7 @@ class DownloadController extends AbstractController
     protected function createResponse(FileManagerReadResponseTransfer $fileManagerReadResponseTransfer)
     {
         $response = new Response($fileManagerReadResponseTransfer->getContent());
-        $fileName = $fileManagerReadResponseTransfer->getFile()->getFileName();
+        $fileName = $fileManagerReadResponseTransfer->getFileInfo()->getStorageFileName();
         $contentType = $fileManagerReadResponseTransfer->getFileInfo()->getType();
         $disposition = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
 
