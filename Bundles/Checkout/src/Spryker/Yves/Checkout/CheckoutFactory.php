f3fc717aa75cf94acec944ea470d8a6de2632f22
Denis Turkov <denis.turkov@spryker.com>
2018-02-16 17:18:54 +0000

    CORE-777: Remove useless method

--- Bundles/Checkout/src/Spryker/Yves/Checkout/CheckoutFactory.php
+++ Bundles/Checkout/src/Spryker/Yves/Checkout/CheckoutFactory.php
@@ -13,14 +13,6 @@ use Spryker\Yves\Kernel\AbstractFactory;
 class CheckoutFactory extends AbstractFactory
 {
     /**
-     * @return \Spryker\Yves\StepEngine\Dependency\Plugin\Form\SubFormPluginCollection
-     */
-    public function createPaymentMethodSubForms()
-    {
-        return $this->getProvidedDependency(CheckoutDependencyProvider::PAYMENT_SUB_FORMS);
-    }
-
-    /**
      * @return \Spryker\Yves\Checkout\Form\FormFactory
      */
     public function createCheckoutFormFactory()
