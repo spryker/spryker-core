44920c55e55f641a70958c34d36f7afad4b53a78
Oleh Hladchenko <glad.oleg@gmail.com>
2018-03-01 14:02:43 +0200

    CORE-3519 Added CustomerNote test structure.

---
+++ Bundles/CustomerNote/tests/SprykerTest/Zed/CustomerNote/Business/CustomerNoteFacadeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\Customer\Business;
+
+use Codeception\Test\Unit;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Zed
+ * @group CustomerNote
+ * @group Business
+ * @group Facade
+ * @group CustomerNoteFacadeTest
+ * Add your own group annotations below this line
+ */
+class CustomerNoteFacadeTest extends Unit
+{
+    public function testAddNote()
+    {
+        $this->assertTrue(true);
+    }
+}
