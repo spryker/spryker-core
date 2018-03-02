14127c76d043577e8cc2ef933335ae107363fb19
Tamas Nyulas <tamas.nyulas@spryker.com>
2018-03-02 16:34:38 +0100

    CORE-2347 MultiCart basics

---
+++ Bundles/MultiCart/src/Spryker/Zed/MultiCart/Business/MultiCartFacade.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\MultiCart\Business;
+
+use Spryker\Zed\Kernel\Business\AbstractFacade;
+
+class MultiCartFacade extends AbstractFacade implements MultiCartFacadeInterface
+{
+}
