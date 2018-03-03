bf457cd607c391ac701b16a56f89775309a8eb32
Serhii Sikachov <sergey.sikachev@spryker.com>
2018-03-03 19:47:59 +0000

    CORE-3520. Add persistence factory.

---
+++ Bundles/CompanyUnitAddressLabel/src/Spryker/Zed/CompanyUnitAddressLabel/Persistence/CompanyUnitAddressLabelPersistenceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\CompanyUnitAddressLabel\Persistence;
+
+use Orm\Zed\CompanyUnitAddressLabel\Persistence\SpyCompanyUnitAddressLabelQuery;
+use Spryker\Zed\Kernel\Persistence\AbstractPersistenceFactory;
+
+/**
+ * @method \Spryker\Zed\CompanyUnitAddressLabel\CompanyUnitAddressLabelConfig getConfig()
+ * @method \Spryker\Zed\CompanyUnitAddressLabel\Persistence\CompanyUnitAddressLabelQueryContainerInterface getQueryContainer()
+ */
+class CompanyUnitAddressLabelPersistenceFactory extends AbstractPersistenceFactory
+{
+    /**
+     * @return \Orm\Zed\CompanyUnitAddressLabel\Persistence\SpyCompanyUnitAddressLabelQuery
+     */
+    public function createCompanyUnitAddressLabelQuery()
+    {
+        return SpyCompanyUnitAddressLabelQuery::create();
+    }
+}
