7abebf8478c77199d7802503cc3f43fd8e958f2e
Sergey Sikachev <sergey.sikachev@spryker.com>
2018-03-14 16:19:53 +0000

    CORE-3610. SalesExtension module.

---
+++ Bundles/SalesExtension/src/Spryker/Zed/SalesExtension/Dependency/Plugin/OrderListFilterPluginInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\SalesExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\OrderListTransfer;
+
+interface OrderListFilterPluginInterface
+{
+    /**
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\OrderListTransfer $orderListTransfer
+     *
+     * @return \Generated\Shared\Transfer\OrderListTransfer
+     */
+    public function filterOrders(OrderListTransfer $orderListTransfer): OrderListTransfer;
+}
