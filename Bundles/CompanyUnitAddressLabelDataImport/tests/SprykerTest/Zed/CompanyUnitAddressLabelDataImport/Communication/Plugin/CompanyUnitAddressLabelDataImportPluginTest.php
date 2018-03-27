2d75f3e6ae75cae1be3481861c79163b44e39b12
René Klatt <rene.klatt@spryker.com>
2018-03-27 08:25:14 +0000

    core-3219 / core-3865 moved key column to DataImport Modules

--- Bundles/CompanyUnitAddressLabelDataImport/tests/SprykerTest/Zed/CompanyUnitAddressLabelDataImport/Communication/Plugin/CompanyUnitAddressLabelDataImportPluginTest.php
+++ Bundles/CompanyUnitAddressLabelDataImport/tests/SprykerTest/Zed/CompanyUnitAddressLabelDataImport/Communication/Plugin/CompanyUnitAddressLabelDataImportPluginTest.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * Copyright © 2018-present Spryker Systems GmbH. All rights reserved.
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
- */
-
 namespace SprykerTest\Zed\CompanyUnitAddressLabelDataImport\Communication\Plugin;
 
 use Codeception\Test\Unit;
