17feb1e56abc2dac83d8e130403613544bc57968
Oleh Hladchenko <glad.oleg@gmail.com>
2018-04-09 01:15:54 +0300

    CORE-4382 Implement CompanySupplierDataImportPlugins tests.

--- Bundles/CompanySupplierDataImport/tests/SprykerTest/Zed/CompanySupplierDataImport/_support/Helper/CompanySupplierDataImportHelper.php
+++ Bundles/CompanySupplierDataImport/tests/SprykerTest/Zed/CompanySupplierDataImport/_support/Helper/CompanySupplierDataImportHelper.php
@@ -8,7 +8,32 @@
 namespace SprykerTest\Zed\CompanySupplierDataImport\Helper;
 
 use Codeception\Module;
+use Orm\Zed\CompanySupplier\Persistence\SpyCompanySupplierToProductQuery;
 
 class CompanySupplierDataImportHelper extends Module
 {
+    /**
+     * @return void
+     */
+    public function ensureDatabaseTableIsEmpty(): void
+    {
+        $this->getCompanySupplierToProductQuery()->find()->delete();
+    }
+
+    /**
+     * @return void
+     */
+    public function assertDatabaseTableContainsData(): void
+    {
+        $companySupplierToProductQuery = $this->getCompanySupplierToProductQuery();
+        $this->assertTrue(($companySupplierToProductQuery->count() > 0), 'Expected at least one entry in the database table but database table is empty.');
+    }
+
+    /**
+     * @return \Orm\Zed\CompanySupplier\Persistence\SpyCompanySupplierToProductQuery
+     */
+    protected function getCompanySupplierToProductQuery(): SpyCompanySupplierToProductQuery
+    {
+        return SpyCompanySupplierToProductQuery::create();
+    }
 }
