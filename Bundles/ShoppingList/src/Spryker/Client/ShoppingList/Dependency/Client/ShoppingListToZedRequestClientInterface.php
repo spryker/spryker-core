c4652b3507ac737c80cf2b3bae73485473be87db
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2018-04-11 13:12:12 +0200

    core-2347 update doc block to remove deprecation

--- Bundles/ShoppingList/src/Spryker/Client/ShoppingList/Dependency/Client/ShoppingListToZedRequestClientInterface.php
+++ Bundles/ShoppingList/src/Spryker/Client/ShoppingList/Dependency/Client/ShoppingListToZedRequestClientInterface.php
@@ -14,7 +14,7 @@ interface ShoppingListToZedRequestClientInterface
     /**
      * @param string $url
      * @param \Spryker\Shared\Kernel\Transfer\TransferInterface $object
-     * @param array|int|null $requestOptions Deprecated: Do not use "int" anymore, please use an array for requestOptions.
+     * @param array|int|null $requestOptions
      *
      * @return \Spryker\Shared\Kernel\Transfer\TransferInterface
      */
