a763923d9f1e9fa9cffff64bece5e2b4c8c4736b
Kyrylo Khatsko <kirill.khatsko@spryker.com>
2018-04-24 10:37:32 +0300

    CORE-2655 Barcode: add plugin function

--- Bundles/BarcodeExtension/src/Spryker/BarcodeExtension/Service/Dependency/Plugin/BarcodeGeneratorPluginInterface.php
+++ Bundles/BarcodeExtension/src/Spryker/BarcodeExtension/Service/Dependency/Plugin/BarcodeGeneratorPluginInterface.php
@@ -7,6 +7,18 @@
 
 namespace Spryker\BarcodeExtension\Service\Dependency\Plugin;
 
-class BarcodeGeneratorPluginInterface
+interface BarcodeGeneratorPluginInterface
 {
+    /**
+     * Specification:
+     * - Generate image of barcode by provided $text
+     * - Every plugin should implement one coding standard (like EAN-13) and one image style (size, fonts e.t.c.)
+     * - Image passed as encoded string inside BarcodeResponseTransfer
+     *
+     * @api
+     *
+     * @param string $text
+     * @return string
+     */
+    public function generate(string $text): string;
 }
