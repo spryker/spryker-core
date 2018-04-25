2df33ecd09f500cee7be9f289b53cbac5d7ababe
mscherer <mark.scherer@spryker.com>
2018-04-25 15:37:10 +0200

    Fix CS.

--- Bundles/ManualOrderEntryGui/src/Spryker/Zed/ManualOrderEntryGui/Communication/Plugin/AddressManualOrderEntryFormPlugin.php
+++ Bundles/ManualOrderEntryGui/src/Spryker/Zed/ManualOrderEntryGui/Communication/Plugin/AddressManualOrderEntryFormPlugin.php
@@ -82,7 +82,7 @@ class AddressManualOrderEntryFormPlugin extends AbstractPlugin implements Manual
      *
      * @return bool
      */
-    public function isFormPreFilled(QuoteTransfer $quoteTransfer = null): bool
+    public function isFormPreFilled(?QuoteTransfer $quoteTransfer = null): bool
     {
         return false;
     }
