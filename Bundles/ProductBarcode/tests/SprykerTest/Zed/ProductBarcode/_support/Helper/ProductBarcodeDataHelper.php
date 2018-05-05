4c69c9dc81df1e3a0be436d5f1c3c8df75ee76d4
Kyrylo Khatsko <kirill.khatsko@spryker.com>
2018-05-05 17:59:20 +0300

    CORE-2655 Barcode: fix tests typehint

--- Bundles/ProductBarcode/tests/SprykerTest/Zed/ProductBarcode/_support/Helper/ProductBarcodeDataHelper.php
+++ Bundles/ProductBarcode/tests/SprykerTest/Zed/ProductBarcode/_support/Helper/ProductBarcodeDataHelper.php
@@ -4,13 +4,14 @@ namespace SprykerTest\Zed\ProductBarcode\Helper;
 
 use Codeception\Module;
 use Generated\Shared\DataBuilder\ProductConcreteBuilder;
+use Generated\Shared\Transfer\ProductConcreteTransfer;
 
 class ProductBarcodeDataHelper extends Module
 {
     /**
      * @return \Generated\Shared\Transfer\ProductConcreteTransfer
      */
-    public function getProductConcrete()
+    public function getProductConcrete(): ProductConcreteTransfer
     {
         return (new ProductConcreteBuilder())->build();
     }
