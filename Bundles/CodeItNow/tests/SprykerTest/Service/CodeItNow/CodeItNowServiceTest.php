4144a5b005f7d3f291dd6f0c178e1653e0007039
Kyrylo Khatsko <kirill.khatsko@spryker.com>
2018-05-06 18:04:04 +0300

    CORE-2655 Barcode: add tests to codeItNow

---
+++ Bundles/CodeItNow/tests/SprykerTest/Service/CodeItNow/CodeItNowServiceTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SprykerTest\Service\CodeItNow;
+
+use Codeception\TestCase\Test;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Service
+ * @group CodeItNow
+ * @group CodeItNowServiceTest
+ * Add your own group annotations below this line
+ */
+class CodeItNowServiceTest extends Test
+{
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+}
