2186f4bb468a617b0003a6ad0a376878cb020978
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-05-10 10:46:54 +0300

    CORE-2655 Barcode: Draft commit with Mock changes (WIP)

--- Bundles/Barcode/tests/SprykerTest/Service/Barcode/BarcodeServiceTest.php
+++ Bundles/Barcode/tests/SprykerTest/Service/Barcode/BarcodeServiceTest.php
@@ -3,6 +3,8 @@
 namespace SprykerTest\Service\Barcode;
 
 use Codeception\TestCase\Test;
+use Exception;
+use Generated\Shared\Transfer\BarcodeResponseTransfer;
 use Spryker\Service\Barcode\BarcodeDependencyProvider;
 
 /**
@@ -21,33 +23,139 @@ class BarcodeServiceTest extends Test
     protected const GENERATED_CODE = 'generated string';
     protected const GENERATED_ENCODING = 'data:image/png;base64';
 
+    protected const TEST_PLUGIN_0_ENCODING = 'test_plugin_0_encoding';
+    protected const TEST_PLUGIN_1_ENCODING = 'test_plugin_1_encoding';
+
+    /**
+     * @var \Spryker\Service\BarcodeExtension\Dependency\Plugin\BarcodeGeneratorPluginInterface[]
+     */
+    protected $registeredBarcodeGeneratorPlugins;
+
     /**
      * @var \Spryker\Service\BarcodeExtension\Dependency\Plugin\BarcodeGeneratorPluginInterface
      */
-    protected $barcodePlugin;
+    protected $barcodeGeneratorPlugin;
 
     /**
      * @return void
      */
-    protected function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
-        $this->barcodePlugin = $this->tester->getBarcodePluginMock();
-        $this->tester->setDependency(BarcodeDependencyProvider::PLUGINS_BARCODE_GENERATOR, [
-            $this->barcodePlugin,
-        ]);
+        $this->registeredBarcodeGeneratorPlugins = [
+            $this->tester->getBarcodePluginMock(static::TEST_PLUGIN_0_ENCODING),
+            $this->tester->getBarcodePluginMockUsingAlphaAnonymousClass(static::TEST_PLUGIN_1_ENCODING),
+            $this->tester->getBarcodePluginMockUsingBetaAnonymousClass(static::TEST_PLUGIN_1_ENCODING),
+        ];
+
+        $this->barcodeGeneratorPlugin = reset($this->registeredBarcodeGeneratorPlugins);
+
+        $this->tester->setDependency(BarcodeDependencyProvider::PLUGINS_BARCODE_GENERATOR, $this->registeredBarcodeGeneratorPlugins);
+    }
+
+    /**
+     * @throws \Exception
+     *
+     * @return void
+     */
+    public function testGenerateBarcode(): void
+    {
+
+        // this works
+//        throw new \Exception($this
+//            ->tester
+//            ->getBarcodePluginMock()
+//            ->generate('text')->getEncoding());
+
+
+        // this doesn't
+//        throw new \Exception($this
+//            ->generateBarcode(get_class(
+//                $this->tester->getBarcodePluginMock()
+//            ))->getEncoding());
+
+
+        // what about real one?
+//        throw new \Exception($this->generateBarcode(Code128BarcodeGeneratorPlugin::class)->getEncoding());
+
+
+        $test = [
+            'registered' => [],
+            'newMock' => get_class($this->tester->getBarcodePluginMock()),
+        ];
+
+        foreach ($this->registeredBarcodeGeneratorPlugins as $plugin) {
+            $test['registered'][] = get_class($plugin);
+        }
+
+        $test['in_array'] = in_array($test['newMock'], $test['registered']);
+
+        throw new Exception(json_encode($test, JSON_PRETTY_PRINT));
+
+//        $barcodeGeneratorPluginWithDefaultEncoding = get_class($this->tester->getBarcodePluginMock());
+//        $barcodeResponseTransfer = $this->generateBarcode($barcodeGeneratorPluginWithDefaultEncoding);
+//
+//        $this->assertSame(static::GENERATED_CODE, $barcodeResponseTransfer->getCode());
+//        $this->assertSame(static::GENERATED_ENCODING, $barcodeResponseTransfer->getEncoding());
     }
 
     /**
      * @return void
      */
-    public function testGenerateBarcode()
+    public function testGeneratedBarcodeResponseHasCorrectType(): void
     {
-        $barcodeResponseTransfer = $this->tester->getBarcodeService()
-            ->generateBarcode(static::GENERATED_CODE, get_class($this->barcodePlugin));
+        $barcodeResponseTransfer = $this->generateBarcode();
 
-        $this->assertSame(static::GENERATED_CODE, $barcodeResponseTransfer->getCode());
-        $this->assertSame(static::GENERATED_ENCODING, $barcodeResponseTransfer->getEncoding());
+        $this->assertInstanceOf(BarcodeResponseTransfer::class, $barcodeResponseTransfer);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGeneratedBarcodeResponseIsNotEmpty(): void
+    {
+        $barcodeResponseTransfer = $this->generateBarcode();
+
+        $this->assertNotEmpty($barcodeResponseTransfer->getCode());
+        $this->assertNotEmpty($barcodeResponseTransfer->getEncoding());
+    }
+
+    /**
+     * @return void
+     */
+    public function testBarcodeGeneratorUsesPluginFullQualifiedClassName(): void
+    {
+        $barcodeGeneratorPluginClass = get_class($this->barcodeGeneratorPlugin);
+
+        $this->assertTrue(class_exists($barcodeGeneratorPluginClass));
+    }
+
+    /**
+     * @return void
+     */
+    public function testBarcodeGeneratorUsesRegisteredPlugin(): void
+    {
+        $barcodeResponseTransfer = $this->generateBarcode();
+
+        $this->assertSame(static::TEST_PLUGIN_0_ENCODING, $barcodeResponseTransfer->getEncoding());
+    }
+
+    /**
+     * @param null|string $generatorPlugin
+     *
+     * @return \Generated\Shared\Transfer\BarcodeResponseTransfer
+     */
+    protected function generateBarcode(?string $generatorPlugin = null): BarcodeResponseTransfer
+    {
+        $generatorPluginClass = $generatorPlugin ?: get_class($this->barcodeGeneratorPlugin);
+
+        return $this
+            ->tester
+            ->getBarcodeService()
+            ->generateBarcode(
+                static::GENERATED_CODE,
+                $generatorPluginClass
+            );
     }
 }
--- Bundles/Barcode/tests/SprykerTest/Service/Barcode/_support/Helper/BarcodeMockHelper.php
+++ Bundles/Barcode/tests/SprykerTest/Service/Barcode/_support/Helper/BarcodeMockHelper.php
@@ -14,21 +14,25 @@ class BarcodeMockHelper extends Module
     protected const GENERATED_ENCODING = 'data:image/png;base64';
 
     /**
+     * @param null|string $encoding
+     *
      * @return \PHPUnit\Framework\MockObject\MockObject|\Spryker\Service\BarcodeExtension\Dependency\Plugin\BarcodeGeneratorPluginInterface
      */
-    public function getBarcodePluginMock(): BarcodeGeneratorPluginInterface
+    public function getBarcodePluginMock(?string $encoding = null): BarcodeGeneratorPluginInterface
     {
+        $responseEncoding = $encoding ?: static::GENERATED_ENCODING;
+
         $barcodePlugin = Stub::makeEmpty(BarcodeGeneratorPluginInterface::class, [
-            'generate' => function (string $text): BarcodeResponseTransfer {
+            'generate' => function (string $text) use ($responseEncoding): BarcodeResponseTransfer {
                 return (new BarcodeResponseTransfer())
                     ->setCode($text)
-                    ->setEncoding(static::GENERATED_ENCODING);
+                    ->setEncoding($responseEncoding);
             },
         ]);
 
         $barcodeTransfer = (new BarcodeResponseTransfer())
             ->setCode(static::GENERATED_CODE)
-            ->setEncoding(static::GENERATED_ENCODING);
+            ->setEncoding($responseEncoding);
 
         $barcodePlugin
             ->expects(Expected::once()->getMatcher())
@@ -36,5 +40,131 @@ class BarcodeMockHelper extends Module
             ->willReturn($barcodeTransfer);
 
         return $barcodePlugin;
+    }
+
+    /**
+     * @param null|string $encoding
+     *
+     * @return \Spryker\Service\BarcodeExtension\Dependency\Plugin\BarcodeGeneratorPluginInterface
+     */
+    public function getBarcodePluginMockUsingDefaultAnonymousClass(?string $encoding = null): BarcodeGeneratorPluginInterface
+    {
+        $responseEncoding = $encoding ?: static::GENERATED_ENCODING;
+
+        return new class($responseEncoding) implements BarcodeGeneratorPluginInterface {
+            /**
+             * @var string
+             */
+            protected $dummy = 'default';
+
+            /**
+             * @var string
+             */
+            protected $encoding;
+
+            /**
+             * @param string $encoding
+             */
+            public function __construct(string $encoding)
+            {
+                $this->encoding = $encoding;
+            }
+
+            /**
+             * @param string $text
+             *
+             * @return \Generated\Shared\Transfer\BarcodeResponseTransfer
+             */
+            public function generate(string $text): BarcodeResponseTransfer
+            {
+                return (new BarcodeResponseTransfer())
+                    ->setCode($text)
+                    ->setEncoding($this->encoding);
+            }
+        };
+    }
+
+    /**
+     * @param null|string $encoding
+     *
+     * @return \Spryker\Service\BarcodeExtension\Dependency\Plugin\BarcodeGeneratorPluginInterface
+     */
+    public function getBarcodePluginMockUsingAlphaAnonymousClass(?string $encoding = null): BarcodeGeneratorPluginInterface
+    {
+        $responseEncoding = $encoding ?: static::GENERATED_ENCODING;
+
+        return new class($responseEncoding) implements BarcodeGeneratorPluginInterface {
+            /**
+             * @var string
+             */
+            protected $dummy = 'alpha';
+
+            /**
+             * @var string
+             */
+            protected $encoding;
+
+            /**
+             * @param string $encoding
+             */
+            public function __construct(string $encoding)
+            {
+                $this->encoding = $encoding;
+            }
+
+            /**
+             * @param string $text
+             *
+             * @return \Generated\Shared\Transfer\BarcodeResponseTransfer
+             */
+            public function generate(string $text): BarcodeResponseTransfer
+            {
+                return (new BarcodeResponseTransfer())
+                    ->setCode($text)
+                    ->setEncoding($this->encoding);
+            }
+        };
+    }
+
+    /**
+     * @param null|string $encoding
+     *
+     * @return \Spryker\Service\BarcodeExtension\Dependency\Plugin\BarcodeGeneratorPluginInterface
+     */
+    public function getBarcodePluginMockUsingBetaAnonymousClass(?string $encoding = null): BarcodeGeneratorPluginInterface
+    {
+        $responseEncoding = $encoding ?: static::GENERATED_ENCODING;
+
+        return new class($responseEncoding) implements BarcodeGeneratorPluginInterface {
+            /**
+             * @var string
+             */
+            protected $dummy = 'beta';
+
+            /**
+             * @var string
+             */
+            protected $encoding;
+
+            /**
+             * @param string $encoding
+             */
+            public function __construct(string $encoding)
+            {
+                $this->encoding = $encoding;
+            }
+
+            /**
+             * @param string $text
+             *
+             * @return \Generated\Shared\Transfer\BarcodeResponseTransfer
+             */
+            public function generate(string $text): BarcodeResponseTransfer
+            {
+                return (new BarcodeResponseTransfer())
+                    ->setCode($text)
+                    ->setEncoding($this->encoding);
+            }
+        };
     }
 }
