6c652f52998bb390815ae9bb31090a34cdd4d01a
Kyrylo Khatsko <kirill.khatsko@spryker.com>
2018-05-18 16:55:17 +0300

    PS-815 BU Hierarchy: change parent on company click

--- Bundles/CompanyBusinessUnitGui/assets/Zed/js/modules/main.js
+++ Bundles/CompanyBusinessUnitGui/assets/Zed/js/modules/main.js
@@ -8,6 +8,79 @@
 require('ZedGui');
 
 $(document).ready( function () {
-    alert('working !');
+    const companyField = $('input#company-business-unit_id_company_business_unit') || {value: null};
+    const parentField = $('select#company-business-unit_fk_parent_company_business_unit');
 
+    /**
+     * @returns Null|string
+     */
+    function getCompanyId() {
+        alert ('company id: ' + companyField.value)
+        return companyField.value;
+    }
+
+    /**
+     * @returns {string: string}
+     */
+    function getBusinessUnitList() {
+        const idCompany = getCompanyId();
+
+        alert ('getBusinessUnitList company: ' + idCompany)
+        if (!idCompany) {
+            return {};
+        }
+
+        return {
+            idCompany: 'some name',
+        };
+    }
+
+    function cleanParents() {
+        alert ('cleanParents')
+        if (!cleanParents) {
+            return;
+        }
+
+        alert ('cleanParents parents: ' + cleanParents.id)
+        while (parentField.firstChild) {
+            parentField.removeChild(parentField.firstChild);
+        }
+    }
+
+    function setParentNames() {
+
+        alert ('setParentNames')
+
+        if (!parentField) {
+            return;
+        }
+        cleanParents();
+        const parentList = getBusinessUnitList();
+        const fragment = document.createDocumentFragment();
+
+        parentList.forEach(function(parentBU, index) {
+            alert('setParentNames each: ' + )
+            let opt = document.createElement('option');
+            opt.innerHTML = parentBU;
+            opt.value = parentBU;
+            fragment.appendChild(opt);
+        });
+
+        parentField.appendChild(fragment);
+    }
+
+    function addListenerOnCompany() {
+
+        alert ('addListenerOnCompany')
+        if (!companyField) {
+            return;
+        }
+
+        companyField.change(function () {
+            alert ('change')
+            setParentNames();
+        });
+    }
+
+    addListenerOnCompany();
 });
