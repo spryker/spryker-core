e44c4bd3801686efd767a8bc557fb240136b56c7
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-05-24 15:28:06 +0300

    PS-1037: Implemented PersistenceFactory for ProductAlternative

---
+++ Bundles/ProductAlternative/src/Spryker/Zed/ProductAlternative/Persistence/ProductAlternativePersistenceFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductAlternative\Persistence;
+
+use Orm\Zed\ProductAlternative\Persistence\SpyProductAlternativeQuery;
+use Spryker\Zed\Kernel\Persistence\AbstractPersistenceFactory;
+use Spryker\Zed\ProductAlternative\Persistence\Mapper\ProductAlternativeMapper;
+use Spryker\Zed\ProductAlternative\Persistence\Mapper\ProductAlternativeMapperInterface;
+
+class ProductAlternativePersistenceFactory extends AbstractPersistenceFactory
+{
+    /**
+     * @return \Orm\Zed\ProductAlternative\Persistence\SpyProductAlternativeQuery
+     */
+    public function createProductAlternativeQuery(): SpyProductAlternativeQuery
+    {
+        return SpyProductAlternativeQuery::create();
+    }
+
+    /**
+     * @return \Spryker\Zed\ProductAlternative\Persistence\Mapper\ProductAlternativeMapperInterface
+     */
+    public function createProductAlternativeMapper(): ProductAlternativeMapperInterface
+    {
+        return new ProductAlternativeMapper();
+    }
+}
