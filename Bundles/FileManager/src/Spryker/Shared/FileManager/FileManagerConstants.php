69dd83ffb62cc519e7718ec800056228f8185cb7
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2018-05-25 10:34:20 +0300

    PRYMB2B-1457 Fixed broken constant

--- Bundles/FileManager/src/Spryker/Shared/FileManager/FileManagerConstants.php
+++ Bundles/FileManager/src/Spryker/Shared/FileManager/FileManagerConstants.php
@@ -10,7 +10,7 @@ namespace Spryker\Shared\FileManager;
 interface FileManagerConstants
 {
     const STORAGE_NAME = 'FILE_MANAGER:STORAGE_NAME';
-    const FILE_NAME_VERSION_DELIMITER = 'FILE_MANAGER:FILE_NAME_VERSION_DELIMITER';
+    const FILE_NAME_VERSION_DELIMITER = '-';
 
     const URL_PARAM_ID_FILE_INFO = 'id-file-info';
 }
