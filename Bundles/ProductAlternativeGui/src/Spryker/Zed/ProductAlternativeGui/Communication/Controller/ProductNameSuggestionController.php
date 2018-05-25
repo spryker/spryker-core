dc9a22d4abe2fbf1c9679b8c46aa969087dcdd64
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-05-25 17:46:40 +0300

    PS-1037: Created suggestion controllers

---
+++ Bundles/ProductAlternativeGui/src/Spryker/Zed/ProductAlternativeGui/Communication/Controller/ProductNameSuggestionController.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductAlternativeGui\Communication\Controller;
+
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @method \Spryker\Zed\ProductAlternativeGui\Business\ProductAlternativeGuiFacadeInterface getFacade()
+ */
+class ProductNameSuggestionController extends AbstractController
+{
+    protected const PARAM_NAME = 'name';
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function indexAction(Request $request): JsonResponse
+    {
+        $searchName = $request->query->get(static::PARAM_NAME);
+        $suggestions = $this
+            ->getFacade()
+            ->suggestProductName($searchName);
+
+        return $this->jsonResponse($suggestions);
+    }
+}
