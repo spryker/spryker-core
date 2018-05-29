9b0e84c459951920567425f422733fa747906064
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-05-29 12:42:57 +0300

    PS-1037: Added bunch of constants to ProductConstants to allow them to be used in other modules

--- Bundles/Product/src/Spryker/Shared/Product/ProductConstants.php
+++ Bundles/Product/src/Spryker/Shared/Product/ProductConstants.php
@@ -9,5 +9,8 @@ namespace Spryker\Shared\Product;
 
 interface ProductConstants
 {
-    public const PRODUCT_LIMIT_SEARCH_DEFAULT = 10;
+    public const PRODUCT_FILTER_LIMIT_DEFAULT = 10;
+    public const PRODUCT_FILTER_ABSTRACT_ID_KEY = 'abstract_id';
+    public const PRODUCT_FILTER_CONCRETE_ID_KEY = 'concrete_id';
+    public const PRODUCT_FILTER_RESULT_KEY = 'result';
 }
