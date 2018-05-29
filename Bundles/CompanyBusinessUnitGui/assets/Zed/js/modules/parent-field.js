e13b610883846b309dfb5dee2e03576ccf300941
Kyrylo Khatsko <kirill.khatsko@spryker.com>
2018-05-29 17:27:53 +0300

    PS-815 BU Hierarchy: PS-1263 fix JS

--- Bundles/CompanyBusinessUnitGui/assets/Zed/js/modules/parent-field.js
+++ Bundles/CompanyBusinessUnitGui/assets/Zed/js/modules/parent-field.js
@@ -5,8 +5,21 @@
 
 'use strict';
 
+/**
+ * @see \Spryker\Zed\CompanyBusinessUnitGui\Communication\Form\CompanyBusinessUnitForm
+ * @type {string}
+ */
 const companyFieldPath = 'select#company-business-unit_fk_company';
+/**
+ * @see \Spryker\Zed\CompanyBusinessUnitGui\Communication\Form\CompanyBusinessUnitForm
+ * @type {string}
+ */
 const parentFieldPath = 'select#company-business-unit_fk_parent_company_business_unit';
+/**
+ * @see \Spryker\Zed\CompanyBusinessUnitGui\Communication\Form\DataProvider\CompanyBusinessUnitFormDataProvider::OPTION_ATTRIBUTE_DATA
+ * @type {string}
+ */
+const attributeIdCompany = 'id_company';
 
 function initialize() {
     const companyField = new CompanyFieldHandler();
@@ -18,30 +31,18 @@ function CompanyFieldHandler() {
     const $companyField = $(companyFieldPath);
     const $parentField = $(parentFieldPath);
 
+    function addListenerOnCompany() {
+        if (apllyeble()) {
+            setParentNames();
+            $companyField.change(setParentNames);
+        }
+    }
+
     /**
-     * @returns Null|string
+     * @returns {bool}
      */
-    function getCompanyId() {
-        return $companyField.val();
-    }
-
-    function blinkParentField() {
-        $parentField.effect("highlight", {}, 3000);
-    }
-
-    function toggleOption() {
-        const companyId = getCompanyId();
-        const $parentOption = $(this);
-
-        if (!$parentOption.val()) {
-            return;
-        }
-
-        if ($parentOption.data('id_company') == companyId) {
-            $parentOption.show();
-        } else {
-            $parentOption.hide();
-        }
+    function apllyeble() {
+        return $parentField.length && $companyField.length;
     }
 
     function setParentNames() {
@@ -50,12 +51,32 @@ function CompanyFieldHandler() {
         blinkParentField();
     }
 
-    function addListenerOnCompany() {
-        setParentNames();
+    function toggleOption() {
+        const companyId = parseInt(getCompanyId());
+        const $parentOption = $(this);
 
-        if ($parentField && $companyField) {
-            $companyField.change(setParentNames);
+        if (!$parentOption.val()) {
+            return;
         }
+
+        if (!companyId) {
+            $parentOption.hide();
+        } else if ($parentOption.data(attributeIdCompany) === companyId) {
+            $parentOption.show();
+        } else {
+            $parentOption.hide();
+        }
+    }
+
+    /**
+     * @returns Null|{string}
+     */
+    function getCompanyId() {
+        return $companyField.val();
+    }
+
+    function blinkParentField() {
+        $parentField.effect("highlight", {}, 3000);
     }
 
     return {
