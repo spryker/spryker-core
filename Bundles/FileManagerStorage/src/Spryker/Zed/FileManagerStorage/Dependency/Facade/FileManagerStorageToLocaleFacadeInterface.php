c39903c2efe6a2996c2df99d7d6b0a949336cbf5
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2018-05-30 14:53:07 +0300

    PRYMB2B-1457 Removed unused methods from FileManagerStorageToLocaleFacadeBridge

--- Bundles/FileManagerStorage/src/Spryker/Zed/FileManagerStorage/Dependency/Facade/FileManagerStorageToLocaleFacadeInterface.php
+++ Bundles/FileManagerStorage/src/Spryker/Zed/FileManagerStorage/Dependency/Facade/FileManagerStorageToLocaleFacadeInterface.php
@@ -10,68 +10,6 @@ namespace Spryker\Zed\FileManagerStorage\Dependency\Facade;
 interface FileManagerStorageToLocaleFacadeInterface
 {
     /**
-     * @param string $localeName
-     *
-     * @return bool
-     */
-    public function hasLocale($localeName);
-
-    /**
-     * @param string $localeName
-     *
-     * @return \Generated\Shared\Transfer\LocaleTransfer
-     */
-    public function getLocale($localeName);
-
-    /**
-     * @param string $localeCode
-     *
-     * @return \Generated\Shared\Transfer\LocaleTransfer
-     */
-    public function getLocaleByCode($localeCode);
-
-    /**
-     * @param int $idLocale
-     *
-     * @return \Generated\Shared\Transfer\LocaleTransfer
-     */
-    public function getLocaleById($idLocale);
-
-    /**
-     * @return string
-     */
-    public function getCurrentLocaleName();
-
-    /**
-     * @return array
-     */
-    public function getAvailableLocales();
-
-    /**
-     * @return \Generated\Shared\Transfer\LocaleTransfer
-     */
-    public function getCurrentLocale();
-
-    /**
-     * @param string $localeName
-     *
-     * @return \Generated\Shared\Transfer\LocaleTransfer
-     */
-    public function createLocale($localeName);
-
-    /**
-     * @param string $localeName
-     *
-     * @return void
-     */
-    public function deleteLocale($localeName);
-
-    /**
-     * @return void
-     */
-    public function install();
-
-    /**
      * @return \Generated\Shared\Transfer\LocaleTransfer[]
      */
     public function getLocaleCollection();
