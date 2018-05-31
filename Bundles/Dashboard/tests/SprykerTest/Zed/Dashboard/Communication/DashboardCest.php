6320ec58b1e4996c631aad09d2cf8cade3202692
vseselov <alexander.veselov@spryker.com>
2018-05-31 14:03:07 +0300

    PS-1272: Created test

---
+++ Bundles/Dashboard/tests/SprykerTest/Zed/Dashboard/Communication/DashboardCest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\Dashboard\Communication;
+
+use SprykerTest\Zed\Dashboard\DashboardCommunicationTester;
+use SprykerTest\Zed\Dashboard\PageObject\DashboardPage;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Zed
+ * @group Dashboard
+ * @group Communication
+ * @group DashboardCest
+ * Add your own group annotations below this line
+ */
+class DashboardCest
+{
+    /**
+     * @param \SprykerTest\Zed\Dashboard\DashboardCommunicationTester $i
+     *
+     * @return void
+     */
+    public function breadcrumbIsVisible(DashboardCommunicationTester $i)
+    {
+        $i->amOnPage(DashboardPage::URL);
+        $i->seeBreadcrumbNavigation('Dashboard / Dashboard');
+    }
+
+    /**
+     * @param \SprykerTest\Zed\Dashboard\DashboardCommunicationTester $i
+     *
+     * @return void
+     */
+    public function titleIsVisible(DashboardCommunicationTester $i)
+    {
+        $i->amOnPage(DashboardPage::URL);
+        $i->canSee(DashboardPage::TITLE, 'h2');
+    }
+}
