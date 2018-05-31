3873e9e8bc2d17d62a3ce2462c7c2a5fd6d00c59
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-05-31 14:33:06 +0300

    PS-1037: Created ProductAlternativeEditDataProvider as a DataProvider for form

---
+++ Bundles/ProductAlternativeGui/src/Spryker/Zed/ProductAlternativeGui/Communication/Plugin/ProductAlternativeEditDataProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductAlternativeGui\Communication\Plugin;
+
+use Generated\Shared\Transfer\ProductConcreteTransfer;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\ProductManagementExtension\Dependency\Plugin\ProductConcreteFormEditDataProviderExpanderPluginInterface;
+
+/**
+ * @method \Spryker\Zed\ProductAlternativeGui\Communication\ProductAlternativeGuiCommunicationFactory getFactory()
+ * @method \Spryker\Zed\ProductAlternativeGui\Business\ProductAlternativeGuiFacadeInterface getFacade()
+ */
+class ProductAlternativeEditDataProvider extends AbstractPlugin implements ProductConcreteFormEditDataProviderExpanderPluginInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ProductConcreteTransfer $productConcrete
+     * @param array $formData
+     *
+     * @return void
+     */
+    public function expand(ProductConcreteTransfer $productConcrete, array &$formData): void
+    {
+        $productAlternatives = $this
+            ->getFactory()
+            ->getProductAlternativeFacade()
+            ->getProductAlternativesByIdProductConcrete(
+                $productConcrete
+            );
+
+        $formData[ProductConcreteTransfer::ALTERNATIVE_PRODUCTS] = $productAlternatives->getProductAlternatives();
+    }
+}
