836a8b240aaca76cecc7c298bbd998ac069752eb
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-06-05 14:49:37 +0300

    PS-1037: Added bridge ProductAlternative -> LocaleFacade to be able to get product names

---
+++ Bundles/ProductAlternative/src/Spryker/Zed/ProductAlternative/Dependency/Facade/ProductAlternativeToLocaleFacadeInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductAlternative\Dependency\Facade;
+
+use Generated\Shared\Transfer\LocaleTransfer;
+
+interface ProductAlternativeToLocaleFacadeInterface
+{
+    /**
+     * @return \Generated\Shared\Transfer\LocaleTransfer
+     */
+    public function getCurrentLocale(): LocaleTransfer;
+}
