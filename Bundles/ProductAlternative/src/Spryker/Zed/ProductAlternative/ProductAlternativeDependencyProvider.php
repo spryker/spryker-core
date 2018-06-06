41fca22a7b2595acecb38b8787414adc14e92644
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-06-06 11:07:46 +0300

    PS-1037: Updated ProductAlternativeDependencyProvider: registered product, product category and locale dependencies

--- Bundles/ProductAlternative/src/Spryker/Zed/ProductAlternative/ProductAlternativeDependencyProvider.php
+++ Bundles/ProductAlternative/src/Spryker/Zed/ProductAlternative/ProductAlternativeDependencyProvider.php
@@ -8,7 +8,76 @@
 namespace Spryker\Zed\ProductAlternative;
 
 use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\ProductAlternative\Dependency\Facade\ProductAlternativeToLocaleFacadeBridge;
+use Spryker\Zed\ProductAlternative\Dependency\QueryContainer\ProductAlternativeToProductCategoryQueryContainerBridge;
+use Spryker\Zed\ProductAlternative\Dependency\QueryContainer\ProductAlternativeToProductQueryContainerBridge;
 
 class ProductAlternativeDependencyProvider extends AbstractBundleDependencyProvider
 {
+    public const QUERY_CONTAINER_PRODUCT = 'QUERY_CONTAINER_PRODUCT';
+    public const QUERY_CONTAINER_PRODUCT_CATEGORY = 'QUERY_CONTAINER_PRODUCT_CATEGORY';
+    public const FACADE_LOCALE = 'FACADE_LOCALE';
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideBusinessLayerDependencies(Container $container)
+    {
+        $container = $this->addProductQueryContainer($container);
+        $container = $this->addProductCategoryQueryContainer($container);
+        $container = $this->addLocaleFacade($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addProductQueryContainer(Container $container)
+    {
+        $container[static::QUERY_CONTAINER_PRODUCT] = function (Container $container) {
+            return new ProductAlternativeToProductQueryContainerBridge(
+                $container->getLocator()->product()->queryContainer()
+            );
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addProductCategoryQueryContainer(Container $container)
+    {
+        $container[static::QUERY_CONTAINER_PRODUCT_CATEGORY] = function (Container $container) {
+            return new ProductAlternativeToProductCategoryQueryContainerBridge(
+                $container->getLocator()->productCategory()->queryContainer()
+            );
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addLocaleFacade(Container $container)
+    {
+        $container[static::FACADE_LOCALE] = function (Container $container) {
+            return new ProductAlternativeToLocaleFacadeBridge(
+                $container->getLocator()->locale()->facade()
+            );
+        };
+
+        return $container;
+    }
 }
