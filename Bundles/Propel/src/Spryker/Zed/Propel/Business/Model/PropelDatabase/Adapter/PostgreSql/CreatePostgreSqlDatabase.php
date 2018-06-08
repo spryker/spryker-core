effbc5ac40f268ad9204e7964b99c6973cc53be8
Valerii Pravoslavnyi <valerio8787@gmail.com>
2018-06-08 11:54:27 +0300

    PS-674: Added handling of using $ character in propel config values.

--- Bundles/Propel/src/Spryker/Zed/Propel/Business/Model/PropelDatabase/Adapter/PostgreSql/CreatePostgreSqlDatabase.php
+++ Bundles/Propel/src/Spryker/Zed/Propel/Business/Model/PropelDatabase/Adapter/PostgreSql/CreatePostgreSqlDatabase.php
@@ -10,11 +10,13 @@ namespace Spryker\Zed\Propel\Business\Model\PropelDatabase\Adapter\PostgreSql;
 use RuntimeException;
 use Spryker\Shared\Config\Config;
 use Spryker\Shared\Propel\PropelConstants;
+use Spryker\Zed\Propel\Business\Exception\UnSupportedCharactersInConfigurationValueException;
 use Spryker\Zed\Propel\Business\Model\PropelDatabase\Command\CreateDatabaseInterface;
 use Symfony\Component\Process\Process;
 
 class CreatePostgreSqlDatabase implements CreateDatabaseInterface
 {
+    protected const SHELL_CHARACTER = '$';
     /**
      * @return void
      */
@@ -52,12 +54,12 @@ class CreatePostgreSqlDatabase implements CreateDatabaseInterface
     {
         return sprintf(
             'PGPASSWORD=%s psql -h %s -p %s -U %s -w -lqt %s | cut -d \| -f 1 | grep -w %s | wc -l',
-            Config::get(PropelConstants::ZED_DB_PASSWORD),
+            $this->getConfigValue(PropelConstants::ZED_DB_PASSWORD),
             Config::get(PropelConstants::ZED_DB_HOST),
             Config::get(PropelConstants::ZED_DB_PORT),
-            Config::get(PropelConstants::ZED_DB_USERNAME),
-            Config::get(PropelConstants::ZED_DB_DATABASE),
-            Config::get(PropelConstants::ZED_DB_DATABASE)
+            $this->getConfigValue(PropelConstants::ZED_DB_USERNAME),
+            $this->getConfigValue(PropelConstants::ZED_DB_DATABASE),
+            $this->getConfigValue(PropelConstants::ZED_DB_DATABASE)
         );
     }
 
@@ -80,11 +82,11 @@ class CreatePostgreSqlDatabase implements CreateDatabaseInterface
     {
         return sprintf(
             'PGPASSWORD=%s psql -h %s -p %s -U %s -w -c "CREATE DATABASE \"%s\" WITH ENCODING=\'UTF8\' LC_COLLATE=\'en_US.UTF-8\' LC_CTYPE=\'en_US.UTF-8\' CONNECTION LIMIT=-1 TEMPLATE=\"template0\"; " %s',
-            Config::get(PropelConstants::ZED_DB_PASSWORD),
+            $this->getConfigValue(PropelConstants::ZED_DB_PASSWORD),
             Config::get(PropelConstants::ZED_DB_HOST),
             Config::get(PropelConstants::ZED_DB_PORT),
-            Config::get(PropelConstants::ZED_DB_USERNAME),
-            Config::get(PropelConstants::ZED_DB_DATABASE),
+            $this->getConfigValue(PropelConstants::ZED_DB_USERNAME),
+            $this->getConfigValue(PropelConstants::ZED_DB_DATABASE),
             'postgres'
         );
     }
@@ -96,8 +98,8 @@ class CreatePostgreSqlDatabase implements CreateDatabaseInterface
     {
         return sprintf(
             'PGPASSWORD=%s sudo createdb %s -E UTF8 -T template0',
-            Config::get(PropelConstants::ZED_DB_PASSWORD),
-            Config::get(PropelConstants::ZED_DB_DATABASE)
+            $this->getConfigValue(PropelConstants::ZED_DB_PASSWORD),
+            $this->getConfigValue(PropelConstants::ZED_DB_DATABASE)
         );
     }
 
@@ -128,5 +130,26 @@ class CreatePostgreSqlDatabase implements CreateDatabaseInterface
     protected function useSudo()
     {
         return Config::get(PropelConstants::USE_SUDO_TO_MANAGE_DATABASE, true);
+    }
+
+    /**
+     * @param string $key
+     *
+     * @throws \Spryker\Zed\Propel\Business\Exception\UnSupportedCharactersInConfigurationValueException
+     *
+     * @return mixed
+     */
+    protected function getConfigValue(string $key)
+    {
+        $value = Config::get($key);
+        if (mb_strpos($value, static::SHELL_CHARACTER) !== false) {
+            throw new UnSupportedCharactersInConfigurationValueException(sprintf(
+                'Configuration value for key "%s" contains unsupported characters (\'%s\') that is forbidden by security reason.',
+                $key,
+                static::SHELL_CHARACTER
+            ));
+        }
+
+        return $value;
     }
 }
