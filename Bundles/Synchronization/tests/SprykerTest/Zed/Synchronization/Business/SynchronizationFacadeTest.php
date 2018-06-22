fce413ecabfb8ea5169a2121135a3ae9d97c8105
Ruslan Dovhospynyi <dovhospynyi@MacBook-Pro-15-Dovgospiniy.local>
2018-06-22 12:16:50 +0300

    PS-1312 functional tests for synchronization module

--- Bundles/Synchronization/tests/SprykerTest/Zed/Synchronization/Business/SynchronizationFacadeTest.php
+++ Bundles/Synchronization/tests/SprykerTest/Zed/Synchronization/Business/SynchronizationFacadeTest.php
@@ -9,8 +9,8 @@ namespace SprykerTest\Zed\Synchronization\Business;
 
 use Codeception\Test\Unit;
 use Elastica\Exception\NotFoundException;
+use Spryker\Zed\CategoryPageSearch\Communication\Plugin\Synchronization\CategoryPageSynchronizationDataPlugin;
 use Spryker\Zed\Kernel\Container;
-use Spryker\Zed\Synchronization\Business\Message\QueueMessageCreatorInterface;
 use Spryker\Zed\Synchronization\Business\SynchronizationBusinessFactory;
 use Spryker\Zed\Synchronization\Business\SynchronizationFacade;
 use Spryker\Zed\Synchronization\Dependency\Client\SynchronizationToQueueClientInterface;
@@ -18,6 +18,40 @@ use Spryker\Zed\Synchronization\Dependency\Client\SynchronizationToSearchClientI
 use Spryker\Zed\Synchronization\Dependency\Client\SynchronizationToStorageClientInterface;
 use Spryker\Zed\Synchronization\Dependency\Service\SynchronizationToUtilEncodingServiceInterface;
 use Spryker\Zed\Synchronization\SynchronizationDependencyProvider;
+use Spryker\Zed\AvailabilityStorage\Communication\Plugin\Synchronization\AvailabilitySynchronizationDataPlugin;
+use Spryker\Zed\CategoryStorage\Communication\Plugin\Synchronization\CategoryNodeSynchronizationDataPlugin;
+use Spryker\Zed\CategoryStorage\Communication\Plugin\Synchronization\CategoryTreeSynchronizationDataPlugin;
+use Spryker\Zed\CmsBlockCategoryStorage\Communication\Plugin\Synchronization\CmsBlockCategorySynchronizationDataPlugin;
+use Spryker\Zed\CmsBlockProductStorage\Communication\Plugin\Synchronization\CmsBlockProductSynchronizationDataPlugin;
+use Spryker\Zed\CmsBlockStorage\Communication\Plugin\Synchronization\CmsBlockSynchronizationDataPlugin;
+use Spryker\Zed\CmsPageSearch\Communication\Plugin\Synchronization\CmsPageSynchronizationDataPlugin;
+use Spryker\Zed\CmsStorage\Communication\Plugin\Synchronization\CmsSynchronizationDataPlugin;
+use Spryker\Zed\GlossaryStorage\Communication\Plugin\Synchronization\GlossarySynchronizationDataPlugin;
+use Spryker\Zed\NavigationStorage\Communication\Plugin\Synchronization\NavigationSynchronizationDataPlugin;
+use Spryker\Zed\PriceProductStorage\Communication\Plugin\Synchronization\PriceProductAbstractSynchronizationDataPlugin;
+use Spryker\Zed\PriceProductStorage\Communication\Plugin\Synchronization\PriceProductConcreteSynchronizationDataPlugin;
+use Spryker\Zed\ProductCategoryFilterStorage\Communication\Plugin\Synchronization\ProductCategoryFilterSynchronizationDataPlugin;
+use Spryker\Zed\ProductCategoryStorage\Communication\Plugin\Synchronization\ProductCategorySynchronizationDataPlugin;
+use Spryker\Zed\ProductGroupStorage\Communication\Plugin\Synchronization\ProductGroupSynchronizationDataPlugin;
+use Spryker\Zed\ProductImageStorage\Communication\Plugin\Synchronization\ProductAbstractImageSynchronizationDataPlugin;
+use Spryker\Zed\ProductImageStorage\Communication\Plugin\Synchronization\ProductConcreteImageSynchronizationDataPlugin;
+use Spryker\Zed\ProductLabelStorage\Communication\Plugin\Synchronization\ProductAbstractLabelSynchronizationDataPlugin;
+use Spryker\Zed\ProductLabelStorage\Communication\Plugin\Synchronization\ProductLabelDictionarySynchronizationDataPlugin;
+use Spryker\Zed\ProductMeasurementUnitStorage\Communication\Plugin\Synchronization\ProductConcreteMeasurementUnitSynchronizationDataPlugin;
+use Spryker\Zed\ProductMeasurementUnitStorage\Communication\Plugin\Synchronization\ProductMeasurementUnitSynchronizationDataPlugin;
+use Spryker\Zed\ProductOptionStorage\Communication\Plugin\Synchronization\ProductOptionSynchronizationDataPlugin;
+use Spryker\Zed\ProductPageSearch\Communication\Plugin\Synchronization\ProductPageSynchronizationDataPlugin;
+use Spryker\Zed\ProductQuantityStorage\Communication\Plugin\Synchronization\ProductQuantitySynchronizationDataPlugin;
+use Spryker\Zed\ProductRelationStorage\Communication\Plugin\Synchronization\ProductRelationSynchronizationDataPlugin;
+use Spryker\Zed\ProductReviewSearch\Communication\Plugin\Synchronization\ProductReviewSynchronizationDataPlugin as ProductReviewSearchSynchronizationDataPlugin;
+use Spryker\Zed\ProductReviewStorage\Communication\Plugin\Synchronization\ProductReviewSynchronizationDataPlugin;
+use Spryker\Zed\ProductSearchConfigStorage\Communication\Plugin\Synchronization\ProductSearchConfigSynchronizationDataPlugin;
+use Spryker\Zed\ProductSetPageSearch\Communication\Plugin\Synchronization\ProductSetSynchronizationDataPlugin as ProductSetSearchSynchronizationDataPlugin;
+use Spryker\Zed\ProductSetStorage\Communication\Plugin\Synchronization\ProductSetSynchronizationDataPlugin;
+use Spryker\Zed\ProductStorage\Communication\Plugin\Synchronization\ProductAbstractSynchronizationDataPlugin;
+use Spryker\Zed\ProductStorage\Communication\Plugin\Synchronization\ProductConcreteSynchronizationDataPlugin;
+use Spryker\Zed\UrlStorage\Communication\Plugin\Synchronization\UrlRedirectSynchronizationDataPlugin;
+use Spryker\Zed\UrlStorage\Communication\Plugin\Synchronization\UrlSynchronizationDataPlugin;
 
 /**
  * Auto-generated group annotations
@@ -187,12 +221,18 @@ class SynchronizationFacadeTest extends Unit
     {
         $container = new Container();
         $container[SynchronizationDependencyProvider::CLIENT_QUEUE] = function (Container $container) {
-            return $this->createQueueClientBridge();
+            $queueMock = $this->createQueueClientBridge();
+            $queueMock->expects($this->atLeastOnce())->method('sendMessages');
+
+            return $queueMock;
         };
 
         $container[SynchronizationDependencyProvider::PLUGINS_SYNCHRONIZATION_DATA] = function (Container $container) {
             return $this->createSynchronizationDataPlugins();
         };
+
+        $this->prepareFacade($container);
+        $this->synchronizationFacade->exportSynchronizedData([], []);
     }
 
     /**
@@ -209,12 +249,48 @@ class SynchronizationFacadeTest extends Unit
     }
 
     /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
+     * @return array
      */
     protected function createSynchronizationDataPlugins()
     {
         return [
-
+            //Search plugins
+            new CategoryPageSynchronizationDataPlugin(),
+            new CmsPageSynchronizationDataPlugin(),
+            new ProductPageSynchronizationDataPlugin(),
+            new ProductReviewSearchSynchronizationDataPlugin(),
+            new ProductSetSearchSynchronizationDataPlugin(),
+            //Storage plugins
+            new AvailabilitySynchronizationDataPlugin(),
+            new CategoryTreeSynchronizationDataPlugin(),
+            new CategoryNodeSynchronizationDataPlugin(),
+            new CmsBlockCategorySynchronizationDataPlugin(),
+            new CmsBlockProductSynchronizationDataPlugin(),
+            new CmsBlockSynchronizationDataPlugin(),
+            new CmsSynchronizationDataPlugin(),
+            new NavigationSynchronizationDataPlugin(),
+            new GlossarySynchronizationDataPlugin(),
+            new PriceProductConcreteSynchronizationDataPlugin(),
+            new PriceProductAbstractSynchronizationDataPlugin(),
+            new ProductCategoryFilterSynchronizationDataPlugin(),
+            new ProductCategorySynchronizationDataPlugin(),
+            new ProductGroupSynchronizationDataPlugin(),
+            new ProductAbstractImageSynchronizationDataPlugin(),
+            new ProductConcreteImageSynchronizationDataPlugin(),
+            new ProductAbstractLabelSynchronizationDataPlugin(),
+            new ProductLabelDictionarySynchronizationDataPlugin(),
+            new ProductMeasurementUnitSynchronizationDataPlugin(),
+            new ProductConcreteMeasurementUnitSynchronizationDataPlugin(),
+            new ProductQuantitySynchronizationDataPlugin(),
+            new ProductOptionSynchronizationDataPlugin(),
+            new ProductRelationSynchronizationDataPlugin(),
+            new ProductReviewSynchronizationDataPlugin(),
+            new ProductSearchConfigSynchronizationDataPlugin(),
+            new ProductSetSynchronizationDataPlugin(),
+            new ProductConcreteSynchronizationDataPlugin(),
+            new ProductAbstractSynchronizationDataPlugin(),
+            new UrlRedirectSynchronizationDataPlugin(),
+            new UrlSynchronizationDataPlugin(),
         ];
     }
 
