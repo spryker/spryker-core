fec44f9cc1ec8b33f66ba131aec9e1f67340c46d
Alexandr Veselov <alexander.veselov@spryker.com>
2018-06-26 15:11:49 +0000

    CORE-2413: create plotly js dependecy

---
+++ Bundles/Chart/assets/Zed/js/spryker-zed-plotly-main.entry.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+'use strict';
+
+var Plotly = require('plotly.js');
+
+window.Plotly = Plotly;
