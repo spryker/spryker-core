1c5473504434574cef45ac849012ff08b4aed41b
Oleh Okhremchuk <oleg.okhremchuk@spryker.com>
2018-07-03 19:00:11 +0300

    GLUE-171 Updated status codes according documentation

--- Bundles/CustomersRestApi/src/Spryker/Glue/CustomersRestApi/CustomersRestApiConfig.php
+++ Bundles/CustomersRestApi/src/Spryker/Glue/CustomersRestApi/CustomersRestApiConfig.php
@@ -12,8 +12,8 @@ class CustomersRestApiConfig extends AbstractBundleConfig
 {
     public const RESOURCE_REGISTER_CUSTOMERS = 'register-customer';
 
-    public const RESPONSE_CODE_CUSTOMER_ALREADY_EXISTS = '500';
-    public const RESPONSE_CODE_CUSTOMER_CANT_REGISTER_CUSTOMER = '501';
+    public const RESPONSE_CODE_CUSTOMER_ALREADY_EXISTS = '400';
+    public const RESPONSE_CODE_CUSTOMER_CANT_REGISTER_CUSTOMER = '401';
 
     public const RESPONSE_MESSAGE_CUSTOMER_ALREADY_EXISTS = 'Customer with this email already exists.';
     public const RESPONSE_MESSAGE_CUSTOMER_CANT_REGISTER_CUSTOMER = 'Can`t register customer.';
