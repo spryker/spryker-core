b802498a78f884f9dbca7d70584e1e448e20e656
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-07-06 12:40:01 +0300

    PS-2045: Added new property 'errorType' to CheckoutErrorTransfer; Created AvailabilityConstants with ERROR_TYPE_AVAILABILITY constant; Added errorType set to CheckoutErrorTransfer objects in Availability module

--- Bundles/Availability/src/Spryker/Zed/Availability/Business/Model/ProductsAvailableCheckoutPreCondition.php
+++ Bundles/Availability/src/Spryker/Zed/Availability/Business/Model/ProductsAvailableCheckoutPreCondition.php
@@ -11,6 +11,7 @@ use Generated\Shared\Transfer\CheckoutErrorTransfer;
 use Generated\Shared\Transfer\CheckoutResponseTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
 use Generated\Shared\Transfer\StoreTransfer;
+use Spryker\Shared\Availability\AvailabilityConstants;
 use Spryker\Zed\Availability\AvailabilityConfig;
 
 class ProductsAvailableCheckoutPreCondition implements ProductsAvailableCheckoutPreConditionInterface
@@ -117,6 +118,7 @@ class ProductsAvailableCheckoutPreCondition implements ProductsAvailableCheckout
         $checkoutErrorTransfer
             ->setErrorCode($this->availabilityConfig->getProductUnavailableErrorCode())
             ->setMessage(static::CHECKOUT_PRODUCT_UNAVAILABLE_TRANSLATION_KEY)
+            ->setErrorType(AvailabilityConstants::ERROR_TYPE_AVAILABILITY)
             ->setParameters([
                 static::CHECKOUT_PRODUCT_UNAVAILABLE_PARAMETER_SKU => $sku,
             ]);
