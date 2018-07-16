af5b74ba88b882bacb52c554f179f4c712b6b63b
AntonKhabiuk <brotherosirus@gmail.com>
2018-07-16 09:46:19 +0300

    GLUE-239 Add File docblock

--- Bundles/UtilUuidGenerator/src/Spryker/Service/UtilUuidGenerator/UtilUuidGeneratorService.php
+++ Bundles/UtilUuidGenerator/src/Spryker/Service/UtilUuidGenerator/UtilUuidGeneratorService.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
 
 namespace Spryker\Service\UtilUuidGenerator;
 
