4dc0cee63fb9c663e1485aa095a3eb0cab12b151
Valerii Pravoslavnyi <valerio8787@gmail.com>
2018-07-16 10:11:26 +0300

    Increased phpstan level, clenaups

--- Bundles/SearchRestApi/src/Spryker/Glue/SearchRestApi/Controller/SuggestionsResourceController.php
+++ Bundles/SearchRestApi/src/Spryker/Glue/SearchRestApi/Controller/SuggestionsResourceController.php
@@ -6,21 +6,22 @@
 
 namespace Spryker\Glue\SearchRestApi\Controller;
 
-use Spryker\Glue\GlueApplication\Rest\Controller\AbstractRestController;
 use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface;
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+use Spryker\Glue\Kernel\Controller\AbstractController;
 
 /**
  * @method \Spryker\Glue\SearchRestApi\SearchRestApiFactory getFactory()
  */
-class SuggestionsResourceController extends AbstractRestController
+class SuggestionsResourceController extends AbstractController
 {
     /**
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
      * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface
      */
-    public function getAction(): RestResponseInterface
+    public function getAction(RestRequestInterface $restRequest): RestResponseInterface
     {
-        $restRequest = $this->getRestRequest();
-
         return $this->getFactory()
             ->createSuggestionsReader()
             ->catalogSuggestionsSearch($restRequest);
