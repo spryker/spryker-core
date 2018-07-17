ec2eda3a38beac5771adcc4f439d7c51deb475b1
Oleh Okhremchuk <oleg.okhremchuk@spryker.com>
2018-07-17 18:03:20 +0300

    GLUE-27 autogenerated files fixes

---
+++ Bundles/SearchRestApi/tests/SprykerTest/Glue/SearchRestApi/_support/SearchRestApiProcessorTester.php
@@ -0,0 +1,26 @@
+<?php
+namespace SprykerTest\Glue\SearchRestApi;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class SearchRestApiProcessorTester extends \Codeception\Actor
+{
+    use _generated\SearchRestApiProcessorTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+}
