bab1d15c0833f07934691905f4f29ce8fec0549a
mscherer <mark.scherer@spryker.com>
2018-07-28 12:04:44 +0200

    Fix CS.

--- Bundles/DataImport/src/Spryker/Zed/DataImport/Business/DataImportBusinessFactory.php
+++ Bundles/DataImport/src/Spryker/Zed/DataImport/Business/DataImportBusinessFactory.php
@@ -112,7 +112,7 @@ class DataImportBusinessFactory extends AbstractBusinessFactory
      * @param string $importType
      * @param \Spryker\Zed\DataImport\Business\Model\DataReader\DataReaderInterface $reader
      *
-     * @return \Spryker\Zed\DataImport\Business\Model\DataImporterInterface|\Spryker\Zed\DataImport\Business\Model\DataImporterBeforeImportAwareInterface|\Spryker\Zed\DataImport\Business\Model\DataImporterAfterImportAwareInterface
+     * @return \Spryker\Zed\DataImport\Business\Model\DataImporterInterface|\Spryker\Zed\DataImport\Business\Model\DataSet\DataSetStepBrokerAwareInterface
      */
     public function createDataImporter($importType, DataReaderInterface $reader)
     {
