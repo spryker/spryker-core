bdd16da35bb6bebd2d46d00691329455da53856c
Evhen Odokienko <evhen.odokienko@spryker.com>
2018-07-31 13:03:43 +0300

    PS-3171:PS-3197 Create modules: ShoppingListStorage
    PS-3171 Enabler: Create infrastructure

---
+++ Bundles/ShoppingListStorage/src/Spryker/Zed/ShoppingListStorage/Business/ShoppingListStorageBusinessFactory.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ShoppingListStorage\Business;
+
+use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
+
+class ShoppingListStorageBusinessFactory extends AbstractBusinessFactory
+{
+}
---
+++ Bundles/ShoppingListStorage/src/Spryker/Zed/ShoppingListStorage/Business/ShoppingListStorageFacade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ShoppingListStorage\Business;
+
+use Spryker\Zed\Kernel\Business\AbstractFacade;
+
+/**
+ * @method \Spryker\Zed\ShoppingListStorage\Business\ShoppingListStorageBusinessFactory getFactory()
+ */
+class ShoppingListStorageFacade extends AbstractFacade implements ShoppingListStorageFacadeInterface
+{
+}
