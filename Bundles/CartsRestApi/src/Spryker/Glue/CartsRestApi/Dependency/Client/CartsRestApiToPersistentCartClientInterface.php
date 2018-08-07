7f2cbef203b02c0f26adbbf10461938f8a8c1c67
Eugenia <eugenia.poidenko@spryker.com>
2018-08-07 19:10:59 +0300

    GLUE-57 Create a new cart | GLUE-659 Implement cart creation via PersistentCartClient * Added create cart action * Adde relation to the persistent cart

---
+++ Bundles/CartsRestApi/src/Spryker/Glue/CartsRestApi/Dependency/Client/CartsRestApiToPersistentCartClientInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\CartsRestApi\Dependency\Client;
+
+use Generated\Shared\Transfer\QuoteResponseTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+
+interface CartsRestApiToPersistentCartClientInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\QuoteResponseTransfer
+     */
+    public function deleteQuote(QuoteTransfer $quoteTransfer): QuoteResponseTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\QuoteResponseTransfer
+     */
+    public function createQuote(QuoteTransfer $quoteTransfer): QuoteResponseTransfer;
+}
