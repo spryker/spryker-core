c836e566611274de8f56221474000b6caece5ead
Eugenia <eugenia.poidenko@spryker.com>
2018-08-08 16:43:43 +0300

    GLUE-54 Add items to an existing cart. GLUE-56 Update item quantity GLUE-55 Remove items from a cart

---
+++ Bundles/CartsRestApi/src/Spryker/Glue/CartsRestApi/Dependency/Client/CartsRestApiToCartClientBridge.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\CartsRestApi\Dependency\Client;
+
+use Generated\Shared\Transfer\CartChangeTransfer;
+use Generated\Shared\Transfer\ItemTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+
+class CartsRestApiToCartClientBridge implements CartsRestApiToCartClientInterface
+{
+    /**
+     * @var \Spryker\Client\Cart\CartClientInterface
+     */
+    protected $cartClient;
+
+    /**
+     * @param \Spryker\Client\Cart\CartClientInterface $cartClient
+     */
+    public function __construct($cartClient)
+    {
+        $this->cartClient = $cartClient;
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function getQuote(): QuoteTransfer
+    {
+        return $this->cartClient->getQuote();
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ItemTransfer $itemTransfer
+     * @param array $params
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function addItem(ItemTransfer $itemTransfer, array $params = []): QuoteTransfer
+    {
+        return $this->cartClient->addItem($itemTransfer, $params);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CartChangeTransfer $itemTransfer
+     * @param array $params
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function addValidItems(CartChangeTransfer $itemTransfer, array $params = []): QuoteTransfer
+    {
+        return $this->cartClient->addValidItems($itemTransfer, $params);
+    }
+
+    /**
+     * @param string $sku
+     * @param string|null $groupKey
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function removeItem($sku, $groupKey = null): QuoteTransfer
+    {
+        return $this->cartClient->removeItem($sku, $groupKey);
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\QuoteResponseTransfer
+     */
+    public function validateQuote()
+    {
+        return $this->cartClient->validateQuote();
+    }
+
+    /**
+     * @return void
+     */
+    public function clearQuote()
+    {
+        $this->cartClient->clearQuote();
+    }
+
+    /**
+     * @param string $sku
+     * @param string|null $groupKey
+     * @param int $quantity
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function changeItemQuantity($sku, $groupKey = null, $quantity = 1)
+    {
+        return $this->cartClient->changeItemQuantity($sku, $groupKey, $quantity);
+    }
+}
