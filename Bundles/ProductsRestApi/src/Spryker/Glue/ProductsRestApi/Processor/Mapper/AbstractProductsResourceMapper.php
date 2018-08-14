62332b3a14ef1f200f0f81318c95347ed057238e
Valerii Pravoslavnyi <valerio8787@gmail.com>
2018-08-15 00:38:25 +0300

    GlUE-142: removed product concrete ids from reponse

--- Bundles/ProductsRestApi/src/Spryker/Glue/ProductsRestApi/Processor/Mapper/AbstractProductsResourceMapper.php
+++ Bundles/ProductsRestApi/src/Spryker/Glue/ProductsRestApi/Processor/Mapper/AbstractProductsResourceMapper.php
@@ -14,6 +14,9 @@ use Spryker\Glue\ProductsRestApi\ProductsRestApiConfig;
 
 class AbstractProductsResourceMapper implements AbstractProductsResourceMapperInterface
 {
+    protected const KEY_PRODUCT_CONCRETE_IDS = 'product_concrete_ids';
+    protected const KEY_ATTRIBUTE_VARIANTS = 'attribute_variants';
+    protected const KEY_ID_PRODUCT_CONCRETE = 'id_product_concrete';
     /**
      * @var \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
      */
@@ -36,11 +39,40 @@ class AbstractProductsResourceMapper implements AbstractProductsResourceMapperIn
     {
         $restAbstractProductsAttributesTransfer = (new AbstractProductsRestAttributesTransfer())
             ->fromArray($abstractProductData, true);
+        $this->changeIdsToSkus($restAbstractProductsAttributesTransfer);
 
         return $this->restResourceBuilder->createRestResource(
             ProductsRestApiConfig::RESOURCE_ABSTRACT_PRODUCTS,
             $restAbstractProductsAttributesTransfer->getSku(),
             $restAbstractProductsAttributesTransfer
         );
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\AbstractProductsRestAttributesTransfer $restAbstractProductsAttributesTransfer
+     *
+     * @return \Generated\Shared\Transfer\AbstractProductsRestAttributesTransfer
+     */
+    protected function changeIdsToSkus(
+        AbstractProductsRestAttributesTransfer $restAbstractProductsAttributesTransfer
+    ): AbstractProductsRestAttributesTransfer {
+
+        $attributeMap = $restAbstractProductsAttributesTransfer->getAttributeMap();
+        if (!isset($attributeMap[static::KEY_PRODUCT_CONCRETE_IDS])) {
+            return $restAbstractProductsAttributesTransfer;
+        }
+        $productConcreteIds = array_flip($attributeMap[static::KEY_PRODUCT_CONCRETE_IDS]);
+
+        if (isset($attributeMap[static::KEY_ATTRIBUTE_VARIANTS])) {
+            foreach ($attributeMap[static::KEY_ATTRIBUTE_VARIANTS] as $key => $data) {
+                $attributeMap[static::KEY_ATTRIBUTE_VARIANTS][$key][static::KEY_ID_PRODUCT_CONCRETE] =
+                    $productConcreteIds[$data[static::KEY_ID_PRODUCT_CONCRETE]];
+            }
+        }
+
+        $attributeMap[static::KEY_PRODUCT_CONCRETE_IDS] = array_values($productConcreteIds);
+
+        return $restAbstractProductsAttributesTransfer
+            ->setAttributeMap($attributeMap);
     }
 }
