b57dc23ee31ac3c3d3f889c0c5abdea04573ab32
Mahmoud Ali <mahmoud.ali@spryker.com>
2018-08-22 16:38:36 +0200

    PS-4244 Don't send to queue for ProductPackagingUnitSynchronizationDataPluginTest

--- Bundles/ProductPackagingUnitStorage/tests/SprykerTest/Zed/ProductPackagingUnitStorage/Communication/Plugin/Synchronization/ProductPackagingUnitSynchronizationDataPluginTest.php
+++ Bundles/ProductPackagingUnitStorage/tests/SprykerTest/Zed/ProductPackagingUnitStorage/Communication/Plugin/Synchronization/ProductPackagingUnitSynchronizationDataPluginTest.php
@@ -8,7 +8,17 @@
 namespace SprykerTest\Zed\ProductPackagingUnitStorage\Communication\Plugin\Synchronization;
 
 use Codeception\Test\Unit;
+use Generated\Shared\Transfer\SpyProductAbstractEntityTransfer;
+use Generated\Shared\Transfer\SpyProductEntityTransfer;
+use Generated\Shared\Transfer\SpyProductPackagingLeadProductEntityTransfer;
+use Generated\Shared\Transfer\SpyProductPackagingUnitEntityTransfer;
+use Generated\Shared\Transfer\SpyProductPackagingUnitTypeEntityTransfer;
+use Spryker\Client\Kernel\Container;
+use Spryker\Client\Queue\QueueDependencyProvider;
+use Spryker\Zed\ProductPackagingUnitStorage\Business\ProductPackagingUnitStorageBusinessFactory;
+use Spryker\Zed\ProductPackagingUnitStorage\Business\ProductPackagingUnitStorageFacade;
 use Spryker\Zed\ProductPackagingUnitStorage\Communication\Plugin\Synchronization\ProductPackagingUnitSynchronizationDataPlugin;
+use SprykerTest\Zed\ProductPackagingUnitStorage\ProductPackagingUnitStorageConfigMock;
 
 /**
  * Auto-generated group annotations
@@ -24,11 +34,27 @@ use Spryker\Zed\ProductPackagingUnitStorage\Communication\Plugin\Synchronization
 class ProductPackagingUnitSynchronizationDataPluginTest extends Unit
 {
     protected const TEST_INVALID_ID = 111;
+    protected const PACKAGING_TYPE_DEFAULT = 'item';
+    protected const PACKAGING_TYPE = 'box';
 
     /**
      * @var \SprykerTest\Zed\ProductPackagingUnitStorage\ProductPackagingUnitStorageCommunicationTester
      */
     protected $tester;
+
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->tester->setDependency(QueueDependencyProvider::QUEUE_ADAPTERS, function (Container $container) {
+            return [
+                $container->getLocator()->rabbitMq()->client()->createQueueAdapter(),
+            ];
+        });
+    }
 
     /**
      * @return void
@@ -48,10 +74,60 @@ class ProductPackagingUnitSynchronizationDataPluginTest extends Unit
      */
     public function testGetDataWithoutIds(): void
     {
+        $this->tester->assertStorageDatabaseTableIsEmpty();
+        $this->haveBoxProductPackagingUnit();
+
         $productPackagingUnitSynchronizationDataPlugin = $this->getProductPackagingUnitSynchronizationDataPlugin();
         $synchronizationDataTransfers = $productPackagingUnitSynchronizationDataPlugin->getData();
 
         $this->assertNotEmpty($synchronizationDataTransfers);
+    }
+
+    /**
+     * @return void
+     */
+    protected function haveBoxProductPackagingUnit(): void
+    {
+        $itemProductConcreteTransfer = $this->tester->haveProduct();
+        $boxProductConcreteTransfer = $this->tester->haveProduct([
+            SpyProductEntityTransfer::FK_PRODUCT_ABSTRACT => $itemProductConcreteTransfer->getFkProductAbstract(),
+        ], [
+            SpyProductAbstractEntityTransfer::ID_PRODUCT_ABSTRACT => $itemProductConcreteTransfer->getFkProductAbstract(),
+        ]);
+
+        $this->tester->haveProductPackagingLeadProduct([
+            SpyProductPackagingLeadProductEntityTransfer::FK_PRODUCT => $itemProductConcreteTransfer->getIdProductConcrete(),
+            SpyProductPackagingLeadProductEntityTransfer::FK_PRODUCT_ABSTRACT => $itemProductConcreteTransfer->getFkProductAbstract(),
+        ]);
+
+        $boxProductPackagingUnitType = $this->tester->haveProductPackagingUnitType([SpyProductPackagingUnitTypeEntityTransfer::NAME => static::PACKAGING_TYPE]);
+        $itemProductPackagingUnitType = $this->tester->haveProductPackagingUnitType([SpyProductPackagingUnitTypeEntityTransfer::NAME => static::PACKAGING_TYPE_DEFAULT]);
+
+        $this->tester->haveProductPackagingUnit([
+            SpyProductPackagingUnitEntityTransfer::FK_PRODUCT => $itemProductConcreteTransfer->getIdProductConcrete(),
+            SpyProductPackagingUnitEntityTransfer::FK_PRODUCT_PACKAGING_UNIT_TYPE => $itemProductPackagingUnitType->getIdProductPackagingUnitType(),
+        ]);
+
+        $this->tester->haveProductPackagingUnit([
+            SpyProductPackagingUnitEntityTransfer::FK_PRODUCT => $boxProductConcreteTransfer->getIdProductConcrete(),
+            SpyProductPackagingUnitEntityTransfer::FK_PRODUCT_PACKAGING_UNIT_TYPE => $boxProductPackagingUnitType->getIdProductPackagingUnitType(),
+        ]);
+
+        $this->getProductPackagingUnitStorageFacade()->publishProductAbstractPackaging([$boxProductConcreteTransfer->getFkProductAbstract()]);
+    }
+
+    /**
+     * @return \Spryker\Zed\ProductPackagingUnitStorage\Business\ProductPackagingUnitStorageFacade
+     */
+    protected function getProductPackagingUnitStorageFacade()
+    {
+        $factory = new ProductPackagingUnitStorageBusinessFactory();
+        $factory->setConfig(new ProductPackagingUnitStorageConfigMock());
+
+        $facade = new ProductPackagingUnitStorageFacade();
+        $facade->setFactory($factory);
+
+        return $facade;
     }
 
     /**
