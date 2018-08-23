02f0707fdbad8e54a92694e7a95258139aa86117
Stanislav Matveyev <stanislav.matveyev@spryker.com>
2018-08-23 12:37:28 +0300

    PS-27: fixed CS

--- Bundles/CustomerAccessStorage/src/Spryker/Shared/CustomerAccessStorage/CustomerAccessStorageConstants.php
+++ Bundles/CustomerAccessStorage/src/Spryker/Shared/CustomerAccessStorage/CustomerAccessStorageConstants.php
@@ -7,6 +7,9 @@
 
 namespace Spryker\Shared\CustomerAccessStorage;
 
+/**
+ * Declares global environment configuration keys. Do not use it for other class constants.
+ */
 interface CustomerAccessStorageConstants
 {
     /**
