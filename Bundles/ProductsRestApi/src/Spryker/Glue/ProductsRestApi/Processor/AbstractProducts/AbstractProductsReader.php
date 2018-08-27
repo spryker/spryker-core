6cda3fe7ea90e27246f608c4f3945e8ae785ab9d
AntonKhabiuk <brotherosirus@gmail.com>
2018-08-27 17:59:07 +0300

    GLUE-525 Implement Relationship between Catalog search and Product

--- Bundles/ProductsRestApi/src/Spryker/Glue/ProductsRestApi/Processor/AbstractProducts/AbstractProductsReader.php
+++ Bundles/ProductsRestApi/src/Spryker/Glue/ProductsRestApi/Processor/AbstractProducts/AbstractProductsReader.php
@@ -99,6 +99,27 @@ class AbstractProductsReader implements AbstractProductsReaderInterface
     }
 
     /**
+     * @param string $productAbstractSku
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface|null
+     */
+    public function findOneByProductAbstractSku(string $productAbstractSku, RestRequestInterface $restRequest): ?RestResourceInterface
+    {
+        $abstractProductData = $this->productResourceAliasStorageClient->findProductAbstractStorageDataBySku(
+            $productAbstractSku,
+            $restRequest->getMetadata()->getLocale()
+        );
+
+        if (!$abstractProductData) {
+            return null;
+        }
+
+        return $this->abstractProductsResourceMapper
+            ->mapAbstractProductsResponseAttributesTransferToRestResponse($abstractProductData);
+    }
+
+    /**
      * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface $restResource
      * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
      *
