346e238e8ce33b1b958bbdfaf268a09454c74efb
eugenia.poidenko <eugenia.poidenko@spryker.com>
2018-08-27 11:08:31 +0000

    GLUE-603 Rest request validator: cr edits

--- Bundles/RestRequestValidator/src/Spryker/Zed/RestRequestValidator/Dependency/Service/RestRequestValidatorToFileSystemBridge.php
+++ Bundles/RestRequestValidator/src/Spryker/Zed/RestRequestValidator/Dependency/Service/RestRequestValidatorToFileSystemBridge.php
@@ -7,6 +7,10 @@
 
 namespace Spryker\Zed\RestRequestValidator\Dependency\Service;
 
+use Generated\Shared\Transfer\FileSystemContentTransfer;
+use Generated\Shared\Transfer\FileSystemCreateDirectoryTransfer;
+use Generated\Shared\Transfer\FileSystemQueryTransfer;
+
 class RestRequestValidatorToFileSystemBridge implements RestRequestValidatorToFileSystemInterface
 {
     /**
@@ -20,5 +24,35 @@ class RestRequestValidatorToFileSystemBridge implements RestRequestValidatorToFi
     public function __construct($fileSystemService)
     {
         $this->fileSystemService = $fileSystemService;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\FileSystemCreateDirectoryTransfer $fileSystemCreateDirectoryTransfer
+     *
+     * @return void
+     */
+    public function createDirectory(FileSystemCreateDirectoryTransfer $fileSystemCreateDirectoryTransfer): void
+    {
+        $this->fileSystemService->createDirectory($fileSystemCreateDirectoryTransfer);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\FileSystemQueryTransfer $fileSystemQueryTransfer
+     *
+     * @return void
+     */
+    public function has(FileSystemQueryTransfer $fileSystemQueryTransfer): void
+    {
+        $this->fileSystemService->has($fileSystemQueryTransfer);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\FileSystemContentTransfer $fileSystemContentTransfer
+     *
+     * @return void
+     */
+    public function write(FileSystemContentTransfer $fileSystemContentTransfer): void
+    {
+        $this->fileSystemService->write($fileSystemContentTransfer);
     }
 }
