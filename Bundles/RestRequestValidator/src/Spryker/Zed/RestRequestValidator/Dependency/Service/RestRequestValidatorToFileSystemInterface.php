7d0027daa6de41c70f58028315fa85e92bd8700b
eugenia.poidenko <eugenia.poidenko@spryker.com>
2018-08-27 11:08:31 +0000

    GLUE-603 Rest request validator: cr edits

--- Bundles/RestRequestValidator/src/Spryker/Zed/RestRequestValidator/Dependency/Service/RestRequestValidatorToFileSystemInterface.php
+++ Bundles/RestRequestValidator/src/Spryker/Zed/RestRequestValidator/Dependency/Service/RestRequestValidatorToFileSystemInterface.php
@@ -7,6 +7,30 @@
 
 namespace Spryker\Zed\RestRequestValidator\Dependency\Service;
 
+use Generated\Shared\Transfer\FileSystemContentTransfer;
+use Generated\Shared\Transfer\FileSystemCreateDirectoryTransfer;
+use Generated\Shared\Transfer\FileSystemQueryTransfer;
+
 interface RestRequestValidatorToFileSystemInterface
 {
+    /**
+     * @param \Generated\Shared\Transfer\FileSystemCreateDirectoryTransfer $fileSystemCreateDirectoryTransfer
+     *
+     * @return void
+     */
+    public function createDirectory(FileSystemCreateDirectoryTransfer $fileSystemCreateDirectoryTransfer): void;
+
+    /**
+     * @param \Generated\Shared\Transfer\FileSystemQueryTransfer $fileSystemQueryTransfer
+     *
+     * @return void
+     */
+    public function has(FileSystemQueryTransfer $fileSystemQueryTransfer): void;
+
+    /**
+     * @param \Generated\Shared\Transfer\FileSystemContentTransfer $fileSystemContentTransfer
+     *
+     * @return void
+     */
+    public function write(FileSystemContentTransfer $fileSystemContentTransfer): void;
 }
