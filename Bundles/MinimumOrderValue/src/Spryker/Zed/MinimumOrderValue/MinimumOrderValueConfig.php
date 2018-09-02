e2a6c090a70b26105836202291171482e4898764
Oleh Hladchenko <glad.oleg@gmail.com>
2018-09-02 21:50:24 +0300

    PS-3856 Refactoring.

--- Bundles/MinimumOrderValue/src/Spryker/Zed/MinimumOrderValue/MinimumOrderValueConfig.php
+++ Bundles/MinimumOrderValue/src/Spryker/Zed/MinimumOrderValue/MinimumOrderValueConfig.php
@@ -11,13 +11,4 @@ use Spryker\Zed\Kernel\AbstractBundleConfig;
 
 class MinimumOrderValueConfig extends AbstractBundleConfig
 {
-    protected const MINIMUM_ORDER_VALUE_EXPENSE_TYPE = 'MINIMUM_ORDER_VALUE_EXPENSE_TYPE';
-
-    /**
-     * @return string
-     */
-    public function getMinimumOrderValueExpenseType(): string
-    {
-        return static::MINIMUM_ORDER_VALUE_EXPENSE_TYPE;
-    }
 }
