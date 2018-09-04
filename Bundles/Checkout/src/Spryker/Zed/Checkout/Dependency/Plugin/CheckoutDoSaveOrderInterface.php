06bb7831acf8e6c77223fd4a7c0fdd2f2aea52bf
Oleh Hladchenko <glad.oleg@gmail.com>
2018-09-04 17:44:11 +0300

    PS-3856 Refactoring. [skip-ci]

--- Bundles/Checkout/src/Spryker/Zed/Checkout/Dependency/Plugin/CheckoutDoSaveOrderInterface.php
+++ Bundles/Checkout/src/Spryker/Zed/Checkout/Dependency/Plugin/CheckoutDoSaveOrderInterface.php
@@ -9,6 +9,9 @@ namespace Spryker\Zed\Checkout\Dependency\Plugin;
 
 use Spryker\Zed\CheckoutExtension\Dependency\Plugin\CheckoutDoSaveOrderInterface as ExtensionCheckoutDoSaveOrderInterface;
 
+/**
+ * @deprecated Use Spryker\Zed\CheckoutExtension\Dependency\Plugin\CheckoutDoSaveOrderInterface instead.
+ */
 interface CheckoutDoSaveOrderInterface extends ExtensionCheckoutDoSaveOrderInterface
 {
 }
