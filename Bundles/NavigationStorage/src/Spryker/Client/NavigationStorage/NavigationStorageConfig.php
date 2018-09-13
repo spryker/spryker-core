6f33e68b59f28b8648a2d80ccf4682c495490800
René Klatt <rene.klatt@spryker.com>
2018-09-13 14:21:08 +0000

    added collector compatibility mode for NavigationStorage

--- Bundles/NavigationStorage/src/Spryker/Client/NavigationStorage/NavigationStorageConfig.php
+++ Bundles/NavigationStorage/src/Spryker/Client/NavigationStorage/NavigationStorageConfig.php
@@ -5,20 +5,20 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Client\UrlStorage;
+namespace Spryker\Client\NavigationStorage;
 
 use Spryker\Client\Kernel\AbstractBundleConfig;
 
-class UrlStorageConfig extends AbstractBundleConfig
+class NavigationStorageConfig extends AbstractBundleConfig
 {
     /**
      * To be able to work with data exported with collectors to redis, we need to bring this module into compatibility
-     * mode. If this is turned on the UrlClient will be used instead.
+     * mode. If this is turned on the NavigationClient will be used instead.
      *
      * @return bool
      */
     public static function isCollectorCompatibilityMode()
     {
-        return false;
+        return true;
     }
 }
