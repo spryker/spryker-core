d2fb44a0a3947409e9a73e31a9b8184433fa6d9d
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-09-14 12:29:39 +0300

    PS-2600: AA Review Fixes: Fixed `PermissionBusinessFactory::createPermissionPlugin` missing second argument; Added new test for new `PermissionFacade::getRegisteredNonInfrastructuralPermissions` method

---
+++ Bundles/Permission/tests/SprykerTest/Zed/Permission/Business/PermissionFacadeTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\Permission\Business;
+
+use ArrayObject;
+use Codeception\Test\Unit;
+use Spryker\Zed\Permission\Business\PermissionFacadeInterface;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Zed
+ * @group Permission
+ * @group Business
+ * @group Facade
+ * @group PermissionFacadeTest
+ * Add your own group annotations below this line
+ */
+class PermissionFacadeTest extends Unit
+{
+    /**
+     * @var \SprykerTest\Zed\Permission\PermissionBusinessTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testGetRegisteredNonInfrastructuralPermissionsDoesNotReturnInfrastructuralPermissions(): void
+    {
+        $availablePermissions = $this->getPermissionFacade()
+            ->findAll()
+            ->getPermissions();
+
+        $registeredNonInfrastructuralPermissions = $this->getPermissionFacade()
+            ->getRegisteredNonInfrastructuralPermissions()
+            ->getPermissions();
+
+        $this->assertCount(
+            $this->getNonInfrastructuralPermissionsCount($availablePermissions),
+            $registeredNonInfrastructuralPermissions
+        );
+    }
+
+    /**
+     * @param \ArrayObject|\Generated\Shared\Transfer\PermissionTransfer[] $availablePermissions
+     *
+     * @return int
+     */
+    protected function getNonInfrastructuralPermissionsCount(ArrayObject $availablePermissions): int
+    {
+        $nonInfrastructuralPermissionsCount = 0;
+        foreach ($availablePermissions as $availablePermission) {
+            if (!$availablePermission->getIsInfrastructural()) {
+                $nonInfrastructuralPermissionsCount++;
+            }
+        }
+
+        return $nonInfrastructuralPermissionsCount;
+    }
+
+    /**
+     * @return \Spryker\Zed\Kernel\Business\AbstractFacade|\Spryker\Zed\Permission\Business\PermissionFacadeInterface
+     */
+    protected function getPermissionFacade(): PermissionFacadeInterface
+    {
+        return $this->tester->getFacade();
+    }
+}
