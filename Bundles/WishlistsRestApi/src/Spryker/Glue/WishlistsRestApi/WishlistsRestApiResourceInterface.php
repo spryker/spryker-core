99c32f8ce868f3a03481151e6e7fa7a3249eceec
cherniavskyi <yaroslav.cherniavskyi@spryker.com>
2018-09-17 16:27:46 +0300

    GLUE-381 fixed indenting [skip ci]

--- Bundles/WishlistsRestApi/src/Spryker/Glue/WishlistsRestApi/WishlistsRestApiResourceInterface.php
+++ Bundles/WishlistsRestApi/src/Spryker/Glue/WishlistsRestApi/WishlistsRestApiResourceInterface.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
  * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
+
 namespace Spryker\Glue\WishlistsRestApi;
 
 use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
