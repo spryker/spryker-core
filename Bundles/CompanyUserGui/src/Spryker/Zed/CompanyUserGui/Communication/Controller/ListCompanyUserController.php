e064b2dfead27ee2d044c54b3ed896bf494bd834
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-09-24 15:21:27 +0300

    PS-3933: Updated `ListCompanyUserController` to render `CompanyUserTable` data

--- Bundles/CompanyUserGui/src/Spryker/Zed/CompanyUserGui/Communication/Controller/ListCompanyUserController.php
+++ Bundles/CompanyUserGui/src/Spryker/Zed/CompanyUserGui/Communication/Controller/ListCompanyUserController.php
@@ -8,17 +8,33 @@
 namespace Spryker\Zed\CompanyUserGui\Communication\Controller;
 
 use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @method \Spryker\Zed\CompanyUserGui\Communication\CompanyUserGuiCommunicationFactory getFactory()
+ */
 class ListCompanyUserController extends AbstractController
 {
     /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     *
      * @return array
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        return $this->viewResponse([]);
+        $companyUserTable = $this->getFactory()
+            ->createCompanyUserTable();
+
+        return $this->viewResponse([
+            'companyUserTable' => $companyUserTable->render(),
+        ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function tableAction()
+    {
+        $companyUserTable = $this->getFactory()
+            ->createCompanyUserTable();
+
+        return $this->jsonResponse($companyUserTable->fetchData());
     }
 }
