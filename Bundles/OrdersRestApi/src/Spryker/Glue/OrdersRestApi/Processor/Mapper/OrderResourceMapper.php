229c296823379b95eab0db076e160f1d3d7f734a
AntonKhabiuk <brotherosirus@gmail.com>
2018-10-03 18:17:52 +0300

    GLUE-1128 Update transfers and mapping [skip ci]

--- Bundles/OrdersRestApi/src/Spryker/Glue/OrdersRestApi/Processor/Mapper/OrderResourceMapper.php
+++ Bundles/OrdersRestApi/src/Spryker/Glue/OrdersRestApi/Processor/Mapper/OrderResourceMapper.php
@@ -41,6 +41,8 @@ class OrderResourceMapper implements OrderResourceMapperInterface
 
         $restOrderDetailsAttributesTransfer = (new RestOrderDetailsAttributesTransfer())->fromArray($orderTransfer->toArray(), true);
         $restOrderDetailsAttributesTransfer->getTotals()->setTaxTotal($orderTransfer->getTotals()->getTaxTotal()->getAmount());
+        $restOrderDetailsAttributesTransfer->getBillingAddress()->setIsBilling(true);
+        $restOrderDetailsAttributesTransfer->getShippingAddress()->setIsShipping(true);
 
         return $restOrderDetailsAttributesTransfer;
     }
