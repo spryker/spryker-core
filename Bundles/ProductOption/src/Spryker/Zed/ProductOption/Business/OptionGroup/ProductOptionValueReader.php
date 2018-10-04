43c3d8e9f7f9a72f41ce839a8a2c71e6c584b35b
laktionova <helen.laktionova@spryker.com>
2018-10-04 15:20:09 +0300

    ps-2957: AA review fix
    * Added require options;

--- Bundles/ProductOption/src/Spryker/Zed/ProductOption/Business/OptionGroup/ProductOptionValueReader.php
+++ Bundles/ProductOption/src/Spryker/Zed/ProductOption/Business/OptionGroup/ProductOptionValueReader.php
@@ -64,6 +64,8 @@ class ProductOptionValueReader implements ProductOptionValueReaderInterface
      */
     public function getProductOptionCollectionByProductOptionCriteria(ProductOptionCriteriaTransfer $productOptionCriteriaTransfer): ProductOptionCollectionTransfer
     {
+        $productOptionCriteriaTransfer->requireProductOptionIds();
+
         $productOptionValueEntities = $this->productOptionQueryContainer
             ->queryProductOptionByProductOptionCriteria($productOptionCriteriaTransfer)
             ->find();
