b952965712dd70e69d0b125504c68bb3291eaf76
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2018-10-04 15:44:37 +0000

    GLUE-164 AA review fixes [skip ci]

--- Bundles/RestRequestValidator/src/Spryker/Glue/RestRequestValidator/RestRequestValidatorConfig.php
+++ Bundles/RestRequestValidator/src/Spryker/Glue/RestRequestValidator/RestRequestValidatorConfig.php
@@ -1,13 +1,15 @@
 <?php
 
 /**
- * Copyright© 2016-present Spryker Systems GmbH. All rights reserved.
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
 namespace Spryker\Glue\RestRequestValidator;
 
 use Spryker\Glue\Kernel\AbstractBundleConfig;
+use Spryker\Shared\Kernel\KernelConstants;
+use Spryker\Shared\Kernel\Store;
 use Spryker\Shared\RestRequestValidator\RestRequestValidatorConfig as RestRequestValidatorConfigShared;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -16,17 +18,15 @@ class RestRequestValidatorConfig extends AbstractBundleConfig
     public const RESPONSE_CODE_REQUEST_INVALID = '901';
 
     protected const CONSTRAINTS_NAMESPACE_SYMFONY_COMPONENT_VALIDATOR = 'Symfony\\Component\\Validator\\Constraints\\';
-    protected const CONSTRAINTS_NAMESPACE_REST_REQUEST_VALIDATOR = 'Spryker\\Glue\\RestRequestValidator\\Constraints\\';
+    protected const CONSTRAINTS_NAMESPACE_PROJECT_STORE_REST_REQUEST_VALIDATOR = '\\Glue\\RestRequestValidator%s\\Constraints\\';
+    protected const CONSTRAINTS_NAMESPACE_REST_REQUEST_VALIDATOR = '\\Glue\\RestRequestValidator\\Constraints\\';
 
     /**
      * @return string[]
      */
     public function getAvailableConstraintNamespaces(): array
     {
-        return [
-            static::CONSTRAINTS_NAMESPACE_SYMFONY_COMPONENT_VALIDATOR,
-            static::CONSTRAINTS_NAMESPACE_REST_REQUEST_VALIDATOR,
-        ];
+        return array_merge($this->getProjectNamespaces(), $this->getCoreNamespaces());
     }
 
     /**
@@ -57,5 +57,36 @@ class RestRequestValidatorConfig extends AbstractBundleConfig
             'allowExtraFields' => true,
             'groups' => ['Default'],
         ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getCoreNamespaces(): array
+    {
+        $coreConstraintNamespaces = [];
+
+        foreach ($this->get(KernelConstants::CORE_NAMESPACES) as $coreNamespace) {
+            $coreConstraintNamespaces[] = $coreNamespace . static::CONSTRAINTS_NAMESPACE_REST_REQUEST_VALIDATOR;
+        }
+        $coreConstraintNamespaces[] = static::CONSTRAINTS_NAMESPACE_SYMFONY_COMPONENT_VALIDATOR;
+
+        return $coreConstraintNamespaces;
+    }
+
+    /**
+     * @return array
+     */
+    private function getProjectNamespaces(): array
+    {
+        $projectConstraintNamespaces = [];
+
+        foreach ($this->get(KernelConstants::PROJECT_NAMESPACES) as $projectNamespaces) {
+            $projectConstraintNamespaces[] = $projectNamespaces .
+                sprintf(static::CONSTRAINTS_NAMESPACE_PROJECT_STORE_REST_REQUEST_VALIDATOR, Store::getInstance()->getCurrencyIsoCode());
+            $projectConstraintNamespaces[] = $projectNamespaces . static::CONSTRAINTS_NAMESPACE_REST_REQUEST_VALIDATOR;
+        }
+
+        return $projectConstraintNamespaces;
     }
 }
