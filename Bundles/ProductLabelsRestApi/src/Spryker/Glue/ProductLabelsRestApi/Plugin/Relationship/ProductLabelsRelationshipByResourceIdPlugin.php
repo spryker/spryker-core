bae4daa7e910a6fb90654ab832d18dc04dfd62dc
cherniavskyi <yaroslav.cherniavskyi@spryker.com>
2018-10-12 12:53:50 +0300

    Revert "Revert "Merge branch 'feature/glue-1136/glue-1139-relationship-between-product-abstract-and-labels' into feature/glue-1136/dev-product-labels""
    This reverts commit f46a116cf1750530e82305fb92e1613c6bbf472f.

---
+++ Bundles/ProductLabelsRestApi/src/Spryker/Glue/ProductLabelsRestApi/Plugin/Relationship/ProductLabelsRelationshipByResourceIdPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\ProductLabelsRestApi\Plugin\Relationship;
+
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+use Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRelationshipPluginInterface;
+use Spryker\Glue\Kernel\AbstractPlugin;
+use Spryker\Glue\ProductLabelsRestApi\ProductLabelsRestApiConfig;
+
+/**
+ * @method \Spryker\Glue\ProductLabelsRestApi\ProductLabelsRestApiFactory getFactory()
+ */
+class ProductLabelsRelationshipByResourceIdPlugin extends AbstractPlugin implements ResourceRelationshipPluginInterface
+{
+    /**
+     * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface[] $resources
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
+     * @return void
+     */
+    public function addResourceRelationships(array $resources, RestRequestInterface $restRequest): void
+    {
+        $this->getFactory()->createProductLabelsResourceRelationshipExpander()->addRelationshipsByAbstractSku($resources, $restRequest);
+    }
+
+    /**
+     * @return string
+     */
+    public function getRelationshipResourceType(): string
+    {
+        return ProductLabelsRestApiConfig::RESOURCE_PRODUCT_LABELS;
+    }
+}
