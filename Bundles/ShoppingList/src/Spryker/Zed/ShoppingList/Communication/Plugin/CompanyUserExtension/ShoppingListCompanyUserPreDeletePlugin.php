8e9afac1fe65ab65979dba68969be221d2d67a81
Mahmoud Ali <mahmoud.ali@spryker.com>
2018-11-01 13:24:35 +0100

    PS-5832 SharedCartCompanyUserPreDeletePlugin

--- Bundles/ShoppingList/src/Spryker/Zed/ShoppingList/Communication/Plugin/CompanyUserExtension/ShoppingListCompanyUserPreDeletePlugin.php
+++ Bundles/ShoppingList/src/Spryker/Zed/ShoppingList/Communication/Plugin/CompanyUserExtension/ShoppingListCompanyUserPreDeletePlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\ShoppingList\Communication\Plugin\CompanyUser;
+namespace Spryker\Zed\ShoppingList\Communication\Plugin\CompanyUserExtension;
 
 use Generated\Shared\Transfer\CompanyUserTransfer;
 use Generated\Shared\Transfer\ShoppingListShareRequestTransfer;
