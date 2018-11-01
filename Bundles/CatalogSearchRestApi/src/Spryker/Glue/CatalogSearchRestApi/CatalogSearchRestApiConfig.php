571fbf3f793d5639c8b03d4548d6ecd2c902d594
eugenia.poidenko <eugenia.poidenko@spryker.com>
2018-11-01 09:47:30 +0000

    GLUE-1056 cr fixes [skip ci]

--- Bundles/CatalogSearchRestApi/src/Spryker/Glue/CatalogSearchRestApi/CatalogSearchRestApiConfig.php
+++ Bundles/CatalogSearchRestApi/src/Spryker/Glue/CatalogSearchRestApi/CatalogSearchRestApiConfig.php
@@ -14,14 +14,14 @@ class CatalogSearchRestApiConfig extends AbstractBundleConfig
     public const RESOURCE_CATALOG_SEARCH = 'catalog-search';
     public const RESOURCE_CATALOG_SEARCH_SUGGESTIONS = 'catalog-search-suggestions';
 
+    /** @deprecated Price mode check is moved to validation plugin. This response code will not be used anymore. */
+    public const RESPONSE_CODE_INVALID_CURRENCY = '501';
+    /** @deprecated Price mode check is moved to validation plugin. This response code will not be thrown anymore. */
+    public const RESPONSE_CODE_INVALID_PRICE_MODE = '502';
     /** @deprecated Currency check is moved to validation plugin. This response message will not be used anymore. */
     public const RESPONSE_DETAIL_INVALID_CURRENCY = 'Invalid currency.';
     /** @deprecated Currency check is moved to validation plugin. This response code will not be thrown anymore. */
     public const RESPONSE_DETAIL_INVALID_PRICE_MODE = 'Invalid price mode.';
-    /** @deprecated Price mode check is moved to validation plugin. This response code will not be thrown anymore. */
-    public const RESPONSE_CODE_INVALID_PRICE_MODE = '502';
-    /** @deprecated Price mode check is moved to validation plugin. This response code will not be used anymore. */
-    public const RESPONSE_CODE_INVALID_CURRENCY = '501';
 
     /** @deprecated Currency parameter handling is moved to BeforeAction plugin. Not used anymore. */
     public const CURRENCY_STRING_PARAMETER = 'currency';
