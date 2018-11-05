634b965c490b8b3575ce5f17dcb0e7e121b0f66b
Oleh Hladchenko <glad.oleg@gmail.com>
2018-11-05 14:12:46 +0200

    GLUE-1869 Fixed validation issue.

--- Bundles/ProductGroupStorage/src/Spryker/Zed/ProductGroupStorage/Dependency/QueryContainer/ProductGroupStorageToProductGroupQueryContainerBridge.php
+++ Bundles/ProductGroupStorage/src/Spryker/Zed/ProductGroupStorage/Dependency/QueryContainer/ProductGroupStorageToProductGroupQueryContainerBridge.php
@@ -25,7 +25,7 @@ class ProductGroupStorageToProductGroupQueryContainerBridge implements ProductGr
     /**
      * @api
      *
-     * @return \Orm\Zed\ProductGroup\Persistence\SpyProductGroupQuery
+     * @return \Orm\Zed\ProductGroup\Persistence\SpyProductAbstractGroupQuery
      */
     public function queryAllProductAbstractGroups()
     {
