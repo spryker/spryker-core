2039ce350fb7488f3e7fb29647d3b6acc32d33a2
Karoly Gerner <karoly.gerner@spryker.com>
2018-11-14 15:07:30 +0000

    Remvoing dobule class docblocks

--- Bundles/Sales/src/Spryker/Zed/Sales/Communication/Controller/CustomerController.php
+++ Bundles/Sales/src/Spryker/Zed/Sales/Communication/Controller/CustomerController.php
@@ -15,11 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
  * @method \Spryker\Zed\Sales\Communication\SalesCommunicationFactory getFactory()
  * @method \Spryker\Zed\Sales\Business\SalesFacadeInterface getFacade()
  */
-
-/**
- * @method \Spryker\Zed\Sales\Persistence\SalesQueryContainerInterface getQueryContainer()
- * @method \Spryker\Zed\Sales\Persistence\SalesRepositoryInterface getRepository()
- */
 class CustomerController extends AbstractController
 {
     /**
