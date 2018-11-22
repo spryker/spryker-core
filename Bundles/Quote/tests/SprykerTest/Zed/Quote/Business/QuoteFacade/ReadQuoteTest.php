982a7ae5a0e63b38dd3694590f20555c1e7b44d1
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2018-11-22 14:37:51 +0000

    GLUE-391 QuoteFacade::findQuoteByUuid tests [skip ci]

--- Bundles/Quote/tests/SprykerTest/Zed/Quote/Business/QuoteFacade/ReadQuoteTest.php
+++ Bundles/Quote/tests/SprykerTest/Zed/Quote/Business/QuoteFacade/ReadQuoteTest.php
@@ -22,6 +22,8 @@ use Generated\Shared\Transfer\QuoteTransfer;
  */
 class ReadQuoteTest extends Unit
 {
+    protected const WRONG_UUID = 'wrong-uuid-1';
+
     /**
      * @var \SprykerTest\Zed\Quote\QuoteBusinessTester
      */
@@ -30,7 +32,7 @@ class ReadQuoteTest extends Unit
     /**
      * @return void
      */
-    public function testReadQuoteFromDatabaseByCustomer()
+    public function testReadQuoteFromDatabaseByCustomer(): void
     {
         // Arrange
         $customerTransfer = $this->tester->haveCustomer();
@@ -39,10 +41,57 @@ class ReadQuoteTest extends Unit
         ]);
 
         // Act
-        $quoteResponseTransfer = $this->tester->getFacade()->findQuoteByCustomer($customerTransfer);
+        /** @var \Spryker\Zed\Quote\Business\QuoteFacadeInterface $quoteFacade */
+        $quoteFacade = $this->tester->getFacade();
+        $quoteResponseTransfer = $quoteFacade->findQuoteByCustomer($customerTransfer);
 
         // Assert
         $this->assertTrue($quoteResponseTransfer->getIsSuccessful(), 'Quote response transfer should have ben successful.');
         $this->assertEquals($quoteTransfer->getIdQuote(), $quoteResponseTransfer->getQuoteTransfer()->getIdQuote(), 'Quote response should have expected quote ID from database.');
+    }
+
+    /**
+     * @return void
+     */
+    public function testReadQuoteFromDatabaseByUuid(): void
+    {
+        // Arrange
+        $customerTransfer = $this->tester->haveCustomer();
+        $quoteTransfer = $this->tester->havePersistentQuote([
+            QuoteTransfer::CUSTOMER => $customerTransfer,
+        ]);
+
+        // Act
+        /** @var \Spryker\Zed\Quote\Business\QuoteFacadeInterface $quoteFacade */
+        $quoteFacade = $this->tester->getFacade();
+        $quoteResponseTransfer = $quoteFacade->findQuoteByUuid($quoteTransfer);
+
+        // Assert
+        $this->assertTrue($quoteResponseTransfer->getIsSuccessful(), 'Quote search should have been successful.');
+        $this->assertNotNull($quoteResponseTransfer->getQuoteTransfer(), 'Quote response should have quote.');
+        $this->assertEquals($quoteTransfer->getIdQuote(), $quoteResponseTransfer->getQuoteTransfer()->getIdQuote(), 'Quote response expected quote ID from database.');
+    }
+
+    /**
+     * @return void
+     */
+    public function testFailToReadQuoteFromDatabaseByUuid(): void
+    {
+        // Arrange
+        $customerTransfer = $this->tester->haveCustomer();
+        $quoteTransfer = $this->tester->havePersistentQuote([
+            QuoteTransfer::CUSTOMER => $customerTransfer,
+        ]);
+
+        $quoteTransfer->setUuid(static::WRONG_UUID);
+
+        // Act
+        /** @var \Spryker\Zed\Quote\Business\QuoteFacadeInterface $quoteFacade */
+        $quoteFacade = $this->tester->getFacade();
+        $quoteResponseTransfer = $quoteFacade->findQuoteByUuid($quoteTransfer);
+
+        // Assert
+        $this->assertFalse($quoteResponseTransfer->getIsSuccessful(), 'Quote search should have failed.');
+        $this->assertNull($quoteResponseTransfer->getQuoteTransfer(), 'Quote response should have no quote.');
     }
 }
