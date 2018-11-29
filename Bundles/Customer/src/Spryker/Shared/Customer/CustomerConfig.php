74a0a0c06f0db39665511188e2b750f29027d991
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2018-11-29 10:21:45 +0200

    cc-1720: adjust customer configuration

---
+++ Bundles/Customer/src/Spryker/Shared/Customer/CustomerConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\Customer;
+
+use Spryker\Shared\Kernel\AbstractSharedConfig;
+
+class CustomerConfig extends AbstractSharedConfig
+{
+    public const CUSTOMER_SESSION_KEY = 'customer data';
+}
