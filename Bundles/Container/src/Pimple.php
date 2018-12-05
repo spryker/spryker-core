239bde5b7df90d6dbb189b4f535304b804bbb564
René Klatt <rene.klatt@spryker.com>
2018-12-05 11:37:15 +0000

    te-642 added Container

---
+++ Bundles/Container/src/Pimple.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright © 2018-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+use Spryker\Service\Container\Container;
+
+/**
+ * @deprecated This class exists only for BC reasons. Please switch to the `Spryker\Service\Container\ContainerInterface`.
+ */
+class Pimple extends Container
+{
+}
