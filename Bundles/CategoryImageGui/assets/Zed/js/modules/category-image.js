faeefdb6f0b4dd7bcb5acb0fddb969cdf2b9fca9
Pavlo Asaulenko <asaulenko.pavel@gmail.com>
2018-12-05 13:48:31 +0200

    CC-523 Move js assets to CategoryImageGui module

---
+++ Bundles/CategoryImageGui/assets/Zed/js/modules/category-image.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+'use strict';
+
+$(document).ready(function() {
+    /**
+     * Register global event listeners
+     */
+    $('body')
+        .on('click', '.add-another-image-set', addAnotherImageSet)
+        .on('click', '.add-another-image-collection', addAnotherImageCollection)
+        .on('click', '.remove-image-set', deleteImageSet)
+        .on('click', '.remove-image-collection', deleteImageCollection);
+
+    /**
+     * @param event
+     */
+    function addAnotherImageCollection(event) {
+        event.preventDefault();
+
+        var prototypeTemplate = $(event.target).closest('[data-image-collection-prototype]');
+
+        var imageSet = $(event.target).closest('.image-set');
+        var imageSetIndex = imageSet.data('imageSetIndex');
+        var imageCollectionIndex = imageSet.find('.image-collection').length;
+
+        var newOptionFormHTML = prototypeTemplate
+            .data('imageCollectionPrototype')
+            .replace(/__image_set_name__/g, imageSetIndex)
+            .replace(/__name__/g, imageCollectionIndex);
+
+        $(event.target).parent().find('.image-collection-container').append($(newOptionFormHTML));
+    }
+
+    /**
+     * @param event
+     */
+    function addAnotherImageSet(event) {
+        event.preventDefault();
+
+        var prototypeTemplate = $(event.target).closest('[data-image-set-prototype][data-image-collection-prototype]');
+        var imageSetIndex = prototypeTemplate.data('currentImageSetIndex') + 1;
+        prototypeTemplate.data('currentImageSetIndex', imageSetIndex);
+
+        var imageSetPrototype = prototypeTemplate
+            .data('imageSetPrototype')
+            .replace(/__image_set_name__/g, imageSetIndex);
+
+        var imageSet = $(imageSetPrototype);
+        imageSet.data('imageSetIndex', imageSetIndex);
+        $(event.target).before(imageSet);
+
+        var imageCollectionPrototype = prototypeTemplate
+            .data('imageCollectionPrototype')
+            .replace(/__image_set_name__/g, imageSetIndex)
+            .replace(/__name__/g, 0);
+
+        imageSet.find('.image-collection-container').append($(imageCollectionPrototype));
+    }
+
+    /**
+     * @param event
+     */
+    function deleteImageSet(event) {
+        event.preventDefault();
+
+        $(this).closest('.image-set').remove();
+    }
+
+    /**
+     * @param event
+     */
+    function deleteImageCollection(event) {
+        event.preventDefault();
+
+        $(this).closest('.image-collection').remove();
+    }
+
+    /**
+     * Init image set index
+     */
+    $('.image-set').each(function(i, imageSet) {
+        var imageSetIndex = $(imageSet).closest('[data-image-set-prototype]').find('.image-set').index(imageSet);
+
+        $(imageSet).data('imageSetIndex', imageSetIndex);
+    });
+    $('.image-set-container').each(function(i, imageSetContainer) {
+        var currentImageSetIndex = $(imageSetContainer).find('.image-set').length - 1;
+
+        $(imageSetContainer).data('currentImageSetIndex', currentImageSetIndex);
+    });
+});
