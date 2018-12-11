f7d049fe7fca5e2a5c9debe67dd9a5414ea1645b
laktionova <helen.laktionova@spryker.com>
2018-12-11 17:50:31 +0200

    PS-6285:
     - In QuoteApproval module implemented new permission plugin PlaceOrderPermissionPlugin in shared layer.
     - Added plugin to PermissionDependencyProvider.

---
+++ Bundles/QuoteApproval/src/Spryker/Shared/QuoteApproval/Plugin/Permission/PlaceOrderPermissionPlugin.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\QuoteApproval\Plugin\Permission;
+
+use Spryker\Shared\PermissionExtension\Dependency\Plugin\ExecutablePermissionPluginInterface;
+
+class PlaceOrderPermissionPlugin implements ExecutablePermissionPluginInterface
+{
+    public const KEY = 'PlaceOrderPermissionPlugin';
+
+    protected const FIELD_MULTI_CURRENCY = 'multi_currency';
+
+    /**
+     * {@inheritdoc}
+     * - Checks if customer is allowed to place order with cent amount up to some value for specific currency, provided in configuration.
+     * - Returns false, if quote is not provided.
+     * - Returns true, if configuration does not have cent amount for specific currency set.
+     *
+     * @param array $configuration
+     * @param \Generated\Shared\Transfer\QuoteTransfer|null $quoteTransfer
+     *
+     * @return bool
+     */
+    public function can(array $configuration, $quoteTransfer = null): bool
+    {
+        if ($quoteTransfer === null) {
+            return false;
+        }
+
+        $centAmount = $quoteTransfer->getTotals()->getGrandTotal();
+        $currencyCode = $quoteTransfer->getCurrency()->getCode();
+
+        if (!isset($configuration[static::FIELD_MULTI_CURRENCY]) || !isset($configuration[static::FIELD_MULTI_CURRENCY][$currencyCode])) {
+            return true;
+        }
+
+        if ($configuration[static::FIELD_MULTI_CURRENCY][$currencyCode] <= (int)$centAmount) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getConfigurationSignature(): array
+    {
+        return [
+            static::FIELD_MULTI_CURRENCY => static::CONFIG_FIELD_TYPE_MULTI_CURRENCY,
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return static::KEY;
+    }
+}
