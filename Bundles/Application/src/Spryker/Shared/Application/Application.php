4471d8d592c8bf02f784f4c77cda1c15bfbc5d20
gechetspr <serhii.chepela@spryker.com>
2018-12-19 13:24:46 +0200

    TE-634 Adding new shared application and fixing container for tests

---
+++ Bundles/Application/src/Spryker/Shared/Application/Application.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\Application;
+
+use Spryker\Service\Container\ContainerInterface;
+use Spryker\Shared\ApplicationExtension\Provider\BootableServiceInterface;
+use Spryker\Shared\ApplicationExtension\Provider\ServiceProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\TerminableInterface;
+
+class Application implements HttpKernelInterface, TerminableInterface
+{
+    /**
+     * @var \Spryker\Shared\ApplicationExtension\Provider\ServiceProviderInterface[]
+     */
+    protected $services = [];
+
+    /**
+     * @var \Spryker\Shared\ApplicationExtension\Provider\BootableServiceInterface[]
+     */
+    protected $bootableServices = [];
+
+    /**
+     * @var \Spryker\Service\Container\ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var bool
+     */
+    protected $booted = false;
+
+    /**
+     * @param \Spryker\Service\Container\ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param \Spryker\Shared\ApplicationExtension\Provider\ServiceProviderInterface $serviceProvider
+     *
+     * @return $this
+     */
+    public function registerServiceProvider(ServiceProviderInterface $serviceProvider): self
+    {
+        $this->services[] = $serviceProvider;
+        $serviceProvider->provide($this->container);
+
+        if ($serviceProvider instanceof BootableServiceInterface) {
+            $this->bootableServices[] = $serviceProvider;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function boot(): self
+    {
+        if (!$this->booted) {
+            $this->booted = true;
+            $this->bootServices();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return void
+     */
+    protected function bootServices(): void
+    {
+        foreach ($this->bootableServices as $bootableService) {
+            $bootableService->boot($this->container);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function run()
+    {
+        $request = Request::createFromGlobals();
+
+        $this->container->set('request', $request);
+
+        $response = $this->handle($request);
+        $response->send();
+        $this->terminate($request, $response);
+    }
+
+    /**
+     * @internal Don't use this method unless you know why.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param int $type
+     * @param bool $catch
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true): Response
+    {
+        $response = $this->getKernel()->handle($request);
+
+        return $response;
+    }
+
+    /**
+     * @return \Symfony\Component\HttpKernel\HttpKernel
+     */
+    protected function getKernel(): HttpKernel
+    {
+        return $this->container->get('kernel');
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     *
+     * @return void
+     */
+    public function terminate(Request $request, Response $response)
+    {
+        $this->getKernel()->terminate($request, $response);
+    }
+}
---
+++ Bundles/Application/tests/SprykerTest/Shared/Application/ApplicationTest.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Shared\Application;
+
+use Codeception\Test\Unit;
+use Spryker\Service\Container\Container;
+use Spryker\Service\Container\ContainerInterface;
+use Spryker\Shared\Application\Application;
+use Spryker\Shared\ApplicationExtension\Provider\BootableServiceInterface;
+use Spryker\Shared\ApplicationExtension\Provider\ServiceProviderInterface;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Shared
+ * @group Application
+ * @group ApplicationTest
+ * Add your own group annotations below this line
+ */
+class ApplicationTest extends Unit
+{
+    public const SERVICE = 'SERVICE';
+    public const SERVICE_PROPERTY = 'SERVICE_PROPERTY';
+
+    /**
+     * @return void
+     */
+    public function testApplicationRegisterServiceProvider(): void
+    {
+        //Arrange
+        $container = $this->createContainer();
+        $application = $this->createApplication($container);
+
+        //Act
+        $application->registerServiceProvider($this->createServiceProvider());
+
+        //Assert
+        $this->assertTrue($container->has(static::SERVICE));
+    }
+
+    /**
+     * @return void
+     */
+    public function testApplicationRunBootableService(): void
+    {
+        //Arrange
+        $container = $this->createContainer();
+        $application = $this->createApplication($container);
+
+        //Act
+        $application->registerServiceProvider($this->createBootableServiceProvider());
+        $application->boot();
+
+        //Assert
+        $this->assertTrue($container->has(static::SERVICE));
+    }
+
+    /**
+     * @return void
+     */
+    public function testApplicationRunsBootableServicesOnlyOnce(): void
+    {
+        //Arrange
+        $container = $this->createContainer();
+        $application = $this->createApplication($container);
+        $serviceProvider = $this->createBootableServiceProvider();
+
+        //Act
+        $application->registerServiceProvider($serviceProvider);
+        $application->boot();
+        $application->boot();
+
+        //Assert
+        $this->assertSame(1, $serviceProvider::$runs);
+    }
+
+    /**
+     * @return \Spryker\Service\Container\ContainerInterface
+     */
+    protected function createContainer(): ContainerInterface
+    {
+        return new Container();
+    }
+
+    /**
+     * @param \Spryker\Service\Container\ContainerInterface $container
+     *
+     * @return \Spryker\Shared\Application\Application
+     */
+    protected function createApplication(ContainerInterface $container): Application
+    {
+        return new Application($container);
+    }
+
+    /**
+     * @return \Spryker\Shared\ApplicationExtension\Provider\ServiceProviderInterface
+     */
+    protected function createServiceProvider(): ServiceProviderInterface
+    {
+        return new class implements ServiceProviderInterface
+        {
+            /**
+             * @param \Spryker\Service\Container\ContainerInterface $container
+             *
+             * @return void
+             */
+            public function provide(ContainerInterface $container): void
+            {
+                $container->set(ApplicationTest::SERVICE, function () {
+                    return [ApplicationTest::SERVICE_PROPERTY => true];
+                });
+            }
+        };
+    }
+
+    /**
+     * @return \Spryker\Shared\ApplicationExtension\Provider\BootableServiceInterface
+     */
+    protected function createBootableServiceProvider(): BootableServiceInterface
+    {
+        return new class implements ServiceProviderInterface, BootableServiceInterface
+        {
+            /**
+             * @var int
+             */
+            public static $runs = 0;
+
+            /**
+             * @param \Spryker\Service\Container\ContainerInterface $container
+             *
+             * @return void
+             */
+            public function provide(ContainerInterface $container): void
+            {
+            }
+
+            /**
+             * @param \Spryker\Service\Container\ContainerInterface $container
+             *
+             * @return void
+             */
+            public function boot(ContainerInterface $container): void
+            {
+                static::$runs++;
+                $container->set(ApplicationTest::SERVICE, function () {
+                    return [ApplicationTest::SERVICE_PROPERTY => true];
+                });
+            }
+        };
+    }
+}
