33ecb84ddefb77645fe3da07eb6e3d2436d8ea0f
gechetspr <serhii.chepela@spryker.com>
2018-12-20 11:48:40 +0200

    TE-634 moving interfaces to another directory and renaming some methods

--- Bundles/ApplicationExtension/src/Spryker/Shared/ApplicationExtension/Dependency/Plugin/ApplicationExtensionInterface.php
+++ Bundles/ApplicationExtension/src/Spryker/Shared/ApplicationExtension/Dependency/Plugin/ApplicationExtensionInterface.php
@@ -5,13 +5,18 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Shared\ApplicationExtension\Provider;
+namespace Spryker\Shared\ApplicationExtension\Dependency\Plugin;
 
 use Spryker\Service\Container\ContainerInterface;
 
 interface ApplicationExtensionInterface
 {
     /**
+     * Specification:
+     * - Provides extension for application. Executes before application boot.
+     *
+     * @api
+     *
      * @param \Spryker\Service\Container\ContainerInterface $container
      *
      * @return void
