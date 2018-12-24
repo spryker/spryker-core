a0f7e8f3476fc3d8935e347c469268729b3f2746
Nikolay Ostrizhnii <mykola.ostrizhnii@spryker.com>
2018-12-24 18:03:27 +0200

    TE-648: Adding Pimple for BC reasons

---
+++ Bundles/Container/src/Spryker/Service/Container/Pimple.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Service\Container;
+
+/**
+ * @deprecated This class exists only for BC reasons. Please switch to the `Spryker\Service\Container\ContainerInterface`.
+ */
+class Pimple extends Container
+{
+}
