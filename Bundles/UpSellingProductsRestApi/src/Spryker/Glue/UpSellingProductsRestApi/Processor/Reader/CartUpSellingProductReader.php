c8f6871156b91d9af7464f15ee7b745180144136
voitov <tigran.voitov@spryker.com>
2019-01-08 14:08:12 +0200

    glue-1141 fixed up-selling product readers

--- Bundles/UpSellingProductsRestApi/src/Spryker/Glue/UpSellingProductsRestApi/Processor/Reader/CartUpSellingProductReader.php
+++ Bundles/UpSellingProductsRestApi/src/Spryker/Glue/UpSellingProductsRestApi/Processor/Reader/CartUpSellingProductReader.php
@@ -8,55 +8,11 @@
 namespace Spryker\Glue\UpSellingProductsRestApi\Processor\Reader;
 
 use Spryker\Glue\CartsRestApi\CartsRestApiConfig;
-use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface;
 use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface;
-use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface;
 use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
-use Spryker\Glue\UpSellingProductsRestApi\Dependency\Client\UpSellingProductsRestApiToProductRelationStorageClientInterface;
-use Spryker\Glue\UpSellingProductsRestApi\Processor\Mapper\UpSellingProductsResourceMapperInterface;
 
 class CartUpSellingProductReader extends UpSellingProductReader
 {
-    /**
-     * @var \Spryker\Glue\UpSellingProductsRestApi\Processor\Reader\QuoteReaderInterface
-     */
-    protected $quoteReader;
-
-    /**
-     * @var \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
-     */
-    protected $restResourceBuilder;
-
-    /**
-     * @param \Spryker\Glue\UpSellingProductsRestApi\Processor\Reader\QuoteReaderInterface $quoteReader
-     * @param \Spryker\Glue\UpSellingProductsRestApi\Dependency\Client\UpSellingProductsRestApiToProductRelationStorageClientInterface $productRelationStorageClient
-     * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface $restResourceBuilder
-     * @param \Spryker\Glue\UpSellingProductsRestApi\Processor\Mapper\UpSellingProductsResourceMapperInterface $upSellingProductsResourceMapper
-     */
-    public function __construct(
-        QuoteReaderInterface $quoteReader,
-        UpSellingProductsRestApiToProductRelationStorageClientInterface $productRelationStorageClient,
-        RestResourceBuilderInterface $restResourceBuilder,
-        UpSellingProductsResourceMapperInterface $upSellingProductsResourceMapper
-    ) {
-        parent::__construct(
-            $quoteReader,
-            $productRelationStorageClient,
-            $restResourceBuilder,
-            $upSellingProductsResourceMapper
-        );
-    }
-
-    /**
-     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
-     *
-     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface
-     */
-    public function readUpSellingProducts(RestRequestInterface $restRequest): RestResponseInterface
-    {
-        return parent::readUpSellingProducts($restRequest);
-    }
-
     /**
      * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
      *
--- Bundles/UpSellingProductsRestApi/src/Spryker/Glue/UpSellingProductsRestApi/Processor/Reader/GuestCartUpSellingProductReader.php
+++ Bundles/UpSellingProductsRestApi/src/Spryker/Glue/UpSellingProductsRestApi/Processor/Reader/GuestCartUpSellingProductReader.php
@@ -9,46 +9,13 @@ namespace Spryker\Glue\UpSellingProductsRestApi\Processor\Reader;
 
 use Generated\Shared\Transfer\RestErrorMessageTransfer;
 use Spryker\Glue\CartsRestApi\CartsRestApiConfig;
-use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface;
 use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface;
 use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface;
 use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
-use Spryker\Glue\UpSellingProductsRestApi\Dependency\Client\UpSellingProductsRestApiToProductRelationStorageClientInterface;
-use Spryker\Glue\UpSellingProductsRestApi\Processor\Mapper\UpSellingProductsResourceMapperInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class GuestCartUpSellingProductReader extends UpSellingProductReader
 {
-    /**
-     * @var \Spryker\Glue\UpSellingProductsRestApi\Processor\Reader\QuoteReaderInterface
-     */
-    protected $quoteReader;
-
-    /**
-     * @var \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
-     */
-    protected $restResourceBuilder;
-
-    /**
-     * @param \Spryker\Glue\UpSellingProductsRestApi\Processor\Reader\QuoteReaderInterface $quoteReader
-     * @param \Spryker\Glue\UpSellingProductsRestApi\Dependency\Client\UpSellingProductsRestApiToProductRelationStorageClientInterface $productRelationStorageClient
-     * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface $restResourceBuilder
-     * @param \Spryker\Glue\UpSellingProductsRestApi\Processor\Mapper\UpSellingProductsResourceMapperInterface $upSellingProductsResourceMapper
-     */
-    public function __construct(
-        QuoteReaderInterface $quoteReader,
-        UpSellingProductsRestApiToProductRelationStorageClientInterface $productRelationStorageClient,
-        RestResourceBuilderInterface $restResourceBuilder,
-        UpSellingProductsResourceMapperInterface $upSellingProductsResourceMapper
-    ) {
-        parent::__construct(
-            $quoteReader,
-            $productRelationStorageClient,
-            $restResourceBuilder,
-            $upSellingProductsResourceMapper
-        );
-    }
-
     /**
      * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
      *
