7ab92770c56ba31d8b43533ea83f3b5fb6d47fb9
Dima Aseev <dmitriy.aseev@spryker.com>
2019-01-09 13:51:47 +0200

    PS-6795: Finished enabler for quote requests.

---
+++ Bundles/QuoteRequest/src/Spryker/Shared/QuoteRequest/QuoteRequestConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\QuoteRequest;
+
+use Spryker\Shared\Kernel\AbstractSharedConfig;
+
+class QuoteRequestConfig extends AbstractSharedConfig
+{
+    public const STATUS_DRAFT = 'Draft';
+    public const STATUS_WAITING = 'Waiting';
+    public const STATUS_IN_PROGRESS = 'In progress';
+    public const STATUS_READY = 'Ready';
+    public const STATUS_CLOSED = 'Closed';
+}
