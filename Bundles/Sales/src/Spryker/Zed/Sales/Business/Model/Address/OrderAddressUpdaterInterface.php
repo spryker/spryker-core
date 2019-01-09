33689bb37d54ebe602eb5d426dae0ef99d0645d3
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-01-09 15:29:30 +0200

    CC-1742: Deprecated OrderAddressUpdater. Updated Shipment/spy_sales.schema.xml
    * Deprecated OrderAddressUpdater and OrderAddressUpdaterInterface.
    * Moved changes from ShipmentGui/spy_sales.schema.xml to Shipment/spy_sales.schema.xml.

---
+++ Bundles/Sales/src/Spryker/Zed/Sales/Business/Model/Address/OrderAddressUpdaterInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Sales\Business\Model\Address;
+
+use Generated\Shared\Transfer\AddressTransfer;
+
+/**
+ * @deprecated Use OrderAddressWriterInterface interface instead.
+ */
+interface OrderAddressUpdaterInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\AddressTransfer $addressTransfer
+     * @param int $idAddress
+     *
+     * @return bool
+     */
+    public function update(AddressTransfer $addressTransfer, $idAddress);
+}
