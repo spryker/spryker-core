c3b9917bf964199fc78609f27b4fe62bf96d848a
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2019-01-10 17:30:49 +0200

    PS-6928: fixed after AA review

--- Bundles/CustomersRestApi/src/Spryker/Glue/CustomersRestApi/CustomersRestApiConfig.php
+++ Bundles/CustomersRestApi/src/Spryker/Glue/CustomersRestApi/CustomersRestApiConfig.php
@@ -71,5 +71,8 @@ class CustomersRestApiConfig extends AbstractBundleConfig
     public const RESPONSE_CODE_CUSTOMER_EMAIL_INVALID = '416';
     public const RESPONSE_MESSAGE_CUSTOMER_EMAIL_INVALID = 'Invalid Email address format.';
 
+    public const RESPONSE_CODE_CUSTOMER_EMAIL_LENGTH_EXCEEDED = '417';
+    public const RESPONSE_MESSAGE_CUSTOMER_EMAIL_LENGTH_EXCEEDED = 'Too long email address value.';
+
     public const FORMAT_SELF_LINK_ADDRESS_RESOURCE = '%s/%s/%s/%s';
 }
