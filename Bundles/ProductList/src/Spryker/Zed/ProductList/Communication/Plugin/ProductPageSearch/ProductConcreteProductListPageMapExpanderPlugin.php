c1762d86415b1fe94a7b2a9aa8a7e88865ed6621
Stanislav Matveyev <stanislav.matveyev@spryker.com>
2019-01-11 13:52:34 +0200

    PS-6638: moved plugins from ProductPageSearchExtension to ProductPageSearch folder

--- Bundles/ProductList/src/Spryker/Zed/ProductList/Communication/Plugin/ProductPageSearch/ProductConcreteProductListPageMapExpanderPlugin.php
+++ Bundles/ProductList/src/Spryker/Zed/ProductList/Communication/Plugin/ProductPageSearch/ProductConcreteProductListPageMapExpanderPlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\ProductList\Communication\Plugin\ProductPageSearchExtension;
+namespace Spryker\Zed\ProductList\Communication\Plugin\ProductPageSearch;
 
 use Generated\Shared\Transfer\LocaleTransfer;
 use Generated\Shared\Transfer\PageMapTransfer;
