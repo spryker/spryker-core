73745bc62da756534a5fdfe62f164e5f3e4ae1d0
Anton Smarovydlo <anton.smarovydlo@gmail.com>
2019-01-14 15:23:56 +0000

    Revert "PS-4358: make ApproveQuotePermissionPlugin and PlaceOrderPermission plugin store dependent"
    This reverts commit 63104eb9f1289941c92973bad5a483db2e16bfbf.

--- Bundles/QuoteApproval/src/Spryker/Shared/QuoteApproval/Plugin/Permission/ApproveQuotePermissionPlugin.php
+++ Bundles/QuoteApproval/src/Spryker/Shared/QuoteApproval/Plugin/Permission/ApproveQuotePermissionPlugin.php
@@ -15,7 +15,6 @@ class ApproveQuotePermissionPlugin implements ExecutablePermissionPluginInterfac
     public const KEY = 'ApproveQuotePermissionPlugin';
 
     protected const FIELD_MULTI_CURRENCY = 'multi_currency';
-    protected const KEY_SEPARATOR = '_';
 
     /**
      * {@inheritdoc}
@@ -38,14 +37,12 @@ class ApproveQuotePermissionPlugin implements ExecutablePermissionPluginInterfac
 
         $centAmount = $quoteTransfer->getTotals()->getGrandTotal();
         $currencyCode = $quoteTransfer->getCurrency()->getCode();
-        $storeName = $quoteTransfer->getStore()->getName();
-        $key = $storeName . static::KEY_SEPARATOR . $currencyCode;
 
-        if (!isset($configuration[static::FIELD_MULTI_CURRENCY][$key])) {
+        if (!isset($configuration[static::FIELD_MULTI_CURRENCY][$currencyCode])) {
             return true;
         }
 
-        if ($configuration[static::FIELD_MULTI_CURRENCY][$key] < (int)$centAmount) {
+        if ($configuration[static::FIELD_MULTI_CURRENCY][$currencyCode] < (int)$centAmount) {
             return false;
         }
 
