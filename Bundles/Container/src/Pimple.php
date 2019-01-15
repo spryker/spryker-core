1c20d8dfaab44dbb1a64af43bd8d46a142dbf729
Nikolay Ostrizhnii <mykola.ostrizhnii@spryker.com>
2019-01-15 14:27:19 +0200

    TE-648: Ignorig CS error for Pimple

--- Bundles/Container/src/Pimple.php
+++ Bundles/Container/src/Pimple.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
@@ -8,6 +7,7 @@
 use Spryker\Service\Container\Container;
 
 /**
+ * phpcs:disable
  * @deprecated This class exists only for BC reasons. Please switch to the `Spryker\Service\Container\ContainerInterface`.
  */
 class Pimple extends Container
