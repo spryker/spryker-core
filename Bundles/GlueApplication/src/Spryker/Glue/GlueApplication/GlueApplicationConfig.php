35cf218d4f68f2c7a58e8d0e06d3dcbf61295c76
AntonKhabiuk <brotherosirus@gmail.com>
2019-01-16 17:45:46 +0200

    GLUE-2767 Fix BC break

--- Bundles/GlueApplication/src/Spryker/Glue/GlueApplication/GlueApplicationConfig.php
+++ Bundles/GlueApplication/src/Spryker/Glue/GlueApplication/GlueApplicationConfig.php
@@ -42,7 +42,7 @@ class GlueApplicationConfig extends AbstractBundleConfig
      */
     public function getCorsAllowOrigin(): string
     {
-        return $this->get(GlueApplicationConstants::GLUE_APPLICATION_CORS_ALLOW_ORIGIN);
+        return $this->get(GlueApplicationConstants::GLUE_APPLICATION_CORS_ALLOW_ORIGIN, '');
     }
 
     /**
