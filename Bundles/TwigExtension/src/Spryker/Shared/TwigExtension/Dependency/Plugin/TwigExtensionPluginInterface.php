51982efe4235b53018786a2dc82f01afac879e7c
gechetspr <serhii.chepela@spryker.com>
2019-01-17 14:32:23 +0200

    TE-968 created TwigExtensionInterface

---
+++ Bundles/TwigExtension/src/Spryker/Shared/TwigExtension/Dependency/Plugin/TwigExtensionPluginInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\TwigExtension\Dependency\Plugin;
+
+use Twig\Environment;
+
+interface TwigExtensionPluginInterface
+{
+    /**
+     * Specification:
+     * - Allows to extend Twig with additional functionality (e.g. functions, global variables, etc.)
+     *
+     * @api
+     *
+     * @param \Twig\Environment $twig
+     *
+     * @return \Twig\Environment
+     */
+    public function extend(Environment $twig): Environment;
+}
