91dadaead516dd1839a23b52fdeff1ba202a86d3
denys.sokolov <denys.sokolov@spryker.com>
2019-01-25 15:59:07 +0200

    glue-3910: introduced company business unit module with relationship plugin.

---
+++ Bundles/CompanyBusinessUnitsRestApi/src/Spryker/Glue/CompanyBusinessUnitsRestApi/Processor/CompanyBusinessUnit/Relationship/CompanyBusinessUnitResourceRelationshipExpander.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\CompanyBusinessUnitsRestApi\Processor\CompanyBusinessUnit\Relationship;
+
+use Generated\Shared\Transfer\CompanyUserTransfer;
+use Generated\Shared\Transfer\RestCompanyBusinessUnitAttributesTransfer;
+use Spryker\Glue\CompanyBusinessUnitsRestApi\CompanyBusinessUnitsRestApiConfig;
+use Spryker\Glue\CompanyBusinessUnitsRestApi\Processor\CompanyBusinessUnit\Mapper\CompanyBusinessUnitMapperInterface;
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResource;
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+
+class CompanyBusinessUnitResourceRelationshipExpander implements CompanyBusinessUnitResourceRelationshipExpanderInterface
+{
+    /**
+     * @var \Spryker\Glue\CompanyBusinessUnitsRestApi\Processor\CompanyBusinessUnit\Mapper\CompanyBusinessUnitMapperInterface
+     */
+    protected $companyBusinessUnitMapper;
+
+    /**
+     * @param \Spryker\Glue\CompanyBusinessUnitsRestApi\Processor\CompanyBusinessUnit\Mapper\CompanyBusinessUnitMapperInterface $companyBusinessUnitMapper
+     */
+    public function __construct(CompanyBusinessUnitMapperInterface $companyBusinessUnitMapper)
+    {
+        $this->companyBusinessUnitMapper = $companyBusinessUnitMapper;
+    }
+
+    /**
+     * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface[] $resources
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface[]
+     */
+    public function addResourceRelationships(array $resources, RestRequestInterface $restRequest): array
+    {
+        foreach ($resources as $resource) {
+            $payload = $resource->getPayload();
+            if ($payload === null || $payload instanceof CompanyUserTransfer === false) {
+                continue;
+            }
+
+            $companyBusinessUnitTransfer = $payload->getCompanyBusinessUnit();
+            if ($companyBusinessUnitTransfer === null) {
+                continue;
+            }
+
+            $restCompanyBusinessUnitAttributesTransfer = $this->companyBusinessUnitMapper
+                ->mapCompanyBusinessUnitTransferToRestCompanyBusinessUnitAttributesTransfer(
+                    $companyBusinessUnitTransfer,
+                    new RestCompanyBusinessUnitAttributesTransfer()
+                );
+
+            $companyBusinessUnitResource = new RestResource(
+                CompanyBusinessUnitsRestApiConfig::RESOURCE_COMPANY_BUSINESS_UNITS,
+                $companyBusinessUnitTransfer->getUuid(),
+                $restCompanyBusinessUnitAttributesTransfer
+            );
+
+            $resource->addRelationship($companyBusinessUnitResource);
+        }
+
+        return $resources;
+    }
+}
