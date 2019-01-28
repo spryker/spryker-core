fae1ddeba75b94e77b2798df8a5c7e3fed6e5bcf
Alexander Veselov <alexander.veselov@spryker.com>
2019-01-28 16:34:24 +0000

    CMS-177: CMS-178: content banner

---
+++ Bundles/ContentBanner/src/Spryker/Client/ContentBanner/Plugin/BannerTermExecutorPlugin.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\ContentBanner\Plugin;
+
+use Generated\Shared\Transfer\ContentBannerTransfer;
+use Spryker\Client\ContentStorageExtension\Plugin\ContentTermExecutorPluginInterface;
+use Spryker\Client\Kernel\AbstractPlugin;
+use Spryker\Shared\ContentBanner\ContentBannerConfig;
+
+/**
+ * @method \Spryker\Client\ContentBanner\ContentBannerFactory getFactory()
+ */
+class BannerTermExecutorPlugin extends AbstractPlugin implements ContentTermExecutorPluginInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param array $parameters
+     *
+     * @return array
+     */
+    public function execute(array $parameters): array
+    {
+        return $this->getFactory()
+            ->createBannerTermExecutor()
+            ->execute(
+                $this->mapParametersToTransferObject($parameters)
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @return string
+     */
+    public function getTermKey(): string
+    {
+        return ContentBannerConfig::CONTENT_TERM_BANNER;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @return string
+     */
+    public function getTypeKey(): string
+    {
+        return ContentBannerConfig::CONTENT_TYPE_BANNER;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param array $parameters
+     *
+     * @return \Generated\Shared\Transfer\ContentBannerTransfer
+     */
+    protected function mapParametersToTransferObject(array $parameters): ContentBannerTransfer
+    {
+        $contentBannerTransfer = new ContentBannerTransfer();
+        $contentBannerTransfer->fromArray($parameters, true);
+
+        return $contentBannerTransfer;
+    }
+}
---
+++ Bundles/ContentBannerGui/src/Spryker/Zed/ContentBannerGui/Communication/Form/BannerContentTermForm.php
@@ -0,0 +1,180 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ContentBannerGui\Communication\Form;
+
+use Spryker\Zed\Kernel\Communication\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Required;
+
+class BannerContentTermForm extends AbstractType
+{
+    public const FIELD_TITLE = 'title';
+    public const FIELD_SUB_TITLE = 'subTitle';
+    public const FIELD_IMAGE_URL = 'imageUrl';
+    public const FIELD_CLICK_URL = 'clickUrl';
+    public const FIELD_ALT_TEXT = 'altText';
+
+    /**
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
+     *
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'validation_groups' => function (FormInterface $form) {
+                /** @var \Generated\Shared\Transfer\LocalizedContentTransfer $localizedContentTransfer */
+                $localizedContentTransfer = $form->getParent()->getData();
+                if ($localizedContentTransfer->getFkLocale() === null) {
+                    return [Constraint::DEFAULT_GROUP];
+                }
+                /** @var \Generated\Shared\Transfer\ContentBannerTransfer $contentBanner */
+                $contentBanner = $form->getNormData();
+
+                foreach ($contentBanner->toArray() as $field) {
+                    if (!empty($field)) {
+                        return [Constraint::DEFAULT_GROUP];
+                    }
+                }
+
+                return [];
+            },
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'banner';
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     *
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $this->addTitleField($builder);
+        $this->addSubTitleField($builder);
+        $this->addImageUrlField($builder);
+        $this->addClickUrlField($builder);
+        $this->addAltTextField($builder);
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    protected function addTitleField(FormBuilderInterface $builder)
+    {
+        $builder->add(static::FIELD_TITLE, TextType::class, [
+            'attr' => [
+                'placeholder' => 'Title',
+            ],
+            'label' => false,
+            'constraints' => $this->getTextFieldConstraints(),
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    protected function addSubTitleField(FormBuilderInterface $builder)
+    {
+        $builder->add(static::FIELD_SUB_TITLE, TextType::class, [
+            'attr' => [
+                'placeholder' => 'Sub Title',
+            ],
+            'label' => false,
+            'constraints' => $this->getTextFieldConstraints(),
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    protected function addImageUrlField(FormBuilderInterface $builder)
+    {
+        $builder->add(static::FIELD_IMAGE_URL, TextType::class, [
+            'attr' => [
+                'placeholder' => 'Image URL',
+            ],
+            'label' => false,
+            'constraints' => $this->getTextFieldConstraints(),
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    protected function addClickUrlField(FormBuilderInterface $builder)
+    {
+        $builder->add(static::FIELD_CLICK_URL, TextType::class, [
+            'attr' => [
+                'placeholder' => 'Click URL',
+            ],
+            'label' => false,
+            'constraints' => $this->getTextFieldConstraints(),
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    protected function addAltTextField(FormBuilderInterface $builder)
+    {
+        $builder->add(static::FIELD_ALT_TEXT, TextType::class, [
+            'attr' => [
+                'placeholder' => 'Alt-text',
+            ],
+            'label' => false,
+            'constraints' => $this->getTextFieldConstraints(),
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTextFieldConstraints(): array
+    {
+        return [
+            new Required(),
+            new NotBlank(),
+            new Length(['max' => 100]),
+        ];
+    }
+}
