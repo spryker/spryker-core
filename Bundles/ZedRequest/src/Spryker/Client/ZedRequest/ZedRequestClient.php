bf08987dbdd9ef3fb5e0a585a6c55cb84bfdcd62
Ruslan Salii <ruslan.salii@spryker.com>
2019-01-31 15:55:03 +0200

    rolled back previous solution

--- Bundles/ZedRequest/src/Spryker/Client/ZedRequest/ZedRequestClient.php
+++ Bundles/ZedRequest/src/Spryker/Client/ZedRequest/ZedRequestClient.php
@@ -116,23 +116,4 @@ class ZedRequestClient extends AbstractClient implements ZedRequestClientInterfa
             );
         }
     }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @api
-     *
-     * @return \Spryker\Shared\Kernel\Transfer\TransferInterface|null
-     */
-    public function findLastResponseTransfer(): ?TransferInterface
-    {
-        $zedClient = $this->getFactory()
-            ->getCashedClient();
-
-        if (!$zedClient->hasLastResponse()) {
-            return null;
-        }
-
-        return $zedClient->getLastResponse()->getTransfer();
-    }
 }
