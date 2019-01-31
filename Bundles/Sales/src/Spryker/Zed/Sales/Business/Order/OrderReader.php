184d1eda1110ebf0888080a97947196b52a286f6
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-01-31 12:50:49 +0200

    CC-2949: Updated "OrderReader::findOrderByIdSalesOrder()".
    * Updated "OrderReader::findOrderByIdSalesOrder()" method.

--- Bundles/Sales/src/Spryker/Zed/Sales/Business/Order/OrderReader.php
+++ Bundles/Sales/src/Spryker/Zed/Sales/Business/Order/OrderReader.php
@@ -24,7 +24,7 @@ class OrderReader extends OrderReaderWithoutMultiShippingAddress
             ->querySalesOrderDetailsWithoutShippingAddress($idSalesOrder)
             ->findOne();
 
-        if (!$orderEntity) {
+        if ($orderEntity === null) {
             return null;
         }
 
