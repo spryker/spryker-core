c469baeb4374130a20fdee7e15b5e07794a8c390
AntonKhabiuk <brotherosirus@gmail.com>
2019-01-31 11:43:02 +0200

    GLUE-3192 Add facade tests, add structure for listener tests [skip ci]

---
+++ Bundles/CompanyUserStorage/tests/SprykerTest/CompanyUserStorage/Communication/Plugin/Event/Listener/CompanyUserStoragePublishListenerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\CompanyUserStorage\Communication\Plugin\Event\Listener;
+
+use Codeception\Test\Unit;
+use Spryker\Zed\CompanyUserStorage\Communication\Plugin\Event\Listener\CompanyUserStoragePublishListener;
+
+class CompanyUserStoragePublishListenerTest extends Unit
+{
+
+}
