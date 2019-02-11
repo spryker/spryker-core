0a88ce9b04101d53ad9b30850f1d9f1b820d5155
voitov <tigran.voitov@spryker.com>
2019-02-11 13:08:25 +0200

    glue-2279 CR fixes, returned and deprecated codes and messages to config.

--- Bundles/CartsRestApi/src/Spryker/Glue/CartsRestApi/CartsRestApiConfig.php
+++ Bundles/CartsRestApi/src/Spryker/Glue/CartsRestApi/CartsRestApiConfig.php
@@ -39,6 +39,106 @@ class CartsRestApiConfig extends AbstractBundleConfig
     public const ACTION_GUEST_CART_ITEMS_PATCH = 'patch';
     public const ACTION_GUEST_CART_ITEMS_DELETE = 'delete';
 
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_CART_NOT_FOUND
+     */
+    public const RESPONSE_CODE_CART_NOT_FOUND = '101';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_ITEM_VALIDATION
+     */
+    public const RESPONSE_CODE_ITEM_VALIDATION = '102';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_ITEM_NOT_FOUND
+     */
+    public const RESPONSE_CODE_ITEM_NOT_FOUND = '103';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_CART_ID_MISSING
+     */
+    public const RESPONSE_CODE_CART_ID_MISSING = '104';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_FAILED_DELETING_CART
+     */
+    public const RESPONSE_CODE_FAILED_DELETING_CART = '105';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_FAILED_DELETING_CART_ITEM
+     */
+    public const RESPONSE_CODE_FAILED_DELETING_CART_ITEM = '106';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_FAILED_CREATING_CART
+     */
+    public const RESPONSE_CODE_FAILED_CREATING_CART = '107';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_MISSING_REQUIRED_PARAMETER
+     */
+    public const RESPONSE_CODE_MISSING_REQUIRED_PARAMETER = '108';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_ANONYMOUS_CUSTOMER_UNIQUE_ID_EMPTY
+     */
+    public const RESPONSE_CODE_ANONYMOUS_CUSTOMER_UNIQUE_ID_EMPTY = '109';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::RESPONSE_CODE_CUSTOMER_ALREADY_HAS_CART
+     */
+    public const RESPONSE_CODE_CUSTOMER_ALREADY_HAS_CART = '110';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_CART_ID_MISSING
+     */
+    public const EXCEPTION_MESSAGE_CART_ID_MISSING = 'Cart uuid is missing.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_CART_ITEM_NOT_FOUND
+     */
+    public const EXCEPTION_MESSAGE_CART_ITEM_NOT_FOUND = 'Item with the given group key not found in the cart.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_FAILED_TO_CREATE_CART
+     */
+    public const EXCEPTION_MESSAGE_FAILED_TO_CREATE_CART = 'Failed to create cart.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_CART_WITH_ID_NOT_FOUND
+     */
+    public const EXCEPTION_MESSAGE_CART_WITH_ID_NOT_FOUND = 'Cart with given uuid not found.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_FAILED_DELETING_CART
+     */
+    public const EXCEPTION_MESSAGE_FAILED_DELETING_CART = 'Cart could not be deleted.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_FAILED_DELETING_CART_ITEM
+     */
+    public const EXCEPTION_MESSAGE_FAILED_DELETING_CART_ITEM = 'Cart item could not be deleted.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_MISSING_REQUIRED_PARAMETER
+     */
+    public const EXCEPTION_MESSAGE_MISSING_REQUIRED_PARAMETER = 'Cart uuid or item group key is not specified.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_ANONYMOUS_CUSTOMER_UNIQUE_ID_EMPTY
+     */
+    public const EXCEPTION_MESSAGE_ANONYMOUS_CUSTOMER_UNIQUE_ID_EMPTY = 'Anonymous customer unique id is empty.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::EXCEPTION_MESSAGE_CUSTOMER_ALREADY_HAS_CART
+     */
+    public const EXCEPTION_MESSAGE_CUSTOMER_ALREADY_HAS_CART = 'Customer already has a cart.';
+
+    /**
+     * @deprecated use \Spryker\Shared\CartsRestApi\CartsRestApiConfig::HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID
+     */
+    public const HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID = 'X-Anonymous-Customer-Unique-Id';
+
     protected const GUEST_CART_RESOURCES = [
         CartsRestApiConfig::RESOURCE_GUEST_CARTS,
         CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
