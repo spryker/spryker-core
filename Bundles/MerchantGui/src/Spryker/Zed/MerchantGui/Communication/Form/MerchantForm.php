16d6bbe54d917f3ad809a7bdf42078b8e3c456d7
laktionova <helen.laktionova@spryker.com>
2019-02-12 14:02:06 +0200

    MP-685: Removed unused class.

--- Bundles/MerchantGui/src/Spryker/Zed/MerchantGui/Communication/Form/MerchantForm.php
+++ Bundles/MerchantGui/src/Spryker/Zed/MerchantGui/Communication/Form/MerchantForm.php
@@ -22,7 +22,6 @@ use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Required;
-use Symfony\Component\Validator\Constraints\Type;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
