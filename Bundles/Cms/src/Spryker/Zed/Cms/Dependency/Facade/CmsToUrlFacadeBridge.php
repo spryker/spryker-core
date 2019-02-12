c2d70504fcb7b4915e815a6509095451493a868e
Evhen Odokienko <evhen.odokienko@spryker.com>
2019-02-12 12:23:59 +0200

    PS-2515 resolve conflicts

--- Bundles/Cms/src/Spryker/Zed/Cms/Dependency/Facade/CmsToUrlFacadeBridge.php
+++ Bundles/Cms/src/Spryker/Zed/Cms/Dependency/Facade/CmsToUrlFacadeBridge.php
@@ -70,13 +70,13 @@ class CmsToUrlFacadeBridge implements CmsToUrlFacadeInterface
     }
 
     /**
-     * @param \Generated\Shared\Transfer\UrlTransfer|string $urlTransfer Deprecated: String format is accepted for BC reasons only.
+     * @param \Generated\Shared\Transfer\UrlTransfer $urlTransfer
      *
      * @return bool
      */
-    public function hasUrl($urlTransfer)
+    public function hasUrlCaseInsensitive(UrlTransfer $urlTransfer): bool
     {
-        return $this->urlFacade->hasUrl($urlTransfer);
+        return $this->urlFacade->hasUrlCaseInsensitive($urlTransfer);
     }
 
     /**
--- Bundles/Cms/src/Spryker/Zed/Cms/Dependency/Facade/CmsToUrlFacadeInterface.php
+++ Bundles/Cms/src/Spryker/Zed/Cms/Dependency/Facade/CmsToUrlFacadeInterface.php
@@ -45,11 +45,11 @@ interface CmsToUrlFacadeInterface
     public function deactivateUrl(UrlTransfer $urlTransfer);
 
     /**
-     * @param \Generated\Shared\Transfer\UrlTransfer|string $urlTransfer Deprecated: String format is accepted for BC reasons only.
+     * @param \Generated\Shared\Transfer\UrlTransfer $urlTransfer
      *
      * @return bool
      */
-    public function hasUrl($urlTransfer);
+    public function hasUrlCaseInsensitive(UrlTransfer $urlTransfer): bool;
 
     /**
      * @param \Generated\Shared\Transfer\UrlTransfer $urlTransfer
