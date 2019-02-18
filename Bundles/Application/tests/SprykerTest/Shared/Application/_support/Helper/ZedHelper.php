0bbfb7a61186031051510aa1b65aa56ca3eb9031
Serhii Babich <sergeghost@gmail.com>
2019-02-18 18:17:02 +0200

    CC-1264 fix _after method

--- Bundles/Application/tests/SprykerTest/Shared/Application/_support/Helper/ZedHelper.php
+++ Bundles/Application/tests/SprykerTest/Shared/Application/_support/Helper/ZedHelper.php
@@ -30,7 +30,7 @@ class ZedHelper extends Module
         $tester = $this->getWebDriver();
 
         try {
-            $tester->seeElement('Logout', '//a[@href="/auth/logout"]');
+            $tester->seeElement('Logout', ['href' => '/auth/logout']);
             $tester->click('Logout');
         } catch (\Exception $exception) {
 //            NOP
