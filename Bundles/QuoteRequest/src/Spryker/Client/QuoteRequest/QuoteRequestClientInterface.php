0e2b4d0c85b44f48aea0da873b8c0f09ec13d8a1
Dima Aseev <dmitriy.aseev@spryker.com>
2019-03-01 17:11:23 +0200

    PS-6792: Small changes.

--- Bundles/QuoteRequest/src/Spryker/Client/QuoteRequest/QuoteRequestClientInterface.php
+++ Bundles/QuoteRequest/src/Spryker/Client/QuoteRequest/QuoteRequestClientInterface.php
@@ -115,7 +115,7 @@ interface QuoteRequestClientInterface
      * - Retrieves "Request for Quote" entity filtered by reference.
      * - Expects "Request for Quote" status to be "in-progress".
      * - Expects "Request for Quote" quoteInProgress property exists.
-     * - Expects "Request for Quote" validUntil property exists and grater that current time.
+     * - Expects "Request for Quote" validUntil property exists and greater than current time.
      * - Changes status from "in-progress" to "ready".
      * - Resets isHidden flag to false.
      * - Creates version from quoteInProgress property.
