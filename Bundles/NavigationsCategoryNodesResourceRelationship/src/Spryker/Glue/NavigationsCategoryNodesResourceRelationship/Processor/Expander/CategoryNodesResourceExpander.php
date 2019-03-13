6f9f3ab6f092ed39d1813ebc7f2677dffa15b5ed
voitov <tigran.voitov@spryker.com>
2019-03-13 19:09:13 +0200

    Glue-6065 small fixes

--- Bundles/NavigationsCategoryNodesResourceRelationship/src/Spryker/Glue/NavigationsCategoryNodesResourceRelationship/Processor/Expander/CategoryNodesResourceExpander.php
+++ Bundles/NavigationsCategoryNodesResourceRelationship/src/Spryker/Glue/NavigationsCategoryNodesResourceRelationship/Processor/Expander/CategoryNodesResourceExpander.php
@@ -42,7 +42,7 @@ class CategoryNodesResourceExpander implements CategoryNodesResourceExpanderInte
     public function expandResourceWithCategoryNode(array $resources, RestRequestInterface $restRequest): void
     {
         foreach ($resources as $resource) {
-            if (empty($resource->getAttributes()->offsetGet(static::KEY_NODES))) {
+            if (!$resource->getAttributes()->offsetGet(static::KEY_NODES)) {
                 continue;
             }
 
@@ -64,8 +64,8 @@ class CategoryNodesResourceExpander implements CategoryNodesResourceExpanderInte
         RestRequestInterface $restRequest,
         RestNavigationNodeTransfer $restNavigationNodeTransfer
     ): void {
-        if (!empty($restNavigationNodeTransfer->offsetGet(static::KEY_RESOURCE_ID))
-            && !empty($restNavigationNodeTransfer->offsetGet(static::KEY_NODE_TYPE))
+        if ($restNavigationNodeTransfer->offsetGet(static::KEY_RESOURCE_ID)
+            && $restNavigationNodeTransfer->offsetGet(static::KEY_NODE_TYPE)
             && $restNavigationNodeTransfer->offsetGet(static::KEY_NODE_TYPE) === static::NODE_TYPE_VALUE_CATEGORY
         ) {
             $categoryNode = $this->categoriesResource->findCategoryNodeById(
