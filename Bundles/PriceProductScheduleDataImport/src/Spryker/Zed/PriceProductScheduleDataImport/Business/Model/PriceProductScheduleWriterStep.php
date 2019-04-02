de4ffba68b87dc3467f5e3261439ae186a4703e1
Serhii Babich <sergeghost@gmail.com>
2019-04-02 14:35:44 +0300

    CC-4053 undo changes from packege.lock files, undo exception file and refactoring code

--- Bundles/PriceProductScheduleDataImport/src/Spryker/Zed/PriceProductScheduleDataImport/Business/Model/PriceProductScheduleWriterStep.php
+++ Bundles/PriceProductScheduleDataImport/src/Spryker/Zed/PriceProductScheduleDataImport/Business/Model/PriceProductScheduleWriterStep.php
@@ -55,7 +55,7 @@ class PriceProductScheduleWriterStep implements DataImportStepInterface
 
         $priceProductScheduleEntity = $priceProductScheduleQuery->findOneOrCreate();
 
-        if ($priceProductScheduleEntity->isNew()) {
+        if ($priceProductScheduleEntity->isNew() === false) {
             return;
         }
 
