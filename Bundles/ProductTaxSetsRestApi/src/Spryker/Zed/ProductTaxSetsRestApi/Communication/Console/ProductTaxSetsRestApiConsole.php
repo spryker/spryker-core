ce3b1794756b7b34a36c4f3f4f0d68252e690120
René Klatt <rene.klatt@spryker.com>
2019-04-03 08:15:16 +0000

    te-1407 Fixed found issues

--- Bundles/ProductTaxSetsRestApi/src/Spryker/Zed/ProductTaxSetsRestApi/Communication/Console/ProductTaxSetsRestApiConsole.php
+++ Bundles/ProductTaxSetsRestApi/src/Spryker/Zed/ProductTaxSetsRestApi/Communication/Console/ProductTaxSetsRestApiConsole.php
@@ -36,10 +36,12 @@ class ProductTaxSetsRestApiConsole extends Console
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
-     * @return void
+     * @return int|null
      */
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
         $this->getFacade()->updateTaxSetsWithoutUuid();
+
+        return null;
     }
 }
