43a1fef6ce8c112afc2656a0d8708417a3dc84a9
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-04-04 19:54:22 +0300

    PS-9056: disabled quite unlock after approval rejection when quote in process of quotation.

--- Bundles/Quote/src/Spryker/Zed/Quote/Business/Quote/QuoteLocker.php
+++ Bundles/Quote/src/Spryker/Zed/Quote/Business/Quote/QuoteLocker.php
@@ -12,6 +12,19 @@ use Generated\Shared\Transfer\QuoteTransfer;
 class QuoteLocker implements QuoteLockerInterface
 {
     /**
+     * @var \Spryker\Zed\QuoteExtension\Dependency\Plugin\QuoteUnlockPreCheckPluginInterface[]
+     */
+    protected $quoteUnlockPreCheckPlugins;
+
+    /**
+     * @param \Spryker\Zed\QuoteExtension\Dependency\Plugin\QuoteUnlockPreCheckPluginInterface[] $quoteUnlockPreCheckPlugins
+     */
+    public function __construct(array $quoteUnlockPreCheckPlugins)
+    {
+        $this->quoteUnlockPreCheckPlugins = $quoteUnlockPreCheckPlugins;
+    }
+
+    /**
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
      *
      * @return \Generated\Shared\Transfer\QuoteTransfer
@@ -28,6 +41,26 @@ class QuoteLocker implements QuoteLockerInterface
      */
     public function unlock(QuoteTransfer $quoteTransfer): QuoteTransfer
     {
+        if (!$this->canUnlockQuote($quoteTransfer)) {
+            return $quoteTransfer;
+        }
+
         return $quoteTransfer->setIsLocked(false);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return bool
+     */
+    protected function canUnlockQuote(QuoteTransfer $quoteTransfer): bool
+    {
+        foreach ($this->quoteUnlockPreCheckPlugins as $quoteUnlockPreCheckPlugin) {
+            if (!$quoteUnlockPreCheckPlugin->can($quoteTransfer)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
