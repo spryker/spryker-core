cdcaefd64291c5412e2a09f9b4a0c4e032cd2e68
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-04-12 16:39:11 +0300

    PS-9108: fixed Tl review comments

--- Bundles/QuoteRequestAgent/src/Spryker/Client/QuoteRequestAgent/Dependency/Client/QuoteRequestAgentToZedRequestClientInterface.php
+++ Bundles/QuoteRequestAgent/src/Spryker/Client/QuoteRequestAgent/Dependency/Client/QuoteRequestAgentToZedRequestClientInterface.php
@@ -19,7 +19,7 @@ interface QuoteRequestAgentToZedRequestClientInterface
     /**
      * @param string $url
      * @param \Spryker\Shared\Kernel\Transfer\TransferInterface $object
-     * @param array|int|null $requestOptions Deprecated: Do not use "int" anymore, please use an array for requestOptions.
+     * @param array|int|null $requestOptions
      *
      * @return \Spryker\Shared\Kernel\Transfer\TransferInterface
      */
