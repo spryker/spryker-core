4e73be52ac4b5073f774709ceac77828cd93d761
Sergey Samoylov <sergey.samoylov@spryker.com>
2019-04-17 17:00:52 +0300

    cms-371: cms-1540: fix bridge

--- Bundles/ContentGui/src/Spryker/Zed/ContentGui/Dependency/Service/ContentGuiToUtilEncodingBridge.php
+++ Bundles/ContentGui/src/Spryker/Zed/ContentGui/Dependency/Service/ContentGuiToUtilEncodingBridge.php
@@ -29,7 +29,7 @@ class ContentGuiToUtilEncodingBridge implements ContentGuiToUtilEncodingInterfac
      *
      * @return string
      */
-    public function encodeJson($value, $options = null, $depth = null): string
+    public function encodeJson($value, $options = null, $depth = null)
     {
         return $this->utilEncodingService->encodeJson($value, $options, $depth);
     }
