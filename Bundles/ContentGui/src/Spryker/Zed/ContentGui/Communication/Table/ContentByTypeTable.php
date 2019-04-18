5606456d675eb7bd07c065d78a0bc80552cceb3c
Sergey Samoylov <sergey.samoylov@spryker.com>
2019-04-18 16:26:06 +0300

    cms-371: cms-1945: create ContentByTypeTable

---
+++ Bundles/ContentGui/src/Spryker/Zed/ContentGui/Communication/Table/ContentByTypeTable.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ContentGui\Communication\Table;
+
+use Orm\Zed\Content\Persistence\Map\SpyContentTableMap;
+use Orm\Zed\Content\Persistence\SpyContentQuery;
+use Spryker\Zed\Gui\Communication\Table\AbstractTable;
+use Spryker\Zed\Gui\Communication\Table\TableConfiguration;
+
+class ContentByTypeTable extends AbstractTable
+{
+    /**
+     * @var string
+     */
+    private $contentType;
+
+    /**
+     * @var \Orm\Zed\Content\Persistence\SpyContentQuery
+     */
+    private $contentQuery;
+
+    /**
+     * @param \Orm\Zed\Content\Persistence\SpyContentQuery $contentQuery
+     * @param string|null $contentType
+     */
+    public function __construct(
+        SpyContentQuery $contentQuery,
+        ?string $contentType = null
+    ) {
+        $this->contentQuery = $contentQuery;
+        $this->contentType = $contentType;
+    }
+
+    /**
+     * @param \Spryker\Zed\Gui\Communication\Table\TableConfiguration $config
+     *
+     * @return \Spryker\Zed\Gui\Communication\Table\TableConfiguration
+     */
+    protected function configure(TableConfiguration $config): TableConfiguration
+    {
+        $config = $this->setHeader($config);
+        $config->setSortable([
+            ContentTableConstants::COL_ID_CONTENT,
+            ContentTableConstants::COL_NAME,
+        ]);
+        $config->addRawColumn(ContentTableConstants::COL_ACTIONS);
+        $config->setDefaultSortField(ContentTableConstants::COL_ID_CONTENT, TableConfiguration::SORT_DESC);
+        $config->setSearchable([
+            ContentTableConstants::COL_ID_CONTENT,
+            ContentTableConstants::COL_NAME,
+        ]);
+        return $config;
+    }
+
+    /**
+     * @param \Spryker\Zed\Gui\Communication\Table\TableConfiguration $config
+     *
+     * @return \Spryker\Zed\Gui\Communication\Table\TableConfiguration
+     */
+    protected function setHeader(TableConfiguration $config): TableConfiguration
+    {
+        $header = [
+            ContentTableConstants::COL_ID_CONTENT => 'Content Item ID',
+            ContentTableConstants::COL_NAME => 'Name',
+            ContentTableConstants::COL_ACTIONS => '',
+        ];
+        $config->setHeader($header);
+        return $config;
+    }
+
+    /**
+     * @param \Spryker\Zed\Gui\Communication\Table\TableConfiguration $config
+     *
+     * @return array
+     */
+    protected function prepareData(TableConfiguration $config): array
+    {
+        if ($this->contentType) {
+            $this->contentQuery->filterByContentTypeKey($this->contentType);
+        }
+
+        $contentItems = $this->runQuery($this->contentQuery, $config);
+        $results = [];
+        foreach ($contentItems as $key => $contentItem) {
+            $checked = $key === 0;
+
+            $results[] = [
+                ContentTableConstants::COL_ID_CONTENT => $contentItem[SpyContentTableMap::COL_ID_CONTENT],
+                ContentTableConstants::COL_NAME => $contentItem[SpyContentTableMap::COL_NAME],
+                ContentTableConstants::COL_ACTIONS => $this->buildLinks($contentItem, $checked),
+            ];
+        }
+        return $results;
+    }
+
+    /**
+     * @param array $contentItem
+     * @param bool $checked
+     *
+     * @return string
+     */
+    protected function buildLinks(array $contentItem, bool $checked = false): string
+    {
+        $selectedAttr = $checked ? 'checked="checked"' : '';
+
+        return sprintf('<input type="radio" %s name="content-item" value="%s"/>', $selectedAttr, $contentItem[ContentTableConstants::COL_ID_CONTENT]);
+    }
+}
