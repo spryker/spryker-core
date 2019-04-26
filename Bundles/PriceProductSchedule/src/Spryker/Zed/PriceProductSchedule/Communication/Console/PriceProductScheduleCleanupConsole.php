4fc23c11853312bf57f124fb59c2db660e0eb339
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-04-26 10:24:06 +0300

    cc-3400 fix console commands

--- Bundles/PriceProductSchedule/src/Spryker/Zed/PriceProductSchedule/Communication/Console/PriceProductScheduleCleanupConsole.php
+++ Bundles/PriceProductSchedule/src/Spryker/Zed/PriceProductSchedule/Communication/Console/PriceProductScheduleCleanupConsole.php
@@ -41,11 +41,13 @@ class PriceProductScheduleCleanupConsole extends Console
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
-     * @return void
+     * @return int|null
      */
-    protected function execute(InputInterface $input, OutputInterface $output): void
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
     {
         $daysRetained = (int)$this->input->getArgument(static::DAYS_RETAINED);
         $this->getFacade()->cleanAppliedScheduledPrices($daysRetained);
+
+        return static::CODE_SUCCESS;
     }
 }
