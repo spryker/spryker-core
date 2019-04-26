393ddc1e9d665169bfc0dce1fc4d73fc67fc38e6
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2019-04-26 18:13:58 +0300

    PS-6556: Created SharedCartFacadeTest and placeholders for future ResourceShareStrategy-related tests

---
+++ Bundles/SharedCart/tests/SprykerTest/Zed/SharedCart/Business/SharedCartFacadeTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\SharedCart\Business;
+
+use Codeception\TestCase\Test;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Zed
+ * @group SharedCart
+ * @group Business
+ * @group Facade
+ * @group SharedCartFacadeTest
+ * Add your own group annotations below this line
+ */
+class SharedCartFacadeTest extends Test
+{
+    /**
+     * @var \SprykerTest\Zed\SharedCart\SharedCartBusinessTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testApplyActivatorStrategyPluginShouldXXX(): void
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    /**
+     * @return void
+     */
+    public function testApplyDataExpanderStrategyPluginShouldXXX(): void
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
