ed3d8c5ddf0bb4da3cb614df61282e5d6af13816
Theodoros Liokos <theodoros.liokos@spryker.com>
2019-04-30 14:12:02 +0200

    Added Content Id in CI Editing Page

--- Bundles/ContentGui/src/Spryker/Zed/ContentGui/Communication/Controller/EditContentController.php
+++ Bundles/ContentGui/src/Spryker/Zed/ContentGui/Communication/Controller/EditContentController.php
@@ -61,6 +61,7 @@ class EditContentController extends AbstractController
             'contentTabs' => $contentTabs->createView(),
             'contentForm' => $contentForm->createView(),
             'backButton' => static::URL_REDIRECT_CONTENT_LIST_PAGE,
+            'contentId' => $contentId,
             'contentName' => $this->getFactory()->createContentResolver()->getContentPlugin($termKey)->getTermKey(),
         ]);
     }
