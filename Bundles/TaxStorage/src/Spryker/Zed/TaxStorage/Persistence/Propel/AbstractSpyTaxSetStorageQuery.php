5efe207f3642c974483b7050b972169f349a0101
Aleksandr Kovalchuk <avtop25495@gmail.com>
2019-05-02 17:08:38 +0300

    GLUE-3315

--- Bundles/TaxStorage/src/Spryker/Zed/TaxStorage/Persistence/Propel/AbstractSpyTaxSetStorageQuery.php
+++ Bundles/TaxStorage/src/Spryker/Zed/TaxStorage/Persistence/Propel/AbstractSpyTaxSetStorageQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace Spryker\Zed\TaxStorage\Persistence\Propel;
 
 use Orm\Zed\TaxStorage\Persistence\Base\SpyTaxSetStorageQuery as BaseSpyTaxSetStorageQuery;
@@ -10,9 +15,8 @@ use Orm\Zed\TaxStorage\Persistence\Base\SpyTaxSetStorageQuery as BaseSpyTaxSetSt
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
- *
  */
 abstract class AbstractSpyTaxSetStorageQuery extends BaseSpyTaxSetStorageQuery
 {
