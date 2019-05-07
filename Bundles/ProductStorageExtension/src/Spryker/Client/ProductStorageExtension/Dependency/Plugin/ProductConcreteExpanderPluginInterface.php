64794dbdb51dc93ec0059aa85b693ec742754d6f
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-05-07 09:08:46 +0300

    cc-2223 refactoring after cr

--- Bundles/ProductStorageExtension/src/Spryker/Client/ProductStorageExtension/Dependency/Plugin/ProductConcreteExpanderPluginInterface.php
+++ Bundles/ProductStorageExtension/src/Spryker/Client/ProductStorageExtension/Dependency/Plugin/ProductConcreteExpanderPluginInterface.php
@@ -1,11 +1,25 @@
 <?php
 
-namespace Spryker\Client\ProductStorageExtension\Dependency\Plugin;
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
 
+namespace Spryker\Client\ProductStorageExtension\Dependency\Plugin;
 
 use Generated\Shared\Transfer\ProductConcreteTransfer;
 
 interface ProductConcreteExpanderPluginInterface
 {
+    /**
+     * Specification:
+     * - expands and returns ProductConcreteTransfer
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ProductConcreteTransfer $productConcreteTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductConcreteTransfer
+     */
     public function expand(ProductConcreteTransfer $productConcreteTransfer): ProductConcreteTransfer;
 }
