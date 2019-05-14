80fd86d32786445dc19d3ce55b4f6086736436fb
Alexander Veselov <alexander.veselov@spryker.com>
2019-05-14 06:29:38 +0000

    CMS-2104: fixed ContentBannerDataImportToUtilEncodingInterface signature

--- Bundles/ContentBannerDataImport/src/Spryker/Zed/ContentBannerDataImport/Dependency/Service/ContentBannerDataImportToUtilEncodingBridge.php
+++ Bundles/ContentBannerDataImport/src/Spryker/Zed/ContentBannerDataImport/Dependency/Service/ContentBannerDataImportToUtilEncodingBridge.php
@@ -29,7 +29,7 @@ class ContentBannerDataImportToUtilEncodingBridge implements ContentBannerDataIm
      *
      * @return string|null
      */
-    public function encodeJson($value, $options = null, $depth = null): ?string
+    public function encodeJson($value, $options = null, $depth = null)
     {
         return $this->utilEncodingService->encodeJson($value, $options, $depth);
     }
