446417937637c21e46c554c75d2e78823cac94bf
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2019-05-15 09:31:02 +0300

    PS-6556: [TL CR] Removed ShareCartByUuidActivatorStrategy::getShareDetailFromShareCartRequestTransfer() method

--- Bundles/SharedCart/src/Spryker/Zed/SharedCart/Business/ResourceShare/ShareCartByUuidActivatorStrategy.php
+++ Bundles/SharedCart/src/Spryker/Zed/SharedCart/Business/ResourceShare/ShareCartByUuidActivatorStrategy.php
@@ -10,8 +10,6 @@ namespace Spryker\Zed\SharedCart\Business\ResourceShare;
 use Generated\Shared\Transfer\MessageTransfer;
 use Generated\Shared\Transfer\ResourceShareRequestTransfer;
 use Generated\Shared\Transfer\ResourceShareResponseTransfer;
-use Generated\Shared\Transfer\ShareCartRequestTransfer;
-use Generated\Shared\Transfer\ShareDetailTransfer;
 use Spryker\Shared\SharedCart\SharedCartConfig;
 use Spryker\Zed\SharedCart\Business\Model\QuoteCompanyUserWriterInterface;
 use Spryker\Zed\SharedCart\Persistence\SharedCartEntityManagerInterface;
@@ -99,7 +97,7 @@ class ShareCartByUuidActivatorStrategy implements ShareCartByUuidActivatorStrate
                 ->setResourceShare($resourceShareRequestTransfer->getResourceShare());
         }
 
-        $newShareDetailTransfer = $this->getShareDetailFromShareCartRequestTransfer($shareCartRequestTransfer);
+        $newShareDetailTransfer = $shareCartRequestTransfer->getShareDetails()->offsetGet(0);
         $newQuotePermissionGroupTransfer = $newShareDetailTransfer->getQuotePermissionGroup();
         $storedQuotePermissionGroupTransfer = $storedShareDetailTransfer->getQuotePermissionGroup();
 
@@ -116,18 +114,6 @@ class ShareCartByUuidActivatorStrategy implements ShareCartByUuidActivatorStrate
         return (new ResourceShareResponseTransfer())
             ->setIsSuccessful(true)
             ->setResourceShare($resourceShareRequestTransfer->getResourceShare());
-    }
-
-    /**
-     * @param \Generated\Shared\Transfer\ShareCartRequestTransfer $shareCartRequestTransfer
-     *
-     * @return \Generated\Shared\Transfer\ShareDetailTransfer
-     */
-    protected function getShareDetailFromShareCartRequestTransfer(ShareCartRequestTransfer $shareCartRequestTransfer): ShareDetailTransfer
-    {
-        $shareDetails = $shareCartRequestTransfer->getShareDetails();
-
-        return $shareDetails->offsetGet(0);
     }
 
     /**
