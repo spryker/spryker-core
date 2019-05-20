2038b246cff26bd60c94f23abd5b7fda7ed0d363
René Klatt <rene.klatt@spryker.com>
2019-05-20 14:47:46 +0000

    TE-1070 copied search.json files to index-name.json files.

---
+++ Bundles/Search/src/Spryker/Zed/Search/Communication/Console/SearchValidateDefinitionConsole.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Search\Communication\Console;
+
+use Spryker\Zed\Kernel\Communication\Console\Console;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @method \Spryker\Zed\Search\Business\SearchFacadeInterface getFacade()
+ * @method \Spryker\Zed\Search\Communication\SearchCommunicationFactory getFactory()
+ */
+class SearchConsole extends Console
+{
+    public const COMMAND_NAME = 'search:validate:definition';
+    public const DESCRIPTION = 'This command will validate all definitions.';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName(static::COMMAND_NAME);
+        $this->setDescription(static::DESCRIPTION);
+
+        parent::configure();
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $validationResultTransfer = $this->getFacade()->validateDefinitions();
+    }
+}
