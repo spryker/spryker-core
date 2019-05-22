e48c620c80d47051fe525a7cb9d74363f941d57c
voitov <tigran.voitov@spryker.com>
2019-05-22 13:01:58 +0300

    glue-2279 removed unused stuff

--- Bundles/CartsRestApi/src/Spryker/Glue/CartsRestApi/Processor/Cart/CartReader.php
+++ Bundles/CartsRestApi/src/Spryker/Glue/CartsRestApi/Processor/Cart/CartReader.php
@@ -9,7 +9,6 @@ namespace Spryker\Glue\CartsRestApi\Processor\Cart;
 
 use Generated\Shared\Transfer\QuoteCollectionTransfer;
 use Generated\Shared\Transfer\QuoteCriteriaFilterTransfer;
-use Generated\Shared\Transfer\QuoteResponseTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
 use Spryker\Client\CartsRestApi\CartsRestApiClientInterface;
 use Spryker\Glue\CartsRestApi\Processor\Mapper\CartsResourceMapperInterface;
@@ -114,33 +113,6 @@ class CartReader implements CartReaderInterface
         }
 
         return $this->getRestQuoteCollectionResponse($restRequest, $quoteCollectionTransfer);
-    }
-
-    /**
-     * @param string $uuidCart
-     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
-     *
-     * @return \Generated\Shared\Transfer\QuoteResponseTransfer
-     */
-    public function getQuoteTransferByUuid(string $uuidCart, RestRequestInterface $restRequest): QuoteResponseTransfer
-    {
-        $quoteCollectionTransfer = $this->getCustomerQuotes($restRequest);
-
-        if ($quoteCollectionTransfer->getQuotes()->count() === 0) {
-            return (new QuoteResponseTransfer())
-                ->setIsSuccessful(false);
-        }
-
-        foreach ($quoteCollectionTransfer->getQuotes() as $quoteTransfer) {
-            if ($quoteTransfer->getUuid() === $uuidCart) {
-                return (new QuoteResponseTransfer())
-                    ->setIsSuccessful(true)
-                    ->setQuoteTransfer($quoteTransfer);
-            }
-        }
-
-        return (new QuoteResponseTransfer())
-            ->setIsSuccessful(false);
     }
 
     /**
