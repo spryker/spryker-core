853876028be0cbf9742ec5b0487ea588922ed812
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2019-05-23 18:03:51 +0300

    CC-4655 Implemented tests

--- Bundles/Oms/tests/SprykerTest/Zed/Oms/_support/OmsCommunicationTester.php
+++ Bundles/Oms/tests/SprykerTest/Zed/Oms/_support/OmsCommunicationTester.php
@@ -8,6 +8,13 @@
 namespace SprykerTest\Zed\Oms;
 
 use Codeception\Actor;
+use Generated\Shared\Transfer\OrderTransfer;
+use Silex\Application;
+use Spryker\Zed\Oms\Business\OmsFacadeInterface;
+use Spryker\Zed\Oms\Communication\Controller\TriggerController;
+use Spryker\Zed\Oms\Communication\OmsCommunicationFactory;
+use Spryker\Zed\Sales\Business\SalesFacadeInterface;
+use Symfony\Component\Form\FormInterface;
 
 /**
  * Inherited Methods
@@ -28,7 +35,69 @@ class OmsCommunicationTester extends Actor
 {
     use _generated\OmsCommunicationTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+    /**
+     * @return \Spryker\Zed\Oms\Communication\Controller\TriggerController
+     */
+    public function createTriggerController(): TriggerController
+    {
+        $triggerController = new TriggerController();
+        $triggerController->setApplication(new Application());
+
+        return $triggerController;
+    }
+
+    /**
+     * @return \Spryker\Zed\Oms\Business\OmsFacadeInterface
+     */
+    public function getOmsFacade(): OmsFacadeInterface
+    {
+        return $this->getLocator()->oms()->facade();
+    }
+
+    /**
+     * @param string[] $processes
+     *
+     * @return void
+     */
+    public function haveTestStatemachine(array $processes): void
+    {
+        $this->configureTestStateMachine($processes);
+    }
+
+    /**
+     * @param array $orderData
+     * @param string $omsProcessName
+     *
+     * @return \Generated\Shared\Transfer\OrderTransfer
+     */
+    public function haveOrderTransfer(array $orderData, string $omsProcessName): OrderTransfer
+    {
+        $saveOrderTransfer = $this->haveOrder($orderData, $omsProcessName);
+
+        return $this->getSalesFacade()->findOrderByIdSalesOrder($saveOrderTransfer->getIdSalesOrder());
+    }
+
+    /**
+     * @return \Spryker\Zed\Sales\Business\SalesFacadeInterface
+     */
+    public function getSalesFacade(): SalesFacadeInterface
+    {
+        return $this->getLocator()->sales()->facade();
+    }
+
+    /**
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    public function createOmsTriggerForm(): FormInterface
+    {
+        return $this->createOmsCommunicationFactory()->createOmsTriggerForm();
+    }
+
+    /**
+     * @return \Spryker\Zed\Oms\Communication\OmsCommunicationFactory
+     */
+    protected function createOmsCommunicationFactory(): OmsCommunicationFactory
+    {
+        return new OmsCommunicationFactory();
+    }
 }
