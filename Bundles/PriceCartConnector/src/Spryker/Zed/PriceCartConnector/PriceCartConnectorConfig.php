3e0f49ef809e0cdf09b4e0cc3d38dbd01e07e84f
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-05-29 14:36:44 +0300

    cc-2223 added api and spec to the config

--- Bundles/PriceCartConnector/src/Spryker/Zed/PriceCartConnector/PriceCartConnectorConfig.php
+++ Bundles/PriceCartConnector/src/Spryker/Zed/PriceCartConnector/PriceCartConnectorConfig.php
@@ -14,6 +14,11 @@ class PriceCartConnectorConfig extends AbstractBundleConfig
     protected const MIN_PRICE_RESTRICTION = 1;
 
     /**
+     * Specification:
+     * - Min price used for validation price of a product in the Spryker\Zed\PriceCartConnector\Business\Validator\PriceProductValidator.
+     *
+     * @api
+     *
      * @return int
      */
     public function getMinPriceRestriction(): int
