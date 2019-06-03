169ae3ebeaf3278d96acb9d00e09abdf4fabfaac
Oleh Hladchenko <glad.oleg@gmail.com>
2019-06-03 18:56:17 +0300

    GLUE-7473 Fix config.

--- Bundles/OauthPermission/src/Spryker/Zed/OauthPermission/OauthPermissionConfig.php
+++ Bundles/OauthPermission/src/Spryker/Zed/OauthPermission/OauthPermissionConfig.php
@@ -11,13 +11,11 @@ use Spryker\Zed\Kernel\AbstractBundleConfig;
 
 class OauthPermissionConfig extends AbstractBundleConfig
 {
-    protected const KEYS_OAUTH_USER_IDENTIFIER_FILTER = [];
-
     /**
      * @return array
      */
     public function getOauthUserIdentifierFilterKeys(): array
     {
-        return static::KEYS_OAUTH_USER_IDENTIFIER_FILTER;
+        return [];
     }
 }
