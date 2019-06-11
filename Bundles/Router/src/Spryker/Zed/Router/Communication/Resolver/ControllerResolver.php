f7dfe49912a715b479ac76e4665da164dabd908b
René Klatt <rene.klatt@spryker.com>
2019-06-11 13:40:14 +0000

    TE-1211 Added support for render(controller()) removed usage of render(controller()).

--- Bundles/Router/src/Spryker/Zed/Router/Communication/Resolver/ControllerResolver.php
+++ Bundles/Router/src/Spryker/Zed/Router/Communication/Resolver/ControllerResolver.php
@@ -9,6 +9,8 @@ namespace Spryker\Zed\Router\Communication\Resolver;
 
 use InvalidArgumentException;
 use Spryker\Service\Container\ContainerInterface;
+use Spryker\Zed\Kernel\ClassResolver\Controller\ControllerResolver as ControllerClassResolver;
+use Spryker\Zed\Kernel\Communication\BundleControllerAction;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 
@@ -43,6 +45,10 @@ class ControllerResolver implements ControllerResolverInterface
         }
 
         if (is_string($controller)) {
+            if (strpos($controller, '/') !== false) {
+                return $this->resolveControllerFromUri($controller, $request);
+            }
+
             [$controllerServiceIdentifier, $actionName] = explode(':', $controller);
             if ($this->container->has($controllerServiceIdentifier)) {
                 return [$this->container->get($controllerServiceIdentifier), $actionName];
@@ -101,5 +107,36 @@ class ControllerResolver implements ControllerResolverInterface
      */
     public function getArguments(Request $request, $controller)
     {
+    }
+
+    /**
+     * @deprecated Will be removed without replacement. This method only exists for calls to `render(controller('/foo/bar/baz'))` which can be simplified by using `render('/foo/bar/baz')` instead.
+     *
+     * @param string $uri
+     * @param Request $request
+     *
+     * @return array
+     */
+    private function resolveControllerFromUri(string $uri, Request $request): array
+    {
+        [$module, $controllerName, $actionName] = explode('/', ltrim($uri, '/'));
+
+        $bundleControllerAction = new BundleControllerAction($module, $controllerName, $actionName);
+        $controllerResolver = new ControllerClassResolver();
+
+        $controller = $controllerResolver->resolve($bundleControllerAction);
+        $controller = $this->injectContainerAndInitialize($controller);
+
+        $request->attributes->set(
+            '_template',
+            sprintf(
+                '%s/%s/%s',
+                $bundleControllerAction->getBundle(),
+                $bundleControllerAction->getController(),
+                $bundleControllerAction->getAction()
+            )
+        );
+
+        return [$controller, $bundleControllerAction->getAction() . 'Action'];
     }
 }
