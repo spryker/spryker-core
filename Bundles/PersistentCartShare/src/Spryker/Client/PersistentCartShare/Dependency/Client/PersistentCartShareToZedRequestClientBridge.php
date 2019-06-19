3ed7428de911576a70519da7a12a5c47a521d283
Evhen Odokienko <evhen.odokienko@spryker.com>
2019-06-19 12:30:38 +0300

    PS-6554: Clean PersistentCartShare deprecations

--- Bundles/PersistentCartShare/src/Spryker/Client/PersistentCartShare/Dependency/Client/PersistentCartShareToZedRequestClientBridge.php
+++ Bundles/PersistentCartShare/src/Spryker/Client/PersistentCartShare/Dependency/Client/PersistentCartShareToZedRequestClientBridge.php
@@ -27,7 +27,7 @@ class PersistentCartShareToZedRequestClientBridge implements PersistentCartShare
     /**
      * @param string $url
      * @param \Spryker\Shared\Kernel\Transfer\TransferInterface $object
-     * @param array|int|null $requestOptions Deprecated: Do not use "int" anymore, please use an array for requestOptions.
+     * @param array|null $requestOptions
      *
      * @return \Spryker\Shared\Kernel\Transfer\TransferInterface
      */
