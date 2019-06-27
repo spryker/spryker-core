4448ba54f4015d26bbb7bbc196051f1530a3e6a4
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2019-06-27 10:43:36 +0300

    CC-4482 added Referrer-Policy and Feature-Policy headers to the HeadersSecurityServiceProvider.

--- Bundles/Application/src/Spryker/Shared/Application/ServiceProvider/HeadersSecurityServiceProvider.php
+++ Bundles/Application/src/Spryker/Shared/Application/ServiceProvider/HeadersSecurityServiceProvider.php
@@ -35,6 +35,16 @@ class HeadersSecurityServiceProvider implements ServiceProviderInterface
     public const HEADER_X_XSS_PROTECTION = 'X-XSS-Protection';
 
     /**
+     * @const string
+     */
+    public const HEADER_REFERER_POLICY = 'Referrer-Policy';
+
+    /**
+     * @const string
+     */
+    public const HEADER_FEATURE_POLICY = 'Feature-Policy';
+
+    /**
      * {@inheritdoc}
      *
      * @param \Silex\Application $app
@@ -72,5 +82,7 @@ class HeadersSecurityServiceProvider implements ServiceProviderInterface
         $event->getResponse()->headers->set(static::HEADER_CONTENT_SECURITY_POLICY, 'frame-ancestors \'self\'');
         $event->getResponse()->headers->set(static::HEADER_X_CONTENT_TYPE_OPTIONS, 'nosniff');
         $event->getResponse()->headers->set(static::HEADER_X_XSS_PROTECTION, '1; mode=block');
+        $event->getResponse()->headers->set(static::HEADER_REFERER_POLICY, 'same-origin');
+        $event->getResponse()->headers->set(static::HEADER_FEATURE_POLICY, '*');
     }
 }
