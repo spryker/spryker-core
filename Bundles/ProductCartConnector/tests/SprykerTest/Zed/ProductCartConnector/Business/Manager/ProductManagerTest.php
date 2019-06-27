3dd3c5b64e77496631ab8fed39c73a9baaa30d89
gechetspr <serhii.chepela@spryker.com>
2019-06-27 16:53:05 +0300

    TE-3719 travis fixes

--- Bundles/ProductCartConnector/tests/SprykerTest/Zed/ProductCartConnector/Business/Manager/ProductManagerTest.php
+++ Bundles/ProductCartConnector/tests/SprykerTest/Zed/ProductCartConnector/Business/Manager/ProductManagerTest.php
@@ -149,6 +149,7 @@ class ProductManagerTest extends Unit
                 'hasProductConcrete',
                 'isProductConcreteActive',
                 'getRawProductConcreteBySku',
+                'getRawProductConcreteTransfersByConcreteSkus',
             ])
             ->disableOriginalConstructor()
             ->getMock();
