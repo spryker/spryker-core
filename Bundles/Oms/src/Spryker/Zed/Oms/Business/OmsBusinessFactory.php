c284e71f0b84972e06de556be7133bbacd3fcdd9
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-07-02 10:20:28 +0300

    CC-5370, 5371. Reverted float stock.
    * Fixed Travis error.

--- Bundles/Oms/src/Spryker/Zed/Oms/Business/OmsBusinessFactory.php
+++ Bundles/Oms/src/Spryker/Zed/Oms/Business/OmsBusinessFactory.php
@@ -31,6 +31,7 @@ use Spryker\Zed\Oms\Business\Util\OrderItemMatrix;
 use Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject;
 use Spryker\Zed\Oms\Business\Util\Reservation;
 use Spryker\Zed\Oms\Business\Util\TransitionLog;
+use Spryker\Zed\Oms\Dependency\Service\OmsToShipmentServiceInterface;
 use Spryker\Zed\Oms\OmsDependencyProvider;
 
 /**
@@ -352,5 +353,13 @@ class OmsBusinessFactory extends AbstractBusinessFactory
         return new OrderStateMachineFlagReader(
             $this->createOrderStateMachineBuilder()
         );
+    }
+
+    /**
+     * @return \Spryker\Zed\Oms\Dependency\Service\OmsToShipmentServiceInterface
+     */
+    protected function getShipmentService(): OmsToShipmentServiceInterface
+    {
+        return $this->getProvidedDependency(OmsDependencyProvider::SERVICE_SHIPMENT);
     }
 }
