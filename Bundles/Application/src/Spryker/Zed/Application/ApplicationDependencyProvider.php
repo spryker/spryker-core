52fda1d1d4634f08221eb9ca1caea3f0ff02646b
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2019-07-03 11:04:48 +0300

    CC-4482 fixed uses.

--- Bundles/Application/src/Spryker/Zed/Application/ApplicationDependencyProvider.php
+++ Bundles/Application/src/Spryker/Zed/Application/ApplicationDependencyProvider.php
@@ -11,8 +11,6 @@ use Silex\Provider\FormServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
-use Spryker\Shared\Application\ServiceProvider\HeadersSecurityServiceProvider;
-use Spryker\Shared\Config\Environment;
 use Spryker\Shared\ErrorHandler\Plugin\ServiceProvider\WhoopsErrorHandlerServiceProvider;
 use Spryker\Zed\Application\Communication\Plugin\ServiceProvider\HeaderServiceProvider;
 use Spryker\Zed\Application\Communication\Plugin\ServiceProvider\MvcRoutingServiceProvider;
