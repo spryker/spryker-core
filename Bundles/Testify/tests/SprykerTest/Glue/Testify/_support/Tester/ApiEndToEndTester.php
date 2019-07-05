8ff9d8699856fb26bf50bd912ca4b8bd266097e7
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2019-07-05 16:35:46 +0200

    GLUE-1623 moved folder to its own directory that it is autoloaded

--- Bundles/Testify/tests/SprykerTest/Glue/Testify/_support/Tester/ApiEndToEndTester.php
+++ Bundles/Testify/tests/SprykerTest/Glue/Testify/_support/Tester/ApiEndToEndTester.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace SprykerTest\Glue\Testify\Helper;
+namespace SprykerTest\Glue\Testify\Tester;
 
 use Spryker\Shared\Config\Config;
 use Spryker\Shared\Testify\TestifyConstants;
