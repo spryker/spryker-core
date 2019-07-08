e3b02d03ff1e3f7972c86deb63d2f8cd48f65a7c
Serhii Babich <sergeghost@gmail.com>
2019-07-08 12:15:18 +0300

    CC-5385 update plugin folder

--- Bundles/GiftCard/src/Spryker/Zed/GiftCard/Communication/Plugin/Shipment/GiftCardShipmentGroupMethodFilterPlugin.php
+++ Bundles/GiftCard/src/Spryker/Zed/GiftCard/Communication/Plugin/Shipment/GiftCardShipmentGroupMethodFilterPlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\GiftCard\Communication\Plugin;
+namespace Spryker\Zed\GiftCard\Communication\Plugin\Shipment;
 
 use ArrayObject;
 use Generated\Shared\Transfer\QuoteTransfer;
