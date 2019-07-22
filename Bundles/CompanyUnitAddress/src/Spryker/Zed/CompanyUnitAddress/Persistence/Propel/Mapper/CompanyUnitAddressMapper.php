ee8305ee379797b092cb03836d1de666fa7560ef
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2019-07-22 11:58:06 +0300

    PS-9952: Introduced `CompanyUnitAddressMapper::mapCompanyUnitAddressEntityTransfersToCompanyUnitAddressCollectionTransfer()`

--- Bundles/CompanyUnitAddress/src/Spryker/Zed/CompanyUnitAddress/Persistence/Propel/Mapper/CompanyUnitAddressMapper.php
+++ Bundles/CompanyUnitAddress/src/Spryker/Zed/CompanyUnitAddress/Persistence/Propel/Mapper/CompanyUnitAddressMapper.php
@@ -9,6 +9,7 @@ namespace Spryker\Zed\CompanyUnitAddress\Persistence\Propel\Mapper;
 
 use Generated\Shared\Transfer\CompanyBusinessUnitCollectionTransfer;
 use Generated\Shared\Transfer\CompanyBusinessUnitTransfer;
+use Generated\Shared\Transfer\CompanyUnitAddressCollectionTransfer;
 use Generated\Shared\Transfer\CompanyUnitAddressTransfer;
 use Generated\Shared\Transfer\SpyCompanyUnitAddressEntityTransfer;
 use Orm\Zed\CompanyUnitAddress\Persistence\SpyCompanyUnitAddress;
@@ -37,6 +38,27 @@ class CompanyUnitAddressMapper implements CompanyUnitAddressMapperInterface
         $companyUnitAddressTransfer->setCompanyBusinessUnits($companyBusinessUnitTransfers);
 
         return $companyUnitAddressTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\SpyCompanyUnitAddressEntityTransfer[] $companyUnitAddressEntityTransfers
+     *
+     * @return \Generated\Shared\Transfer\CompanyUnitAddressCollectionTransfer
+     */
+    public function mapCompanyUnitAddressEntityTransfersToCompanyUnitAddressCollectionTransfer(
+        array $companyUnitAddressEntityTransfers
+    ): CompanyUnitAddressCollectionTransfer {
+        $companyUnitAddressCollectionTransfer = new CompanyUnitAddressCollectionTransfer();
+        foreach ($companyUnitAddressEntityTransfers as $companyUnitAddressEntityTransfer) {
+            $unitAddressTransfer = $this->mapEntityTransferToCompanyUnitAddressTransfer(
+                $companyUnitAddressEntityTransfer,
+                new CompanyUnitAddressTransfer()
+            );
+
+            $companyUnitAddressCollectionTransfer->addCompanyUnitAddress($unitAddressTransfer);
+        }
+
+        return $companyUnitAddressCollectionTransfer;
     }
 
     /**
