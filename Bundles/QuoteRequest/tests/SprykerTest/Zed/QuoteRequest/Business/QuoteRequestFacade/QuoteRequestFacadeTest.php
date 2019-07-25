18f697456d7348137c2c3af361e218cf67bffdbd
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2019-07-25 10:32:29 +0300

    PS-9977 Fixed CS

--- Bundles/QuoteRequest/tests/SprykerTest/Zed/QuoteRequest/Business/QuoteRequestFacade/QuoteRequestFacadeTest.php
+++ Bundles/QuoteRequest/tests/SprykerTest/Zed/QuoteRequest/Business/QuoteRequestFacade/QuoteRequestFacadeTest.php
@@ -1475,6 +1475,7 @@ class QuoteRequestFacadeTest extends Unit
      */
     public function testDeleteQuoteRequestsForCompanyUserWillSucessfullyFinishWhenNoQuoteRequestsAssigned(): void
     {
+        // Arrange
         $isSuccessful = true;
 
         // Act
