d01a3037a8edeb558d4b050900df30e6460a540b
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2019-08-13 14:10:29 +0300

    PS-7713: Introduced `CustomerErrorTransfer::$params` transfer object property, Introduced `CustomerConfig::MIN_LENGTH_CUSTOMER_PASSWORD` and `CustomerConfig::MAX_LENGTH_CUSTOMER_PASSWORD` config options

--- Bundles/Customer/src/Spryker/Zed/Customer/CustomerConfig.php
+++ Bundles/Customer/src/Spryker/Zed/Customer/CustomerConfig.php
@@ -18,6 +18,13 @@ class CustomerConfig extends AbstractBundleConfig
     public const ERROR_CODE_CUSTOMER_ALREADY_REGISTERED = 4001;
     public const ERROR_CODE_CUSTOMER_INVALID_EMAIL = 4002;
 
+    protected const MIN_LENGTH_CUSTOMER_PASSWORD = 1;
+
+    /**
+     * @var int Length restriction, based on symfony BCryptPasswordEncoder limitation.
+     */
+    protected const MAX_LENGTH_CUSTOMER_PASSWORD = 72;
+
     /**
      * @return string
      */
@@ -86,5 +93,21 @@ class CustomerConfig extends AbstractBundleConfig
     public function getCustomerDetailExternalBlocksUrls()
     {
         return [];
+    }
+
+    /**
+     * @return int
+     */
+    public function getCustomerPasswordMinLength(): int
+    {
+        return static::MIN_LENGTH_CUSTOMER_PASSWORD;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCustomerPasswordMaxLength(): int
+    {
+        return static::MAX_LENGTH_CUSTOMER_PASSWORD;
     }
 }
