643a1880aeb20559a3f1e1de887f438ea5fef7ad
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2019-08-15 16:30:04 +0300

    GLUE-8892 Fixed SetCustomerBeforeActionPlugin to use the SessionCreator to perform customer save.

--- Bundles/CustomersRestApi/src/Spryker/Glue/CustomersRestApi/Plugin/SetCustomerBeforeActionPlugin.php
+++ Bundles/CustomersRestApi/src/Spryker/Glue/CustomersRestApi/Plugin/SetCustomerBeforeActionPlugin.php
@@ -7,7 +7,6 @@
 
 namespace Spryker\Glue\CustomersRestApi\Plugin;
 
-use Generated\Shared\Transfer\CustomerTransfer;
 use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
 use Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ControllerBeforeActionPluginInterface;
 use Spryker\Glue\Kernel\AbstractPlugin;
@@ -18,9 +17,13 @@ use Spryker\Glue\Kernel\AbstractPlugin;
 class SetCustomerBeforeActionPlugin extends AbstractPlugin implements ControllerBeforeActionPluginInterface
 {
     /**
-     * @api
-     *
      * {@inheritdoc}
+     * - Sets current authorized user to session.
+     * - Executes CustomerExpanderPluginInterface plugin stack before setting the customer to session.
+     * - Sets the CustomerTransfer to session without execution of CustomerSessionSetPluginInterface plugins.
+     * - Will abort if RestRequestInterface::restUser is not set.
+     *
+     * @api
      *
      * @param string $action
      * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
@@ -29,20 +32,8 @@ class SetCustomerBeforeActionPlugin extends AbstractPlugin implements Controller
      */
     public function beforeAction(string $action, RestRequestInterface $restRequest): void
     {
-        if (!$restRequest->getRestUser()) {
-            return;
-        }
-
-        $user = $restRequest->getRestUser();
-
-        //workaround for customer data, most clients use session client to retrieve customer data.
-        $customerTransfer = (new CustomerTransfer())
-            ->setIdCustomer($user->getSurrogateIdentifier() ? (int)$user->getSurrogateIdentifier() : null)
-            ->setIsDirty(false)
-            ->setCustomerReference($user->getNaturalIdentifier());
-
         $this->getFactory()
-            ->getCustomerClient()
-            ->setCustomerRawData($customerTransfer);
+            ->createSessionCreator()
+            ->setCustomer($restRequest);
     }
 }
