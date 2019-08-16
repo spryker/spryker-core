54bba2fb0508a3d0aafe13c58717174cf4fd859a
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-08-16 19:40:41 +0300

    PS-9685: prevent LazyLoading for MR

--- Bundles/MerchantRelationship/src/Spryker/Zed/MerchantRelationship/Persistence/MerchantRelationshipRepository.php
+++ Bundles/MerchantRelationship/src/Spryker/Zed/MerchantRelationship/Persistence/MerchantRelationshipRepository.php
@@ -117,6 +117,8 @@ class MerchantRelationshipRepository extends AbstractRepository implements Merch
         /** @var \Propel\Runtime\Collection\ObjectCollection|\Orm\Zed\MerchantRelationship\Persistence\SpyMerchantRelationship[] $merchantRelationshipEntities */
         $merchantRelationshipEntities = $this->getFactory()
             ->createMerchantRelationshipQuery()
+            ->joinWithCompanyBusinessUnit()
+            ->joinWithMerchant()
             ->useSpyMerchantRelationshipToCompanyBusinessUnitQuery()
                 ->filterByFkCompanyBusinessUnit($idCompanyBusinessUnit)
             ->endUse()
