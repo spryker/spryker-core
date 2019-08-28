90b640073da2939e72ba4b3be5a3bf879c661093
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-08-28 12:59:28 +0300

    cc-4787 fix update and apply

--- Bundles/PriceProductSchedule/src/Spryker/Zed/PriceProductSchedule/Business/PriceProductSchedule/Updater/PriceProductScheduleUpdater.php
+++ Bundles/PriceProductSchedule/src/Spryker/Zed/PriceProductSchedule/Business/PriceProductSchedule/Updater/PriceProductScheduleUpdater.php
@@ -64,6 +64,7 @@ class PriceProductScheduleUpdater implements PriceProductScheduleUpdaterInterfac
         $priceProductScheduleResponseTransfer = new PriceProductScheduleResponseTransfer();
         $priceProductScheduleTransfer->requirePriceProductScheduleList()
             ->requireIdPriceProductSchedule();
+        $priceProductScheduleTransfer->setIsCurrent(false);
         $priceProductScheduleTransfer = $this->priceProductScheduleEntityManager
             ->savePriceProductSchedule($priceProductScheduleTransfer);
         $priceProductScheduleResponseTransfer->setPriceProductSchedule($priceProductScheduleTransfer);
