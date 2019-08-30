0af902d7ebfe0f26bc8c3376283562bc931b8694
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2019-08-30 14:27:15 +0300

    PS-10439 Added TemplateController

---
+++ Bundles/ConfigurableBundleGui/src/Spryker/Zed/ConfigurableBundleGui/Communication/TemplateController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ConfigurableBundleGui\Communication\Controller;
+
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+
+/**
+ * @method \Spryker\Zed\ConfigurableBundleGui\Business\ConfigurableBundleGuiFacadeInterface getFacade()
+ */
+class TemplateController extends AbstractController
+{
+}
