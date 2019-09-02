71a2c6f162afeee19e3db28979ff025717a739fd
Serhii Babich <sergeghost@gmail.com>
2019-09-02 08:57:22 +0300

    CC-6161 fix wrong order detail view for customer

--- Bundles/Sales/src/Spryker/Zed/Sales/Business/SalesBusinessFactory.php
+++ Bundles/Sales/src/Spryker/Zed/Sales/Business/SalesBusinessFactory.php
@@ -10,8 +10,6 @@ namespace Spryker\Zed\Sales\Business;
 use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 use Spryker\Zed\Sales\Business\Address\OrderAddressWriter;
 use Spryker\Zed\Sales\Business\Address\OrderAddressWriterInterface;
-use Spryker\Zed\Sales\Business\ConfigReader\ConfigReader;
-use Spryker\Zed\Sales\Business\ConfigReader\ConfigReaderInterface;
 use Spryker\Zed\Sales\Business\Expander\SalesAddressExpander;
 use Spryker\Zed\Sales\Business\Expander\SalesAddressExpanderInterface;
 use Spryker\Zed\Sales\Business\Expense\ExpenseUpdater;
@@ -225,7 +223,6 @@ class SalesBusinessFactory extends AbstractBusinessFactory
         return new OrderHydratorWithMultiShippingAddress(
             $this->getQueryContainer(),
             $this->getOmsFacade(),
-            $this->createSalesOrderItemGrouper(),
             $this->getHydrateOrderPlugins()
         );
     }
@@ -455,13 +452,5 @@ class SalesBusinessFactory extends AbstractBusinessFactory
     public function getCustomerFacade(): SalesToCustomerInterface
     {
         return $this->getProvidedDependency(SalesDependencyProvider::FACADE_CUSTOMER);
-    }
-
-    /**
-     * @return \Spryker\Zed\Sales\Business\ConfigReader\ConfigReaderInterface
-     */
-    public function createConfigReader(): ConfigReaderInterface
-    {
-        return new ConfigReader($this->getConfig());
     }
 }
