6a8d1aeffc5fc3eb74dac1184e72834935945a6f
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-09-05 16:58:33 +0300

    cc-6245 fix user output

---
+++ Bundles/PriceProductSchedule/src/Spryker/Zed/PriceProductSchedule/Business/PriceProductScheduleList/Expander/PriceProductScheduleListExpander.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\PriceProductSchedule\Business\PriceProductScheduleList\Expander;
+
+use Generated\Shared\Transfer\PriceProductScheduleListTransfer;
+use Spryker\Zed\PriceProductSchedule\Dependency\Facade\PriceProductScheduleToUserFacadeInterface;
+
+class PriceProductScheduleListExpander implements PriceProductScheduleListExpanderInterface
+{
+    /**
+     * @var \Spryker\Zed\PriceProductSchedule\Dependency\Facade\PriceProductScheduleToUserFacadeInterface
+     */
+    protected $userFacade;
+
+    /**
+     * @param \Spryker\Zed\PriceProductSchedule\Dependency\Facade\PriceProductScheduleToUserFacadeInterface $userFacade
+     */
+    public function __construct(PriceProductScheduleToUserFacadeInterface $userFacade)
+    {
+        $this->userFacade = $userFacade;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PriceProductScheduleListTransfer $priceProductScheduleListTransfer
+     *
+     * @return \Generated\Shared\Transfer\PriceProductScheduleListTransfer
+     */
+    public function expandPriceProductScheduleListWithCurrentUser(PriceProductScheduleListTransfer $priceProductScheduleListTransfer): PriceProductScheduleListTransfer
+    {
+        $userTransfer = $this->userFacade->getCurrentUser();
+
+        $priceProductScheduleListTransfer->setUser($userTransfer)
+            ->setFkUser($userTransfer->getIdUser());
+
+        return $priceProductScheduleListTransfer;
+    }
+}
