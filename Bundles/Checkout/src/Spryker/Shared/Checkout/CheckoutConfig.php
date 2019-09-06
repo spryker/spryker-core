cc2477d783edc16f579a5791f1ade1a779d357c2
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-09-06 17:58:29 +0300

    CC-6257. Refactoring after next AA CR, part 4
    * Updated CheckoutConfig.

--- Bundles/Checkout/src/Spryker/Shared/Checkout/CheckoutConfig.php
+++ Bundles/Checkout/src/Spryker/Shared/Checkout/CheckoutConfig.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Shared\CheckoutPage;
+namespace Spryker\Shared\Checkout;
 
 use Spryker\Shared\Kernel\AbstractBundleConfig;
 
