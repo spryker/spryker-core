499c3cb951f34cbac1b1b64508085c1eee3ee097
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-09-10 15:12:31 +0300

    PS-10458: use glossary instead of Zed translator for CB template name

---
+++ Bundles/SalesConfigurableBundle/src/Spryker/Zed/SalesConfigurableBundle/Dependency/Facade/SalesConfigurableBundleToGlossaryFacadeBridge.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\SalesConfigurableBundle\Dependency\Facade;
+
+use Generated\Shared\Transfer\LocaleTransfer;
+
+class SalesConfigurableBundleToGlossaryFacadeBridge implements SalesConfigurableBundleToGlossaryFacadeInterface
+{
+    /**
+     * @var \Spryker\Zed\Glossary\Business\GlossaryFacadeInterface
+     */
+    protected $glossaryFacade;
+
+    /**
+     * @param \Spryker\Zed\Glossary\Business\GlossaryFacadeInterface $glossaryFacade
+     */
+    public function __construct($glossaryFacade)
+    {
+        $this->glossaryFacade = $glossaryFacade;
+    }
+
+    /**
+     * @param string $keyName
+     * @param array $data
+     * @param \Generated\Shared\Transfer\LocaleTransfer|null $localeTransfer
+     *
+     * @return string
+     */
+    public function translate($keyName, array $data = [], LocaleTransfer $localeTransfer = null)
+    {
+        return $this->glossaryFacade->translate($keyName, $data, $localeTransfer);
+    }
+}
---
+++ Bundles/SalesConfigurableBundle/src/Spryker/Zed/SalesConfigurableBundle/Dependency/Facade/SalesConfigurableBundleToGlossaryFacadeInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\SalesConfigurableBundle\Dependency\Facade;
+
+use Generated\Shared\Transfer\LocaleTransfer;
+
+interface SalesConfigurableBundleToGlossaryFacadeInterface
+{
+    /**
+     * @param string $keyName
+     * @param array $data
+     * @param \Generated\Shared\Transfer\LocaleTransfer|null $localeTransfer
+     *
+     * @throws \Spryker\Zed\Glossary\Business\Exception\MissingTranslationException
+     *
+     * @return string
+     */
+    public function translate($keyName, array $data = [], LocaleTransfer $localeTransfer = null);
+}
