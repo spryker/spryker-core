ea090eadd0769df8cd0cd3dfda0ea6afe45628d6
Alexander <alexander.veselov@spryker.com>
2019-09-23 12:28:54 +0000

    MP-716: MP-37: Created MerchantProfileDataImport module

---
+++ Bundles/MerchantProfile/src/Spryker/Zed/MerchantProfile/Persistence/Propel/AbstractSpyMerchantProfileQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace  Spryker\Zed\MerchantProfile\Persistence\Propel;
+
+use Orm\Zed\MerchantProfile\Persistence\Base\SpyMerchantProfileQuery as BaseSpyMerchantProfileQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_merchant_profile' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+abstract class AbstractSpyMerchantProfileQuery extends BaseSpyMerchantProfileQuery
+{
+
+}
