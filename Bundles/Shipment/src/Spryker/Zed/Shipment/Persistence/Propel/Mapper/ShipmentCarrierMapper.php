3b27bd897616198aec255f39683191855e0f2e27
Serhii Babich <sergeghost@gmail.com>
2019-09-23 09:21:17 +0300

    CC-6406 fix mapping

--- Bundles/Shipment/src/Spryker/Zed/Shipment/Persistence/Propel/Mapper/ShipmentCarrierMapper.php
+++ Bundles/Shipment/src/Spryker/Zed/Shipment/Persistence/Propel/Mapper/ShipmentCarrierMapper.php
@@ -22,6 +22,6 @@ class ShipmentCarrierMapper
         SpyShipmentCarrier $salesShipmentEntity,
         ShipmentCarrierTransfer $shipmentCarrierTransfer
     ): ShipmentCarrierTransfer {
-        return $shipmentCarrierTransfer->fromArray($salesShipmentEntity->toArray());
+        return $shipmentCarrierTransfer->fromArray($salesShipmentEntity->toArray(), true);
     }
 }
