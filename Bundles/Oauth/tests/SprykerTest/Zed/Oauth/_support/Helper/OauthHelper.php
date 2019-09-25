60b126aac13d7a33357dc06b91acbc3dc7aa3dd8
voitov <tigran.voitov@spryker.com>
2019-09-25 16:40:00 +0300

    glue-8968 fixed authRestApiHelper and created new for Oauth module

--- Bundles/Oauth/tests/SprykerTest/Zed/Oauth/_support/Helper/OauthHelper.php
+++ Bundles/Oauth/tests/SprykerTest/Zed/Oauth/_support/Helper/OauthHelper.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace SprykerTest\Glue\AuthRestApi\Helper;
+namespace SprykerTest\Zed\Oauth\Helper;
 
 use Codeception\Module;
 use Generated\Shared\Transfer\CustomerTransfer;
@@ -13,7 +13,7 @@ use Generated\Shared\Transfer\OauthRequestTransfer;
 use Generated\Shared\Transfer\OauthResponseTransfer;
 use SprykerTest\Shared\Testify\Helper\LocatorHelperTrait;
 
-class AuthRestApiHelper extends Module
+class OauthHelper extends Module
 {
     use LocatorHelperTrait;
 
@@ -25,24 +25,19 @@ class AuthRestApiHelper extends Module
      * @part json
      *
      * @param \Generated\Shared\Transfer\CustomerTransfer $customerTransfer
-     * @param string|null $anonymousCustomerReference
      *
      * @return \Generated\Shared\Transfer\OauthResponseTransfer
      */
-    public function haveAuthorizationToGlue(CustomerTransfer $customerTransfer, $anonymousCustomerReference = null): OauthResponseTransfer
+    public function haveAuthorizationToGlue(CustomerTransfer $customerTransfer): OauthResponseTransfer
     {
         $oauthRequestTransfer = (new OauthRequestTransfer())
             ->setGrantType('password')
             ->setUsername($customerTransfer->getEmail())
             ->setPassword($customerTransfer->getNewPassword());
 
-        if ($anonymousCustomerReference) {
-            $oauthRequestTransfer->setCustomerReference($anonymousCustomerReference);
-        }
-
         return $this->getLocator()
-            ->authRestApi()
+            ->oauth()
             ->facade()
-            ->processAccessToken($oauthRequestTransfer);
+            ->processAccessTokenRequest($oauthRequestTransfer);
     }
 }
