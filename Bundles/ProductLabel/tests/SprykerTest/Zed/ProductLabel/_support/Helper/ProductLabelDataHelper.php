e8adc00639d62bdc581d294213054bc171b7706b
Denis Gnusov <denis.gnusov@spryker.com>
2019-09-26 12:26:04 +0300

    GLUE-8748 Added WishlistsDataHelper

--- Bundles/ProductLabel/tests/SprykerTest/Zed/ProductLabel/_support/Helper/ProductLabelDataHelper.php
+++ Bundles/ProductLabel/tests/SprykerTest/Zed/ProductLabel/_support/Helper/ProductLabelDataHelper.php
@@ -9,6 +9,9 @@ namespace SprykerTest\Zed\ProductLabel\Helper;
 
 use Codeception\Module;
 use Generated\Shared\DataBuilder\ProductLabelBuilder;
+use Generated\Shared\DataBuilder\ProductLabelLocalizedAttributesBuilder;
+use Generated\Shared\Transfer\ProductLabelLocalizedAttributesTransfer;
+use Generated\Shared\Transfer\ProductLabelTransfer;
 use SprykerTest\Shared\Testify\Helper\LocatorHelperTrait;
 
 class ProductLabelDataHelper extends Module
@@ -22,9 +25,22 @@ class ProductLabelDataHelper extends Module
      */
     public function haveProductLabel(array $seedData = [])
     {
-        $productLabelTransfer = (new ProductLabelBuilder($seedData))->build();
+        $productLabelFacade = $this->getProductLabelFacade();
+        /** @var \Generated\Shared\Transfer\ProductLabelTransfer $productLabelTransfer */
+        $productLabelTransfer = (new ProductLabelBuilder($seedData + [
+                ProductLabelTransfer::VALID_FROM => null,
+                ProductLabelTransfer::VALID_TO => null,
+            ]))->build();
         $productLabelTransfer->setIdProductLabel(null);
-        $this->getProductLabelFacade()->createLabel($productLabelTransfer);
+        $productLabelFacade->createLabel($productLabelTransfer);
+
+        $productLabelLocalizedAttributesTransfer = (new ProductLabelLocalizedAttributesBuilder([
+            ProductLabelLocalizedAttributesTransfer::FK_LOCALE => $this->getLocator()->locale()->facade()->getCurrentLocale()->getIdLocale(),
+            ProductLabelLocalizedAttributesTransfer::FK_PRODUCT_LABEL => $productLabelTransfer->getIdProductLabel(),
+        ]))->build();
+        $productLabelTransfer->addLocalizedAttributes($productLabelLocalizedAttributesTransfer);
+
+        $productLabelFacade->updateLabel($productLabelTransfer);
 
         return $productLabelTransfer;
     }
