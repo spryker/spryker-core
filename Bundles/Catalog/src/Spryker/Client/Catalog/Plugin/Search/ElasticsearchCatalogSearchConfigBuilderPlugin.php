fddfd942abccd1b3b5bac4dce8f5e4ff1ddddae2
Pavlo Asaulenko <asaulenko.pavel@gmail.com>
2019-10-11 12:56:22 +0300

    TE-3980 Moved searh configuration plugin to another namespace

--- Bundles/Catalog/src/Spryker/Client/Catalog/Plugin/Search/ElasticsearchCatalogSearchConfigBuilderPlugin.php
+++ Bundles/Catalog/src/Spryker/Client/Catalog/Plugin/Search/ElasticsearchCatalogSearchConfigBuilderPlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Client\Catalog\Plugin\Config;
+namespace Spryker\Client\Catalog\Plugin\Search;
 
 use Generated\Shared\Transfer\SearchConfigurationTransfer;
 use Spryker\Client\Kernel\AbstractPlugin;
