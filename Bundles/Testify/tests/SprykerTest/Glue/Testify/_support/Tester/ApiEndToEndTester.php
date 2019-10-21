e62fdd3d71181572aff8f8e49f69157d6ffc9371
Denis Gnusov <denis.gnusov@spryker.com>
2019-10-21 10:50:54 +0300

    GLUE-8748 Updated after TL CR

--- Bundles/Testify/tests/SprykerTest/Glue/Testify/_support/Tester/ApiEndToEndTester.php
+++ Bundles/Testify/tests/SprykerTest/Glue/Testify/_support/Tester/ApiEndToEndTester.php
@@ -7,6 +7,14 @@
 
 namespace SprykerTest\Glue\Testify\Tester;
 
+use Spryker\Glue\AlternativeProductsRestApi\AlternativeProductsRestApiConfig;
+use Spryker\Glue\CartsRestApi\CartsRestApiConfig;
+use Spryker\Glue\GlueApplication\Rest\RequestConstantsInterface;
+use Spryker\Glue\ProductLabelsRestApi\ProductLabelsRestApiConfig;
+use Spryker\Glue\ProductsRestApi\ProductsRestApiConfig;
+use Spryker\Glue\RelatedProductsRestApi\RelatedProductsRestApiConfig;
+use Spryker\Glue\UpSellingProductsRestApi\UpSellingProductsRestApiConfig;
+use Spryker\Glue\WishlistsRestApi\WishlistsRestApiConfig;
 use Spryker\Shared\Config\Config;
 use Spryker\Shared\Testify\TestifyConstants;
 use SprykerTest\Shared\Testify\Tester\EndToEndTester;
@@ -39,5 +47,241 @@ abstract class ApiEndToEndTester extends EndToEndTester
     public function formatFullUrl(string $url, $params = []): string
     {
         return rtrim(Config::get(TestifyConstants::GLUE_APPLICATION_DOMAIN) . '/' . $this->formatUrl($url, $params), '/');
+    }
+
+    /**
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function formatQueryInclude(array $includes = []): string
+    {
+        if (!$includes) {
+            return '';
+        }
+
+        return '?' . RequestConstantsInterface::QUERY_INCLUDE . '=' . implode(',', $includes);
+    }
+
+    /**
+     * @param string $productAbstractSku
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildProductAbstractUrl(string $productAbstractSku, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceAbstractProducts}/{productAbstractSku}' . $this->formatQueryInclude($includes),
+            [
+                'resourceAbstractProducts' => ProductsRestApiConfig::RESOURCE_ABSTRACT_PRODUCTS,
+                'productAbstractSku' => $productAbstractSku,
+            ]
+        );
+    }
+
+    /**
+     * @param string $productConcreteSku
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildProductConcreteUrl(string $productConcreteSku, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceConcreteProducts}/{productConcreteSku}' . $this->formatQueryInclude($includes),
+            [
+                'resourceConcreteProducts' => ProductsRestApiConfig::RESOURCE_CONCRETE_PRODUCTS,
+                'productConcreteSku' => $productConcreteSku,
+            ]
+        );
+    }
+
+    /**
+     * @param string $productConcreteSku
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildAbstractAlternativeProductsUrl(string $productConcreteSku, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceConcreteProducts}/{productConcreteSku}/{resourceAbstractAlternativeProducts}' . $this->formatQueryInclude($includes),
+            [
+                'resourceConcreteProducts' => ProductsRestApiConfig::RESOURCE_CONCRETE_PRODUCTS,
+                'resourceAbstractAlternativeProducts' => AlternativeProductsRestApiConfig::RELATIONSHIP_NAME_ABSTRACT_ALTERNATIVE_PRODUCTS,
+                'productConcreteSku' => $productConcreteSku,
+            ]
+        );
+    }
+
+    /**
+     * @param string $productConcreteSku
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildConcreteAlternativeProductsUrl(string $productConcreteSku, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceConcreteProducts}/{productConcreteSku}/{resourceConcreteAlternativeProducts}' . $this->formatQueryInclude($includes),
+            [
+                'resourceConcreteProducts' => ProductsRestApiConfig::RESOURCE_CONCRETE_PRODUCTS,
+                'resourceConcreteAlternativeProducts' => AlternativeProductsRestApiConfig::RELATIONSHIP_NAME_CONCRETE_ALTERNATIVE_PRODUCTS,
+                'productConcreteSku' => $productConcreteSku,
+            ]
+        );
+    }
+
+    /**
+     * @param string $productAbstractSku
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildRelatedProductsUrl(string $productAbstractSku, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceAbstractProducts}/{productAbstractSku}/{resourceRelatedProducts}' . $this->formatQueryInclude($includes),
+            [
+                'resourceAbstractProducts' => ProductsRestApiConfig::RESOURCE_ABSTRACT_PRODUCTS,
+                'resourceRelatedProducts' => RelatedProductsRestApiConfig::CONTROLLER_RELATED_PRODUCTS,
+                'productAbstractSku' => $productAbstractSku,
+            ]
+        );
+    }
+
+    /**
+     * @param int $idProductLabel
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildProductLabelUrl(int $idProductLabel, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceProductLabels}/{idProductLabel}' . $this->formatQueryInclude($includes),
+            [
+                'resourceProductLabels' => ProductLabelsRestApiConfig::RESOURCE_PRODUCT_LABELS,
+                'idProductLabel' => $idProductLabel,
+            ]
+        );
+    }
+
+    /**
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildCartsUrl(array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceCarts}' . $this->formatQueryInclude($includes),
+            [
+                'resourceCarts' => CartsRestApiConfig::RESOURCE_CARTS,
+            ]
+        );
+    }
+
+    /**
+     * @param string $cartUuid
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildCartUrl(string $cartUuid, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceCarts}/{cartUuid}' . $this->formatQueryInclude($includes),
+            [
+                'resourceCarts' => CartsRestApiConfig::RESOURCE_CARTS,
+                'cartUuid' => $cartUuid,
+            ]
+        );
+    }
+
+    /**
+     * @param string $cartUuid
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildCartUpSellingProductsUrl(string $cartUuid, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceCarts}/{cartUuid}/{resourceUpSellingProducts}' . $this->formatQueryInclude($includes),
+            [
+                'resourceCarts' => CartsRestApiConfig::RESOURCE_CARTS,
+                'resourceUpSellingProducts' => UpSellingProductsRestApiConfig::RELATIONSHIP_NAME_UP_SELLING_PRODUCTS,
+                'cartUuid' => $cartUuid,
+            ]
+        );
+    }
+
+    /**
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildGuestCartsUrl(array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceGuestCarts}' . $this->formatQueryInclude($includes),
+            [
+                'resourceGuestCarts' => CartsRestApiConfig::RESOURCE_GUEST_CARTS,
+            ]
+        );
+    }
+
+    /**
+     * @param string $guestCartUuid
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildGuestCartUrl(string $guestCartUuid, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceGuestCarts}/{guestCartUuid}' . $this->formatQueryInclude($includes),
+            [
+                'resourceGuestCarts' => CartsRestApiConfig::RESOURCE_GUEST_CARTS,
+                'guestCartUuid' => $guestCartUuid,
+            ]
+        );
+    }
+
+    /**
+     * @param string $cartUuid
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildGuestCartUpSellingProductsUrl(string $cartUuid, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceGuestCarts}/{cartUuid}/{resourceUpSellingProducts}' . $this->formatQueryInclude($includes),
+            [
+                'resourceGuestCarts' => CartsRestApiConfig::RESOURCE_GUEST_CARTS,
+                'resourceUpSellingProducts' => UpSellingProductsRestApiConfig::RELATIONSHIP_NAME_UP_SELLING_PRODUCTS,
+                'cartUuid' => $cartUuid,
+            ]
+        );
+    }
+
+    /**
+     * @param string $wishlistUuid
+     * @param array $includes
+     *
+     * @return string
+     */
+    public function buildWishlistUrl(string $wishlistUuid, array $includes = []): string
+    {
+        return $this->formatFullUrl(
+            '{resourceWishlists}/{wishlistUuid}' . $this->formatQueryInclude($includes),
+            [
+                'resourceWishlists' => WishlistsRestApiConfig::RESOURCE_WISHLISTS,
+                'wishlistUuid' => $wishlistUuid,
+            ]
+        );
     }
 }
--- Bundles/Testify/tests/SprykerTest/Shared/Testify/_support/StepOverride/StepOverrideTrait.php
+++ Bundles/Testify/tests/SprykerTest/Shared/Testify/_support/StepOverride/StepOverrideTrait.php
@@ -89,4 +89,142 @@ trait StepOverrideTrait
 
         return $this;
     }
+
+    /**
+     * @param string $type
+     *
+     * @return static
+     */
+    public function amSureResponseDataContainsSingleResourceOfType(string $type): self
+    {
+        return $this->amSure(sprintf('The returned resource is of type %s', $type));
+    }
+
+    /**
+     * @return static
+     */
+    public function amSureSingleResourceIdEqualTo(): self
+    {
+        return $this->amSure('The returned resource has correct id');
+    }
+
+    /**
+     * @return static
+     */
+    public function amSureSingleResourceHasSelfLink(): self
+    {
+        return $this->amSure('The returned resource has correct self-link');
+    }
+
+    /**
+     * @param string $type
+     * @param string $id
+     *
+     * @return static
+     */
+    public function amSureSingleResourceHasRelationshipByTypeAndId(string $type, string $id): self
+    {
+        return $this->amSure(
+            sprintf('The returned resource has a relation to resource of type %s with id %s', $type, $id)
+        );
+    }
+
+    /**
+     * @param string $type
+     * @param string $id
+     *
+     * @return static
+     */
+    public function amSureIncludesContainsResourceByTypeAndId(string $type, string $id): self
+    {
+        return $this->amSure(
+            sprintf('The returned resource has include of type %s with id %s', $type, $id)
+        );
+    }
+
+    /**
+     * @param string $type
+     * @param string $id
+     *
+     * @return static
+     */
+    public function amSureIncludedResourceByTypeAndIdHasSelfLink(string $type, string $id): self
+    {
+        return $this->amSure(
+            sprintf('The include of type %s with id %s has correct self-link', $type, $id)
+        );
+    }
+
+    /**
+     * @param string $type
+     * @param string $id
+     * @param string $relationType
+     * @param string $relationId
+     *
+     * @return static
+     */
+    public function amSureIncludedResourceByTypeAndIdHasRelationshipByTypeAndId(
+        string $type,
+        string $id,
+        string $relationType,
+        string $relationId
+    ): self {
+        return $this->amSure(
+            sprintf('The resource of type %s with id %s has a relation to the resource of type %s with id %s', $type, $id, $relationType, $relationId)
+        );
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return static
+     */
+    public function amSureResponseDataContainsResourceCollectionOfType(string $type): self
+    {
+        return $this->amSure(sprintf('The response data contains resource collection of type %s', $type));
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return static
+     */
+    public function amSureResourceCollectionHasResourceWithId(string $id): self
+    {
+        return $this->amSure(sprintf('The response resource collection has resource with id %s', $id));
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return static
+     */
+    public function amSureResourceByIdHasSelfLink(string $id): self
+    {
+        return $this->amSure(sprintf('The resource with id %s has correct self-link', $id));
+    }
+
+    /**
+     * @param string $id
+     * @param string $relationType
+     * @param string $relationId
+     *
+     * @return static
+     */
+    public function amSureResourceByIdHasRelationshipByTypeAndId(string $id, string $relationType, string $relationId): self
+    {
+        return $this->amSure(
+            sprintf('The resource with id %s has a relation to resource of type %s with id %s', $id, $relationType, $relationId)
+        );
+    }
+
+    /**
+     * @param string $attribute
+     *
+     * @return static
+     */
+    public function amSureSingleResourceHasAttribute(string $attribute): self
+    {
+        return $this->amSure(sprintf('The returned resource contains `%s` attribute', $attribute));
+    }
 }
