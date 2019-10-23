8939b6200560feae253a6b5dcb9c784700dd9f19
vhrychenko <aptah199494@gmail.com>
2019-10-23 18:43:23 +0300

    MP-1118 Added dataset interface

---
+++ Bundles/ProductOfferStockDataImport/src/Spryker/Zed/ProductOfferStockDataImport/Business/DataSet/MerchantProductOfferDataSetInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductOfferStockDataImport\Business\DataSet;
+
+interface MerchantProductOfferDataSetInterface
+{
+    public const QUANTITY = 'quantity';
+    public const FK_PRODUCT_OFFER = 'fk_product_offer';
+    public const FK_STOCK = 'fk_stock';
+}
