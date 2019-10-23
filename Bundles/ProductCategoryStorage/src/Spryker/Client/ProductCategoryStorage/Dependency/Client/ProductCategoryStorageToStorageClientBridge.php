342ab0839cecf944277302ede394be49101ade5a
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2019-10-23 18:59:57 +0300

    GLUE-9654 fixed type specification in StorageClient::getMulti().

--- Bundles/ProductCategoryStorage/src/Spryker/Client/ProductCategoryStorage/Dependency/Client/ProductCategoryStorageToStorageClientBridge.php
+++ Bundles/ProductCategoryStorage/src/Spryker/Client/ProductCategoryStorage/Dependency/Client/ProductCategoryStorageToStorageClientBridge.php
@@ -33,7 +33,7 @@ class ProductCategoryStorageToStorageClientBridge implements ProductCategoryStor
     }
 
     /**
-     * @param array $keys
+     * @param string[] $keys
      *
      * @return array
      */
--- Bundles/ProductCategoryStorage/src/Spryker/Client/ProductCategoryStorage/Dependency/Client/ProductCategoryStorageToStorageClientInterface.php
+++ Bundles/ProductCategoryStorage/src/Spryker/Client/ProductCategoryStorage/Dependency/Client/ProductCategoryStorageToStorageClientInterface.php
@@ -17,7 +17,7 @@ interface ProductCategoryStorageToStorageClientInterface
     public function get($key);
 
     /**
-     * @param array $keys
+     * @param string[] $keys
      *
      * @return array
      */
