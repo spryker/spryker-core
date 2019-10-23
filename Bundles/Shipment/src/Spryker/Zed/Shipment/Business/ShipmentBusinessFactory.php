9942805c4be073470fd24139db1ea35b06657fc9
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-10-23 12:20:31 +0300

    cc-6208 fix typehint

--- Bundles/Shipment/src/Spryker/Zed/Shipment/Business/ShipmentBusinessFactory.php
+++ Bundles/Shipment/src/Spryker/Zed/Shipment/Business/ShipmentBusinessFactory.php
@@ -576,7 +576,7 @@ class ShipmentBusinessFactory extends AbstractBusinessFactory
     }
 
     /**
-     * @return array
+     * @return array[]
      */
     public function getShipmentMethodPlugins(): array
     {
