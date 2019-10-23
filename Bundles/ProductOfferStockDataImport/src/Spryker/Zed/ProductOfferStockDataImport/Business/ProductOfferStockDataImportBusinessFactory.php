39fb87ce1a8e227185b59a78b16243e9010574e2
vhrychenko <aptah199494@gmail.com>
2019-10-23 18:14:19 +0300

    MP-1118 Data importer for ProductList.

---
+++ Bundles/ProductOfferStockDataImport/src/Spryker/Zed/ProductOfferStockDataImport/Business/ProductOfferStockDataImportBusinessFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductOfferStockDataImport\Business;
+
+use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
+
+/**
+ * @method \Spryker\Zed\ProductOfferStockDataImport\ProductOfferStockDataImportConfig getConfig()
+ * @method \Spryker\Zed\ProductOfferStockDataImport\Persistence\ProductOfferStockDataImportEntityManagerInterface getEntityManager()
+ * @method \Spryker\Zed\ProductOfferStockDataImport\Persistence\ProductOfferStockDataImportRepositoryInterface getRepository()
+ */
+class ProductOfferStockDataImportBusinessFactory extends AbstractBusinessFactory
+{
+}
---
+++ Bundles/ProductOfferStockDataImport/src/Spryker/Zed/ProductOfferStockDataImport/Business/ProductOfferStockDataImportFacade.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductOfferStockDataImport\Business;
+
+use Spryker\Zed\Kernel\Business\AbstractFacade;
+
+/**
+ * @method \Spryker\Zed\ProductOfferStockDataImport\Business\ProductOfferStockDataImportBusinessFactory getFactory()
+ * @method \Spryker\Zed\ProductOfferStockDataImport\Persistence\ProductOfferStockDataImportRepositoryInterface getRepository()
+ * @method \Spryker\Zed\ProductOfferStockDataImport\Persistence\ProductOfferStockDataImportEntityManagerInterface getEntityManager()
+ */
+class ProductOfferStockDataImportFacade extends AbstractFacade implements ProductOfferStockDataImportFacadeInterface
+{
+}
