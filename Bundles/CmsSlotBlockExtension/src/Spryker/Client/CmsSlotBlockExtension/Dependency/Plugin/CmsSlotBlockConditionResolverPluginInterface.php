35b25dde9cf7c90c259e7ee4faecf0439fcd061e
ilyakubanov <ilya.kubanov@spryker.com>
2019-10-31 15:40:50 +0200

    CMS-2963: introduced CmsSlotBlockExtension module

---
+++ Bundles/CmsSlotBlockExtension/src/Spryker/Client/CmsSlotBlockExtension/Dependency/Plugin/CmsSlotBlockConditionResolverPluginInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\CmsSlotBlockExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\CmsBlockTransfer;
+
+interface CmsSlotBlockConditionResolverPluginInterface
+{
+    /**
+     * Specification:
+     * - Returns true if this plugin is applicable with provided conditions.
+     *
+     * @api
+     *
+     * @param array $conditions
+     *
+     * @return bool
+     */
+    public function isApplicable(array $conditions): bool;
+
+    /**
+     * Specification:
+     * - Returns true if CMS block should be rendered on Yves.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CmsBlockTransfer $cmsBlockTransfer
+     * @param array $conditionData
+     * @param array $cmsSlotData
+     *
+     * @return bool
+     */
+    public function isCmsBlockVisibleInSlot(
+        CmsBlockTransfer $cmsBlockTransfer,
+        array $conditionData,
+        array $cmsSlotData
+    ): bool;
+}
