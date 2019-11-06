123383c42e55a36e51add8f13ee006aa2ba97c86
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2019-11-06 14:48:13 +0000

    TE-723 Fix queue names

--- Bundles/Publisher/README.md
+++ Bundles/Publisher/README.md
@@ -1,6 +1,5 @@
 # Publisher Module
 [![Build Status](https://travis-ci.org/spryker/publisher.svg)](https://travis-ci.org/spryker/publisher)
-[![Coverage Status](https://coveralls.io/repos/github/spryker/publisher/badge.svg)](https://coveralls.io/github/spryker/publisher)
 
 Publisher module is a registerer for publisher plugins to Event\Listener.
 
---
+++ Bundles/Publisher/src/Spryker/Shared/Publisher/PublisherConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\Publisher;
+
+use Spryker\Shared\Kernel\AbstractBundleConfig;
+
+class PublisherConfig extends AbstractBundleConfig
+{
+    public const PUBLISHER_QUEUE_NAME = 'publisher';
+    public const PUBLISHER_ERROR_QUEUE_NAME = 'publisher.error';
+    public const PUBLISHER_RETRY_QUEUE_NAME = 'publisher.retry';
+    public const PUBLISHER_ERROR_ROUTING_KEY = 'error';
+    public const PUBLISHER_RETRY_ROUTING_KEY = 'retry';
+}
