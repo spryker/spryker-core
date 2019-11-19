b19b3a43b121971af61254e11c59e88174f5942e
Dima Aseev <dmitriy.aseev@spryker.com>
2019-11-19 15:30:02 +0200

    PS-11440: Fixed issues after QA.

--- Bundles/ConfigurableBundleGui/src/Spryker/Zed/ConfigurableBundleGui/Communication/Tabs/ConfigurableBundleTemplateCreateTabs.php
+++ Bundles/ConfigurableBundleGui/src/Spryker/Zed/ConfigurableBundleGui/Communication/Tabs/ConfigurableBundleTemplateCreateTabs.php
@@ -7,6 +7,19 @@
 
 namespace Spryker\Zed\ConfigurableBundleGui\Communication\Tabs;
 
+use Generated\Shared\Transfer\TabsViewTransfer;
+
 class ConfigurableBundleTemplateCreateTabs extends AbstractConfigurableBundleTabs
 {
+    /**
+     * @param \Generated\Shared\Transfer\TabsViewTransfer $tabsViewTransfer
+     *
+     * @return \Generated\Shared\Transfer\TabsViewTransfer
+     */
+    protected function build(TabsViewTransfer $tabsViewTransfer): TabsViewTransfer
+    {
+        $tabsViewTransfer = parent::build($tabsViewTransfer);
+
+        return $tabsViewTransfer->setIsNavigable(false);
+    }
 }
