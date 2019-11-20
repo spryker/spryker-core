f4943176cf4464d34193c9a50912177aef2c909d
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-11-20 15:01:37 +0200

    CC-5883 add data table for payment methods

---
+++ Bundles/PaymentGui/src/Spryker/Zed/PaymentGui/PaymentGuiDependencyProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\PaymentGui;
+
+use Orm\Zed\Payment\Persistence\SpyPaymentMethodQuery;
+use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Zed\Kernel\Container;
+
+class PaymentGuiDependencyProvider extends AbstractBundleDependencyProvider
+{
+    public const PROPEL_QUERY_PAYMENT_METHOD = 'PROPEL_QUERY_PAYMENT_METHOD';
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideCommunicationLayerDependencies(Container $container): Container
+    {
+        $container = $this->addShipmentMethodQuery($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addShipmentMethodQuery(Container $container): Container
+    {
+        $container->set(static::PROPEL_QUERY_PAYMENT_METHOD, function () {
+            return SpyPaymentMethodQuery::create();
+        });
+
+        return $container;
+    }
+}
