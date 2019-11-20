ea5bb57f06e54791a43c28fb5457babeed2b20e4
Ihor Samusenko <samusenkoiv@gmail.com>
2019-11-20 18:41:58 +0200

    GLUE-9024 add GlueRest:grabDataFromResponseByJsonPathSmartGet

--- Bundles/Testify/tests/SprykerTest/Glue/Testify/_support/Helper/GlueRest.php
+++ Bundles/Testify/tests/SprykerTest/Glue/Testify/_support/Helper/GlueRest.php
@@ -122,6 +122,18 @@ class GlueRest extends REST implements LastConnectionProviderInterface
     /**
      * @part json
      *
+     * @param string $jsonPath
+     *
+     * @return array|mixed
+     */
+    public function grabDataFromResponseByJsonPathSmart(string $jsonPath)
+    {
+        return (new JsonObject($this->connectionModule->_getResponseContent(), true))->get($jsonPath);
+    }
+
+    /**
+     * @part json
+     *
      * @param string $link
      *
      * @return void
@@ -221,7 +233,7 @@ class GlueRest extends REST implements LastConnectionProviderInterface
         $this->getJsonPathModule()->seeResponseJsonPathContains([
             'type' => $type,
         ], '$.data[*]');
-        $this->assertCount($size, $this->grabDataFromResponseByJsonPath('$.data')[0]);
+        $this->assertCount($size, $this->grabDataFromResponseByJsonPathSmartGet('$.data')[0]);
     }
 
     /**
@@ -510,16 +522,14 @@ class GlueRest extends REST implements LastConnectionProviderInterface
     }
 
     /**
-     * @throws \Codeception\Exception\ModuleException
-     *
      * @return \SprykerTest\Glue\Testify\Helper\JsonPath
      */
     protected function getJsonPathModule(): JsonPath
     {
-        $this->jsonPathModule = $this->jsonPathModule ?: $this->findModule(JsonPath::class);
-
-        if ($this->jsonPathModule === null) {
-            throw new ModuleException('GlueRest', 'The module requires JsonPath');
+        if (!$this->jsonPathModule instanceof JsonPath) {
+            /** @var \SprykerTest\Glue\Testify\Helper\JsonPath $module */
+            $module = $this->locateModule(JsonPath::class);
+            $this->jsonPathModule = $module;
         }
 
         return $this->jsonPathModule;
