02c9f120c4c494aa08f8d74b36d14fd8d435de3e
René Klatt <rene.klatt@spryker.com>
2019-11-22 05:23:50 +0000

    TE-4500 Deprecated FlashMessengerServiceProvider and added FlashMessengerApplicationPlugin.

---
+++ Bundles/Messenger/src/Spryker/Yves/Messenger/Plugin/Application/FlashMessengerApplicationPlugin.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Yves\Messenger\Plugin\Application;
+
+use Spryker\Service\Container\ContainerInterface;
+use Spryker\Shared\ApplicationExtension\Dependency\Plugin\ApplicationPluginInterface;
+use Spryker\Yves\Kernel\AbstractPlugin;
+use Spryker\Yves\Messenger\FlashMessenger\FlashMessenger;
+
+class FlashMessengerApplicationPlugin extends AbstractPlugin implements ApplicationPluginInterface
+{
+    protected const SERVICE_FLASH_MESSENGER = 'flash_messenger';
+
+    /**
+     * @uses \Spryker\Yves\Session\Plugin\Application\SessionApplicationPlugin::SERVICE_SESSION
+     */
+    protected const SERVICE_SESSION = 'session';
+
+    /**
+     * @param \Spryker\Service\Container\ContainerInterface $container
+     *
+     * @return \Spryker\Service\Container\ContainerInterface
+     */
+    public function provide(ContainerInterface $container): ContainerInterface
+    {
+        $container->set(static::SERVICE_FLASH_MESSENGER, function (ContainerInterface $container) {
+            return new FlashMessenger($container->get(static::SERVICE_SESSION)->getFlashBag());
+        });
+
+        return $container;
+    }
+}
