cfc3fd5b4d4005a80d896032e466c11a98cff193
Pavlo Asaulenko <asaulenko.pavel@gmail.com>
2019-11-25 09:44:14 +0200

    TE-4207 Adjusted tests

--- Bundles/Search/tests/SprykerTest/Zed/Search/Business/SearchFacadeBCTest.php
+++ Bundles/Search/tests/SprykerTest/Zed/Search/Business/SearchFacadeBCTest.php
@@ -8,8 +8,17 @@
 namespace SprykerTest\Zed\Search\Business;
 
 use Codeception\Test\Unit;
+use Elastica\Snapshot;
+use Generated\Shared\Transfer\LocaleTransfer;
 use Psr\Log\NullLogger;
+use Spryker\Client\Search\Provider\SearchClientProvider;
 use Spryker\Shared\Search\SearchConstants;
+use Spryker\Zed\CategoryPageSearch\Communication\Plugin\Search\CategoryNodeDataPageMapBuilder;
+use Spryker\Zed\ProductPageSearch\Communication\Plugin\Search\ProductPageMapPlugin;
+use Spryker\Zed\Search\Business\Model\Elasticsearch\SnapshotHandler;
+use Spryker\Zed\Search\Business\SearchBusinessFactory;
+use Spryker\Zed\Search\Business\SearchFacadeInterface;
+use Spryker\Zed\Search\SearchDependencyProvider;
 
 /**
  * Auto-generated group annotations
@@ -26,7 +35,9 @@ use Spryker\Shared\Search\SearchConstants;
  */
 class SearchFacadeBCTest extends Unit
 {
-    public const INDEX_NAME = 'de_search_devtest';
+    protected const INDEX_NAME = 'de_search_devtest';
+    protected const REPOSITORY_LOCATION_FILE_NAME = 'search_test_file';
+    protected const REPOSITORY_NAME = 'search_test_repository';
 
     /**
      * @var \SprykerTest\Zed\Search\SearchBusinessTester
@@ -41,6 +52,92 @@ class SearchFacadeBCTest extends Unit
         parent::_setUp();
 
         $this->setUpConfiguration();
+        $this->setUpDependencies();
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreateSnapshotRepository(): void
+    {
+        $this->skipIfCi();
+
+        //Arrange
+        $searchFactory = $this->createSearchFactoryMock();
+        $searchFacade = $this->getSearchFacade($searchFactory);
+
+        //Act
+        $result = $searchFacade->createSnapshotRepository(static::REPOSITORY_NAME);
+
+        //Assert
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @return \PHPUnit\Framework\MockObject\MockObject|\Spryker\Zed\Search\Business\SearchBusinessFactory
+     */
+    protected function createSearchFactoryMock()
+    {
+        $searchFactoryMockBuilder = $this->getMockBuilder(SearchBusinessFactory::class)
+            ->setMethods(['createSnapshotHandler']);
+
+        $searchFactoryMock = $searchFactoryMockBuilder->getMock();
+
+        $searchFactoryMock->method('createSnapshotHandler')->willReturn($this->createSnapshotHandlerMock());
+
+        return $searchFactoryMock;
+    }
+
+    /**
+     * @return \PHPUnit\Framework\MockObject\MockObject|\Spryker\Zed\Search\Business\Model\Elasticsearch\SnapshotHandlerInterface
+     */
+    protected function createSnapshotHandlerMock()
+    {
+        $snapshotHandlerMockBuilder = $this->getMockBuilder(SnapshotHandler::class)
+            ->setConstructorArgs([$this->createElasticsearchSnapshot()])
+            ->setMethods(['buildRepositorySettings']);
+
+        $snapshotHandlerMock = $snapshotHandlerMockBuilder->getMock();
+
+        $snapshotHandlerMock->method('buildRepositorySettings')->willReturn(['location' => $this->getVirtualRepositoryLocation()]);
+
+        return $snapshotHandlerMock;
+    }
+
+    /**
+     * @return \Elastica\Snapshot
+     */
+    protected function createElasticsearchSnapshot(): Snapshot
+    {
+        /** @var \Elastica\Client $searchClient */
+        $searchClient = (new SearchClientProvider())->getInstance();
+
+        return new Snapshot($searchClient);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getVirtualRepositoryLocation(): string
+    {
+        return $this->tester->getVirtualDirectory() . static::REPOSITORY_LOCATION_FILE_NAME;
+    }
+
+    /**
+     * @param \PHPUnit\Framework\MockObject\MockObject|\Spryker\Zed\Search\Business\SearchBusinessFactory|null $factory
+     *
+     * @return \Spryker\Zed\Search\Business\SearchFacadeInterface
+     */
+    protected function getSearchFacade($factory = null): SearchFacadeInterface
+    {
+        /** @var \Spryker\Zed\Search\Business\SearchFacade $searchFacade */
+        $searchFacade = $this->tester->getFacade();
+
+        if ($factory) {
+            $searchFacade->setFactory($factory);
+        }
+
+        return $searchFacade;
     }
 
     /**
@@ -103,6 +200,48 @@ class SearchFacadeBCTest extends Unit
         $this->assertTrue($index->exists(), 'Index was expected to be installed but was not.');
 
         $this->tester->getFacade()->delete(static::INDEX_NAME);
+    }
+
+    /**
+     * @dataProvider canMapRawDataToSearchDataProvider
+     *
+     * @param array $inputData
+     * @param array $expected
+     * @param string $localeName
+     * @param string $mapperName
+     *
+     * @return void
+     */
+    public function testCanMapRawDataToSearchData(array $inputData, array $expected, string $localeName, string $mapperName): void
+    {
+        // Arrange
+        $localeTransfer = new LocaleTransfer();
+        $localeTransfer->setLocaleName($localeName);
+
+        // Act
+        $result = $this->tester->getFacade()->transformPageMapToDocumentByMapperName($inputData, $localeTransfer, $mapperName);
+
+        // Assert
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function canMapRawDataToSearchDataProvider(): array
+    {
+        return require codecept_data_dir('Fixtures/SearchDataMap/search_data_map_test_provider_data.php');
+    }
+
+    /**
+     * @return void
+     */
+    protected function setUpDependencies(): void
+    {
+        $this->tester->setDependency(SearchDependencyProvider::PLUGIN_SEARCH_PAGE_MAPS, [
+            new ProductPageMapPlugin(),
+            new CategoryNodeDataPageMapBuilder(),
+        ]);
     }
 
     /**
--- Bundles/Search/tests/SprykerTest/Zed/Search/Business/SearchFacadeTest.php
+++ Bundles/Search/tests/SprykerTest/Zed/Search/Business/SearchFacadeTest.php
@@ -8,13 +8,13 @@
 namespace SprykerTest\Zed\Search\Business;
 
 use Codeception\Test\Unit;
-use Elastica\Snapshot;
 use Generated\Shared\Transfer\LocaleTransfer;
-use Spryker\Client\Search\Provider\SearchClientProvider;
-use Spryker\Shared\Search\SearchConstants;
-use Spryker\Zed\Search\Business\Model\Elasticsearch\SnapshotHandler;
-use Spryker\Zed\Search\Business\SearchBusinessFactory;
-use Spryker\Zed\Search\Business\SearchFacadeInterface;
+use Spryker\Zed\CategoryPageSearch\Communication\Plugin\Search\Elasticsearch\CategoryNodeDataPageMapBuilder;
+use Spryker\Zed\ProductPageSearch\Communication\Plugin\Search\Elasticsearch\ProductPageMapPlugin;
+use Spryker\Zed\Search\SearchDependencyProvider;
+use Spryker\Zed\SearchElasticsearch\Communication\Plugin\Search\ElasticsearchDataMapperPlugin;
+use Spryker\Zed\SearchElasticsearch\Communication\Plugin\Search\PageDataMapperPlugin;
+use Spryker\Zed\SearchElasticsearch\SearchElasticsearchDependencyProvider;
 
 /**
  * Auto-generated group annotations
@@ -29,9 +29,6 @@ use Spryker\Zed\Search\Business\SearchFacadeInterface;
  */
 class SearchFacadeTest extends Unit
 {
-    protected const REPOSITORY_LOCATION_FILE_NAME = 'search_test_file';
-    protected const REPOSITORY_NAME = 'search_test_repository';
-
     /**
      * @var \SprykerTest\Zed\Search\SearchBusinessTester
      */
@@ -40,99 +37,11 @@ class SearchFacadeTest extends Unit
     /**
      * @return void
      */
-    public function testCreateSnapshotRepository(): void
+    protected function _setUp(): void
     {
-        $this->skipIfCi();
-        $this->tester->setConfig(SearchConstants::ELASTICA_PARAMETER__HOST, 'localhost');
-        $this->tester->setConfig(SearchConstants::ELASTICA_PARAMETER__PORT, '10005');
-        $this->tester->setConfig(SearchConstants::ELASTICA_PARAMETER__TRANSPORT, 'http');
+        parent::_setUp();
 
-        //Arrange
-        $searchFactory = $this->createSearchFactoryMock();
-        $searchFacade = $this->getSearchFacade($searchFactory);
-
-        //Act
-        $result = $searchFacade->createSnapshotRepository(static::REPOSITORY_NAME);
-
-        //Assert
-        $this->assertTrue($result);
-    }
-
-    /**
-     * @return void
-     */
-    protected function skipIfCi(): void
-    {
-        if (getenv('TRAVIS')) {
-            $this->markTestSkipped('Travis not set up properly');
-        }
-    }
-
-    /**
-     * @return \PHPUnit\Framework\MockObject\MockObject|\Spryker\Zed\Search\Business\SearchBusinessFactory
-     */
-    protected function createSearchFactoryMock()
-    {
-        $searchFactoryMockBuilder = $this->getMockBuilder(SearchBusinessFactory::class)
-            ->setMethods(['createSnapshotHandler']);
-
-        $searchFactoryMock = $searchFactoryMockBuilder->getMock();
-
-        $searchFactoryMock->method('createSnapshotHandler')->willReturn($this->createSnapshotHandlerMock());
-
-        return $searchFactoryMock;
-    }
-
-    /**
-     * @return \PHPUnit\Framework\MockObject\MockObject|\Spryker\Zed\Search\Business\Model\Elasticsearch\SnapshotHandlerInterface
-     */
-    protected function createSnapshotHandlerMock()
-    {
-        $snapshotHandlerMockBuilder = $this->getMockBuilder(SnapshotHandler::class)
-            ->setConstructorArgs([$this->createElasticsearchSnapshot()])
-            ->setMethods(['buildRepositorySettings']);
-
-        $snapshotHandlerMock = $snapshotHandlerMockBuilder->getMock();
-
-        $snapshotHandlerMock->method('buildRepositorySettings')->willReturn(['location' => $this->getVirtualRepositoryLocation()]);
-
-        return $snapshotHandlerMock;
-    }
-
-    /**
-     * @return \Elastica\Snapshot
-     */
-    protected function createElasticsearchSnapshot(): Snapshot
-    {
-        /** @var \Elastica\Client $searchClient */
-        $searchClient = (new SearchClientProvider())->getInstance();
-
-        return new Snapshot($searchClient);
-    }
-
-    /**
-     * @return string
-     */
-    protected function getVirtualRepositoryLocation(): string
-    {
-        return $this->tester->getVirtualDirectory() . static::REPOSITORY_LOCATION_FILE_NAME;
-    }
-
-    /**
-     * @param \PHPUnit\Framework\MockObject\MockObject|\Spryker\Zed\Search\Business\SearchBusinessFactory|null $factory
-     *
-     * @return \Spryker\Zed\Search\Business\SearchFacadeInterface
-     */
-    protected function getSearchFacade($factory = null): SearchFacadeInterface
-    {
-        /** @var \Spryker\Zed\Search\Business\SearchFacade $searchFacade */
-        $searchFacade = $this->tester->getFacade();
-
-        if ($factory) {
-            $searchFacade->setFactory($factory);
-        }
-
-        return $searchFacade;
+        $this->setUpDependencies();
     }
 
     /**
@@ -164,5 +73,24 @@ class SearchFacadeTest extends Unit
     public function canMapRawDataToSearchDataProvider(): array
     {
         return require codecept_data_dir('Fixtures/SearchDataMap/search_data_map_test_provider_data.php');
+    }
+
+    /**
+     * @return void
+     */
+    protected function setUpDependencies(): void
+    {
+        $this->tester->setDependency(SearchDependencyProvider::PLUGINS_SEARCH_DATA_MAPPER, [
+            new ElasticsearchDataMapperPlugin(),
+        ]);
+
+        $this->tester->setDependency(SearchElasticsearchDependencyProvider::PLUGINS_RESOURCE_DATA_MAPPER, [
+            new PageDataMapperPlugin(),
+        ]);
+
+        $this->tester->setDependency(SearchElasticsearchDependencyProvider::PLUGINS_PAGE_DATA_MAPPER, [
+            new ProductPageMapPlugin(),
+            new CategoryNodeDataPageMapBuilder(),
+        ]);
     }
 }
