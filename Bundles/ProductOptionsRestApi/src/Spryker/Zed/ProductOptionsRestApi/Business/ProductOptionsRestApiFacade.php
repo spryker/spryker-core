a85db68265d20e1cec6d5933dab0e192b8b7e8f1
Denis Gnusov <denis.gnusov@spryker.com>
2019-11-26 17:51:43 +0200

    GLUE-9530 Added mapper

---
+++ Bundles/ProductOptionsRestApi/src/Spryker/Zed/ProductOptionsRestApi/Business/ProductOptionsRestApiFacade.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductOptionsRestApi\Business;
+
+use Generated\Shared\Transfer\CartItemRequestTransfer;
+use Generated\Shared\Transfer\PersistentCartChangeTransfer;
+use Spryker\Zed\Kernel\Business\AbstractFacade;
+
+/**
+ * @method \Spryker\Zed\ProductOptionsRestApi\Business\ProductOptionsRestApiBusinessFactory getFactory()
+ */
+class ProductOptionsRestApiFacade extends AbstractFacade implements ProductOptionsRestApiFacadeInterface
+{
+    /**
+     * Specification:
+     * - Maps CartItemRequestTransfer product options to PersistentCartChangeTransfer product options.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CartItemRequestTransfer $cartItemRequestTransfer
+     * @param \Generated\Shared\Transfer\PersistentCartChangeTransfer $persistentCartChangeTransfer
+     *
+     * @return \Generated\Shared\Transfer\PersistentCartChangeTransfer
+     */
+    public function mapCartItemRequestTransferToPersistentCartChangeTransfer(
+        CartItemRequestTransfer $cartItemRequestTransfer,
+        PersistentCartChangeTransfer $persistentCartChangeTransfer
+    ): PersistentCartChangeTransfer {
+        return $this->getFactory()
+            ->createProductOptionMapper()
+            ->mapCartItemRequestTransferToPersistentCartChangeTransfer(
+                $cartItemRequestTransfer,
+                $persistentCartChangeTransfer
+            );
+    }
+}
