a8a180c14f2fcd85003418f44dbf7376833f6351
mscherer <mark.scherer@spryker.com>
2019-12-05 14:12:45 +0100

    Fix CS for invalid namespaced test classes.

--- Bundles/ApiQueryBuilder/tests/SprykerTest/Zed/ApiQueryBuilder/Persistence/QueryContainerTest.php
+++ Bundles/ApiQueryBuilder/tests/SprykerTest/Zed/ApiQueryBuilder/Persistence/QueryContainerTest.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace Functional\Spryker\Zed\ApiQueryBuilder\Persistence;
 
 use Codeception\Test\Unit;
