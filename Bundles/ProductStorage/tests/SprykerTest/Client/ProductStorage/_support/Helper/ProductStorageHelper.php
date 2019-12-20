d38ad243469b38452b37be77fbb74e4617b6a8c6
Anton Zubariev <anton.zubariev@spryker.com>
2019-12-20 18:10:37 +0200

    [CC-7664] Removed unnecessary docBlock

--- Bundles/ProductStorage/tests/SprykerTest/Client/ProductStorage/_support/Helper/ProductStorageHelper.php
+++ Bundles/ProductStorage/tests/SprykerTest/Client/ProductStorage/_support/Helper/ProductStorageHelper.php
@@ -11,10 +11,6 @@ use Codeception\Module;
 use Generated\Shared\DataBuilder\ProductAbstractStorageBuilder;
 use Generated\Shared\Transfer\ProductAbstractStorageTransfer;
 
-/**
- * This file is part of the Spryker Suite.
- * For full license information, please view the LICENSE file that was distributed with this source code.
- */
 class ProductStorageHelper extends Module
 {
     /**
