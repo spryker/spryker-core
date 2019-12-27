e4800c8a338ca2048d91b365878847c60504be95
artemkosatov <artem.kosatov@spryker.com>
2019-12-27 18:17:55 +0200

    MP-1620: Added filter by merchant.

---
+++ Bundles/MerchantProductOfferSearch/src/Spryker/Client/MerchantProductOfferSearch/Plugin/MerchantNameFacetConfigTransferBuilderPlugin.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\MerchantProductOfferSearch\Plugin;
+
+use Generated\Shared\Search\PageIndexMap;
+use Generated\Shared\Transfer\FacetConfigTransfer;
+use Spryker\Client\Catalog\Dependency\Plugin\FacetConfigTransferBuilderPluginInterface;
+use Spryker\Client\Kernel\AbstractPlugin;
+use Spryker\Shared\Search\SearchConfig;
+
+class MerchantNameFacetConfigTransferBuilderPlugin extends AbstractPlugin implements FacetConfigTransferBuilderPluginInterface
+{
+    public const NAME = 'merchant_name';
+    public const PARAMETER_NAME = 'merchant_name';
+
+    /**
+     * @return \Generated\Shared\Transfer\FacetConfigTransfer
+     */
+    public function build()
+    {
+        return (new FacetConfigTransfer())
+            ->setName(static::NAME)
+            ->setParameterName(static::PARAMETER_NAME)
+            ->setFieldName(PageIndexMap::STRING_FACET)
+            ->setType(SearchConfig::FACET_TYPE_ENUMERATION)
+            ->setIsMultiValued(true);
+    }
+}
