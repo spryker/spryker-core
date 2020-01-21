6db61e359e00e1f63a0b8d4d64a774fee818b96f
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2020-01-21 15:53:56 +0200

    TE-3485: GlossaryStorage tests abstraction adjustments

--- Bundles/GlossaryStorage/tests/SprykerTest/Zed/GlossaryStorage/Business/GlossaryStorageFacadeTest.php
+++ Bundles/GlossaryStorage/tests/SprykerTest/Zed/GlossaryStorage/Business/GlossaryStorageFacadeTest.php
@@ -44,7 +44,7 @@ class GlossaryStorageFacadeTest extends Unit
     /**
      * @return void
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
