9a69fe9829335449f1db09acf8db096c1d6c1ac7
Roberto Filippone <roberto.filippone@spryker.com>
2020-01-30 11:04:42 +0100

    PR Fixes

--- Bundles/Propel/src/Spryker/Zed/Propel/Communication/Console/EntityTransferGeneratorConsole.php
+++ Bundles/Propel/src/Spryker/Zed/Propel/Communication/Console/EntityTransferGeneratorConsole.php
@@ -23,7 +23,7 @@ class EntityTransferGeneratorConsole extends Console
     /**
      * @return void
      */
-    protected function configure()
+    protected function configure(): void
     {
         parent::configure();
         $this
@@ -36,16 +36,20 @@ class EntityTransferGeneratorConsole extends Console
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
-     * @return int|null
+     * @return int
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $transferFacade = $this->getFactory()->getTransferFacade();
         $messenger = $this->getMessenger();
 
-        $transferFacade->deleteGeneratedEntityTransferObjects();
+        $result = $this->runDependingCommand(EntityTransferRemoverConsole::COMMAND_NAME);
+        if ($result != static::CODE_SUCCESS) {
+            return $result;
+        }
+
         $transferFacade->generateEntityTransferObjects($messenger);
 
-        return null;
+        return static::CODE_SUCCESS;
     }
 }
