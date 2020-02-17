e8735fd07529a76786400ff1ff32c1fc3974a4cf
Artem Symynenko <artem.symynenko@spryker.com>
2020-02-17 18:20:02 +0200

    MR-52 add required propel schemas

---
+++ Bundles/MerchantOms/src/Spryker/Zed/MerchantOms/Persistence/Propel/AbstractSpyMerchantOmsProcessQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\MerchantOms\Persistence\Propel;
+
+use Orm\Zed\MerchantOms\Persistence\Base\SpyMerchantOmsProcessQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_merchant_oms_process' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+abstract class AbstractSpyMerchantOmsProcessQuery extends SpyMerchantOmsProcessQuery
+{
+}
