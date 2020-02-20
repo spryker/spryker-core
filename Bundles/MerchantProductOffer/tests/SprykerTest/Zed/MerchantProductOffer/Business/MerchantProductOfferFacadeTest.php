3d904545f5aa97520543262f8bae90df78328db5
vhrychenko <aptah199494@gmail.com>
2020-02-20 20:11:06 +0200

    MP-1725 Add MerchantProductOfferFacade tests

--- Bundles/MerchantProductOffer/tests/SprykerTest/Zed/MerchantProductOffer/Business/MerchantProductOfferFacadeTest.php
+++ Bundles/MerchantProductOffer/tests/SprykerTest/Zed/MerchantProductOffer/Business/MerchantProductOfferFacadeTest.php
@@ -8,6 +8,8 @@
 namespace SprykerTest\Zed\MerchantProductOffer\Business;
 
 use Codeception\Test\Unit;
+use Generated\Shared\Transfer\MerchantProductOfferCriteriaFilterTransfer;
+use Generated\Shared\Transfer\ProductOfferTransfer;
 
 /**
  * Auto-generated group annotations
@@ -26,4 +28,28 @@ class MerchantProductOfferFacadeTest extends Unit
      * @var \SprykerTest\Zed\MerchantProductOffer\MerchantProductOfferBusinessTester
      */
     protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testProductOfferCollectionIsNotEmpty(): void
+    {
+        // Arrange
+        $merchantTransfer = $this->tester->haveMerchant();
+
+        $productOfferTransfer = $this->tester->haveProductOffer([
+            ProductOfferTransfer::FK_MERCHANT => $merchantTransfer->getIdMerchant(),
+        ]);
+
+        $merchantProductOfferCriteriaFilterTransfer = (new MerchantProductOfferCriteriaFilterTransfer())
+            ->setMerchantReference($merchantTransfer->getMerchantReference())
+            ->setSkus([$productOfferTransfer->getConcreteSku()])
+            ->setIsActive(true);
+
+        // Act
+        $productOfferCollectionTransfer = $this->tester->getFacade()->getProductOfferCollection($merchantProductOfferCriteriaFilterTransfer);
+
+        // Assert
+        $this->assertNotEmpty($productOfferCollectionTransfer->getProductOffers()->count());
+    }
 }
