ec1b1112d03c8b62511166aae6d8a4be5519a42a
vhrychenko <aptah199494@gmail.com>
2020-02-20 21:28:21 +0200

    MP-1725 Add MerchantSwitcherFacade tests

---
+++ Bundles/MerchantSwitcher/tests/SprykerTest/Zed/MerchantSwitcher/_support/MerchantSwitcherBusinessTester.php
@@ -0,0 +1,28 @@
+<?php
+namespace SprykerTest\Zed\MerchantSwitcher;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause()
+ *
+ * @method \Spryker\Zed\MerchantSwitcher\Business\MerchantSwitcherFacadeInterface getFacade()
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class MerchantSwitcherBusinessTester extends \Codeception\Actor
+{
+    use _generated\MerchantSwitcherBusinessTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+}
