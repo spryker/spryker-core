47f38e72c092d26f58c251f134a776b1286e87f0
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2020-02-21 12:45:28 +0200

    CC-7413 adjusted product relation table with store relations

--- Bundles/ProductRelationGui/src/Spryker/Zed/ProductRelationGui/Communication/Table/ProductRelationTable.php
+++ Bundles/ProductRelationGui/src/Spryker/Zed/ProductRelationGui/Communication/Table/ProductRelationTable.php
@@ -7,17 +7,17 @@
 
 namespace Spryker\Zed\ProductRelationGui\Communication\Table;
 
-use Generated\Shared\Transfer\ProductAbstractTransfer;
 use Orm\Zed\Product\Persistence\Map\SpyProductAbstractLocalizedAttributesTableMap;
 use Orm\Zed\Product\Persistence\Map\SpyProductAbstractTableMap;
 use Orm\Zed\ProductRelation\Persistence\Map\SpyProductRelationTableMap;
 use Orm\Zed\ProductRelation\Persistence\Map\SpyProductRelationTypeTableMap;
+use Orm\Zed\ProductRelation\Persistence\SpyProductRelation;
 use Orm\Zed\ProductRelation\Persistence\SpyProductRelationQuery;
+use Orm\Zed\Store\Persistence\Map\SpyStoreTableMap;
 use Spryker\Service\UtilText\Model\Url\Url;
 use Spryker\Zed\Gui\Communication\Table\AbstractTable;
 use Spryker\Zed\Gui\Communication\Table\TableConfiguration;
 use Spryker\Zed\ProductRelationGui\Communication\Controller\DeleteController;
-use Spryker\Zed\ProductRelationGui\Communication\Controller\EditController;
 use Spryker\Zed\ProductRelationGui\Communication\Controller\ViewController;
 use Spryker\Zed\ProductRelationGui\Dependency\Facade\ProductRelationGuiToLocaleFacadeInterface;
 use Spryker\Zed\ProductRelationGui\Dependency\Facade\ProductRelationGuiToProductFacadeInterface;
@@ -25,15 +25,24 @@ use Spryker\Zed\ProductRelationGui\ProductRelationGuiConfig;
 
 class ProductRelationTable extends AbstractTable
 {
-    public const COL_ACTIONS = 'Actions';
-    public const COL_NUMBER_OF_RELATED_PRODUCTS = 'numberOfRelatedProducts';
+    protected const HEADER_AVAILABLE_IN_STORE = 'Store';
+    protected const HEADER_ID_PRODUCT_RELATION = 'ID';
+    protected const HEADER_ABSTRACT_SKU = 'Abstract SKU';
+    protected const HEADER_ABSTRACT_PRODUCT_NAME = 'Abstract product name';
+    protected const HEADER_RELATION_TYPE = 'Relation type';
+    protected const HEADER_STATUS = 'Status';
+    protected const HEADER_NUMBER_OF_PRODUCTS = '# of Products';
+    protected const HEADER_PRODUCT_RELATION_KEY = 'Product Relation Key';
+    protected const COL_ACTIONS = 'Actions';
+    protected const COL_NUMBER_OF_RELATED_PRODUCTS = 'numberOfRelatedProducts';
+    protected const COL_LOCALIZED_NAME = 'localized_name';
 
-    public const URL_RELATION_DEACTIVATE = '/product-relation-gui/edit/deactivate';
-    public const URL_RELATION_DELETE = '/product-relation-gui/delete/index';
-    public const URL_RELATION_ACTIVATE = '/product-relation-gui/edit/activate';
-    public const URL_PRODUCT_RELATION_LIST = '/product-relation-gui/list/index';
-    public const URL_PRODUCT_RELATION_EDIT = '/product-relation-gui/edit/index';
-    public const URL_PRODUCT_RELATION_VIEW = '/product-relation-gui/view/index';
+    protected const URL_RELATION_DELETE = '/product-relation-gui/delete/index';
+    protected const URL_PRODUCT_RELATION_LIST = '/product-relation-gui/list/index';
+    protected const URL_PRODUCT_RELATION_EDIT = '/product-relation-gui/edit/index';
+    protected const URL_PRODUCT_RELATION_VIEW = '/product-relation-gui/view/index';
+
+    protected const LABEL_PRIMARY = 'label-primary';
 
     /**
      * @var \Orm\Zed\ProductRelation\Persistence\SpyProductRelationQuery
@@ -102,6 +111,7 @@ class ProductRelationTable extends AbstractTable
         $config->setRawColumns([
             static::COL_ACTIONS,
             SpyProductRelationTableMap::COL_IS_ACTIVE,
+            SpyStoreTableMap::COL_NAME,
         ]);
     }
 
@@ -113,12 +123,14 @@ class ProductRelationTable extends AbstractTable
     protected function setHeaders(TableConfiguration $config): void
     {
         $config->setHeader([
-            SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION => '#',
-            SpyProductAbstractTableMap::COL_SKU => 'Sku',
-            SpyProductAbstractLocalizedAttributesTableMap::COL_NAME => 'Abstract product name',
-            SpyProductRelationTypeTableMap::COL_KEY => 'Relation type',
-            static::COL_NUMBER_OF_RELATED_PRODUCTS => 'Number of products',
-            SpyProductRelationTableMap::COL_IS_ACTIVE => 'Status',
+            SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION => static::HEADER_ID_PRODUCT_RELATION,
+            SpyProductRelationTableMap::COL_PRODUCT_RELATION_KEY => static::HEADER_PRODUCT_RELATION_KEY,
+            SpyProductAbstractTableMap::COL_SKU => static::HEADER_ABSTRACT_SKU,
+            SpyProductAbstractLocalizedAttributesTableMap::COL_NAME => static::HEADER_ABSTRACT_PRODUCT_NAME,
+            SpyProductRelationTypeTableMap::COL_KEY => static::HEADER_RELATION_TYPE,
+            static::COL_NUMBER_OF_RELATED_PRODUCTS => static::HEADER_NUMBER_OF_PRODUCTS,
+            SpyProductRelationTableMap::COL_IS_ACTIVE => static::HEADER_STATUS,
+            SpyStoreTableMap::COL_NAME => static::HEADER_AVAILABLE_IN_STORE,
             static::COL_ACTIONS => static::COL_ACTIONS,
         ]);
     }
@@ -137,6 +149,7 @@ class ProductRelationTable extends AbstractTable
             SpyProductRelationTableMap::COL_IS_ACTIVE,
             SpyProductRelationTypeTableMap::COL_KEY,
             static::COL_NUMBER_OF_RELATED_PRODUCTS,
+            SpyProductRelationTableMap::COL_PRODUCT_RELATION_KEY,
         ]);
     }
 
@@ -151,6 +164,7 @@ class ProductRelationTable extends AbstractTable
             SpyProductAbstractTableMap::COL_SKU,
             SpyProductRelationTypeTableMap::COL_KEY,
             SpyProductAbstractLocalizedAttributesTableMap::COL_NAME,
+            SpyProductRelationTableMap::COL_PRODUCT_RELATION_KEY,
         ]);
     }
 
@@ -177,7 +191,7 @@ class ProductRelationTable extends AbstractTable
         $localeTransfer = $this->localeFacade->getCurrentLocale();
         $query = $this->prepareQuery($localeTransfer->getIdLocale());
 
-        $queryResults = $this->runQuery($query, $config);
+        $queryResults = $this->runQuery($query, $config, true);
 
         $results = [];
         foreach ($queryResults as $item) {
@@ -195,73 +209,66 @@ class ProductRelationTable extends AbstractTable
     protected function prepareQuery(int $idLocale): SpyProductRelationQuery
     {
         return $this->productRelationQuery
-            ->select([
-                SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION,
-                SpyProductAbstractTableMap::COL_SKU,
-                SpyProductRelationTypeTableMap::COL_KEY,
-                SpyProductRelationTableMap::COL_IS_ACTIVE,
-                SpyProductAbstractTableMap::COL_ID_PRODUCT_ABSTRACT,
-                SpyProductAbstractLocalizedAttributesTableMap::COL_NAME,
-            ])
-            ->joinSpyProductAbstract()
+            ->leftJoinWithSpyProductAbstract()
             ->joinSpyProductRelationProductAbstract('num_alias')
+            ->leftJoinWithSpyProductRelationType()
             ->useSpyProductAbstractQuery()
                 ->useSpyProductAbstractLocalizedAttributesQuery()
                     ->filterByFkLocale($idLocale)
                 ->endUse()
             ->endUse()
+            ->withColumn(SpyProductAbstractLocalizedAttributesTableMap::COL_NAME, static::COL_LOCALIZED_NAME)
             ->withColumn("COUNT('num_alias')", static::COL_NUMBER_OF_RELATED_PRODUCTS)
-            ->joinSpyProductRelationType()
             ->groupByIdProductRelation();
     }
 
     /**
-     * @param array $item
+     * @param \Orm\Zed\ProductRelation\Persistence\SpyProductRelation $productRelationEntity
      *
      * @return array
      */
-    protected function mapResults(array $item): array
+    protected function mapResults(SpyProductRelation $productRelationEntity): array
     {
         return [
-            SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION => $item[SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION],
-            SpyProductAbstractTableMap::COL_SKU => $item[SpyProductAbstractTableMap::COL_SKU],
-            SpyProductAbstractLocalizedAttributesTableMap::COL_NAME => $item[SpyProductAbstractLocalizedAttributesTableMap::COL_NAME],
-            SpyProductRelationTypeTableMap::COL_KEY => $item[SpyProductRelationTypeTableMap::COL_KEY],
-            static::COL_NUMBER_OF_RELATED_PRODUCTS => $item[static::COL_NUMBER_OF_RELATED_PRODUCTS],
-            SpyProductRelationTableMap::COL_IS_ACTIVE => $this->buildActiveLabel($item),
-            static::COL_ACTIONS => implode(' ', $this->buildActions($item)),
+            SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION => $productRelationEntity->getIdProductRelation(),
+            SpyProductRelationTableMap::COL_PRODUCT_RELATION_KEY => $productRelationEntity->getProductRelationKey(),
+            SpyProductAbstractTableMap::COL_SKU => $productRelationEntity->getSpyProductAbstract()->getSku(),
+            SpyProductAbstractLocalizedAttributesTableMap::COL_NAME => $productRelationEntity->getVirtualColumn(static::COL_LOCALIZED_NAME),
+            SpyProductRelationTypeTableMap::COL_KEY => $productRelationEntity->getSpyProductRelationType()->getKey(),
+            static::COL_NUMBER_OF_RELATED_PRODUCTS => $productRelationEntity->getVirtualColumn(static::COL_NUMBER_OF_RELATED_PRODUCTS),
+            SpyProductRelationTableMap::COL_IS_ACTIVE => $this->buildActiveLabel($productRelationEntity),
+            SpyStoreTableMap::COL_NAME => $this->getStoreNames($productRelationEntity),
+            static::COL_ACTIONS => implode(' ', $this->buildActions($productRelationEntity->getIdProductRelation())),
         ];
     }
 
     /**
-     * @param array $item
+     * @param int $idProductRelation
      *
      * @return array
      */
-    protected function buildActions(array $item): array
+    protected function buildActions(int $idProductRelation): array
     {
         $buttons = [];
-        $buttons[] = $this->createViewButton($item);
-        $buttons[] = $this->createViewInShopButton($item);
-        $buttons[] = $this->createEditButton($item);
-        $buttons[] = $this->createRelationStatusChangeButton($item);
-        $buttons[] = $this->createDeleteRelationButton($item);
+        $buttons[] = $this->createViewButton($idProductRelation);
+        $buttons[] = $this->createEditButton($idProductRelation);
+        $buttons[] = $this->createDeleteRelationButton($idProductRelation);
 
         return $buttons;
     }
 
     /**
-     * @param array $item
+     * @param int $idProductRelation
      *
      * @return string
      */
-    protected function createViewButton(array $item): string
+    protected function createViewButton(int $idProductRelation): string
     {
         return $this->generateViewButton(
             Url::generate(
                 static::URL_PRODUCT_RELATION_VIEW,
                 [
-                    ViewController::URL_PARAM_ID_PRODUCT_RELATION => $item[SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION],
+                    ViewController::URL_PARAM_ID_PRODUCT_RELATION => $idProductRelation,
                 ]
             ),
             'View'
@@ -269,13 +276,13 @@ class ProductRelationTable extends AbstractTable
     }
 
     /**
-     * @param array $item
+     * @param \Orm\Zed\ProductRelation\Persistence\SpyProductRelation $productRelationEntity
      *
      * @return string
      */
-    protected function buildActiveLabel(array $item): string
+    protected function buildActiveLabel(SpyProductRelation $productRelationEntity): string
     {
-        if (!$item[SpyProductRelationTableMap::COL_IS_ACTIVE]) {
+        if (!$productRelationEntity->getIsActive()) {
             return $this->generateLabel('Inactive', 'label-danger');
         }
 
@@ -283,17 +290,17 @@ class ProductRelationTable extends AbstractTable
     }
 
     /**
-     * @param array $item
+     * @param int $idProductRelation
      *
      * @return string
      */
-    protected function createEditButton(array $item): string
+    protected function createEditButton(int $idProductRelation): string
     {
         return $this->generateEditButton(
             Url::generate(
                 static::URL_PRODUCT_RELATION_EDIT,
                 [
-                    'id-product-relation' => $item[SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION],
+                    'id-product-relation' => $idProductRelation,
                 ]
             ),
             'Edit'
@@ -301,89 +308,39 @@ class ProductRelationTable extends AbstractTable
     }
 
     /**
-     * @param array $item
+     * @param int $idProductRelation
      *
      * @return string
      */
-    protected function createViewInShopButton(array $item): string
-    {
-        $url = $this->findProductUrl($item);
-        if (!$url) {
-            return '';
-        }
-
-        return $this->generateViewButton(
-            $url,
-            'View in Shop',
-            ['target' => '_blank']
-        );
-    }
-
-    /**
-     * @param array $product
-     *
-     * @return string|null
-     */
-    protected function findProductUrl(array $product): ?string
-    {
-        $productAbstract = (new ProductAbstractTransfer())
-            ->setSku($product[SpyProductAbstractTableMap::COL_SKU])
-            ->setIdProductAbstract($product[SpyProductAbstractTableMap::COL_ID_PRODUCT_ABSTRACT]);
-
-        $url = null;
-
-        $idLocale = $this->localeFacade->getCurrentLocale()->getIdLocale();
-        $productUrl = $this->productFacade->getProductUrl($productAbstract);
-        foreach ($productUrl->getUrls() as $localizedProductUrl) {
-            if ($localizedProductUrl->getLocale()->getIdLocale() === $idLocale) {
-                $url = $this->productRelationGuiConfig->getYvesHost() . $localizedProductUrl->getUrl();
-
-                break;
-            }
-        }
-
-        return $url;
-    }
-
-    /**
-     * @param array $item
-     *
-     * @return string
-     */
-    protected function createRelationStatusChangeButton(array $item): string
-    {
-        if ($item[SpyProductRelationTableMap::COL_IS_ACTIVE]) {
-            return $this->generateRemoveButton(
-                Url::generate(static::URL_RELATION_DEACTIVATE, [
-                    EditController::URL_PARAM_ID_PRODUCT_RELATION => $item[SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION],
-                    EditController::URL_PARAM_REDIRECT_URL => static::URL_PRODUCT_RELATION_LIST,
-                ]),
-                'Deactivate'
-            );
-        }
-
-        return $this->generateViewButton(
-            Url::generate(static::URL_RELATION_ACTIVATE, [
-                EditController::URL_PARAM_ID_PRODUCT_RELATION => $item[SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION],
-                EditController::URL_PARAM_REDIRECT_URL => static::URL_PRODUCT_RELATION_LIST,
-            ]),
-            'Activate'
-        );
-    }
-
-    /**
-     * @param array $item
-     *
-     * @return string
-     */
-    protected function createDeleteRelationButton(array $item): string
+    protected function createDeleteRelationButton(int $idProductRelation): string
     {
         return $this->generateRemoveButton(
             Url::generate(static::URL_RELATION_DELETE, [
-                DeleteController::URL_PARAM_ID_PRODUCT_RELATION => $item[SpyProductRelationTableMap::COL_ID_PRODUCT_RELATION],
+                DeleteController::URL_PARAM_ID_PRODUCT_RELATION => $idProductRelation,
                 DeleteController::URL_PARAM_REDIRECT_URL => static::URL_PRODUCT_RELATION_LIST,
             ]),
             'Delete'
         );
+    }
+
+    /**
+     * @param \Orm\Zed\ProductRelation\Persistence\SpyProductRelation $productRelationEntity
+     *
+     * @return string
+     */
+    protected function getStoreNames(SpyProductRelation $productRelationEntity): string
+    {
+        $storeNames = [];
+        foreach ($productRelationEntity->getProductRelationStores() as $productRelationStore) {
+            $storeName = $productRelationStore->getStore()->getName();
+
+            if ($storeName === null) {
+                continue;
+            }
+
+            $storeNames[] = $this->generateLabel($storeName, static::LABEL_PRIMARY);
+        }
+
+        return implode(' ', $storeNames);
     }
 }
