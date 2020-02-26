299ee523b39218de22dfc8944a26bdd45d2b8a80
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2020-02-26 17:58:58 +0200

    TE-3485: Revert glossary storage configuration

--- Bundles/GlossaryStorage/src/Spryker/Zed/GlossaryStorage/Communication/Plugin/Synchronization/GlossarySynchronizationDataPlugin.php
+++ Bundles/GlossaryStorage/src/Spryker/Zed/GlossaryStorage/Communication/Plugin/Synchronization/GlossarySynchronizationDataPlugin.php
@@ -8,7 +8,7 @@
 namespace Spryker\Zed\GlossaryStorage\Communication\Plugin\Synchronization;
 
 use Propel\Runtime\ActiveQuery\ModelCriteria;
-use Spryker\Shared\GlossaryStorage\GlossaryStorageConstants;
+use Spryker\Shared\GlossaryStorage\GlossaryStorageConfig;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 use Spryker\Zed\SynchronizationExtension\Dependency\Plugin\SynchronizationDataQueryContainerPluginInterface;
 
@@ -31,7 +31,7 @@ class GlossarySynchronizationDataPlugin extends AbstractPlugin implements Synchr
      */
     public function getResourceName(): string
     {
-        return GlossaryStorageConstants::RESOURCE_NAME;
+        return GlossaryStorageConfig::TRANSLATION_RESOURCE_NAME;
     }
 
     /**
@@ -89,7 +89,7 @@ class GlossarySynchronizationDataPlugin extends AbstractPlugin implements Synchr
      */
     public function getQueueName(): string
     {
-        return GlossaryStorageConstants::SYNC_STORAGE_QUEUE;
+        return GlossaryStorageConfig::SYNC_STORAGE_TRANSLATION;
     }
 
     /**
