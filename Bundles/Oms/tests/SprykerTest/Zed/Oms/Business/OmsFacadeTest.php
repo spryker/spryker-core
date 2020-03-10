15344454f04acf9056fbc131cea36480d119cf62
AntonKhabiuk <brotherosirus@gmail.com>
2020-03-10 12:33:15 +0200

    CC-7923 Add messages to asserts

--- Bundles/Oms/tests/SprykerTest/Zed/Oms/Business/OmsFacadeTest.php
+++ Bundles/Oms/tests/SprykerTest/Zed/Oms/Business/OmsFacadeTest.php
@@ -223,11 +223,13 @@ class OmsFacadeTest extends Unit
 
         $this->assertNotEquals(
             $orderItems->offsetGet(0)->getFkOmsOrderItemState(),
-            $processedOrderItems->offsetGet(0)->getFkOmsOrderItemState()
+            $processedOrderItems->offsetGet(0)->getFkOmsOrderItemState(),
+            'Order item state is not ID does not equal to an expected value.'
         );
         $this->assertEquals(
             $orderItems->offsetGet(1)->getFkOmsOrderItemState(),
-            $processedOrderItems->offsetGet(1)->getFkOmsOrderItemState()
+            $processedOrderItems->offsetGet(1)->getFkOmsOrderItemState(),
+            'Order item state is not ID does not equal to an expected value.'
         );
     }
 
