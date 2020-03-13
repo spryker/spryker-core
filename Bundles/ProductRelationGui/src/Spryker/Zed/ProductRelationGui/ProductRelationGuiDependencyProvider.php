504f39b0e0de805416d78e755d7568d74a9218bd
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2020-03-13 12:50:50 +0200

    CC-7414 fix typo

--- Bundles/ProductRelationGui/src/Spryker/Zed/ProductRelationGui/ProductRelationGuiDependencyProvider.php
+++ Bundles/ProductRelationGui/src/Spryker/Zed/ProductRelationGui/ProductRelationGuiDependencyProvider.php
@@ -263,7 +263,7 @@ class ProductRelationGuiDependencyProvider extends AbstractBundleDependencyProvi
         throw new MissingStoreRelationFormTypePluginException(
             sprintf(
                 'Missing instance of %s! You need to configure StoreRelationFormType ' .
-                'in your own ProductRelationGuiGuiDependencyProvider::getStoreRelationFormTypePlugin() ' .
+                'in your own ProductRelationGuiDependencyProvider::getStoreRelationFormTypePlugin() ' .
                 'to be able to manage shipment methods.',
                 FormTypeInterface::class
             )
