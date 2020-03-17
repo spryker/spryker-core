67d729e14d23f32ce7cc46326fbd770a149f1c7a
Aleksey Belan <alexey.belan@spryker.com>
2020-03-17 16:04:11 +0200

    BO-405: Created scaffolding

---
+++ Bundles/ProductOfferGuiPage/src/Spryker/Zed/ProductOfferGuiPage/Presentation/Components/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { ProductOfferComponent } from './product-offer/product-offer.component';
+
+@NgModule({
+    declarations: [
+        ProductOfferComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: []
+})
+export class AppModule {
+}
---
+++ Bundles/ProductOfferGuiPage/src/Spryker/Zed/ProductOfferGuiPage/Presentation/Components/app/product-offer/product-offer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'mp-product-offer',
+    templateUrl: './product-offer.component.html',
+    styleUrls: ['./product-offer.component.less']
+})
+export class ProductOfferComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
