a7047e1431b209b1adea7220b90c8555a1663019
Anton Zubariev <anton.zubariev@spryker.com>
2020-03-18 16:14:02 +0200

    [MR-24][MR-553] Fixed unused field in mapping of totals

--- Bundles/MerchantSalesOrder/src/Spryker/Zed/MerchantSalesOrder/Persistence/Propel/Mapper/MerchantSalesOrderMapper.php
+++ Bundles/MerchantSalesOrder/src/Spryker/Zed/MerchantSalesOrder/Persistence/Propel/Mapper/MerchantSalesOrderMapper.php
@@ -139,6 +139,7 @@ class MerchantSalesOrderMapper
 
         return $totalsTransfer
             ->fromArray($merchantSalesOrderTotalsEntity->toArray(), true)
+            ->setExpenseTotal($merchantSalesOrderTotalsEntity->getOrderExpenseTotal())
             ->setTaxTotal($taxTotalTransfer);
     }
 
@@ -155,6 +156,7 @@ class MerchantSalesOrderMapper
         SpyMerchantSalesOrderTotals $merchantSalesOrderTotalsEntity
     ): SpyMerchantSalesOrderTotals {
         $merchantSalesOrderTotalsEntity->fromArray($totalsTransfer->modifiedToArray());
+        $merchantSalesOrderTotalsEntity->setOrderExpenseTotal($totalsTransfer->getExpenseTotal());
         $merchantSalesOrderTotalsEntity->setFkMerchantSalesOrder($idMerchantOrder);
 
         if ($totalsTransfer->getTaxTotal()) {
