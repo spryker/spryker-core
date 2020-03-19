0f2b899460b09b364c63f0a54c3c757af921c64d
Kotiuk <andrii.kotiuk@spryker.com>
2020-03-19 17:28:04 +0200

    GLUE-10203 created MerchantRestApi module with Bridge

---
+++ Bundles/MerchantsRestApi/src/Spryker/Glue/MerchantsRestApi/MerchantsRestApiFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\MerchantsRestApi;
+
+use Spryker\Glue\Kernel\AbstractFactory;
+
+/**
+ * @method \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface getResourceBuilder()
+ * @method \Spryker\Glue\CartCodesRestApi\CartCodesRestApiConfig getConfig()
+ */
+class MerchantsRestApiFactory extends AbstractFactory
+{
+}
