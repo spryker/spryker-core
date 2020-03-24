406751b5929973bff611da112c6aab149dfc9041
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2020-03-24 09:54:19 +0200

    CC-7410 fix data import

--- Bundles/ProductRelationDataImport/src/Spryker/Zed/ProductRelationDataImport/Business/Writer/ProductRelationStore/ProductRelationStoreWriterStep.php
+++ Bundles/ProductRelationDataImport/src/Spryker/Zed/ProductRelationDataImport/Business/Writer/ProductRelationStore/ProductRelationStoreWriterStep.php
@@ -7,10 +7,14 @@
 
 namespace Spryker\Zed\ProductRelationDataImport\Business\Writer\ProductRelationStore;
 
+use Orm\Zed\ProductRelation\Persistence\SpyProductRelationQuery;
 use Orm\Zed\ProductRelation\Persistence\SpyProductRelationStoreQuery;
+use Orm\Zed\Store\Persistence\Map\SpyStoreTableMap;
+use Propel\Runtime\ActiveQuery\Criteria;
 use Spryker\Zed\DataImport\Business\Model\DataImportStep\DataImportStepInterface;
 use Spryker\Zed\DataImport\Business\Model\DataImportStep\PublishAwareStep;
 use Spryker\Zed\DataImport\Business\Model\DataSet\DataSetInterface;
+use Spryker\Zed\ProductRelationDataImport\Business\Exception\DuplicatedStoresException;
 use Spryker\Zed\ProductRelationDataImport\Business\Writer\ProductRelationStore\DataSet\ProductRelationStoreDataSetInterface;
 use Spryker\Zed\ProductRelationDataImport\ProductRelationDataImportConfig;
 
@@ -23,6 +27,8 @@ class ProductRelationStoreWriterStep extends PublishAwareStep implements DataImp
      */
     public function execute(DataSetInterface $dataSet): void
     {
+        $this->validateStores($dataSet);
+
         $productRelationStoreEntity = SpyProductRelationStoreQuery::create()
             ->filterByFkStore($dataSet[ProductRelationStoreDataSetInterface::COL_ID_STORE])
             ->filterByFkProductRelation($dataSet[ProductRelationStoreDataSetInterface::COL_ID_PRODUCT_RELATION])
@@ -33,5 +39,40 @@ class ProductRelationStoreWriterStep extends PublishAwareStep implements DataImp
         $productRelationEntity = $productRelationStoreEntity->getProductRelation();
 
         $this->addPublishEvents(ProductRelationDataImportConfig::PRODUCT_ABSTRACT_RELATION_STORE_PUBLISH, $productRelationEntity->getFkProductAbstract());
+    }
+
+    /**
+     * @param \Spryker\Zed\DataImport\Business\Model\DataSet\DataSetInterface $dataSet
+     *
+     * @throws \Spryker\Zed\ProductRelationDataImport\Business\Exception\DuplicatedStoresException
+     *
+     * @return void
+     */
+    protected function validateStores(DataSetInterface $dataSet): void
+    {
+        $productRelationKey = $dataSet[ProductRelationStoreDataSetInterface::COL_PRODUCT_RELATION_KEY];
+        $productRelationEntity = SpyProductRelationQuery::create()
+            ->filterByProductRelationKey($productRelationKey)
+            ->findOne();
+
+        $storeName = $dataSet[ProductRelationStoreDataSetInterface::COL_STORE_NAME];
+
+        $storesData = SpyProductRelationStoreQuery::create()
+            ->leftJoinWithStore()
+            ->select([
+                SpyStoreTableMap::COL_NAME,
+            ])
+            ->distinct()
+            ->useProductRelationQuery()
+                ->filterByFkProductAbstract($productRelationEntity->getFkProductAbstract())
+                ->filterByFkProductRelationType($productRelationEntity->getFkProductRelationType())
+                ->filterByProductRelationKey($productRelationKey, Criteria::NOT_EQUAL)
+            ->endUse()
+            ->find()
+            ->getData();
+
+        if (in_array($storeName, $storesData)) {
+            throw new DuplicatedStoresException('Impossible assign product relation to the given store');
+        }
     }
 }
