9f557b10db66c770ea6f453b0444a1e9941733c2
Aleksey Belan <alexey.belan@spryker.com>
2020-03-24 09:50:05 +0200

    BO-405: Update module

--- Bundles/ProductOfferGuiPage/src/Spryker/Zed/ProductOfferGuiPage/Presentation/Components/app/product-offer/product-offer.module.ts
+++ Bundles/ProductOfferGuiPage/src/Spryker/Zed/ProductOfferGuiPage/Presentation/Components/app/product-offer/product-offer.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from '@spryker/table';
-import { TableColumnTextComponent } from './table-column-text/table-column-text.component';
-import { TableColumnTextModule } from './table-column-text/table-column-text.module';
-
+import { TableColumnTextComponent, TableColumnTextModule } from '@spryker/table/columns';
 import { ProductOfferComponent } from './product-offer.component';
 
 @NgModule({
