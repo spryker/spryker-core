9400106939302487ce0544042727b67e2e70d9c5
voitov <tigran.voitov@spryker.com>
2020-03-27 14:53:31 +0200

    glue-10167 TL CR fixes

--- Bundles/Category/src/Spryker/Zed/Category/Business/CategoryFacadeInterface.php
+++ Bundles/Category/src/Spryker/Zed/Category/Business/CategoryFacadeInterface.php
@@ -135,7 +135,7 @@ interface CategoryFacadeInterface
      *  - Touches created category-node entities active (via TouchFacade)
      *  - Touches navigation (via TouchFacade)
      *  - Touches created url entities active (via TouchFacade)
-     *  - Trigger CategoryEvents::CATEGORY_NODE_PUBLISH event
+     *  - Triggers CategoryEvents::CATEGORY_NODE_PUBLISH event for parent and children nodes
      *
      * @api
      *
@@ -187,7 +187,7 @@ interface CategoryFacadeInterface
      *   - Touches all category-node entities in path active (via TouchFacade)
      *   - Touches removed URL entities deleted (via TouchFacade)
      *   - Touches navigation active (via TouchFacade)
-     *   - Trigger CategoryEvents::CATEGORY_NODE_PUBLISH event
+     *   - Triggers CategoryEvents::CATEGORY_NODE_PUBLISH event for parent and children nodes
      *
      * @api
      *
@@ -233,7 +233,7 @@ interface CategoryFacadeInterface
      *  - Touches all deleted url entities deleted (via TouchFacade)
      *  - Touches navigation active (via TouchFacade)
      *  - Calls all registered CategoryRelationDeletePluginInterface-plugins directly before removing the category entity
-     *  - Trigger CategoryEvents::CATEGORY_NODE_PUBLISH event
+     *  - Triggers CategoryEvents::CATEGORY_NODE_PUBLISH event for parent nodes
      *
      * @api
      *
@@ -247,7 +247,7 @@ interface CategoryFacadeInterface
      * Specification:
      *  - Finds sub-trees for the category node to be deleted and moves them under the destination node
      *  - Touches deleted category-node deleted (via TouchFacade)
-     *  - Trigger CategoryEvents::CATEGORY_NODE_PUBLISH event
+     *  - Triggers CategoryEvents::CATEGORY_NODE_PUBLISH event for parent nodes
      *
      * @api
      *
