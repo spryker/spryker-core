166b49fae1c29933057e0f40d62f60d03515c428
artemkosatov <artem.kosatov@spryker.com>
2020-04-01 16:22:28 +0300

    MP-2100: Updated MerchantDataImport module by merchant store.

--- Bundles/MerchantDataImport/src/Spryker/Zed/MerchantDataImport/Business/MerchantDataImportBusinessFactory.php
+++ Bundles/MerchantDataImport/src/Spryker/Zed/MerchantDataImport/Business/MerchantDataImportBusinessFactory.php
@@ -8,6 +8,10 @@
 namespace Spryker\Zed\MerchantDataImport\Business;
 
 use Spryker\Zed\DataImport\Business\DataImportBusinessFactory;
+use Spryker\Zed\DataImport\Business\Model\DataImportStep\DataImportStepInterface;
+use Spryker\Zed\MerchantDataImport\Business\MerchantStore\Step\MerchantKeyToIdMerchantStep;
+use Spryker\Zed\MerchantDataImport\Business\MerchantStore\Step\MerchantStoreWriterStep;
+use Spryker\Zed\MerchantDataImport\Business\MerchantStore\Step\StoreNameToIdStoreStep;
 use Spryker\Zed\MerchantDataImport\Business\Model\DataSet\MerchantDataSetInterface;
 use Spryker\Zed\MerchantDataImport\Business\Model\MerchantWriterStep;
 
@@ -38,5 +42,40 @@ class MerchantDataImportBusinessFactory extends DataImportBusinessFactory
         $dataImporter->addDataSetStepBroker($dataSetStepBroker);
 
         return $dataImporter;
+    }
+
+    /**
+     * @return \Spryker\Zed\DataImport\Business\Model\DataImporterAfterImportAwareInterface|\Spryker\Zed\DataImport\Business\Model\DataImporterBeforeImportAwareInterface|\Spryker\Zed\DataImport\Business\Model\DataImporterInterface|\Spryker\Zed\DataImport\Business\Model\DataSet\DataSetStepBrokerAwareInterface
+     */
+    public function createMerchantStoreDataImport()
+    {
+        $dataImporter = $this->getCsvDataImporterFromConfig(
+            $this->getConfig()->getMerchantStoreDataImporterConfiguration()
+        );
+
+        $dataSetStepBroker = $this->createTransactionAwareDataSetStepBroker();
+        $dataSetStepBroker->addStep($this->createMerchantKeyToIdMerchantStep())
+            ->addStep($this->createStoreNameToIdStoreStep())
+            ->addStep(new MerchantStoreWriterStep());
+
+        $dataImporter->addDataSetStepBroker($dataSetStepBroker);
+
+        return $dataImporter;
+    }
+
+    /**
+     * @return \Spryker\Zed\DataImport\Business\Model\DataImportStep\DataImportStepInterface
+     */
+    public function createMerchantKeyToIdMerchantStep(): DataImportStepInterface
+    {
+        return new MerchantKeyToIdMerchantStep();
+    }
+
+    /**
+     * @return \Spryker\Zed\DataImport\Business\Model\DataImportStep\DataImportStepInterface
+     */
+    public function createStoreNameToIdStoreStep(): DataImportStepInterface
+    {
+        return new StoreNameToIdStoreStep();
     }
 }
