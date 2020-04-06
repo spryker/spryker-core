7dfb961cb867d54caea43ff7c1ba811e8f2fbbe1
gechetspr <serhii.chepela@spryker.com>
2020-04-06 11:39:06 +0300

    TE-4316 fixed CI failed test

--- Bundles/ProductBarcode/tests/SprykerTest/Zed/ProductBarcode/Business/ProductBarcodeFacadeTest.php
+++ Bundles/ProductBarcode/tests/SprykerTest/Zed/ProductBarcode/Business/ProductBarcodeFacadeTest.php
@@ -58,6 +58,8 @@ class ProductBarcodeFacadeTest extends Test
         $this->tester->setDependency(BarcodeDependencyProvider::PLUGINS_BARCODE_GENERATOR, [
             $this->barcodePlugin,
         ]);
+
+        $this->tester->clearFactoryContainerCache();
     }
 
     /**
