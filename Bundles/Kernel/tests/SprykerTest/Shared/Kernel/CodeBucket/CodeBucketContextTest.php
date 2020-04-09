6eef7d54e4b80663f23634a37fd4b46bbc8f823e
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2020-04-09 09:32:43 +0300

    continue with tests

--- Bundles/Kernel/tests/SprykerTest/Shared/Kernel/CodeBucket/CodeBucketContextTest.php
+++ Bundles/Kernel/tests/SprykerTest/Shared/Kernel/CodeBucket/CodeBucketContextTest.php
@@ -4,12 +4,18 @@ namespace SprykerTest\Shared\Kernel\CodeBucket;
 
 use Codeception\Test\Unit;
 use Spryker\Shared\Kernel\CodeBucket\Context\CodeBucketContext;
+use Spryker\Shared\Kernel\Store;
 
 class CodeBucketContextTest extends Unit
 {
     public function testGetCurrentCodeBucketWithDynamicStoreModeAndWithoutCustomCodeBucketShouldReturnEmptyCodeBucket(): void
     {
         $codeBucketContextMock = $this->createCodeBucketContext();
+        $reflectionStore = new \ReflectionClass(Store::class);
+        $reflectionIsDynamicStoreMode = $reflectionStore->getProperty('isDynamicStoreMode');
+        $reflectionIsDynamicStoreMode->setValue(true);
+
+        $currentCodeBucket = $codeBucketContextMock->getCurrentCodeBucket();
     }
 
     /**
