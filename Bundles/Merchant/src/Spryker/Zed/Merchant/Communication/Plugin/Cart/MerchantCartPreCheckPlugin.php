784ac1b6b18b698878aa318a556f029a7fbb835d
Alexander <alexander.veselov@spryker.com>
2020-04-14 17:12:31 +0000

    MP-2179: Created validators for plugins

--- Bundles/Merchant/src/Spryker/Zed/Merchant/Communication/Plugin/Cart/MerchantCartPreCheckPlugin.php
+++ Bundles/Merchant/src/Spryker/Zed/Merchant/Communication/Plugin/Cart/MerchantCartPreCheckPlugin.php
@@ -7,28 +7,18 @@
 
 namespace Spryker\Zed\Merchant\Communication\Plugin\Cart;
 
-use ArrayObject;
 use Generated\Shared\Transfer\CartChangeTransfer;
 use Generated\Shared\Transfer\CartPreCheckResponseTransfer;
-use Generated\Shared\Transfer\MerchantCriteriaTransfer;
-use Generated\Shared\Transfer\MessageTransfer;
 use Spryker\Zed\CartExtension\Dependency\Plugin\CartPreCheckPluginInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
  * @method \Spryker\Zed\Merchant\Business\MerchantFacadeInterface getFacade()
+ * @method \Spryker\Zed\Merchant\Communication\MerchantCommunicationFactory getFactory()
  * @method \Spryker\Zed\Merchant\MerchantConfig getConfig()
  */
 class MerchantCartPreCheckPlugin extends AbstractPlugin implements CartPreCheckPluginInterface
 {
-    protected const MESSAGE_TYPE_ERROR = 'error';
-
-    protected const GLOSSARY_KEY_INACTIVE_MERCHANT = 'merchant.message.inactive';
-    protected const GLOSSARY_KEY_REMOVED_MERCHANT = 'merchant.message.removed';
-
-    protected const GLOSSARY_PARAM_SKU = '%sku%';
-    protected const GLOSSARY_PARAM_MERCHANT_NAME = '%merchant_name%';
-
     /**
      * {@inheritDoc}
      * - Checks if cart change transfer has items with inactive merchants.
@@ -42,70 +32,8 @@ class MerchantCartPreCheckPlugin extends AbstractPlugin implements CartPreCheckP
      */
     public function check(CartChangeTransfer $cartChangeTransfer): CartPreCheckResponseTransfer
     {
-        $messageTransfers = [];
-        $merchantTransfers = $this->getMerchantTransfersGroupedByMerchantReference($cartChangeTransfer);
-
-        foreach ($cartChangeTransfer->getItems() as $itemTransfer) {
-            if (!$itemTransfer->getMerchantReference()) {
-                continue;
-            }
-
-            if (!isset($merchantTransfers[$itemTransfer->getMerchantReference()])) {
-                $messageTransfers[] = (new MessageTransfer())
-                    ->setType(static::MESSAGE_TYPE_ERROR)
-                    ->setValue(static::GLOSSARY_KEY_REMOVED_MERCHANT)
-                    ->setParameters([static::GLOSSARY_PARAM_SKU => $itemTransfer->getSku()]);
-
-                continue;
-            }
-
-            $merchantTransfer = $merchantTransfers[$itemTransfer->getMerchantReference()];
-            if (!$merchantTransfer->getIsActive()) {
-                $messageTransfers[] = (new MessageTransfer())
-                    ->setType(static::MESSAGE_TYPE_ERROR)
-                    ->setValue(static::GLOSSARY_KEY_INACTIVE_MERCHANT)
-                    ->setParameters([
-                        static::GLOSSARY_PARAM_SKU => $itemTransfer->getSku(),
-                        static::GLOSSARY_PARAM_MERCHANT_NAME => $merchantTransfer->getName(),
-                    ]);
-            }
-        }
-
-        return (new CartPreCheckResponseTransfer())
-            ->setMessages(new ArrayObject($messageTransfers))
-            ->setIsSuccess(!$messageTransfers);
-    }
-
-    /**
-     * @param \Generated\Shared\Transfer\CartChangeTransfer $cartChangeTransfer
-     *
-     * @return array
-     */
-    protected function getMerchantTransfersGroupedByMerchantReference(CartChangeTransfer $cartChangeTransfer)
-    {
-        $merchantReferences = [];
-        $merchantTransfers = [];
-
-        foreach ($cartChangeTransfer->getItems() as $itemTransfer) {
-            if (!$itemTransfer->getMerchantReference()) {
-                continue;
-            }
-            $merchantReferences[] = $itemTransfer->getMerchantReference();
-        }
-
-        if (!$merchantReferences) {
-            return $merchantTransfers;
-        }
-
-        $merchantReferences = array_unique($merchantReferences);
-        $merchantCollectionTransfer = $this->getFacade()->get(
-            (new MerchantCriteriaTransfer())
-                ->setMerchantReferences($merchantReferences)
-        );
-        foreach ($merchantCollectionTransfer->getMerchants() as $merchantTransfer) {
-            $merchantTransfers[$merchantTransfer->getMerchantReference()] = $merchantTransfer;
-        }
-
-        return $merchantTransfers;
+        return $this->getFactory()
+            ->createMerchantCartValidator()
+            ->check($cartChangeTransfer);
     }
 }
--- Bundles/Merchant/src/Spryker/Zed/Merchant/Communication/Plugin/Checkout/MerchantCheckoutPreConditionPlugin.php
+++ Bundles/Merchant/src/Spryker/Zed/Merchant/Communication/Plugin/Checkout/MerchantCheckoutPreConditionPlugin.php
@@ -7,25 +7,20 @@
 
 namespace Spryker\Zed\Merchant\Communication\Plugin\Checkout;
 
-use ArrayObject;
-use Generated\Shared\Transfer\CheckoutErrorTransfer;
 use Generated\Shared\Transfer\CheckoutResponseTransfer;
-use Generated\Shared\Transfer\MerchantCriteriaTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
 use Spryker\Zed\CheckoutExtension\Dependency\Plugin\CheckoutPreConditionPluginInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
  * @method \Spryker\Zed\Merchant\Business\MerchantFacadeInterface getFacade()
+ * @method \Spryker\Zed\Merchant\Communication\MerchantCommunicationFactory getFactory()
  * @method \Spryker\Zed\Merchant\MerchantConfig getConfig()
  */
 class MerchantCheckoutPreConditionPlugin extends AbstractPlugin implements CheckoutPreConditionPluginInterface
 {
-    protected const GLOSSARY_KEY_INACTIVE_MERCHANT = 'merchant.message.inactive';
     protected const GLOSSARY_KEY_REMOVED_MERCHANT = 'merchant.message.removed';
-
     protected const GLOSSARY_PARAM_SKU = '%sku%';
-    protected const GLOSSARY_PARAM_MERCHANT_NAME = '%merchant_name%';
 
     /**
      * {@inheritDoc}
@@ -41,70 +36,8 @@ class MerchantCheckoutPreConditionPlugin extends AbstractPlugin implements Check
      */
     public function checkCondition(QuoteTransfer $quoteTransfer, CheckoutResponseTransfer $checkoutResponseTransfer): bool
     {
-        $checkoutErrorTransfers = [];
-        $merchantTransfers = $this->getMerchantTransfersGroupedByMerchantReference($quoteTransfer);
-
-        foreach ($quoteTransfer->getItems() as $itemTransfer) {
-            if (!$itemTransfer->getMerchantReference()) {
-                continue;
-            }
-
-            if (!isset($merchantTransfers[$itemTransfer->getMerchantReference()])) {
-                $checkoutErrorTransfers[] = (new CheckoutErrorTransfer())
-                    ->setMessage(static::GLOSSARY_KEY_REMOVED_MERCHANT)
-                    ->setParameters([static::GLOSSARY_PARAM_SKU => $itemTransfer->getSku()]);
-
-                continue;
-            }
-
-            $merchantTransfer = $merchantTransfers[$itemTransfer->getMerchantReference()];
-            if (!$merchantTransfer->getIsActive()) {
-                $checkoutErrorTransfers[] = (new CheckoutErrorTransfer())
-                    ->setMessage(static::GLOSSARY_KEY_INACTIVE_MERCHANT)
-                    ->setParameters([
-                        static::GLOSSARY_PARAM_SKU => $itemTransfer->getSku(),
-                        static::GLOSSARY_PARAM_MERCHANT_NAME => $merchantTransfer->getName(),
-                    ]);
-            }
-        }
-
-        $checkoutResponseTransfer
-            ->setIsSuccess(!$checkoutErrorTransfers)
-            ->setErrors(new ArrayObject($checkoutErrorTransfers));
-
-        return !$checkoutErrorTransfers;
-    }
-
-    /**
-     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
-     *
-     * @return array
-     */
-    protected function getMerchantTransfersGroupedByMerchantReference(QuoteTransfer $quoteTransfer)
-    {
-        $merchantReferences = [];
-        $merchantTransfers = [];
-
-        foreach ($quoteTransfer->getItems() as $itemTransfer) {
-            if (!$itemTransfer->getMerchantReference()) {
-                continue;
-            }
-            $merchantReferences[] = $itemTransfer->getMerchantReference();
-        }
-
-        if (!$merchantReferences) {
-            return $merchantTransfers;
-        }
-
-        $merchantReferences = array_unique($merchantReferences);
-        $merchantCollectionTransfer = $this->getFacade()->get(
-            (new MerchantCriteriaTransfer())
-                ->setMerchantReferences($merchantReferences)
-        );
-        foreach ($merchantCollectionTransfer->getMerchants() as $merchantTransfer) {
-            $merchantTransfers[$merchantTransfer->getMerchantReference()] = $merchantTransfer;
-        }
-
-        return $merchantTransfers;
+        return !$this->getFactory()
+            ->createMerchantCheckoutValidator()
+            ->checkCondition($quoteTransfer, $checkoutResponseTransfer);
     }
 }
