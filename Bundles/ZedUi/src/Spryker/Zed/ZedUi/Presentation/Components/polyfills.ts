e3e989f862015a102e0fec2102c6f9fb9a38f70a
Aleksey Belan <alexey.belan@spryker.com>
2020-04-29 16:50:35 +0300

    BO-671: Added global-this polyfill

--- Bundles/ZedUi/src/Spryker/Zed/ZedUi/Presentation/Components/polyfills.ts
+++ Bundles/ZedUi/src/Spryker/Zed/ZedUi/Presentation/Components/polyfills.ts
@@ -66,6 +66,7 @@ import 'core-js/features/reflect';
 import 'core-js/features/array/flat';
 import 'core-js/features/array/includes';
 import 'core-js/features/object/entries';
+import 'core-js/es/global-this';
 import cssVars from 'css-vars-ponyfill';
 import 'url-search-params-polyfill';
 
