8730f65b4c3186aefdb6872e083123b3d23897c9
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2020-05-08 18:57:56 +0300

    MR-139 cs fixes.

--- Bundles/MerchantSalesOrder/src/Spryker/Zed/MerchantSalesOrder/Business/Expander/OrderExpander.php
+++ Bundles/MerchantSalesOrder/src/Spryker/Zed/MerchantSalesOrder/Business/Expander/OrderExpander.php
@@ -46,6 +46,7 @@ class OrderExpander implements OrderExpanderInterface
 
         return $orderTransfer;
     }
+
     /**
      * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
      *
