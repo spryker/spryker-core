c47d4a823e75159e58e283506b956b6725e1175a
Dima Aseev <dmitriy.aseev@spryker.com>
2020-05-14 15:39:07 +0300

    PS-13398: Added handler and twig skeleton.

--- Bundles/ProductBundle/src/Spryker/Zed/ProductBundle/Communication/Form/DataProvider/ProductBundleReturnCreateFormDataProvider.php
+++ Bundles/ProductBundle/src/Spryker/Zed/ProductBundle/Communication/Form/DataProvider/ProductBundleReturnCreateFormDataProvider.php
@@ -12,10 +12,26 @@ use Generated\Shared\Transfer\ReturnItemTransfer;
 
 class ProductBundleReturnCreateFormDataProvider
 {
+    /**
+     * @uses \Spryker\Zed\SalesReturnGui\Communication\Form\ReturnCreateForm::FIELD_RETURN_ITEMS
+     */
     public const FIELD_RETURN_ITEMS = 'returnItems';
     public const FIELD_RETURN_BUNDLE_ITEMS = 'returnBundleItems';
 
+    /**
+     * @uses \Spryker\Zed\SalesReturnGui\Communication\Form\ReturnCreateForm::OPTION_RETURN_REASONS
+     */
     public const OPTION_RETURN_REASONS = 'option_return_reasons';
+
+    /**
+     * @uses \Spryker\Client\ProductBundle\Grouper\ProductBundleGrouper::BUNDLE_ITEMS
+     */
+    public const BUNDLE_ITEMS = 'bundleItems';
+
+    /**
+     * @uses \Spryker\Client\ProductBundle\Grouper\ProductBundleGrouper::BUNDLE_PRODUCT
+     */
+    public const BUNDLE_PRODUCT = 'bundleProduct';
 
     /**
      * @param array $returnCreateFormData
@@ -26,11 +42,16 @@ class ProductBundleReturnCreateFormDataProvider
     public function expandData(array $returnCreateFormData, OrderTransfer $orderTransfer): array
     {
         $returnCreateFormData = $this->excludeBundleItems($returnCreateFormData);
-//        $returnCreateFormData = $this->addBundleItems($returnCreateFormData);
+        $returnCreateFormData = $this->addReturnBundleItems($returnCreateFormData, $orderTransfer);
 
         return $returnCreateFormData;
     }
 
+    /**
+     * @param array $returnCreateFormData
+     *
+     * @return array
+     */
     protected function excludeBundleItems(array $returnCreateFormData): array
     {
         $filteredReturnItems = [];
@@ -50,6 +71,39 @@ class ProductBundleReturnCreateFormDataProvider
         }
 
         $returnCreateFormData[static::FIELD_RETURN_ITEMS] = $filteredReturnItems;
+
+        return $returnCreateFormData;
+    }
+
+    /**
+     * @param array $returnCreateFormData
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     *
+     * @return array
+     */
+    public function addReturnBundleItems(array $returnCreateFormData, OrderTransfer $orderTransfer): array
+    {
+        $returnBundleItems = [];
+
+        foreach ($orderTransfer->getItems() as $itemTransfer) {
+            $relatedBundleItemIdentifier = $itemTransfer->getRelatedBundleItemIdentifier();
+            $productBundle = $itemTransfer->getProductBundle();
+
+            if (!$relatedBundleItemIdentifier || !$productBundle) {
+                continue;
+            }
+
+            if (!isset($returnBundleItems[$relatedBundleItemIdentifier])) {
+                $returnBundleItems[$relatedBundleItemIdentifier] = [
+                    static::BUNDLE_PRODUCT => $productBundle,
+                    static::BUNDLE_ITEMS => [],
+                ];
+            }
+
+            $returnBundleItems[$relatedBundleItemIdentifier][static::BUNDLE_ITEMS][] = $itemTransfer;
+        }
+
+        $returnCreateFormData[static::FIELD_RETURN_BUNDLE_ITEMS] = $returnBundleItems;
 
         return $returnCreateFormData;
     }
