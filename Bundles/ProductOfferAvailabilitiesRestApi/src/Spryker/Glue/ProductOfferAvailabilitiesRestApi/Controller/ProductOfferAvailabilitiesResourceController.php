5a4796aeb0f277c2cf3632b58448e7f88d7329ae
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2020-05-22 01:09:36 +0300

    MP-2537 Provide product-offer-availabilities resource route.

---
+++ Bundles/ProductOfferAvailabilitiesRestApi/src/Spryker/Glue/ProductOfferAvailabilitiesRestApi/Controller/ProductOfferAvailabilitiesResourceController.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\ProductOfferAvailabilitiesRestApi\Controller;
+
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface;
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+use Spryker\Glue\Kernel\Controller\AbstractController;
+
+/**
+ * @method \Spryker\Glue\ProductOfferAvailabilitiesRestApi\ProductOfferAvailabilitiesRestApiFactory getFactory()
+ */
+class ProductOfferAvailabilitiesResourceController extends AbstractController
+{
+    /**
+     * @Glue({
+     *     "getCollection": {
+     *          "summary": [
+     *              "Retrieves collection of product-offer-availabilities."
+     *          ],
+     *          "parameters": [{
+     *              "name": "Accept-Language",
+     *              "in": "header"
+     *          }],
+     *          "responses": {
+     *              "400": "Concrete product id is not specified."
+     *          }
+     *     }
+     * })
+     *
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface
+     */
+    public function getAction(RestRequestInterface $restRequest): RestResponseInterface
+    {
+        return $this->getFactory()
+            ->createProductOfferAvailabilityReader()
+            ->getProductOfferAvailability($restRequest);
+    }
+}
