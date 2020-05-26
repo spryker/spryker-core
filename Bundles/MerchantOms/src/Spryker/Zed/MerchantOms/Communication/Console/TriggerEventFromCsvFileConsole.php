c8f469f8d796c60c5f5918bc7dba4f7b44b949bb
Alexander <alexander.veselov@spryker.com>
2020-05-26 15:48:31 +0000

    MR-313: Fixed TriggerEventFromCsvFileConsole

--- Bundles/MerchantOms/src/Spryker/Zed/MerchantOms/Communication/Console/TriggerEventFromCsvFileConsole.php
+++ Bundles/MerchantOms/src/Spryker/Zed/MerchantOms/Communication/Console/TriggerEventFromCsvFileConsole.php
@@ -8,7 +8,6 @@
 namespace Spryker\Zed\MerchantOms\Communication\Console;
 
 use Generated\Shared\Transfer\MerchantOmsTriggerRequestTransfer;
-use Generated\Shared\Transfer\MerchantOmsTriggerResponseTransfer;
 use Spryker\Zed\Kernel\Communication\Console\Console;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
@@ -34,6 +33,7 @@ class TriggerEventFromCsvFileConsole extends Console
     protected const TABLE_HEADER_COLUMN_MERCHANT_ORDER_ITEM_REFERENCE = 'merchant_order_item_reference';
     protected const TABLE_HEADER_COLUMN_MERCHANT_ORDER_ITEM_OMS_EVENT_STATE = 'merchant_order_item_oms_event_state';
     protected const TABLE_HEADER_COLUMN_RESULT = 'result';
+    protected const TABLE_HEADER_COLUMN_MESSAGE = 'message';
 
     /**
      * @var \Symfony\Component\Console\Output\ConsoleOutputInterface
@@ -121,38 +121,35 @@ class TriggerEventFromCsvFileConsole extends Console
         $successfullyProcessedRowsCount = 0;
         $rowNumber = $this->getStartFromOption();
 
-        try {
-            while ($rowNumber < $totalRowsCount) {
+        while ($rowNumber < $totalRowsCount) {
                 $rowData = $csvReader->read();
                 $rowNumber++;
 
                 $merchantOmsTriggerRequestTransfer = (new MerchantOmsTriggerRequestTransfer())
                     ->setMerchantOrderItemReference($rowData[static::TABLE_HEADER_COLUMN_MERCHANT_ORDER_ITEM_REFERENCE])
                     ->setMerchantOmsEventName($rowData[static::TABLE_HEADER_COLUMN_MERCHANT_ORDER_ITEM_OMS_EVENT_STATE]);
-
+            try {
                 $merchantOmsTriggerResponseTransfer = $this->getFacade()->triggerEventForMerchantOrderItem($merchantOmsTriggerRequestTransfer);
 
                 if ($merchantOmsTriggerResponseTransfer->getIsSuccessful()) {
                     $successfullyProcessedRowsCount++;
                 }
-
                 $this->logOutput(
                     $rowNumber,
-                    $successfullyProcessedRowsCount,
-                    $totalRowsCount,
                     $merchantOmsTriggerRequestTransfer,
-                    $merchantOmsTriggerResponseTransfer
+                    $merchantOmsTriggerResponseTransfer->getIsSuccessful()
                 );
-
-                if (!$merchantOmsTriggerResponseTransfer->getIsSuccessful() && !$input->getOption(static::OPTION_IGNORE_ERRORS)) {
-                    return static::CODE_ERROR;
-                }
+            } catch (Throwable $exception) {
+                $this->logOutput(
+                    $rowNumber,
+                    $merchantOmsTriggerRequestTransfer,
+                    false,
+                    $exception->getMessage()
+                );
             }
-        } catch (Throwable $exception) {
-            $this->error($exception->getMessage());
-
-            return static::CODE_ERROR;
         }
+
+        $this->info(sprintf('Rows processed: %s/%s', $successfullyProcessedRowsCount, $totalRowsCount));
 
         return static::CODE_SUCCESS;
     }
@@ -197,7 +194,9 @@ class TriggerEventFromCsvFileConsole extends Console
             static::TABLE_HEADER_COLUMN_MERCHANT_ORDER_ITEM_REFERENCE,
             static::TABLE_HEADER_COLUMN_MERCHANT_ORDER_ITEM_OMS_EVENT_STATE,
             static::TABLE_HEADER_COLUMN_RESULT,
+            static::TABLE_HEADER_COLUMN_MESSAGE,
         ]);
+
         $table->render();
 
         $this->outputTable = $table;
@@ -205,37 +204,28 @@ class TriggerEventFromCsvFileConsole extends Console
 
     /**
      * @param int $rowNumber
-     * @param int $successfullyProcessedRowsCount
-     * @param int $totalRowsCount
      * @param \Generated\Shared\Transfer\MerchantOmsTriggerRequestTransfer $merchantOmsTriggerRequestTransfer
-     * @param \Generated\Shared\Transfer\MerchantOmsTriggerResponseTransfer $merchantOmsTriggerResponseTransfer
+     * @param bool $result
+     * @param string|null $message
      *
      * @return void
      */
     protected function logOutput(
         int $rowNumber,
-        int $successfullyProcessedRowsCount,
-        int $totalRowsCount,
         MerchantOmsTriggerRequestTransfer $merchantOmsTriggerRequestTransfer,
-        MerchantOmsTriggerResponseTransfer $merchantOmsTriggerResponseTransfer
+        bool $result,
+        ?string $message = null
     ): void {
         if ($this->output->isVerbose()) {
             $this->outputTable->appendRow([
                 $rowNumber,
                 $merchantOmsTriggerRequestTransfer->getMerchantOrderItemReference(),
                 $merchantOmsTriggerRequestTransfer->getMerchantOmsEventName(),
-                $merchantOmsTriggerResponseTransfer->getMessage(),
+                $result ? 'success' : 'fail',
+                $message,
             ]);
 
             return;
         }
-
-        if ($merchantOmsTriggerResponseTransfer->getIsSuccessful()) {
-            $this->info(sprintf('Rows processed: %s/%s', $successfullyProcessedRowsCount, $totalRowsCount));
-
-            return;
-        }
-
-        $this->error($merchantOmsTriggerResponseTransfer->getMessage());
     }
 }
