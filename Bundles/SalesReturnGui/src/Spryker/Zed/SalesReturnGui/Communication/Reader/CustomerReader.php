ba3bbea35c354b0eca518110525da35be7c88b05
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2020-06-01 16:24:41 +0300

    PS-13533 Adjusted fix

--- Bundles/SalesReturnGui/src/Spryker/Zed/SalesReturnGui/Communication/Reader/CustomerReader.php
+++ Bundles/SalesReturnGui/src/Spryker/Zed/SalesReturnGui/Communication/Reader/CustomerReader.php
@@ -39,14 +39,14 @@ class CustomerReader implements CustomerReaderInterface
     /**
      * @param \Generated\Shared\Transfer\ReturnTransfer $returnTransfer
      *
-     * @return \Generated\Shared\Transfer\CustomerTransfer|null
+     * @return \Generated\Shared\Transfer\CustomerTransfer
      */
-    public function findCustomerFromReturn(ReturnTransfer $returnTransfer): ?CustomerTransfer
+    public function getCustomerFromReturn(ReturnTransfer $returnTransfer): CustomerTransfer
     {
         $customerReference = $returnTransfer->getCustomerReference();
 
         if (!$customerReference) {
-            return $this->getGuestCustomerFromOrder($returnTransfer);
+            return $this->getCustomerFromOrder($returnTransfer);
         }
 
         $customerTransfer = $this->customerFacade
@@ -54,7 +54,7 @@ class CustomerReader implements CustomerReaderInterface
             ->getCustomerTransfer();
 
         if (!$customerTransfer) {
-            return $this->getGuestCustomerFromOrder($returnTransfer);
+            return $this->getCustomerFromOrder($returnTransfer);
         }
 
         return $customerTransfer;
@@ -65,7 +65,7 @@ class CustomerReader implements CustomerReaderInterface
      *
      * @return \Generated\Shared\Transfer\CustomerTransfer
      */
-    protected function getGuestCustomerFromOrder(ReturnTransfer $returnTransfer): CustomerTransfer
+    protected function getCustomerFromOrder(ReturnTransfer $returnTransfer): CustomerTransfer
     {
         $idSalesOrder = $returnTransfer->getReturnItems()
             ->getIterator()
