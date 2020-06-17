e4ebd6d75226d8b01077abd7defb515dadfa909d
Sergey Samoylov <sergey.samoylov@spryker.com>
2020-06-17 16:33:34 +0300

    mp-2690: create client layer for merchant product storage

---
+++ Bundles/MerchantProductStorage/src/Spryker/Client/MerchantProductStorage/MerchantProductStorageClientInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\MerchantProductStorage;
+
+use Generated\Shared\Transfer\MerchantProductStorageTransfer;
+
+interface MerchantProductStorageClientInterface
+{
+    public function findOne(int $idProductAbstract): MerchantProductStorageTransfer;
+}
