3ad0bbe7978824c98a37d0edc4c64cbced3abfc4
artemkosatov <artem.kosatov@spryker.com>
2020-06-22 18:41:01 +0300

    MP-2948: Added tests.

---
+++ Bundles/MerchantProductSearch/tests/SprykerTest/Zed/MerchantProductSearch/Communication/Plugin/Publisher/MerchantProduct/MerchantProductSearchWritePublisherPluginTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\MerchantProductSearch\Communication\Plugin\Publisher\MerchantProduct;
+
+use Codeception\Test\Unit;
+use Generated\Shared\DataBuilder\StoreRelationBuilder;
+use Generated\Shared\Transfer\EventEntityTransfer;
+use Generated\Shared\Transfer\MerchantTransfer;
+use Generated\Shared\Transfer\StoreRelationTransfer;
+use Generated\Shared\Transfer\StoreTransfer;
+use Spryker\Zed\MerchantProduct\Dependency\MerchantProductEvents;
+use Spryker\Zed\MerchantProductSearch\Communication\Plugin\Publisher\MerchantProduct\MerchantProductSearchWritePublisherPlugin;
+
+/**
+ * Auto-generated group annotations
+ *
+ * @group SprykerTest
+ * @group Zed
+ * @group MerchantProductSearch
+ * @group Communication
+ * @group Plugin
+ * @group Publisher
+ * @group MerchantProduct
+ * @group MerchantProductSearchWritePublisherPluginTest
+ * Add your own group annotations below this line
+ */
+class MerchantProductSearchWritePublisherPluginTest extends Unit
+{
+    /**
+     * @var \SprykerTest\Zed\MerchantProductSearch\MerchantProductSearchCommunicationTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->tester->addDependencies();
+    }
+
+    /**
+     * @return void
+     */
+    public function testMerchantProductSearchWritePublisherPluginStoresData(): void
+    {
+        // Arrange
+        $this->tester->ensureMerchantProductAbstractTableIsEmpty();
+        $beforeCount = $this->tester->getProductAbstractPageSearchPropelQuery()->count();
+
+        $storeTransfer = $this->tester->haveStore([StoreTransfer::NAME => 'DE']);
+        $storeRelationTransfer = (new StoreRelationBuilder())->seed([
+            StoreRelationTransfer::ID_STORES => [$storeTransfer->getIdStore()],
+        ])->build();
+        $productConcreteTransfer = $this->tester->haveProduct();
+        $merchantTransfer = $this->tester->haveMerchant([MerchantTransfer::IS_ACTIVE => true, MerchantTransfer::STORE_RELATION => $storeRelationTransfer->toArray()]);
+        $this->tester->saveMerchantProduct($merchantTransfer, $productConcreteTransfer);
+        $this->tester->addProductRelatedData($productConcreteTransfer);
+
+        $merchantProductSearchWritePublisher = new MerchantProductSearchWritePublisherPlugin();
+        $eventTransfers = [
+            (new EventEntityTransfer())->setId($merchantTransfer->getIdMerchant()),
+        ];
+
+        // Act
+        $merchantProductSearchWritePublisher->handleBulk($eventTransfers, MerchantProductEvents::MERCHANT_PRODUCT_ABSTRACT_PUBLISH);
+        $afterCount = $this->tester->getProductAbstractPageSearchPropelQuery()->count();
+
+        // Assert
+        $this->assertGreaterThan($beforeCount, $afterCount);
+        $this->tester->assertProductPageAbstractSearch($merchantTransfer, $productConcreteTransfer);
+    }
+}
