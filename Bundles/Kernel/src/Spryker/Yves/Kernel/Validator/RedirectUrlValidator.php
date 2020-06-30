31fa3d3260402383ea7f4eae64025fc2bbeb2642
Aleksey Kotsuba <aleksey.kotsuba@gmail.com>
2020-06-30 15:04:46 +0300

    TE-4195: Fixed whitelist behavior to not check is strict mode is turned off.

--- Bundles/Kernel/src/Spryker/Yves/Kernel/Validator/RedirectUrlValidator.php
+++ Bundles/Kernel/src/Spryker/Yves/Kernel/Validator/RedirectUrlValidator.php
@@ -31,6 +31,10 @@ class RedirectUrlValidator implements RedirectUrlValidatorInterface
             return;
         }
 
+        if (!Config::get(KernelConstants::STRICT_DOMAIN_REDIRECT, false)) {
+            return;
+        }
+
         $redirectUrl = $response->headers->get(static::HTTP_HEADER_LOCATION);
         $domain = (string)parse_url($redirectUrl, PHP_URL_HOST);
 
@@ -54,12 +58,6 @@ class RedirectUrlValidator implements RedirectUrlValidatorInterface
             return true;
         }
 
-        $allowedDomains = Config::get(KernelConstants::DOMAIN_WHITELIST, []);
-
-        if (empty($allowedDomains)) {
-            return !Config::get(KernelConstants::STRICT_DOMAIN_REDIRECT, false);
-        }
-
-        return in_array($domain, $allowedDomains, true);
+        return in_array($domain, Config::get(KernelConstants::DOMAIN_WHITELIST, []), true);
     }
 }
