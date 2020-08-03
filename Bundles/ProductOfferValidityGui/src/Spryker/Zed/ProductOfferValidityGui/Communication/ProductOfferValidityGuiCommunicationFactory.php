47ade2ac43a5fa9c5da0379c92d57d7375df1eb3
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2020-08-03 13:03:41 +0300

    MP-3277: removed unused code

--- Bundles/ProductOfferValidityGui/src/Spryker/Zed/ProductOfferValidityGui/Communication/ProductOfferValidityGuiCommunicationFactory.php
+++ Bundles/ProductOfferValidityGui/src/Spryker/Zed/ProductOfferValidityGui/Communication/ProductOfferValidityGuiCommunicationFactory.php
@@ -8,19 +8,10 @@
 namespace Spryker\Zed\ProductOfferValidityGui\Communication;
 
 use Spryker\Zed\Kernel\Communication\AbstractCommunicationFactory;
-use Spryker\Zed\ProductOfferValidityGui\Dependency\Facade\ProductOfferValidityGuiToProductOfferValidityFacadeInterface;
-use Spryker\Zed\ProductOfferValidityGui\ProductOfferValidityGuiDependencyProvider;
 
 /**
  * @method \Spryker\Zed\ProductOfferValidityGui\ProductOfferValidityGuiConfig getConfig()
  */
 class ProductOfferValidityGuiCommunicationFactory extends AbstractCommunicationFactory
 {
-    /**
-     * @return \Spryker\Zed\ProductOfferValidityGui\Dependency\Facade\ProductOfferValidityGuiToProductOfferValidityFacadeInterface
-     */
-    public function getProductOfferValidityFacade(): ProductOfferValidityGuiToProductOfferValidityFacadeInterface
-    {
-        return $this->getProvidedDependency(ProductOfferValidityGuiDependencyProvider::FACADE_PRODUCT_OFFER_VALIDITY);
-    }
 }
--- Bundles/ProductOfferValidityGui/src/Spryker/Zed/ProductOfferValidityGui/ProductOfferValidityGuiDependencyProvider.php
+++ Bundles/ProductOfferValidityGui/src/Spryker/Zed/ProductOfferValidityGui/ProductOfferValidityGuiDependencyProvider.php
@@ -8,43 +8,10 @@
 namespace Spryker\Zed\ProductOfferValidityGui;
 
 use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
-use Spryker\Zed\Kernel\Container;
-use Spryker\Zed\ProductOfferValidityGui\Dependency\Facade\ProductOfferValidityGuiToProductOfferValidityFacadeBridge;
 
 /**
  * @method \Spryker\Zed\ProductOfferValidityGui\ProductOfferValidityGuiConfig getConfig()
  */
 class ProductOfferValidityGuiDependencyProvider extends AbstractBundleDependencyProvider
 {
-    public const FACADE_PRODUCT_OFFER_VALIDITY = 'FACADE_PRODUCT_OFFER_VALIDITY';
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
-    public function provideCommunicationLayerDependencies(Container $container): Container
-    {
-        $container = parent::provideCommunicationLayerDependencies($container);
-
-        $container = $this->addProductOfferValidityFacade($container);
-
-        return $container;
-    }
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
-    protected function addProductOfferValidityFacade(Container $container): Container
-    {
-        $container->set(static::FACADE_PRODUCT_OFFER_VALIDITY, function (Container $container) {
-            return new ProductOfferValidityGuiToProductOfferValidityFacadeBridge(
-                $container->getLocator()->productOfferValidity()->facade()
-            );
-        });
-
-        return $container;
-    }
 }
