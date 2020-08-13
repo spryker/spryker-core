503451c21b7c5184cefd69eabca6e3fb0cfb0e30
René Klatt <rene.klatt@spryker.com>
2020-08-13 15:05:21 +0000

    TE-7001 Use ReflectionProperty to reset the Container.

--- Bundles/Container/tests/SprykerTest/Service/Container/_support/Helper/ContainerHelper.php
+++ Bundles/Container/tests/SprykerTest/Service/Container/_support/Helper/ContainerHelper.php
@@ -9,8 +9,8 @@ namespace SprykerTest\Service\Container\Helper;
 
 use Codeception\Module;
 use Codeception\TestInterface;
-use ReflectionClass;
 use ReflectionProperty;
+use Spryker\Service\Container\Container;
 use Spryker\Service\Container\ContainerInterface;
 use Spryker\Shared\Kernel\Container\ContainerProxy;
 
@@ -61,9 +61,14 @@ class ContainerHelper extends Module
      */
     protected function resetStaticProperties(): void
     {
-        $reflectedClass = new ReflectionClass($this->container);
+        $staticProperties = [
+            'globalServices',
+            'globalServiceIdentifier',
+            'globalFrozenServices',
+        ];
 
-        foreach ($reflectedClass->getProperties(ReflectionProperty::IS_STATIC) as $reflectedProperty) {
+        foreach ($staticProperties as $staticProperty) {
+            $reflectedProperty = new ReflectionProperty(Container::class, $staticProperty);
             $reflectedProperty->setAccessible(true);
             $reflectedProperty->setValue([]);
         }
