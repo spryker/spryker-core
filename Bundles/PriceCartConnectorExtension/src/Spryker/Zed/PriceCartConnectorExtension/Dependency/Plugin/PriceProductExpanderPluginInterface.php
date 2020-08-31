6ec1c2922d4c694b66df5bbc93107ff94e6a5d5a
denys.sokolov <denys.sokolov@spryker.com>
2020-08-31 10:06:59 +0300

    СС-9231: Introduced PriceCartConnectorExtension module.

---
+++ Bundles/PriceCartConnectorExtension/src/Spryker/Zed/PriceCartConnectorExtension/Dependency/Plugin/PriceProductExpanderPluginInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\PriceCartConnectorExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\CartChangeTransfer;
+
+/**
+ * Use this plugin to expand the list of the product prices with additional ones base on the data from the cart.
+ */
+interface PriceProductExpanderPluginInterface
+{
+    /**
+     * Specification:
+     * - Expands the list of product price transfers with additional ones based on the data from the cart.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\PriceProductTransfer[] $priceProductTransfers
+     * @param \Generated\Shared\Transfer\CartChangeTransfer $cartChangeTransfer
+     *
+     * @return \Generated\Shared\Transfer\PriceProductTransfer[]
+     */
+    public function expandPriceProductTransfers(
+        array $priceProductTransfers,
+        CartChangeTransfer $cartChangeTransfer
+    ): array;
+}
