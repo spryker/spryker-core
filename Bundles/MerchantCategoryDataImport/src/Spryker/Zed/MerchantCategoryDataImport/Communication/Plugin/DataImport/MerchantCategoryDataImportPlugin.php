f3621dbceacae9b272936fed533b632f3b1f7b42
Sergey Samoylov <sergey.samoylov@spryker.com>
2020-09-14 17:57:18 +0300

    mp-3309: fix namespace

--- Bundles/MerchantCategoryDataImport/src/Spryker/Zed/MerchantCategoryDataImport/Communication/Plugin/DataImport/MerchantCategoryDataImportPlugin.php
+++ Bundles/MerchantCategoryDataImport/src/Spryker/Zed/MerchantCategoryDataImport/Communication/Plugin/DataImport/MerchantCategoryDataImportPlugin.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace Spryker\Zed\MerchantCategoryDataImport\Communication\Plugin;
+namespace Spryker\Zed\MerchantCategoryDataImport\Communication\Plugin\DataImport;
 
 use Generated\Shared\Transfer\DataImporterConfigurationTransfer;
 use Generated\Shared\Transfer\DataImporterReportTransfer;
