1ecc1e884b0837b03ee9bed7cc2f62dd014b9fb6
pivovarov <stanislav.pivovarov@spryker.com>
2020-09-16 11:03:33 +0300

    CC-10509 As Peter, I should see which product is a configurable product in the back office

---
+++ Bundles/ProductConfigurationGui/src/Spryker/Zed/ProductConfigurationGui/Communication/Plugin/ProductManagement/ProductConfigurationTableDataExpanderPlugin.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductConfigurationGui\Communication\Plugin\ProductManagement;
+
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\ProductManagementExtension\Dependency\Plugin\ProductTableDataExpanderPluginInterface;
+
+/**
+ * @method \Spryker\Zed\ProductConfigurationGui\Communication\ProductConfigurationGuiCommunicationFactory getFactory()
+ */
+class ProductConfigurationTableDataExpanderPlugin extends AbstractPlugin implements ProductTableDataExpanderPluginInterface
+{
+    /**
+     * {@inheritDoc}
+     * Specification:
+     * - Checks is product abstract has at least one product concrete with product configuration.
+     * - Expands product items with configurable product type if has or do nothing otherwise.
+     *
+     * @api
+     *
+     * @param array $item
+     *
+     * @return array
+     */
+    public function expand(array $item): array
+    {
+        return $this->getFactory()
+                ->createProductConfigurationTableDataExpander()->expandProductItemWithProductConfigurationType($item);
+    }
+}
---
+++ Bundles/ProductConfigurationGui/src/Spryker/Zed/ProductConfigurationGui/Persistence/ProductConfigurationGuiRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductConfigurationGui\Persistence;
+
+use Generated\Shared\Transfer\ProductConfigurationAggregationTransfer;
+use Orm\Zed\ConfigurableBundle\Persistence\Map\SpyProductConfigurationTableMap;
+use Orm\Zed\Product\Persistence\Map\SpyProductTableMap;
+use Spryker\Zed\Kernel\Persistence\AbstractRepository;
+
+/**
+ * @method \Spryker\Zed\ProductConfigurationGui\Persistence\ProductConfigurationGuiPersistenceFactory getFactory()
+ */
+class ProductConfigurationGuiRepository extends AbstractRepository implements ProductConfigurationGuiRepositoryInterface
+{
+    /** @uses \Orm\Zed\Product\Persistence\Map\SpyProductTableMap::COL_ID_PRODUCT  */
+    protected const COL_ID_PRODUCT = 'spy_product.id_product';
+
+    /** @uses \Orm\Zed\Product\Persistence\Map\SpyProductTableMap::COL_FK_PRODUCT_ABSTRACT  */
+    protected const COL_FK_PRODUCT_ABSTRACT = 'spy_product.fk_product_abstract';
+
+    /** @uses \Orm\Zed\ConfigurableBundle\Persistence\Map\SpyProductConfigurationTableMap::COL_ID_PRODUCT_CONFIGURATION */
+    protected  const COL_ID_PRODUCT_CONFIGURATION = 'spy_product_configuration.id_product_configuration';
+
+    /**
+     * @param string $abstractProductSku
+     *
+     * @return \Generated\Shared\Transfer\ProductConfigurationAggregationTransfer|null
+     */
+    public function findProductConfigurationAggregation(
+        string $abstractProductSku
+    ): ?ProductConfigurationAggregationTransfer {
+        $productConfigurationAggregationData = $this->getFactory()->createProductConfigurationQuery()
+            ->useSpyProductQuery()
+                ->useSpyProductAbstractQuery()
+                    ->filterBySku($abstractProductSku)
+                ->endUse()
+            ->endUse()
+            ->withColumn(
+                sprintf('COUNT(%s)', static::COL_ID_PRODUCT_CONFIGURATION),
+                ProductConfigurationAggregationTransfer::PRODUCT_CONFIGURATION_COUNT
+            )
+            ->withColumn(
+                sprintf('COUNT(%s)', static::COL_ID_PRODUCT),
+                ProductConfigurationAggregationTransfer::PRODUCT_CONCRETE_COUNT
+            )
+            ->select([
+                ProductConfigurationAggregationTransfer::PRODUCT_CONCRETE_COUNT,
+                ProductConfigurationAggregationTransfer::PRODUCT_CONFIGURATION_COUNT,
+            ])
+            ->groupBy(static::COL_FK_PRODUCT_ABSTRACT)->findOne();
+
+        if (empty($productConfigurationAggregationData)) {
+            return null;
+        }
+
+        /** @var array $productConfigurationAggregationData */
+        return (new ProductConfigurationAggregationTransfer())->fromArray($productConfigurationAggregationData);
+    }
+}
