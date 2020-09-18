5e46e2fc3aee2cace425a80dfd34de93b8d203f2
pivovarov <stanislav.pivovarov@spryker.com>
2020-09-18 17:41:21 +0300

    handle comments

--- Bundles/SalesProductConfigurationGui/src/Spryker/Zed/SalesProductConfigurationGui/Communication/Controller/SalesController.php
+++ Bundles/SalesProductConfigurationGui/src/Spryker/Zed/SalesProductConfigurationGui/Communication/Controller/SalesController.php
@@ -18,9 +18,9 @@ class SalesController extends AbstractController
     /**
      * @param \Symfony\Component\HttpFoundation\Request $request
      *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|array
+     * @return array
      */
-    public function orderItemProductConfigurationAction(Request $request)
+    public function orderItemProductConfigurationAction(Request $request): array
     {
         /** @var \Generated\Shared\Transfer\ItemTransfer|null $itemTransfer */
         $itemTransfer = $request->attributes->get('orderItem');
