fd4684576da4833ea67a980a7e1b55d579cfc01b
pivovarov <stanislav.pivovarov@spryker.com>
2020-09-18 17:36:48 +0300

    begin test

---
+++ Bundles/ProductConfiguration/tests/SprykerTest/Client/ProductConfiguration/Client/ProductConfigurationClientTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\ProductConfiguration\Business;
+
+use Codeception\Test\Unit;
+
+/**
+ * Auto-generated group annotations
+ *
+ * @group SprykerTest
+ * @group Client
+ * @group ProductConfiguration
+ * @group ProductConfigurationClientTest
+ * Add your own group annotations below this line
+ */
+class ProductConfigurationClientTest extends Unit
+{
+    /**
+     * @var \SprykerTest\Client\ProductConfiguration\ProductConfigurationClientTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testPrepareProductConfiguratorRedirect(): void
+    {
+
+    }
+}
