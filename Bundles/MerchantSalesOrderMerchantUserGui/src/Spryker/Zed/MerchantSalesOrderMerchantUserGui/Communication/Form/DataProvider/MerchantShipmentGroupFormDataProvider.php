189cada31fbc283dc4d932067f1e892b6c7c1a0d
Sergey Samoylov <sergey.samoylov@spryker.com>
2020-10-02 10:43:30 +0300

    mr-911: fixed breadcrumbs, fixed state column, fixed form

--- Bundles/MerchantSalesOrderMerchantUserGui/src/Spryker/Zed/MerchantSalesOrderMerchantUserGui/Communication/Form/DataProvider/MerchantShipmentGroupFormDataProvider.php
+++ Bundles/MerchantSalesOrderMerchantUserGui/src/Spryker/Zed/MerchantSalesOrderMerchantUserGui/Communication/Form/DataProvider/MerchantShipmentGroupFormDataProvider.php
@@ -193,6 +193,10 @@ class MerchantShipmentGroupFormDataProvider
                 continue;
             }
 
+            if ($shipmentTransfer->getIdSalesShipment() !== $salesOrderItemTransfer->getShipment()->getIdSalesShipment()) {
+                continue;
+            }
+
             $itemsIds[] = $salesOrderItemTransfer->getIdSalesOrderItem();
         }
 
