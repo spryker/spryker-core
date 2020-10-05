09a28bddc67610e3162ea317e62503022dd5efdb
Dima Aseev <dmitriy.aseev@spryker.com>
2020-10-05 17:05:22 +0300

    CC-10910: Fixed namespace.

--- Bundles/ProductAttributesRestApi/src/Spryker/Glue/ProductAttributesRestApi/Plugin/GlueApplication/ProductManagementAttributesResourceRoutePlugin.php
+++ Bundles/ProductAttributesRestApi/src/Spryker/Glue/ProductAttributesRestApi/Plugin/GlueApplication/ProductManagementAttributesResourceRoutePlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Glue\ProductAttributesRestApi\Plugin;
+namespace Spryker\Glue\ProductAttributesRestApi\Plugin\GlueApplication;
 
 use Generated\Shared\Transfer\RestProductManagementAttributeAttributesTransfer;
 use Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRouteCollectionInterface;
