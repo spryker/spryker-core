e8dee739a631bb5e5a7862ac25c5db6e26b116e9
Bezpiatov <aleksandr.bezpiatov@spryker.com>
2020-10-06 11:27:28 +0300

    TE-7283 WebDriverHelper is now BC, removed Phantoman dependency

--- Bundles/Testify/tests/SprykerTest/Shared/Testify/_support/Helper/WebDriverHelper.php
+++ Bundles/Testify/tests/SprykerTest/Shared/Testify/_support/Helper/WebDriverHelper.php
@@ -9,10 +9,11 @@ namespace SprykerTest\Shared\Testify\Helper;
 
 use Codeception\Event\SuiteEvent;
 use Codeception\Events;
-use Codeception\Extension\Phantoman;
+use Codeception\Exception\ExtensionException;
+use Codeception\Extension;
 use Codeception\Module\WebDriver;
 
-class WebDriverHelper extends Phantoman
+class WebDriverHelper extends Extension
 {
     protected const MODULE_NAME_WEBDRIVER = 'WebDriver';
 
@@ -25,8 +26,8 @@ class WebDriverHelper extends Phantoman
     protected const DEFAULT_HOST = '0.0.0.0';
     protected const DEFAULT_PORT = 4444;
     protected const DEFAULT_BROWSER = 'chrome';
-
-    protected const PHANTOMJS_NAME = 'phantomjs';
+    protected const DEFAULT_PATH = 'vendor/bin/phantomjs';
+    protected const DEFAULT_TIMEOUT = 10;
 
     /**
      * @var string[]
@@ -37,14 +38,76 @@ class WebDriverHelper extends Phantoman
     ];
 
     /**
+     * @var resource
+     */
+    protected $resource;
+
+    /**
+     * @var array
+     */
+    protected $pipes;
+
+    /**
+     * @param array $config
+     * @param array $options
+     *
+     * @throws \Codeception\Exception\ExtensionException
+     */
+    public function __construct(array $config, array $options)
+    {
+        if (isset($config['silent']) && $config['silent']) {
+            $options['silent'] = true;
+        }
+
+        parent::__construct($config, $options);
+
+        if (!isset($this->config['path'])) {
+            $this->config['path'] = static::DEFAULT_PATH;
+        }
+
+        if (!file_exists(realpath($this->config['path']))) {
+            throw new ExtensionException($this, "Webdriver executable not found: {$this->config['path']}");
+        }
+
+        if (!isset($this->config['port'])) {
+            $this->config['port'] = static::DEFAULT_PORT;
+        }
+
+        if (!isset($this->config['debug'])) {
+            $this->config['debug'] = false;
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->stopServer();
+    }
+
+    /**
+     * Start local webdriver server if no remote provided.
+     *
      * @param \Codeception\Event\SuiteEvent $e
      *
      * @return void
      */
     public function suiteInit(SuiteEvent $e): void
     {
-        if (!$this->isRemoteEnabled() and $this->isPhantomBrowser()) {
-            parent::suiteInit($e);
+        if (!$this->isRemoteEnabled()) {
+
+            if (isset($this->config['suites'])) {
+                if (is_string($this->config['suites'])) {
+                    $suites = [$this->config['suites']];
+                } else {
+                    $suites = $this->config['suites'];
+                }
+
+                if (!in_array($e->getSuite()->getBaseName(), $suites, true)
+                    && !in_array($e->getSuite()->getName(), $suites, true)) {
+                    return;
+                }
+            }
+
+            $this->startServer();
         }
     }
 
@@ -60,6 +123,180 @@ class WebDriverHelper extends Phantoman
         $this->getWebDriver()->_reconfigure(
             $this->getWebDriverConfig()
         );
+    }
+
+    /**
+     * @throws \Codeception\Exception\ExtensionException
+     *
+     * @return void
+     */
+    protected function startServer(): void
+    {
+        if ($this->resource !== null) {
+            return;
+        }
+
+        $this->writeln(PHP_EOL);
+        $this->writeln("Starting webdriver server.");
+
+        $command = $this->getCommand();
+
+        if ($this->config['debug']) {
+            $this->writeln(PHP_EOL);
+
+            $this->writeln('Generated webdriver command:');
+            $this->writeln($command);
+            $this->writeln(PHP_EOL);
+        }
+
+        $descriptorSpec = [
+            ['pipe', 'r'],
+            ['file', $this->getLogDir() . 'webdriver.output.txt', 'w'],
+            ['file', $this->getLogDir() . 'webdriver.errors.txt', 'a'],
+        ];
+
+        $this->resource = proc_open(
+            $command,
+            $descriptorSpec,
+            $this->pipes,
+            null,
+            null,
+            ['bypass_shell' => true]
+        );
+
+        if (!is_resource($this->resource) || !proc_get_status($this->resource)['running']) {
+            proc_close($this->resource);
+            throw new ExtensionException($this, 'Failed to start webdriver server.');
+        }
+
+        $max_checks = 10;
+        $checks = 0;
+
+        $this->write('Waiting for the webdriver server to be reachable.');
+
+        while (true) {
+            if ($checks >= $max_checks) {
+                throw new ExtensionException($this, 'Webdriver server never became reachable.');
+            }
+
+            // phpcs:disable
+            $fp = @fsockopen(
+                '127.0.0.1',
+                $this->config['port'],
+                $errCode,
+                $errStr,
+                static::DEFAULT_TIMEOUT
+            );
+            // phpcs:enable
+            if ($fp) {
+                $this->writeln('');
+                $this->writeln('Webdriver server now accessible.');
+                fclose($fp);
+                break;
+            }
+
+            $this->write('.');
+            $checks++;
+            sleep(1);
+        }
+
+        $this->writeln('');
+    }
+
+    /**
+     * @return void
+     */
+    protected function stopServer(): void
+    {
+        if ($this->resource !== null) {
+            $this->write('Stopping webdriver server.');
+            $max_checks = 10;
+
+            for ($i = 0; $i < $max_checks; $i++) {
+
+                if ($i === $max_checks - 1 && proc_get_status($this->resource)['running'] === true) {
+                    $this->writeln('');
+                    $this->writeln('Unable to properly shutdown webdriver server.');
+                    unset($this->resource);
+                    break;
+                }
+
+                if (proc_get_status($this->resource)['running'] === false) {
+                    $this->writeln('');
+                    $this->writeln('Webdriver server stopped.');
+                    unset($this->resource);
+                    break;
+                }
+
+                foreach ($this->pipes as $pipe) {
+                    if (is_resource($pipe)) {
+                        fclose($pipe);
+                    }
+                }
+
+                proc_terminate($this->resource, 2);
+                $this->write('.');
+                sleep(1);
+            }
+        }
+    }
+
+    protected function getCommandParametersMapping()
+    {
+        return [
+            'webdriver-port' => '--port',
+            'whitelisted-ips' => '--whitelisted-ips',
+            'url-base' => '--url-base',
+            'port' => '--webdriver',
+            'proxy' => '--proxy',
+            'proxyType' => '--proxy-type',
+            'proxyAuth' => '--proxy-auth',
+            'webSecurity' => '--web-security',
+            'ignoreSslErrors' => '--ignore-ssl-errors',
+            'sslProtocol' => '--ssl-protocol',
+            'sslCertificatesPath' => '--ssl-certificates-path',
+            'remoteDebuggerPort' => '--remote-debugger-port',
+            'remoteDebuggerAutorun' => '--remote-debugger-autorun',
+            'cookiesFile' => '--cookies-file',
+            'diskCache' => '--disk-cache',
+            'maxDiskCacheSize' => '--max-disk-cache-size',
+            'loadImages' => '--load-images',
+            'localStoragePath' => '--local-storage-path',
+            'localStorageQuota' => '--local-storage-quota',
+            'localToRemoteUrlAccess' => '--local-to-remote-url-access',
+            'outputEncoding' => '--output-encoding',
+            'scriptEncoding' => '--script-encoding',
+            'webdriverLoglevel' => '--webdriver-loglevel',
+            'webdriverLogfile' => '--webdriver-logfile',
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getCommandParameters(): string
+    {
+        $mapping = $this->getCommandParametersMapping();
+        $params = [];
+
+        foreach ($this->config as $configKey => $configValue) {
+            if (!empty($mapping[$configKey])) {
+                if (is_bool($configValue)) {
+                    $configValue = $configValue ? 'true' : 'false';
+                }
+                $params[] = $mapping[$configKey] . '=' . $configValue;
+            }
+        }
+
+        return implode(' ', $params);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getCommand(): string
+    {
+        return 'exec ' . escapeshellarg(realpath($this->config['path'])) . ' ' . $this->getCommandParameters();
     }
 
     /**
@@ -99,13 +336,4 @@ class WebDriverHelper extends Phantoman
         return (bool)$isRemoteEnabled;
     }
 
-    /**
-     * @return bool
-     */
-    protected function isPhantomBrowser()
-    {
-        $browser = $this->config[static::KEY_BROWSER] ?? null;
-
-        return ($browser === static::PHANTOMJS_NAME);
-    }
 }
