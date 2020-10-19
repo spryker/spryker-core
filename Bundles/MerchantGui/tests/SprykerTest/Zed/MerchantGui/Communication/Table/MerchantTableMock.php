7105d68207faf71d5cd76c85e94f18d8d57c8fe1
AntonKhabiuk <brotherosirus@gmail.com>
2020-10-19 15:29:53 +0300

    TE-7547 Add Merchant GUI table query test

---
+++ Bundles/MerchantGui/tests/SprykerTest/Zed/MerchantGui/Communication/Table/MerchantTableMock.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\MerchantGui\Communication\Table;
+
+use Spryker\Zed\MerchantGui\Communication\Table\MerchantTable;
+use Symfony\Component\HttpFoundation\Request;
+
+class MerchantTableMock extends MerchantTable
+{
+    /**
+     * @return $this
+     */
+    protected function init()
+    {
+        $this->request = new Request();
+        $config = $this->newTableConfiguration();
+        $config->setPageLength($this->getLimit());
+        $config = $this->configure($config);
+        $this->setConfiguration($config);
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function fetchData(): array
+    {
+        $this->init();
+
+        return $this->prepareData($this->config);
+    }
+}
