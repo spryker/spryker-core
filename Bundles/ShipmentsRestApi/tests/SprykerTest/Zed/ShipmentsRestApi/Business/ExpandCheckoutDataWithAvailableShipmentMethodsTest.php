cffd02515cadd51da69da5d4c5fbc592cfa755c5
Dima Aseev <dmitriy.aseev@spryker.com>
2020-11-05 17:25:40 +0200

    CC-11530: Covered Facade::mapShipmentsToQuote() by tests.

---
+++ Bundles/ShipmentsRestApi/tests/SprykerTest/Zed/ShipmentsRestApi/Business/ExpandCheckoutDataWithAvailableShipmentMethodsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\ShipmentsRestApi\Business;
+
+use Codeception\Test\Unit;
+
+/**
+ * Auto-generated group annotations
+ *
+ * @group SprykerTest
+ * @group Zed
+ * @group ShipmentsRestApi
+ * @group Business
+ * @group ExpandCheckoutDataWithAvailableShipmentMethodsTest
+ * Add your own group annotations below this line
+ */
+class ExpandCheckoutDataWithAvailableShipmentMethodsTest extends Unit
+{
+    /**
+     * @var \SprykerTest\Zed\ShipmentsRestApi\ShipmentsRestApiBusinessTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function test(): void
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
