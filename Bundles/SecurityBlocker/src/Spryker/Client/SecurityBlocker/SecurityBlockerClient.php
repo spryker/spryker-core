abcf1a3922a4ade1a59997c194dfd3788c464f6e
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2020-11-25 13:50:40 +0200

    CC-11479 created module.

---
+++ Bundles/SecurityBlocker/src/Spryker/Client/SecurityBlocker/SecurityBlockerClient.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\SecurityBlocker;
+
+use Generated\Shared\Transfer\AuthContextTransfer;
+use Generated\Shared\Transfer\AuthResponseTransfer;
+use Spryker\Client\Kernel\AbstractClient;
+
+/**
+ * @method \Spryker\Client\SecurityBlocker\SecurityBlockerFactory getFactory()
+ */
+class SecurityBlockerClient extends AbstractClient implements SecurityBlockerClientInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\AuthContextTransfer $AuthContextTransfer
+     *
+     * @return \Generated\Shared\Transfer\AuthResponseTransfer
+     */
+    public function incrementLoginAttempt(AuthContextTransfer $AuthContextTransfer): AuthResponseTransfer
+    {
+        return new AuthResponseTransfer();
+    }
+}
