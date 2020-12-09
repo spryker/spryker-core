5d171e479780db96e10f9bb82a8088fc2c897b87
Yuliia Sakharova <yuliia.sakharova@spryker.com>
2020-12-09 16:17:30 +0200

    MR-430: Add Merchant order data Readers.

---
+++ Bundles/MerchantSalesOrderDataExport/src/Spryker/Zed/MerchantSalesOrderDataExport/Business/Reader/MerchantOrderReader.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\MerchantSalesOrderDataExport\Business\Reader;
+
+use Generated\Shared\Transfer\DataExportBatchTransfer;
+use Generated\Shared\Transfer\DataExportConfigurationTransfer;
+use Spryker\Zed\MerchantSalesOrderDataExport\Persistence\MerchantSalesOrderDataExportRepositoryInterface;
+
+class MerchantOrderReader implements ReaderInterface
+{
+    /**
+     * @var \Spryker\Zed\MerchantSalesOrderDataExport\Persistence\MerchantSalesOrderDataExportRepositoryInterface
+     */
+    protected $merchantSalesOrderDataExportRepository;
+
+    /**
+     * @param \Spryker\Zed\MerchantSalesOrderDataExport\Persistence\MerchantSalesOrderDataExportRepositoryInterface $merchantSalesOrderDataExportRepository
+     */
+    public function __construct(MerchantSalesOrderDataExportRepositoryInterface $merchantSalesOrderDataExportRepository)
+    {
+        $this->merchantSalesOrderDataExportRepository = $merchantSalesOrderDataExportRepository;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\DataExportConfigurationTransfer $dataExportConfigurationTransfer
+     * @param int $offset
+     * @param int $limit
+     *
+     * @return \Generated\Shared\Transfer\DataExportBatchTransfer
+     */
+    public function readBatch(DataExportConfigurationTransfer $dataExportConfigurationTransfer, int $offset, int $limit): DataExportBatchTransfer
+    {
+        return $this->merchantSalesOrderDataExportRepository->getMerchantOrderData($dataExportConfigurationTransfer, $offset, $limit);
+    }
+}
