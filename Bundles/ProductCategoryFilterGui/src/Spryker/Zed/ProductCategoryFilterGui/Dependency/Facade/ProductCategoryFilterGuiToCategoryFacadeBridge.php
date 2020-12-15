820284303aab2677077da22f61f844d6f7f8bf2e
Dima Aseev <dmitriy.aseev@spryker.com>
2020-12-15 15:20:48 +0200

    CC-11972: Reverted CategoryGui module.

--- Bundles/ProductCategoryFilterGui/src/Spryker/Zed/ProductCategoryFilterGui/Dependency/Facade/ProductCategoryFilterGuiToCategoryFacadeBridge.php
+++ Bundles/ProductCategoryFilterGui/src/Spryker/Zed/ProductCategoryFilterGui/Dependency/Facade/ProductCategoryFilterGuiToCategoryFacadeBridge.php
@@ -9,7 +9,6 @@ namespace Spryker\Zed\ProductCategoryFilterGui\Dependency\Facade;
 
 use Generated\Shared\Transfer\CategoryCriteriaTransfer;
 use Generated\Shared\Transfer\CategoryTransfer;
-use Generated\Shared\Transfer\LocaleTransfer;
 
 class ProductCategoryFilterGuiToCategoryFacadeBridge implements ProductCategoryFilterGuiToCategoryFacadeInterface
 {
@@ -24,19 +23,6 @@ class ProductCategoryFilterGuiToCategoryFacadeBridge implements ProductCategoryF
     public function __construct($categoryFacade)
     {
         $this->categoryFacade = $categoryFacade;
-    }
-
-    /**
-     * @deprecated Use {@link \Spryker\Zed\ProductCategoryFilterGui\Dependency\Facade\ProductCategoryFilterGuiToCategoryFacadeBridge::findCategory()} instead.
-     *
-     * @param int $idCategory
-     * @param \Generated\Shared\Transfer\LocaleTransfer $localeTransfer
-     *
-     * @return array
-     */
-    public function getTreeNodeChildrenByIdCategoryAndLocale($idCategory, LocaleTransfer $localeTransfer)
-    {
-        return $this->categoryFacade->getTreeNodeChildrenByIdCategoryAndLocale($idCategory, $localeTransfer);
     }
 
     /**
