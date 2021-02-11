4f50b501e2f1f3e44acb0ffddb4214187901aa94
tobias.rosenstock <tobi.rosenstock@gmail.com>
2021-02-11 17:08:33 +0100

    MP-4183: merchant-order-return-process

--- Bundles/MerchantSalesReturn/src/Spryker/Zed/Communication/Controller/IndexController.php
+++ Bundles/MerchantSalesReturn/src/Spryker/Zed/Communication/Controller/IndexController.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
+ */
+
 namespace Spryker\Zed\MerchantSalesReturn\Communication\Controller;
 
 use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
