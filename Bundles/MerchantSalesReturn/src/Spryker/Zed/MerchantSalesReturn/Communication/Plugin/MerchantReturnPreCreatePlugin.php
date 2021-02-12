64fc0dd17fdc94fddeda6f1efa9a0d8635decde2
tobias.rosenstock <tobi.rosenstock@gmail.com>
2021-02-12 18:20:13 +0100

    MP-4080: WIP

---
+++ Bundles/MerchantSalesReturn/src/Spryker/Zed/MerchantSalesReturn/Communication/Plugin/MerchantReturnPreCreatePlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\MerchantSalesReturn\Communication\Plugin;
+
+class MerchantReturnPreCreatePlugin
+{
+
+}
