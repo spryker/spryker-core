fc5b568dda317b9bbd00cbd0b19b90dd4dc4635e
AntonKhabiuk <brotherosirus@gmail.com>
2021-02-17 14:43:04 +0200

    CC-10112 Support in the orders API

---
+++ Bundles/ProductConfigurationsRestApi/src/Spryker/Glue/ProductConfigurationsRestApi/Plugin/OrdersRestApi/ProductConfigurationRestOrderItemsAttributesMapperPlugin.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\ProductConfigurationsRestApi\Plugin\OrdersRestApi;
+
+use Generated\Shared\Transfer\ItemTransfer;
+use Generated\Shared\Transfer\RestOrderItemsAttributesTransfer;
+use Spryker\Glue\Kernel\AbstractPlugin;
+use Spryker\Glue\OrdersRestApiExtension\Dependency\Plugin\RestOrderItemsAttributesMapperPluginInterface;
+
+/**
+ * @method \Spryker\Glue\ProductConfigurationsRestApi\ProductConfigurationsRestApiFactory getFactory()
+ */
+class ProductConfigurationRestOrderItemsAttributesMapperPlugin extends AbstractPlugin implements RestOrderItemsAttributesMapperPluginInterface
+{
+    /**
+     * {@inheritDoc}
+     * - Maps `ItemTransfer.ProductConfigurationInstance` to `RestOrderItemsAttributesTransfer.SalesOrderItemConfiguration`.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ItemTransfer $itemTransfer
+     * @param \Generated\Shared\Transfer\RestOrderItemsAttributesTransfer $restOrderItemsAttributesTransfer
+     *
+     * @return \Generated\Shared\Transfer\RestOrderItemsAttributesTransfer
+     */
+    public function mapItemTransferToRestOrderItemsAttributesTransfer(
+        ItemTransfer $itemTransfer,
+        RestOrderItemsAttributesTransfer $restOrderItemsAttributesTransfer
+    ): RestOrderItemsAttributesTransfer {
+        return $this->getFactory()
+            ->createProductConfigurationRestOrderAttributesMapper()
+            ->mapItemTransferToRestOrderItemsAttributesTransfer(
+                $itemTransfer,
+                $restOrderItemsAttributesTransfer
+            );
+    }
+}
---
+++ Bundles/ProductConfigurationsRestApi/tests/SprykerTest/Glue/ProductConfigurationsRestApi/Plugin/ProductConfigurationRestOrderItemsAttributesMapperPluginTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Glue\ProductConfigurationsRestApi\Plugin;
+
+use Codeception\Test\Unit;
+use Generated\Shared\DataBuilder\ItemBuilder;
+use Generated\Shared\DataBuilder\RestOrderItemsAttributesBuilder;
+use Spryker\Glue\ProductConfigurationsRestApi\Plugin\OrdersRestApi\ProductConfigurationRestOrderItemsAttributesMapperPlugin;
+
+/**
+ * Auto-generated group annotations
+ *
+ * @group SprykerTest
+ * @group Glue
+ * @group ProductConfigurationsRestApi
+ * @group Plugin
+ * @group ProductConfigurationRestOrderItemsAttributesMapperPluginTest
+ * Add your own group annotations below this line
+ */
+class ProductConfigurationRestOrderItemsAttributesMapperPluginTest extends Unit
+{
+    /**
+     * @return void
+     */
+    public function testMapItemTransferToRestOrderItemsAttributesTransferWillMapProductConfiguration(): void
+    {
+        // Arrange
+        $itemTransfer = (new ItemBuilder())
+            ->withProductConfigurationInstance()
+            ->build();
+        $restOrderItemsAttributesTransfer = (new RestOrderItemsAttributesBuilder())->build();
+
+        $productConfigurationRestOrderItemsAttributesMapperPlugin = new ProductConfigurationRestOrderItemsAttributesMapperPlugin();
+
+        // Act
+        $restOrderItemsAttributesTransfer = $productConfigurationRestOrderItemsAttributesMapperPlugin->mapItemTransferToRestOrderItemsAttributesTransfer(
+            $itemTransfer,
+            $restOrderItemsAttributesTransfer
+        );
+
+        // Assert
+        $this->assertNotNull($restOrderItemsAttributesTransfer->getSalesOrderItemConfiguration());
+
+        $productConfigurationInstanceTransfer = $itemTransfer->getProductConfigurationInstance();
+        $restProductConfigurationInstanceAttributesTransfer = $restOrderItemsAttributesTransfer->getSalesOrderItemConfiguration();
+        $this->assertSame($restProductConfigurationInstanceAttributesTransfer->getConfiguration(), $productConfigurationInstanceTransfer->getConfiguration());
+        $this->assertSame($restProductConfigurationInstanceAttributesTransfer->getConfiguratorKey(), $productConfigurationInstanceTransfer->getConfiguratorKey());
+        $this->assertSame($restProductConfigurationInstanceAttributesTransfer->getDisplayData(), $productConfigurationInstanceTransfer->getDisplayData());
+        $this->assertSame($restProductConfigurationInstanceAttributesTransfer->getIsComplete(), $productConfigurationInstanceTransfer->getIsComplete());
+    }
+
+    /**
+     * @return void
+     */
+    public function testMapItemTransferToRestOrderItemsAttributesTransferWillNotMapProductConfigurationWhenItIsAbsentInItem(): void
+    {
+        // Arrange
+        $itemTransfer = (new ItemBuilder())->build();
+        $itemTransfer->setProductConfigurationInstance(null);
+        $restOrderItemsAttributesTransfer = (new RestOrderItemsAttributesBuilder())->build();
+
+        $productConfigurationRestOrderItemsAttributesMapperPlugin = new ProductConfigurationRestOrderItemsAttributesMapperPlugin();
+
+        // Act
+        $restOrderItemsAttributesTransfer = $productConfigurationRestOrderItemsAttributesMapperPlugin->mapItemTransferToRestOrderItemsAttributesTransfer(
+            $itemTransfer,
+            $restOrderItemsAttributesTransfer
+        );
+
+        // Assert
+        $this->assertNull($restOrderItemsAttributesTransfer->getSalesOrderItemConfiguration());
+    }
+}
