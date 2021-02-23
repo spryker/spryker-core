1c5adb0b99e21fbaa68fe62b32ef00fdc9a83e7a
tobias.rosenstock <tobi.rosenstock@gmail.com>
2021-02-23 12:03:30 +0100

    MP-4201: adjust state machine config

---
+++ Bundles/DummyMarketplacePayment/src/Spryker/Zed/DummyMarketplacePayment/Communication/Plugin/Oms/Command/MarketplaceRefundCommandPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\DummyMarketplacePayment\Communication\Plugin\Oms\Command;
+
+use Orm\Zed\Sales\Persistence\SpySalesOrder;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject;
+use \Spryker\Zed\Oms\Dependency\Plugin\Command\CommandByOrderInterface;
+
+/**
+ * @method \Spryker\Zed\DummyMarketplacePayment\Business\DummyMarketplacePaymentFacadeInterface getFacade()
+ * @method \Spryker\Zed\DummyMarketplacePayment\DummyMarketplacePaymentConfig getConfig()
+ */
+class MarketplaceRefundCommandPlugin extends AbstractPlugin implements CommandByOrderInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @param \Orm\Zed\Sales\Persistence\SpySalesOrderItem[] $orderItems
+     * @param \Orm\Zed\Sales\Persistence\SpySalesOrder $orderEntity
+     * @param \Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject $data
+     *
+     * @return array
+     */
+    public function run(array $orderItems, SpySalesOrder $orderEntity, ReadOnlyArrayObject $data): array
+    {
+        $this->getFacade()->refund($orderItems, $orderEntity);
+
+        return [];
+    }
+}
