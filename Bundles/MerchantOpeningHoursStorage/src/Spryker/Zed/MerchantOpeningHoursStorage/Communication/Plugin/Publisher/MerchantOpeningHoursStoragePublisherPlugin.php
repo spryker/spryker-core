d10256c93bffa1e3f33b6749c75c055eccd72258
volodymyr-bezuhlyi <volodymyr.bezuhlyi@spryker.com>
2021-02-24 18:07:23 +0200

    MP-4287:  MerchantOpeningHoursStorage clean code

--- Bundles/MerchantOpeningHoursStorage/src/Spryker/Zed/MerchantOpeningHoursStorage/Communication/Plugin/Publisher/MerchantOpeningHoursStoragePublisherPlugin.php
+++ Bundles/MerchantOpeningHoursStorage/src/Spryker/Zed/MerchantOpeningHoursStorage/Communication/Plugin/Publisher/MerchantOpeningHoursStoragePublisherPlugin.php
@@ -7,35 +7,37 @@ declare(strict_types = 1);
  * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\MerchantOpeningHoursStorage\Communication\Plugin\Event\Publisher;
+namespace Spryker\Zed\MerchantOpeningHoursStorage\Communication\Plugin\Publisher;
 
+use Orm\Zed\MerchantOpeningHours\Persistence\Map\SpyMerchantOpeningHoursDateScheduleTableMap;
+use Orm\Zed\MerchantOpeningHours\Persistence\Map\SpyMerchantOpeningHoursWeekdayScheduleTableMap;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 use Spryker\Zed\MerchantOpeningHours\Dependency\MerchantOpeningHoursEvents;
 use Spryker\Zed\PublisherExtension\Dependency\Plugin\PublisherPluginInterface;
-use Orm\Zed\MerchantOpeningHours\Persistence\Map\SpyMerchantOpeningHoursWeekdayScheduleTableMap;
-use Orm\Zed\MerchantOpeningHours\Persistence\Map\SpyMerchantOpeningHoursDateScheduleTableMap;
 
 /**
  * @method \Spryker\Zed\MerchantOpeningHoursStorage\Business\MerchantOpeningHoursStorageFacadeInterface getFacade()
+ * @method \Spryker\Zed\MerchantOpeningHoursStorage\MerchantOpeningHoursStorageConfig getConfig()
+ * @method \Spryker\Zed\MerchantOpeningHoursStorage\Communication\MerchantOpeningHoursStorageCommunicationFactory getFactory()
  */
-class MerchantOpeningHoursStoragePublisherPlugin extends AbstractPlugin  implements PublisherPluginInterface
+class MerchantOpeningHoursStoragePublisherPlugin extends AbstractPlugin implements PublisherPluginInterface
 {
-
     /**
      * @inheritDoc
      *
-     * @param array  $eventEntityTransfers
-     * @param string $eventName
-     *
      * @api
+     *
+     * @param array $eventEntityTransfers
+     * @param string $eventName
      */
     public function handleBulk(array $eventEntityTransfers, $eventName): void
     {
-        switch ($eventName){
+        switch ($eventName) {
             case MerchantOpeningHoursEvents::MERCHANT_OPENING_HOURS_PUBLISH:
                 $merchantIds = $this->getFactory()
                     ->getEventBehaviorFacade()
                     ->getEventTransferIds($eventEntityTransfers);
+
                 break;
             case MerchantOpeningHoursEvents::ENTITY_SPY_MERCHANT_OPENING_HOURS_WEEKDAY_SCHEDULE_CREATE:
                 $merchantIds = $this->getFactory()
@@ -44,6 +46,7 @@ class MerchantOpeningHoursStoragePublisherPlugin extends AbstractPlugin  impleme
                         $eventEntityTransfers,
                         SpyMerchantOpeningHoursWeekdayScheduleTableMap::COL_FK_MERCHANT
                     );
+
                 break;
             case MerchantOpeningHoursEvents::ENTITY_SPY_MERCHANT_OPENING_HOURS_DATE_SCHEDULE_CREATE:
                 $merchantIds = $this->getFactory()
@@ -52,13 +55,15 @@ class MerchantOpeningHoursStoragePublisherPlugin extends AbstractPlugin  impleme
                         $eventEntityTransfers,
                         SpyMerchantOpeningHoursDateScheduleTableMap::COL_FK_MERCHANT
                     );
+
                 break;
             default:
                 $merchantIds = [];
+
                 break;
         }
 
-        if (false === empty($merchantIds)) {
+        if (empty($merchantIds) === false) {
             $this->getFacade()->publish($merchantIds);
         }
     }
