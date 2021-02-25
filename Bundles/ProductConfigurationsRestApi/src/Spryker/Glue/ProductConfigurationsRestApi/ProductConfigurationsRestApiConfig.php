07b85dd41907cb316c352786fc5606279d2532ba
AntonKhabiuk <brotherosirus@gmail.com>
2021-02-25 22:11:05 +0200

    CC-10025 Add validation for crt item product configuration instance.

--- Bundles/ProductConfigurationsRestApi/src/Spryker/Glue/ProductConfigurationsRestApi/ProductConfigurationsRestApiConfig.php
+++ Bundles/ProductConfigurationsRestApi/src/Spryker/Glue/ProductConfigurationsRestApi/ProductConfigurationsRestApiConfig.php
@@ -11,4 +11,7 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 
 class ProductConfigurationsRestApiConfig extends AbstractBundleConfig
 {
+    public const RESPONSE_CODE_DEFAULT_PRODUCT_CONFIGURATION_INSTANCE_IS_MISSING = '4701';
+
+    public const ERROR_MESSAGE_DEFAULT_PRODUCT_CONFIGURATION_INSTANCE_IS_MISSING = 'An item with sku %s can\'t have a configuration with the key %s.';
 }
