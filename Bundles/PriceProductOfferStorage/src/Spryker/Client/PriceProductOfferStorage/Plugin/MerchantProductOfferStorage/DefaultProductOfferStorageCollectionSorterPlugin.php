f9b16a54dd52fc5b8ebfddfcaf34e09239876c36
volodymyr-bezuhlyi <volodymyr.bezuhlyi@spryker.com>
2021-02-28 10:49:55 +0200

    MP-4288:  PriceProductOfferStorage module refactored to have a default ProductOfferStorageCollectionSorterPlugin

---
+++ Bundles/PriceProductOfferStorage/src/Spryker/Client/PriceProductOfferStorage/Plugin/MerchantProductOfferStorage/DefaultProductOfferStorageCollectionSorterPlugin.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\PriceProductOfferStorage\Plugin\PriceProductStorage;
+
+use Generated\Shared\Transfer\ProductOfferStorageCollectionTransfer;
+use Spryker\Client\Kernel\AbstractPlugin;
+use Spryker\Client\MerchantProductOfferStorageExtension\Dependency\Plugin\ProductOfferStorageCollectionSorterPluginInterface;
+
+class DefaultProductOfferStorageCollectionSorterPlugin implements ProductOfferStorageCollectionSorterPluginInterface
+{
+    /**
+     * Specification:
+     * - Default plugin to sort ProductOfferStorageCollection. No sorting actually occurs
+     *
+     * @inheritDoc
+     */
+    public function sort(ProductOfferStorageCollectionTransfer $productOfferStorageCollectionTransfer
+    ): ProductOfferStorageCollectionTransfer{
+        return $productOfferStorageCollectionTransfer;
+    }
+}
