c488b1e2260e6c88e1dbd1818f5d3a2a34e39855
gechetspr <serhii.chepela@spryker.com>
2021-03-04 10:17:57 +0200

    TE-5068 reduced availability events

---
+++ Bundles/AvailabilityStorage/src/Spryker/Zed/AvailabilityStorage/Communication/Plugin/Event/Listener/AvailabilityProductAbstractStoragePublishListener.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\AvailabilityStorage\Communication\Plugin\Event\Listener;
+
+use Spryker\Zed\Event\Dependency\Plugin\EventBulkHandlerInterface;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+/**
+ * @method \Spryker\Zed\AvailabilityStorage\Business\AvailabilityStorageBusinessFactory createAvailabilityStorage()
+ * @method \Spryker\Zed\AvailabilityStorage\Communication\AvailabilityStorageCommunicationFactory getFactory()
+ * @method \Spryker\Zed\AvailabilityStorage\Business\AvailabilityStorageFacadeInterface getFacade()
+ * @method \Spryker\Zed\AvailabilityStorage\AvailabilityStorageConfig getConfig()
+ * @method \Spryker\Zed\AvailabilityStorage\Persistence\AvailabilityStorageQueryContainerInterface getQueryContainer()
+ */
+class AvailabilityProductAbstractStoragePublishListener extends AbstractPlugin implements EventBulkHandlerInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\EventEntityTransfer[] $eventTransfers
+     * @param string $eventName
+     *
+     * @return void
+     */
+    public function handleBulk(array $eventTransfers, $eventName): void
+    {
+        $productAbstractIds = $this->getFactory()->getEventBehaviorFacade()->getEventTransferIds($eventTransfers);
+        $this->getFacade()->publishByProductAbstractIds($productAbstractIds);
+    }
+}
