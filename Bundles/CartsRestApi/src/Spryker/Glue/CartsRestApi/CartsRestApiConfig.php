d9c476f2467a7b04268f1b11492037eb33fd98ca
Florian Kraemer <florian.kraemer@spryker.com>
2021-03-23 13:49:17 +0100

    GLUE-11358 Removing unused constant

--- Bundles/CartsRestApi/src/Spryker/Glue/CartsRestApi/CartsRestApiConfig.php
+++ Bundles/CartsRestApi/src/Spryker/Glue/CartsRestApi/CartsRestApiConfig.php
@@ -16,7 +16,6 @@ class CartsRestApiConfig extends AbstractBundleConfig
 {
     public const RESOURCE_CARTS = 'carts';
     public const RESOURCE_CART_ITEMS = 'items';
-    public const RESOURCE_CUSTOMER_CARTS = 'carts';
     public const RESOURCE_CUSTOMERS = 'customers';
     public const RESOURCE_GUEST_CARTS = 'guest-carts';
     public const RESOURCE_GUEST_CARTS_ITEMS = 'guest-cart-items';
