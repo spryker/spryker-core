3377d776bed2dfe767a2bd322e1862ba4f936d22
danil.moroz <danil.moroz@spryker.com>
2021-03-25 13:16:24 +0200

    BO-2447 FE Integration: Services + Tokens

---
+++ Bundles/ProductMerchantPortalGui/src/Spryker/Zed/ProductMerchantPortalGui/Presentation/Components/services/tokens.ts
@@ -0,0 +1,34 @@
+import { InjectionToken, StaticProvider } from '@angular/core';
+import { IdGenerator } from './types';
+import { ConcreteProductSkuGeneratorService } from './concrete-product-sku-generator.service';
+import { ConcreteProductNameGeneratorService } from './concrete-product-name-generator.service';
+
+export const ConcreteProductSkuGeneratorToken = new InjectionToken<IdGenerator>('ConcreteProductSkuGeneratorToken');
+
+export const ConcreteProductSkuGeneratorProviderToken = new InjectionToken<StaticProvider>('ConcreteProductSkuGeneratorProviderToken', {
+    providedIn: 'root',
+    factory: provideConcreteProductSkuGenerator,
+});
+
+export function provideConcreteProductSkuGenerator(): StaticProvider {
+    return {
+        provide: ConcreteProductSkuGeneratorToken,
+        useClass: ConcreteProductSkuGeneratorService,
+        deps: []
+    };
+}
+
+export const ConcreteProductNameGeneratorToken = new InjectionToken<IdGenerator>('ConcreteProductNameGeneratorToken');
+
+export const ConcreteProductNameGeneratorProviderToken = new InjectionToken<StaticProvider>('ConcreteProductNameGeneratorProviderToken', {
+    providedIn: 'root',
+    factory: provideConcreteProductNameGenerator,
+});
+
+export function provideConcreteProductNameGenerator(): StaticProvider {
+    return {
+        provide: ConcreteProductNameGeneratorToken,
+        useClass: ConcreteProductNameGeneratorService,
+        deps: []
+    };
+}
