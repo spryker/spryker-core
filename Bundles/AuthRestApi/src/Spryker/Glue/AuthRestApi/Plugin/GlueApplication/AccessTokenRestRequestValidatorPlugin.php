f7d7b716d4e2bfb3a9735420006aff0648fe7e27
vz-spr <vasyl.zhdankin@spryker.com>
2021-04-12 15:01:51 +0300

    glue-11427: in AccessTokenRestRequestValidatorPlugin.php remove description of deprecation

--- Bundles/AuthRestApi/src/Spryker/Glue/AuthRestApi/Plugin/GlueApplication/AccessTokenRestRequestValidatorPlugin.php
+++ Bundles/AuthRestApi/src/Spryker/Glue/AuthRestApi/Plugin/GlueApplication/AccessTokenRestRequestValidatorPlugin.php
@@ -14,7 +14,7 @@ use Spryker\Glue\Kernel\AbstractPlugin;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @deprecated Use {@link \Spryker\Glue\AuthRestApi\Plugin\GlueApplication\ValidateAccessTokenPlugin} instead.
+ * @deprecated
  *
  * @method \Spryker\Glue\AuthRestApi\AuthRestApiFactory getFactory()
  */
