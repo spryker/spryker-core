5a47224ddb296b705663e42990f48414e9fb1d82
zamlelyi <nikolay.zamlely@spryker.com>
2021-04-16 16:32:50 +0300

    CC-12937: FE part for store selector

--- Bundles/CategoryGui/assets/Zed/js/modules/legacy/logic.js
+++ Bundles/CategoryGui/assets/Zed/js/modules/legacy/logic.js
@@ -8,6 +8,11 @@
 window.serializedList = {};
 
 var categoryHelper = require('./helpers.js');
+var STORE_SELECTOR_ID = '#category_store_relation_id_stores';
+var STORE_SELECTOR_LOADER_CLASS_NAME = '.store-seletor-loader';
+var STORE_SELECTOR_ACTION_URL_ATTRIBUTE = 'action-url';
+var STORE_SELECTOR_ACTION_EVENT_ATTRIBUTE = 'action-event';
+var STORE_SELECTOR_ACTION_FIELD_ATTRIBUTE = 'action-field';
 
 $(document).ready(function () {
     var triggeredFirstEvent = false;
@@ -52,4 +57,51 @@ $(document).ready(function () {
     $('.save-categories-order').click(function () {
         SprykerAjax.updateCategoryNodesOrder(serializedList);
     });
+
+    var storeSelector = $(STORE_SELECTOR_ID);
+    var storeSelectorLoader = storeSelector.parent().find(STORE_SELECTOR_LOADER_CLASS_NAME);
+    var storeSelectorActionUrl = storeSelector.attr(STORE_SELECTOR_ACTION_URL_ATTRIBUTE);
+    var storeSelectorActionEvent = storeSelector.attr(STORE_SELECTOR_ACTION_EVENT_ATTRIBUTE);
+    var storeSelectorActionFieldName = storeSelector.attr(STORE_SELECTOR_ACTION_FIELD_ATTRIBUTE);
+    var createCategoryFormName = storeSelector.closest('form')[0].name;
+    var parentCategorySelector = $(`[name='${createCategoryFormName}[${storeSelectorActionFieldName}]']`);
+    var parentCategoryId = parentCategorySelector.select2('data')[0].id;
+
+    if (!parentCategoryId) {
+        storeSelector.attr('disabled', true);
+    }
+
+    parentCategorySelector.on(storeSelectorActionEvent, function (event) {
+        var selectedCategoryId = $(this).select2('data')[0].id;
+
+        if (selectedCategoryId) {
+            $.ajax({
+                url: `${storeSelectorActionUrl}?id-category-node=${selectedCategoryId}`,
+                success: function(data) {
+                    storeSelector.empty();
+
+                    data.forEach(function (item) {
+                        var optionTemplate = `<option value="${item.id_store}">${item.name}</option>`;
+
+                        if (!item.is_active) {
+                            optionTemplate = `<option disabled value="${item.id_store}">${item.name}</option>`
+                        }
+
+                        storeSelector.append(optionTemplate);
+                        storeSelector.attr('disabled', false);
+                    })
+                },
+                beforeSend: function(){
+                    storeSelectorLoader.addClass('active');
+                },
+                complete: function(){
+                    storeSelectorLoader.removeClass('active');
+                }
+            });
+
+            return;
+        }
+
+        storeSelector.attr('disabled', true);
+    })
 });
