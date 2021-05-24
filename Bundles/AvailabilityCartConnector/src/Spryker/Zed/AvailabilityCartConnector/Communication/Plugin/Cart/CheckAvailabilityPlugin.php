604a9a4dea336749cc042c34fa2d254fa7a1b405
Anton Sakharov <anton.sakharov@spryker.com>
2021-05-24 14:24:50 +0300

    TE-9149: Add CheckAvailabilityPlugin for batch processing

--- Bundles/AvailabilityCartConnector/src/Spryker/Zed/AvailabilityCartConnector/Communication/Plugin/Cart/CheckAvailabilityPlugin.php
+++ Bundles/AvailabilityCartConnector/src/Spryker/Zed/AvailabilityCartConnector/Communication/Plugin/Cart/CheckAvailabilityPlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\AvailabilityCartConnector\Communication\Plugin;
+namespace Spryker\Zed\AvailabilityCartConnector\Communication\Plugin\Cart;
 
 use Generated\Shared\Transfer\CartChangeTransfer;
 use Spryker\Zed\CartExtension\Dependency\Plugin\CartPreCheckPluginInterface;
@@ -23,8 +23,6 @@ class CheckAvailabilityPlugin extends AbstractPlugin implements CartPreCheckPlug
      *  - In case `ItemTransfer.amount` was defined, item availability check will be ignored.
      *
      * @api
-     *
-     * @deprecated Use {@link \Spryker\Zed\AvailabilityCartConnector\Communication\Plugin\Cart\CheckAvailabilityPlugin} instead.
      *
      * @param \Generated\Shared\Transfer\CartChangeTransfer $cartChangeTransfer
      *
