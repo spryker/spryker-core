9331f2459f9dd50499e3dbb9faf546a59145b48f
tobias.rosenstock <tobi.rosenstock@gmail.com>
2021-05-27 09:42:17 +0200

    BO-2049: codestyle fixes

--- Bundles/ProductCategory/tests/SprykerTest/Zed/ProductCategory/Presentation/ProductCategoryAssignCest.php
+++ Bundles/ProductCategory/tests/SprykerTest/Zed/ProductCategory/Presentation/ProductCategoryAssignCest.php
@@ -82,9 +82,9 @@ class ProductCategoryAssignCest
             ProductCategoryAssignPage::DESELECTED_PRODUCTS_CSV_FIELD_SELECTOR,
             (string)$idAbstractProduct
         );
+
         $i->click(ProductCategoryAssignPage::FORM_SUBMIT_SELECTOR);
         $i->waitForElement(ProductCategoryAssignPage::SUCCESS_MESSAGE_SELECTOR);
-
     }
 
     /**
