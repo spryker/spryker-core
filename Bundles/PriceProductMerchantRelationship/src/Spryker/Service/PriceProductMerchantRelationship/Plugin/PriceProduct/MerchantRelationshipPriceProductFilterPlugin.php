a69f38faea0c4beaf982098d71948bfc773cb193
Anton Khabiuk <anton.khabiuk@spryker.com>
2021-07-09 11:14:41 +0300

    CC-12604: Fixed merchant prices. (#8345)
    CC-12604 Fixed merchant prices.

---
+++ Bundles/PriceProductMerchantRelationship/src/Spryker/Service/PriceProductMerchantRelationship/Plugin/PriceProduct/MerchantRelationshipPriceProductFilterPlugin.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Service\PriceProductMerchantRelationship\Plugin\PriceProduct;
+
+use Generated\Shared\Transfer\PriceProductFilterTransfer;
+use Spryker\Service\Kernel\AbstractPlugin;
+use Spryker\Service\PriceProductExtension\Dependency\Plugin\PriceProductFilterPluginInterface;
+use Spryker\Shared\PriceProductMerchantRelationship\PriceProductMerchantRelationshipConfig;
+
+/**
+ * @method \Spryker\Service\PriceProductMerchantRelationship\PriceProductMerchantRelationshipServiceInterface getService()
+ */
+class MerchantRelationshipPriceProductFilterPlugin extends AbstractPlugin implements PriceProductFilterPluginInterface
+{
+    /**
+     * {@inheritDoc}
+     * - Filters `PriceProductTransfers` by `PriceProductFilterTransfer.priceDimension.idMerchantRelationship`.
+     * - Filters out all `PriceProductTransfers` with merchant relationship if `PriceProductFilterTransfer.priceDimension.idMerchantRelationship` is not set.
+     * - Filters out all `PriceProductTransfers` where `PriceProductTransfer.priceDimension.idMerchantRelationship` is different from `PriceProductFilterTransfer.priceDimension.idMerchantRelationship`.
+     * - When `PriceProductFilterTransfer.priceDimension.idMerchantRelationship` is set and `PriceProductTransfer` doesn't have a merchant relationship, it is not filtered out.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\PriceProductTransfer[] $priceProductTransfers
+     * @param \Generated\Shared\Transfer\PriceProductFilterTransfer $priceProductFilterTransfer
+     *
+     * @return \Generated\Shared\Transfer\PriceProductTransfer[]
+     */
+    public function filter(array $priceProductTransfers, PriceProductFilterTransfer $priceProductFilterTransfer): array
+    {
+        return $this->getService()->filterPriceProductsByMerchantRelationship(
+            $priceProductTransfers,
+            $priceProductFilterTransfer
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @return string
+     */
+    public function getDimensionName(): string
+    {
+        return PriceProductMerchantRelationshipConfig::PRICE_DIMENSION_MERCHANT_RELATIONSHIP;
+    }
+}
