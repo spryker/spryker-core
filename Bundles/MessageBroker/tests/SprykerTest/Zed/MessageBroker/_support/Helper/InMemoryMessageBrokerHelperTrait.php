d89d6547397a2c2e821dc6ad228fd7b8fc8b91be
Ruslan Khudenko <khudenko.ruslan@gmail.com>
2022-06-08 10:31:04 +0300

    APPS-4435: Added InMemoryMessageBrokerHelperTrait. (#9425)
    Added InMemoryMessageBrokerHelperTrait to MessageBroker.

---
+++ Bundles/MessageBroker/tests/SprykerTest/Zed/MessageBroker/_support/Helper/InMemoryMessageBrokerHelperTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\MessageBroker\Helper;
+
+trait InMemoryMessageBrokerHelperTrait
+{
+    /**
+     * @return \SprykerTest\Zed\MessageBroker\Helper\InMemoryMessageBrokerHelper
+     */
+    protected function getInMemoryMessageBrokerHelper(): InMemoryMessageBrokerHelper
+    {
+        /** @var \SprykerTest\Zed\MessageBroker\Helper\InMemoryMessageBrokerHelper $inMemoryMessageBrokerHelper */
+        $inMemoryMessageBrokerHelper = $this->getModule('\\' . InMemoryMessageBrokerHelper::class);
+
+        return $inMemoryMessageBrokerHelper;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return \Codeception\Module
+     */
+    abstract protected function getModule($name);
+}
