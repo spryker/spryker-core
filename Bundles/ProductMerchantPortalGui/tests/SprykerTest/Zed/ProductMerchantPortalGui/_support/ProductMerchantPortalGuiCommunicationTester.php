3590005bef242ca2d50b95c4cb20556eacd1c612
Yuliia Sakharova <62552852+sakharova-yuliia@users.noreply.github.com>
2022-06-08 11:18:06 +0200

    MP-6796 Fixed editing of product prices in merchant portal. (#9337)
    MP-6796 Fixed editing of product prices in merchant portal.

--- Bundles/ProductMerchantPortalGui/tests/SprykerTest/Zed/ProductMerchantPortalGui/_support/ProductMerchantPortalGuiCommunicationTester.php
+++ Bundles/ProductMerchantPortalGui/tests/SprykerTest/Zed/ProductMerchantPortalGui/_support/ProductMerchantPortalGuiCommunicationTester.php
@@ -9,11 +9,17 @@ namespace SprykerTest\Zed\ProductMerchantPortalGui;
 
 use Codeception\Actor;
 use Codeception\Util\Stub;
+use Generated\Shared\Transfer\CurrencyTransfer;
+use Generated\Shared\Transfer\MoneyValueTransfer;
 use Generated\Shared\Transfer\PriceProductCriteriaTransfer;
+use Generated\Shared\Transfer\PriceProductDimensionTransfer;
+use Generated\Shared\Transfer\PriceProductTransfer;
+use Generated\Shared\Transfer\PriceTypeTransfer;
 use Spryker\Zed\ProductMerchantPortalGui\Communication\Mapper\Merger\PriceProductMerger;
 use Spryker\Zed\ProductMerchantPortalGui\Communication\Mapper\Merger\PriceProductMergerInterface;
 use Spryker\Zed\ProductMerchantPortalGui\Communication\Mapper\PriceProductMapper;
 use Spryker\Zed\ProductMerchantPortalGui\Communication\Mapper\PriceProductMapperInterface;
+use Spryker\Zed\ProductMerchantPortalGui\Communication\ProductMerchantPortalGuiCommunicationFactory;
 use Spryker\Zed\ProductMerchantPortalGui\Dependency\Facade\ProductMerchantPortalGuiToCurrencyFacadeBridge;
 use Spryker\Zed\ProductMerchantPortalGui\Dependency\Facade\ProductMerchantPortalGuiToCurrencyFacadeInterface;
 use Spryker\Zed\ProductMerchantPortalGui\Dependency\Facade\ProductMerchantPortalGuiToMoneyFacadeBridge;
@@ -22,6 +28,7 @@ use Spryker\Zed\ProductMerchantPortalGui\Dependency\Facade\ProductMerchantPortal
 use Spryker\Zed\ProductMerchantPortalGui\Dependency\Facade\ProductMerchantPortalGuiToPriceProductFacadeInterface;
 use Spryker\Zed\ProductMerchantPortalGui\Dependency\Service\ProductMerchantPortalGuiToUtilEncodingServiceBridge;
 use Spryker\Zed\ProductMerchantPortalGui\Dependency\Service\ProductMerchantPortalGuiToUtilEncodingServiceInterface;
+use Spryker\Zed\ProductMerchantPortalGui\ProductMerchantPortalGuiDependencyProvider;
 use Spryker\Zed\ProductMerchantPortalGuiExtension\Dependency\Plugin\PriceProductMapperPluginInterface;
 
 /**
@@ -43,6 +50,42 @@ use Spryker\Zed\ProductMerchantPortalGuiExtension\Dependency\Plugin\PriceProduct
 class ProductMerchantPortalGuiCommunicationTester extends Actor
 {
     use _generated\ProductMerchantPortalGuiCommunicationTesterActions;
+
+    /**
+     * @var int
+     * Fake IDs here and below are used to not depend on the project data.
+     */
+    public const FAKE_CURRENCY_ID_1 = - 1;
+
+    /**
+     * @var int
+     */
+    public const FAKE_CURRENCY_ID_2 = - 2;
+
+    /**
+     * @var string
+     */
+    public const FAKE_PRICE_TYPE_1 = 'PRICE_TYPE_1';
+
+    /**
+     * @var string
+     */
+    public const FAKE_PRICE_TYPE_2 = 'PRICE_TYPE_2';
+
+    /**
+     * @var string
+     */
+    public const FAKE_PRICE_DIMENSION = 'FAKE_PRICE_DIMENSION';
+
+    /**
+     * @var int
+     */
+    public const FAKE_STORE_ID_1 = - 1;
+
+    /**
+     * @var int
+     */
+    public const FAKE_STORE_ID_2 = - 2;
 
     /**
      * @param array<\Spryker\Zed\ProductMerchantPortalGuiExtension\Dependency\Plugin\PriceProductMapperPluginInterface> $priceProductMapperPlugins
@@ -126,5 +169,124 @@ class ProductMerchantPortalGuiCommunicationTester extends Actor
                     },
             ],
         );
+    }
+
+    /**
+     * @return \Spryker\Zed\ProductMerchantPortalGui\Dependency\Facade\ProductMerchantPortalGuiToPriceProductFacadeBridge
+     */
+    public function createPriceProductFacadeMock(): ProductMerchantPortalGuiToPriceProductFacadeBridge
+    {
+        $fakePriceTypes = $this->getFakePriceTypes();
+
+        return Stub::make(
+            ProductMerchantPortalGuiToPriceProductFacadeBridge::class,
+            [
+                'getPriceTypeValues' => function () use ($fakePriceTypes) {
+                    return $fakePriceTypes;
+                },
+            ],
+        );
+    }
+
+    /**
+     * @return \Spryker\Zed\ProductMerchantPortalGui\Dependency\Facade\ProductMerchantPortalGuiToCurrencyFacadeBridge
+     */
+    public function createCurrencyFacadeMock(): ProductMerchantPortalGuiToCurrencyFacadeBridge
+    {
+        $currencies = $this->getFakeCurrencies();
+
+        return Stub::make(
+            ProductMerchantPortalGuiToCurrencyFacadeBridge::class,
+            [
+                'getByIdCurrency' => function (int $idCurrency) use ($currencies) {
+                    if (array_key_exists($idCurrency, $currencies)) {
+                        return $currencies[$idCurrency];
+                    }
+
+                    return (new CurrencyTransfer());
+                },
+            ],
+        );
+    }
+
+    /**
+     * @return \Spryker\Zed\ProductMerchantPortalGui\Communication\ProductMerchantPortalGuiCommunicationFactory
+     */
+    public function createProductMerchantPortalGuiCommunicationFactoryMock(): ProductMerchantPortalGuiCommunicationFactory
+    {
+        $currencyFacade = $this->createCurrencyFacadeMock();
+        $priceProductFacade = $this->createPriceProductFacadeMock();
+
+        return Stub::make(
+            ProductMerchantPortalGuiCommunicationFactory::class,
+            [
+                'getCurrencyFacade' => function () use ($currencyFacade) {
+                    return $currencyFacade;
+                },
+                'getPriceProductFacade' => function () use ($priceProductFacade) {
+                    return $priceProductFacade;
+                },
+                'resolveDependencyProvider' => function () {
+                    return (new ProductMerchantPortalGuiDependencyProvider());
+                },
+            ],
+        );
+    }
+
+    /**
+     * @param int $priceValue
+     *
+     * @return \Generated\Shared\Transfer\PriceProductTransfer
+     */
+    public function createFakePriceProductTransfer(int $priceValue): PriceProductTransfer
+    {
+        $priceTypeDefault = $this->getFakePriceTypes()[0];
+        $priceDimensionTransfer = $this->createFakePriceDimensionTransfer();
+
+        $priceProductTransfer = (new PriceProductTransfer())
+            ->setIdPriceProduct(rand())
+            ->setFkPriceType($priceTypeDefault->getIdPriceType())
+            ->setPriceType($priceTypeDefault)
+            ->setPriceDimension($priceDimensionTransfer)
+            ->setMoneyValue(
+                (new MoneyValueTransfer())
+                    ->setFkCurrency(static::FAKE_CURRENCY_ID_1)
+                    ->setFkStore(static::FAKE_STORE_ID_1)
+                    ->setGrossAmount($priceValue),
+            );
+
+        return $priceProductTransfer;
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\PriceProductDimensionTransfer
+     */
+    public function createFakePriceDimensionTransfer(): PriceProductDimensionTransfer
+    {
+        return (new PriceProductDimensionTransfer())
+            ->setIdPriceProductDefault(rand())
+            ->setType(static::FAKE_PRICE_DIMENSION);
+    }
+
+    /**
+     * @return array<\Generated\Shared\Transfer\PriceTypeTransfer>
+     */
+    public function getFakePriceTypes(): array
+    {
+        return [
+            (new PriceTypeTransfer())->setName(static::FAKE_PRICE_TYPE_1)->setIdPriceType(-1),
+            (new PriceTypeTransfer())->setName(static::FAKE_PRICE_TYPE_2)->setIdPriceType(-2),
+        ];
+    }
+
+    /**
+     * @return array<\Generated\Shared\Transfer\CurrencyTransfer>
+     */
+    public function getFakeCurrencies(): array
+    {
+        return [
+            static::FAKE_CURRENCY_ID_1 => (new CurrencyTransfer())->setIdCurrency(static::FAKE_CURRENCY_ID_1),
+            static::FAKE_CURRENCY_ID_2 => (new CurrencyTransfer())->setIdCurrency(static::FAKE_CURRENCY_ID_2),
+        ];
     }
 }
