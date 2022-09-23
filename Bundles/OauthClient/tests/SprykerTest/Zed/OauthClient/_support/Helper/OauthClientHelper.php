e5fc2f5fe5d5ea52c45d655b9dfce8860e6af92b
mscherer <mark.scherer@spryker.com>
2022-09-23 15:34:23 +0200

    Fix CS.

--- Bundles/OauthClient/tests/SprykerTest/Zed/OauthClient/_support/Helper/OauthClientHelper.php
+++ Bundles/OauthClient/tests/SprykerTest/Zed/OauthClient/_support/Helper/OauthClientHelper.php
@@ -1,9 +1,11 @@
-<?php declare(strict_types = 1);
+<?php
 
 /**
  * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
+
+declare(strict_types=1);
 
 namespace SprykerTest\Zed\OauthClient\Helper;
 
