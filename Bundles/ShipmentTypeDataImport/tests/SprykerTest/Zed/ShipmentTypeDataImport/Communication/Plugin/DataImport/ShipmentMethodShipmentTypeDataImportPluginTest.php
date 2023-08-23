2b05476eca6206c308252dbea23b946ff1995d7c
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2023-08-23 12:59:37 +0300

    CC-26541 Added helper to autoload (#10433)
    CC-26541 [cc] Delivery Types as a part of Service Point concept - SAPI - PART 1

--- Bundles/ShipmentTypeDataImport/tests/SprykerTest/Zed/ShipmentTypeDataImport/Communication/Plugin/DataImport/ShipmentMethodShipmentTypeDataImportPluginTest.php
+++ Bundles/ShipmentTypeDataImport/tests/SprykerTest/Zed/ShipmentTypeDataImport/Communication/Plugin/DataImport/ShipmentMethodShipmentTypeDataImportPluginTest.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace SprykerTest\Zed\ShipmentType\Communication\Plugin\DataImport;
+namespace SprykerTest\Zed\ShipmentTypeDataImport\Communication\Plugin\DataImport;
 
 use Codeception\Test\Unit;
 use Generated\Shared\Transfer\DataImporterConfigurationTransfer;
