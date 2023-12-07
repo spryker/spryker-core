91d58d3647dd8563bc47352eeaa7e589ed79a302
Dmitry Tsemma <dmitry.tsemma@spryker.com>
2023-12-07 15:36:13 +0200

    FRW-6285 Resource limitation for access tokens. (#10634)
    FRW-6285 Resource limitation for access tokens.

--- Bundles/OauthBackendApi/tests/SprykerTest/Glue/OauthBackendApi/Plugin/GlueApplication/BackendApiAccessTokenValidatorPluginTest.php
+++ Bundles/OauthBackendApi/tests/SprykerTest/Glue/OauthBackendApi/Plugin/GlueApplication/BackendApiAccessTokenValidatorPluginTest.php
@@ -5,38 +5,30 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace SprykerTest\Glue\OauthBackendApi\Plugin;
+namespace SprykerTest\Glue\OauthBackendApi\Plugin\GlueApplication;
 
 use Codeception\Test\Unit;
 use Generated\Shared\Transfer\GlueRequestTransfer;
-use Generated\Shared\Transfer\GlueRequestValidationTransfer;
 use Generated\Shared\Transfer\OauthAccessTokenValidationResponseTransfer;
 use Spryker\Glue\Kernel\Backend\Container;
 use Spryker\Glue\OauthBackendApi\Dependency\Facade\OauthBackendApiToOauthFacadeInterface;
 use Spryker\Glue\OauthBackendApi\OauthBackendApiConfig;
 use Spryker\Glue\OauthBackendApi\OauthBackendApiDependencyProvider;
-use Spryker\Glue\OauthBackendApi\Plugin\AccessTokenValidatorPlugin;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * @deprecated Will be removed in next major.
- *
  * Auto-generated group annotations
  *
  * @group SprykerTest
  * @group Glue
  * @group OauthBackendApi
  * @group Plugin
- * @group AccessTokenValidatorPluginTest
+ * @group GlueApplication
+ * @group BackendApiAccessTokenValidatorPluginTest
  * Add your own group annotations below this line
  */
-class AccessTokenValidatorPluginTest extends Unit
+class BackendApiAccessTokenValidatorPluginTest extends Unit
 {
-    /**
-     * @var string
-     */
-    public const HTTP_AUTHORIZATION = 'HTTP_AUTHORIZATION';
-
     /**
      * @var \SprykerTest\Glue\OauthBackendApi\OauthBackendApiTester
      */
@@ -45,10 +37,10 @@ class AccessTokenValidatorPluginTest extends Unit
     /**
      * @return void
      */
-    public function testValidatorIsNotProtected(): void
+    public function testValidateReturnsTrueWithoutToken(): void
     {
         //Act
-        $result = (new AccessTokenValidatorPlugin())->validate(new GlueRequestTransfer());
+        $result = $this->tester->createBackendApiAccessTokenValidatorPlugin()->validate(new GlueRequestTransfer());
 
         //Assert
         $this->assertTrue($result->getIsValid());
@@ -57,29 +49,7 @@ class AccessTokenValidatorPluginTest extends Unit
     /**
      * @return void
      */
-    public function testValidatorEmptyAuth(): void
-    {
-        //Arrange
-        $glueRequestTransfer = (new GlueRequestTransfer())->setHttpRequestAttributes(['is-protected' => true]);
-
-        //Act
-        $result = (new AccessTokenValidatorPlugin())->validate($glueRequestTransfer);
-
-        //Assert
-        $this->assertInstanceOf(GlueRequestValidationTransfer::class, $result);
-
-        $this->assertFalse($result->getIsValid());
-
-        $glueErrorTransfer = $result->getErrors()->getArrayCopy()[0];
-        $this->assertSame(OauthBackendApiConfig::RESPONSE_CODE_FORBIDDEN, $glueErrorTransfer->getCode());
-        $this->assertSame(Response::HTTP_FORBIDDEN, $glueErrorTransfer->getStatus());
-        $this->assertSame(OauthBackendApiConfig::RESPONSE_DETAIL_MISSING_ACCESS_TOKEN, $glueErrorTransfer->getMessage());
-    }
-
-    /**
-     * @return void
-     */
-    public function testValidatorTokenNotValid(): void
+    public function testValidateReturnsFalseWithInvalidToken(): void
     {
         //Arrange
         $this->tester->setDependency(OauthBackendApiDependencyProvider::FACADE_OAUTH, function (Container $container) {
@@ -90,15 +60,15 @@ class AccessTokenValidatorPluginTest extends Unit
             return $oauthFacade;
         });
         $glueRequestTransfer = (new GlueRequestTransfer())
-            ->setHttpRequestAttributes(['is-protected' => true])
-            ->setMeta(['authorization' => [0 => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9']]);
+            ->setMeta([$this->tester::AUTHORIZATION => [0 => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9']]);
 
         //Act
-        $result = (new AccessTokenValidatorPlugin())->validate($glueRequestTransfer);
+        $result = $this->tester->createBackendApiAccessTokenValidatorPlugin()->validate($glueRequestTransfer);
 
         //Assert
         $this->assertFalse($result->getIsValid());
 
+        $this->assertCount(1, $result->getErrors());
         $glueErrorTransfer = $result->getErrors()->getArrayCopy()[0];
         $this->assertSame(OauthBackendApiConfig::RESPONSE_CODE_ACCESS_CODE_INVALID, $glueErrorTransfer->getCode());
         $this->assertSame(Response::HTTP_UNAUTHORIZED, $glueErrorTransfer->getStatus());
@@ -108,7 +78,7 @@ class AccessTokenValidatorPluginTest extends Unit
     /**
      * @return void
      */
-    public function testValidatorTokenValid(): void
+    public function testValidateReturnsTrueWithValidToken(): void
     {
         //Arrange
         $this->tester->setDependency(OauthBackendApiDependencyProvider::FACADE_OAUTH, function (Container $container) {
@@ -119,11 +89,10 @@ class AccessTokenValidatorPluginTest extends Unit
             return $oauthFacade;
         });
         $glueRequestTransfer = (new GlueRequestTransfer())
-            ->setHttpRequestAttributes(['is-protected' => true])
-            ->setMeta(['authorization' => [0 => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9']]);
+            ->setMeta([$this->tester::AUTHORIZATION => [0 => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9']]);
 
         //Act
-        $result = (new AccessTokenValidatorPlugin())->validate($glueRequestTransfer);
+        $result = $this->tester->createBackendApiAccessTokenValidatorPlugin()->validate($glueRequestTransfer);
 
         //Assert
         $this->assertTrue($result->getIsValid());
