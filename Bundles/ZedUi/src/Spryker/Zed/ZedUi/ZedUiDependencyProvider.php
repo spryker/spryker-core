9c937c53422124eab2e83ddb73317251e11942a0
Olha Livitchuk <olha.livitchuk@spryker.com>
2024-02-16 12:10:34 +0100

    FRW-7359 reverted wip changes

--- Bundles/ZedUi/src/Spryker/Zed/ZedUi/ZedUiDependencyProvider.php
+++ Bundles/ZedUi/src/Spryker/Zed/ZedUi/ZedUiDependencyProvider.php
@@ -19,7 +19,9 @@ class ZedUiDependencyProvider extends AbstractBundleDependencyProvider
      */
     public const SERVICE_UTIL_ENCODING = 'SERVICE_UTIL_ENCODING';
 
-    
+    /**
+     * @var string
+     */
     public const FACADE_TRANSLATOR = 'FACADE_TRANSLATOR';
 
     /**
