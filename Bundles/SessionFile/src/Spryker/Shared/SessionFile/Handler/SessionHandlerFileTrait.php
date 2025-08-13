b48727ac289bc0c09a078c79c27c4175428a1cad
Olha Livitchuk <olha.livitchuk@spryker.com>
2025-08-13 12:38:49 +0300

    FRW-10648 CS fixes

--- Bundles/SessionFile/src/Spryker/Shared/SessionFile/Handler/SessionHandlerFileTrait.php
+++ Bundles/SessionFile/src/Spryker/Shared/SessionFile/Handler/SessionHandlerFileTrait.php
@@ -9,98 +9,97 @@
 namespace Spryker\Shared\SessionFile\Handler;
 
 trait SessionHandlerFileTrait
+{
+    /**
+     * @param string $savePath
+     * @param string $sessionName
+     *
+     * @return bool
+     */
+    #[\ReturnTypeWillChange]
+    public function open(string $savePath, string $sessionName): bool
     {
-        /**
-         * @param string $savePath
-         * @param string $sessionName
-         *
-         * @return bool
-         */
-        #[\ReturnTypeWillChange]
-        public function open(string $savePath, string $sessionName): bool
-        {
-            return $this->executeOpen($savePath, $sessionName);
-        }
+        return $this->executeOpen($savePath, $sessionName);
+    }
 
-        /**
-         * @param string $sessionId
-         *
-         * @return string
-         */
-        #[\ReturnTypeWillChange]
-        public function read(string $sessionId): string
-        {
-            return $this->executeRead($sessionId);
-        }
+    /**
+     * @param string $sessionId
+     *
+     * @return string
+     */
+    #[\ReturnTypeWillChange]
+    public function read(string $sessionId): string
+    {
+        return $this->executeRead($sessionId);
+    }
 
-        /**
-         * @param string $sessionId
-         * @param string $sessionData
-         *
-         * @return bool
-         */
-        #[\ReturnTypeWillChange]
-        public function write(string $sessionId, string $sessionData): bool
-        {
-            return $this->executeWrite($sessionId, $sessionData);
-        }
+    /**
+     * @param string $sessionId
+     * @param string $sessionData
+     *
+     * @return bool
+     */
+    #[\ReturnTypeWillChange]
+    public function write(string $sessionId, string $sessionData): bool
+    {
+        return $this->executeWrite($sessionId, $sessionData);
+    }
 
-        /**
-         * @param string $sessionId
-         *
-         * @return bool
-         */
-        #[\ReturnTypeWillChange]
-        public function destroy(string $sessionId): bool
-        {
-            return $this->executeDestroy($sessionId);
-        }
+    /**
+     * @param string $sessionId
+     *
+     * @return bool
+     */
+    #[\ReturnTypeWillChange]
+    public function destroy(string $sessionId): bool
+    {
+        return $this->executeDestroy($sessionId);
+    }
 
-        /**
-         * @param int $maxLifetime
-         *
-         * @return int|false
-         */
-        #[\ReturnTypeWillChange]
-        public function gc(int $maxLifetime): int|false
-        {
-            return $this->executeGc($maxLifetime);
-        }
+    /**
+     * @param int $maxLifetime
+     *
+     * @return int|false
+     */
+    #[\ReturnTypeWillChange]
+    public function gc(int $maxLifetime): int|false
+    {
+        return $this->executeGc($maxLifetime);
+    }
 
-        /**
-         * @param string $savePath
-         * @param string $sessionName
-         *
-         * @return bool
-         */
-        abstract protected function executeOpen(string $savePath, string $sessionName): bool;
+    /**
+     * @param string $savePath
+     * @param string $sessionName
+     *
+     * @return bool
+     */
+    abstract protected function executeOpen(string $savePath, string $sessionName): bool;
 
-        /**
-         * @param string $sessionId
-         *
-         * @return string
-         */
-        abstract protected function executeRead(string $sessionId): string;
+    /**
+     * @param string $sessionId
+     *
+     * @return string
+     */
+    abstract protected function executeRead(string $sessionId): string;
 
-        /**
-         * @param string $sessionId
-         * @param string $sessionData
-         *
-         * @return bool
-         */
-        abstract protected function executeWrite(string $sessionId, string $sessionData): bool;
+    /**
+     * @param string $sessionId
+     * @param string $sessionData
+     *
+     * @return bool
+     */
+    abstract protected function executeWrite(string $sessionId, string $sessionData): bool;
 
-        /**
-         * @param string $sessionId
-         *
-         * @return bool
-         */
-        abstract protected function executeDestroy(string $sessionId): bool;
+    /**
+     * @param string $sessionId
+     *
+     * @return bool */
+    abstract protected function executeDestroy(string $sessionId): bool;
 
-        /**
-         * @param int $maxLifetime
-         *
-         * @return int|false
-         */
-        abstract protected function executeGc(int $maxLifetime): int|false;
+    /**
+     * @param int $maxLifetime
+     *
+     * @return int|false
+     */
+    abstract protected function executeGc(int $maxLifetime): int|false;
 }
